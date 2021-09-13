d85e0da0783ec286f3be2f762728d534c987d83b
baarkerlounger <db@slothlife.xyz>
2021-09-13 14:56:51 +0100

    Don't need to create our own question markup

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 RSpec.describe "Test Features" do
   it "Displays the name of the app" do
     visit("/form")
-    expect(page).to have_field("Age")
+    expect(page).to have_field("age")
     expect(page).to have_button("Continue")
   end
 end
