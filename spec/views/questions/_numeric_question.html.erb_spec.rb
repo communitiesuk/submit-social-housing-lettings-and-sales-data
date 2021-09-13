7a215965da3f60c0077d86edd4b97ac75c5d67ea
Matthew Phelan <matthew.phelan@madetech.com>
2021-09-13 11:36:27 +0100

    Add min and max to numeric question partial

--- spec/views/questions/_numeric_question.html.erb_spec.rb
+++ spec/views/questions/_numeric_question.html.erb_spec.rb
@@ -1,11 +1,25 @@
 describe 'questions/_numeric_question.html.erb' do
-    context 'when given a label' do
+    context 'when given a label and value constraints' do
         let(:label) { "Test Label" }
+        let(:min) { "1" }
+        let(:max) { "150" }
+        let(:locals) { {label: label, minimum: min, maximum: max} }
 
-        it 'displays a numeric entry field' do
-            render :partial => 'numeric_question', locals: { label: label }
+        before(:each) do
+            render :partial => 'numeric_question', locals: locals
+        end
+
+        it 'displays a numeric entry field with a label' do     
             expect(rendered).to have_selector('//input[@type="number"]')
             expect(rendered).to have_selector("//label[contains('#{label}')]")
+        end
+
+        it 'validates for a given minimum input' do
+            expect(rendered).to have_selector("//input[@min=#{min}]")
+        end
+
+        it 'validates for a given maximum input' do
+            expect(rendered).to have_selector("//input[@max=#{max}]")
         end
     end
 end
