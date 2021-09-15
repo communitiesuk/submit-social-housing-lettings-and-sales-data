d974457f894947b0496462a49d7643e7d1653c49
baarkerlounger <db@slothlife.xyz>
2021-09-15 16:15:52 +0100

    Add Stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,14 +1,9 @@
-import { Application } from "@hotwired/stimulus"
+// Load all the controllers within this directory and all subdirectories. 
+// Controller files must be named *_controller.js.
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
-
-// Configure Stimulus development experience
-application.warnings = true
-application.debug    = false
-window.Stimulus      = application
-
-// Import and register all your controllers within this directory and all subdirectories
-// Controller files must be named *_controller.js or *_controller.ts
-import { definitionsFromContext } from "@hotwired/stimulus"
-const context = require.context("controllers", true, /_controller\.(js|ts)$/)
+const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
