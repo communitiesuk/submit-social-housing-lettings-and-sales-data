ad0da831368a869d93a4e917c4eff1f82d328442
baarkerlounger <db@slothlife.xyz>
2021-10-04 11:24:20 +0100

    Clear answers if conditional is unselected

--- app/javascript/controllers/conditional_question_controller.js
+++ app/javascript/controllers/conditional_question_controller.js
@@ -7,10 +7,15 @@ export default class extends Controller {
     let conditional_for = JSON.parse(this.element.dataset.info);
 
     Object.entries(conditional_for).forEach(([key, values]) => {
+      let el = document.getElementById(key + "_div");
       if(values.includes(selected)) {
-        document.getElementById(key + "_div").style.display = "block"
+        el.style.display = "block";
       } else {
-        document.getElementById(key + "_div").style.display = "none"
+        el.style.display = "none";
+        let buttons = document.getElementsByName(key)
+        Object.entries(buttons).forEach(([idx, button]) => {
+          button.checked = false;
+        });
       }
     });
   }
