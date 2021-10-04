15f1bfb10333a8419a530e9cafa08fd2213de876
Kat <katrina@madetech.com>
2021-10-04 12:12:38 +0100

    Add styling change return of get status

--- app/helpers/tasklist_helper.rb
+++ app/helpers/tasklist_helper.rb
@@ -1,21 +1,43 @@
 module TasklistHelper
+  STATUSES = {
+    :not_started => "Not started",
+    :cannot_start_yet =>  "Cannot start yet",
+    :completed => "Completed",
+    :in_progress => "In progress"
+  }
+
+  STYLES = {
+    :not_started => "govuk-tag--grey",
+    :cannot_start_yet => "govuk-tag--grey",
+    :completed => "",
+    :in_progress => "govuk-tag--blue"
+  }
+
   def get_subsection_status(subsection_name, case_log)
     @form = Form.new(2021, 2022)
     questions = @form.questions_for_subsection(subsection_name).keys
 
     if subsection_name == "declaration"
-      return all_questions_completed(case_log) ? "Not started" : "Cannot start yet"
+      return all_questions_completed(case_log) ? :not_started : :cannot_start_yet
     end
     if questions.all? {|question| case_log[question].blank?}
-      return "Not started"
+      return :not_started
     end
     if questions.all? {|question| case_log[question].present?}
-      return "Completed"
+      return :completed
     end
-    "In progress"
+    :in_progress
   end
 
   def all_questions_completed(case_log)
     case_log.attributes.all? { |_question, answer| answer.present?}
+  end
+
+  def get_status_style(status_label)
+    STYLES[status_label]
+  end
+
+  def get_status_label(status)
+    STATUSES[status]
   end
 end
--- spec/helpers/tasklist_helper_spec.rb
+++ spec/helpers/tasklist_helper_spec.rb
@@ -6,26 +6,26 @@ RSpec.describe TasklistHelper do
     @form = Form.new(2021, 2022)
 
     it "returns not started if none of the questions in the subsection are answered" do
-      expect(get_subsection_status("income_and_benefits", case_log)).to eq("Not started")
+      expect(get_subsection_status("income_and_benefits", case_log)).to eq(:not_started)
     end
 
     it "returns cannot start yet if the subsection is declaration" do
-      expect(get_subsection_status("declaration", case_log)).to eq("Cannot start yet")
+      expect(get_subsection_status("declaration", case_log)).to eq(:cannot_start_yet)
     end
 
     it "returns in progress if some of the questions have been answered" do
       case_log["previous_postcode"] = "P0 5TT"
-      expect(get_subsection_status("local_authority", case_log)).to eq("In progress")
+      expect(get_subsection_status("local_authority", case_log)).to eq(:in_progress)
     end
 
     it "returns completed if all the questions in the subsection have been answered" do
       %w(net_income net_income_frequency net_income_uc_proportion housing_benefit).each {|x| case_log[x] = "value" }
-      expect(get_subsection_status("income_and_benefits", case_log)).to eq("Completed")
+      expect(get_subsection_status("income_and_benefits", case_log)).to eq(:completed)
     end
 
     it "returns not started if the subsection is declaration and all the questions are completed" do
       completed_case_log = CaseLog.new(case_log.attributes.map { |key, value| Hash[key, value || "value"]  }.reduce(:merge))
-      expect(get_subsection_status("declaration", completed_case_log)).to eq("Not started")
+      expect(get_subsection_status("declaration", completed_case_log)).to eq(:not_started)
     end
   end
 end
