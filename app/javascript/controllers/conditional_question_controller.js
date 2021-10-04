f6317c522cea8ee7f74b77337066c58e0675b06b
baarkerlounger <db@slothlife.xyz>
2021-10-04 22:15:56 +0100

    Run conditional logic on load in case radio input is already checked

--- app/javascript/controllers/conditional_question_controller.js
+++ app/javascript/controllers/conditional_question_controller.js
@@ -1,22 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  displayConditional() {
-    let question = this.element.name;
-    let selected = this.element.value;
-    let conditional_for = JSON.parse(this.element.dataset.info);
+  initialize() {
+    this.displayConditional()
+  }
 
-    Object.entries(conditional_for).forEach(([key, values]) => {
-      let el = document.getElementById(key + "_div");
-      if(values.includes(selected)) {
-        el.style.display = "block";
-      } else {
-        el.style.display = "none";
-        let buttons = document.getElementsByName(key)
-        Object.entries(buttons).forEach(([idx, button]) => {
-          button.checked = false;
-        });
-      }
-    });
+  displayConditional() {
+    if(this.element.checked) {
+      let selected = this.element.value
+      let conditional_for = JSON.parse(this.element.dataset.info)
+
+      Object.entries(conditional_for).forEach(([key, values]) => {
+        let el = document.getElementById(key + "_div")
+        if(values.includes(selected)) {
+          el.style.display = "block"
+        } else {
+          el.style.display = "none"
+          let buttons = document.getElementsByName(key)
+          Object.entries(buttons).forEach(([idx, button]) => {
+            button.checked = false;
+          })
+        }
+      })
+    }
   }
 }
