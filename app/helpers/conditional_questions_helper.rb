e93d2144ce860f6df30f06b22c1873131d6053fd
baarkerlounger <db@slothlife.xyz>
2021-10-05 13:47:21 +0100

    Refactor conditional html attributes

--- app/helpers/conditional_questions_helper.rb
+++ app/helpers/conditional_questions_helper.rb
@@ -8,4 +8,14 @@ module ConditionalQuestionsHelper
   def display_question_key_div(page_info, question_key)
     "style='display:none;'".html_safe if conditional_questions_for_page(page_info).include?(question_key)
   end
+
+  def conditional_html_attributes(question)
+    return {} unless question["conditional_for"].present?
+
+    {
+      "data-controller": "conditional-question",
+      "data-action": "conditional-question#displayConditional",
+      "data-info": question["conditional_for"].to_json
+    }
+  end
 end
