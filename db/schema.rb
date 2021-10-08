3d88d6434b04174aae92a4f63a8f916b656e5cb4
Kat <katrina@madetech.com>
2021-10-08 14:04:59 +0100

    Redo the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_110444) do
+ActiveRecord::Schema.define(version: 2021_10_08_130208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
