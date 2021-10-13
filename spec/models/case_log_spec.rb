868abda643bf3e1ef64acc4a4c1496ca29dcf52d
baarkerlounger <db@slothlife.xyz>
2021-10-13 13:36:37 +0100

    Test the whole validation

--- spec/models/case_log_spec.rb
+++ spec/models/case_log_spec.rb
@@ -2,8 +2,16 @@ require "rails_helper"
 
 RSpec.describe Form, type: :model do
   describe "#new" do
+    it "validates age is a number" do
+      expect { CaseLog.create!(tenant_age: "random") }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
     it "validates age is under 120" do
       expect { CaseLog.create!(tenant_age: 121) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "validates age is over 0" do
+      expect { CaseLog.create!(tenant_age: 0) }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
