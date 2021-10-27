d56c8a496e32cbfad867c08bb6fed07824a115a0
baarkerlounger <db@slothlife.xyz>
2021-10-27 16:58:47 +0100

    Fix comment

--- app/admin/income_ranges.rb
+++ app/admin/income_ranges.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register IncomeRange do
   # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   permit_params :economic_status, :soft_min, :soft_max, :hard_min, :hard_max
 end
