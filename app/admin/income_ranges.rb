96fb9dcd3fd0c8742f261964a42c255885abcbb2
baarkerlounger <db@slothlife.xyz>
2021-10-27 16:56:00 +0100

    Permit params so you can actually change the income range

--- app/admin/income_ranges.rb
+++ app/admin/income_ranges.rb
@@ -1,16 +1,4 @@
 ActiveAdmin.register IncomeRange do
   # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :economic_status, :soft_min, :soft_max, :hard_min, :hard_max
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:economic_status, :soft_min, :soft_max, :hard_min, :hard_max]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :economic_status, :soft_min, :soft_max, :hard_min, :hard_max
 end
