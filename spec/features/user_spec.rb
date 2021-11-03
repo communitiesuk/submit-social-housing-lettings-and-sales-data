e389c8c85e6e85d0d7888ac3bcf3a6aeb5a2386d
MadeTech Dushan <dushan@madetech.com>
2021-11-03 11:51:54 +0000

    add second passing spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,10 +1,18 @@
 require "rails_helper"
 RSpec.describe "User Features" do
-    let!(:user) { FactoryBot.build(:user) }
+    let!(:user) { FactoryBot.create(:user) }
     context "A user navigating to case logs" do
         it " is required to log in" do
-            visit("/case_logs")
-            expect(page).to have_current_path("/users/sign_in")
+          visit("/case_logs")
+          expect(page).to have_current_path("/users/sign_in")
         end
+
+        it " is redirected to case logs after signing in" do
+          visit("/case_logs")
+          fill_in("user_email", with: "test@example.com")
+          fill_in("user_password", with: "pAssword1")
+          click_button("Log in")
+          expect(page).to have_current_path("/case_logs")
+        end 
     end
 end
