01d5d64190b19d43373bf5a160036cc10d164ae4
baarkerlounger <db@slothlife.xyz>
2021-11-05 15:03:51 +0000

    Add a request spec for the new controller

--- spec/factories/case_log.rb
+++ spec/factories/case_log.rb
@@ -13,6 +13,12 @@ FactoryBot.define do
       tenant_code { "BZ737" }
       property_postcode { "NW1 7TY" }
     end
+    trait :soft_validations_triggered do
+      status { 1 }
+      person_1_economic_status { "Full-time - 30 hours or more" }
+      net_income { 750 }
+      net_income_frequency { "Weekly" }
+    end
     created_at { Time.zone.now }
     updated_at { Time.zone.now }
   end
