1766578c806d270508fe3a7c62a4698bde6fc90d
baarkerlounger <db@slothlife.xyz>
2021-11-05 13:42:54 +0000

    Use fetch

--- app/javascript/controllers/soft_validations_controller.js
+++ app/javascript/controllers/soft_validations_controller.js
@@ -5,28 +5,24 @@ export default class extends Controller {
 
   initialize() {
     let url = window.location.href + "/soft_validations"
-    let xhr = new XMLHttpRequest()
     let div = this.overrideTarget
-    xhr.open("GET", url, true)
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-              let response = JSON.parse(this.response)
-                if(response["show"]){
-                  div.style.display = "block"
-                  let innerHTML = div.innerHTML
-                  innerHTML = innerHTML.replace("soft-validations-placeholder-message", response["label"])
-                  innerHTML = innerHTML.replace("soft-validations-placeholder-hint-text", response["hint"])
-                  div.innerHTML = innerHTML
-                } else {
-                  div.style.display = "none"
-                  let buttons = document.getElementsByName(`case_log[override_net_income_validation][]`)
-                  Object.entries(buttons).forEach(([idx, button]) => {
-                    button.checked = false;
-                  })
-                }
-            }
+    fetch(url, { headers: { accept: "application/json" } })
+      .then(response => response.json())
+      .then((response) => {
+        if(response["show"]){
+          div.style.display = "block"
+          let innerHTML = div.innerHTML
+          innerHTML = innerHTML.replace("soft-validations-placeholder-message", response["label"])
+          innerHTML = innerHTML.replace("soft-validations-placeholder-hint-text", response["hint"])
+          div.innerHTML = innerHTML
+        } else {
+          div.style.display = "none"
+          let buttons = document.getElementsByName(`case_log[override_net_income_validation][]`)
+          Object.entries(buttons).forEach(([idx, button]) => {
+            button.checked = false
+          })
         }
-    xhr.send()
+      }
+    )
   }
 }
