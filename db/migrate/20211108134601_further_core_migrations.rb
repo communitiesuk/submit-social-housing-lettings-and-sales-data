aec2de4d4f4a779409477b2c76196dabab4df44c
Kat <katrina@madetech.com>
2021-11-09 10:08:40 +0000

    Update previous location and la

--- db/migrate/20211108134601_further_core_migrations.rb
+++ db/migrate/20211108134601_further_core_migrations.rb
@@ -4,6 +4,8 @@ class FurtherCoreMigrations < ActiveRecord::Migration[6.1]
     rename_column :case_logs, :reason_for_leaving_last_settled_home, :reason
     rename_column :case_logs, :property_reference, :propcode
     rename_column :case_logs, :property_major_repairs, :majorrepairs
+    rename_column :case_logs, :property_location, :la
+    rename_column :case_logs, :previous_la, :prevloc
     add_column :case_logs, :postcode, :string
     add_column :case_logs, :postcod2, :string
   end
@@ -13,6 +15,8 @@ class FurtherCoreMigrations < ActiveRecord::Migration[6.1]
     rename_column :case_logs, :reason, :reason_for_leaving_last_settled_home
     rename_column :case_logs, :propcode, :property_reference
     rename_column :case_logs, :majorrepairs, :property_major_repairs
+    rename_column :case_logs, :la, :property_location
+    rename_column :case_logs, :prevloc, :previous_la
     remove_column :case_logs, :postcode
     remove_column :case_logs, :postcod2
   end
