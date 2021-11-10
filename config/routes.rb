f048006a44c91bf7479264194eaddd46d65174f4
baarkerlounger <db@slothlife.xyz>
2021-11-10 14:47:09 +0000

    Add some file type error handling

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   post "/case_logs/:id", to: "case_logs#submit_form", constraints: { id: /\d.+/ }
-  post "/case_logs/bulk_upload", to: "bulk_upload#process_bulk_upload"
-  get "/case_logs/bulk_upload", to: "bulk_upload#show"
+
+  scope :case_logs do
+    post "/bulk_uploads", to: "bulk_upload#bulk_upload"
+    get "/bulk_uploads", to: "bulk_upload#show"
+  end
 
   form_handler = FormHandler.instance
   form = form_handler.get_form("2021_2022")
--- spec/requests/bulk_upload_controller_spec.rb
+++ spec/requests/bulk_upload_controller_spec.rb
@@ -1,37 +1,48 @@
 require "rails_helper"
 
 RSpec.describe BulkUploadController, type: :request do
-  let(:url) { "/case_logs/bulk_upload" }
-
-  before do
-    get url, params: {}
-  end
+  let(:url) { "/case_logs/bulk_uploads" }
 
   describe "GET #show" do
+    before do
+      get url, params: {}
+    end
+
     it "returns a success response" do
       expect(response).to be_successful
     end
 
     it "returns a page with a file upload form" do
-      expect(response.body).to match(/<input id="case-log-bulk-upload-field" class="govuk-file-upload"/)
+      expect(response.body).to match(/<input id="bulk-upload-case-log-bulk-upload-field" class="govuk-file-upload"/)
       expect(response.body).to match(/<button type="submit" formnovalidate="formnovalidate" class="govuk-button"/)
     end
   end
 
   describe "POST #bulk upload" do
-    before do
-      @file = fixture_file_upload("2021_22_lettings_bulk_upload.xlsx", "application/vnd.ms-excel")
-    end
-
-    subject { post url, params: { case_log_bulk_upload: @file } }
+    subject { post url, params: { bulk_upload: { case_log_bulk_upload: @file } } }
 
     context "given a valid file based on the upload template" do
+      before do
+        @file = fixture_file_upload("2021_22_lettings_bulk_upload.xlsx", "application/vnd.ms-excel")
+      end
+
       it "creates case logs for each row in the template" do
         expect { subject }.to change(CaseLog, :count).by(2)
       end
 
       it "redirects to the case log index page" do
         expect(subject).to redirect_to(case_logs_path)
+      end
+    end
+
+    context "given an invalid file type" do
+      before do
+        @file = fixture_file_upload("random.txt", "text/plain")
+        subject
+      end
+
+      it "displays an error message" do
+        expect(response.body).to match(/Invalid file type/)
       end
     end
   end
