42377da6c4b8bf667b51270245d93f4282fb2953
MadeTech Dushan <dushan@madetech.com>
2021-11-10 12:37:59 +0000

    add test around protecting emails

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,6 +5,13 @@ class Users::PasswordsController < Devise::PasswordsController
     render "devise/confirmations/reset"
   end 
 
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+  end
+
   protected
 
     def after_sending_reset_password_instructions_path_for(resource)
