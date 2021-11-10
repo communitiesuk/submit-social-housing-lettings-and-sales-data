e0242ad1cedc184604d404adb278b2cd41dfc628
MadeTech Dushan <dushan@madetech.com>
2021-11-10 12:22:05 +0000

    make a step to protect emails

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,13 +1,13 @@
 class Users::PasswordsController < Devise::PasswordsController
 
   def reset_confirmation
-    @user = User.find(params["id"])
+    @email = params["email"]
     render "devise/confirmations/reset"
   end 
 
   protected
 
     def after_sending_reset_password_instructions_path_for(resource)
-      confirmations_reset_path(id: @user.id) if is_navigational_format?
+      confirmations_reset_path(email: params.dig("user", "email")) if is_navigational_format?
     end
 end
