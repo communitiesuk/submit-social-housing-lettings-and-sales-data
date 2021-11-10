665fa796c6c9dc7c3a50e61f9243861b6d1d808c
baarkerlounger <db@slothlife.xyz>
2021-11-10 15:33:13 +0000

    Collection route

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,23 @@ Rails.application.routes.draw do
 
   post "/case_logs/:id", to: "case_logs#submit_form", constraints: { id: /\d.+/ }
 
-  scope :case_logs do
-    post "/bulk_uploads", to: "bulk_upload#bulk_upload"
-    get "/bulk_uploads", to: "bulk_upload#show"
-  end
-
   form_handler = FormHandler.instance
   form = form_handler.get_form("2021_2022")
+
   resources :case_logs do
+    collection do
+      post "/bulk_uploads", to: "bulk_upload#bulk_upload"
+      get "/bulk_uploads", to: "bulk_upload#show"
+    end
+
     form.all_pages.keys.map do |page|
       get page.to_s, to: "case_logs##{page}"
-      get "#{page}/soft_validations", to: "soft_validations#show"
+      get "#{page}/soft_validations", to: "soft_validations#show" if form.soft_validations_for_page(page)
     end
+
     form.all_subsections.keys.map do |subsection|
       get "#{subsection}/check_answers", to: "case_logs#check_answers"
     end
+
   end
 end
