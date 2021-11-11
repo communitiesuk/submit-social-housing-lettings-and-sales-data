d1db7528421d14c1ffff0250e6df5c0d4fb1c4a3
magicmilo <milobascombe@gmail.com>
2021-11-11 11:45:33 +0000

    only validate real forms

--- app/helpers/json_schema_validation.rb
+++ app/helpers/json_schema_validation.rb
@@ -29,7 +29,8 @@ begin
     return
   end
 
-  directories = ["config/forms", "spec/fixtures/forms"]
+  # directories = ["config/forms", "spec/fixtures/forms"]
+  directories = ["config/forms"]
 
   get_all_form_paths(directories).each do |path|
     puts path
