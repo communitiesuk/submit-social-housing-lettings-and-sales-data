ebd818ef4b5da18d103841f8bdd758caad51faec
baarkerlounger <db@slothlife.xyz>
2021-11-16 11:13:42 +0000

    Update specs

--- spec/features/case_log_spec.rb
+++ spec/features/case_log_spec.rb
@@ -449,7 +449,7 @@ RSpec.describe "Test Features" do
 
   describe "conditional page routing", js: true do
     before do
-      allow_any_instance_of(CaseLogValidator).to receive(:validate_household_pregnancy).and_return(true)
+      allow_any_instance_of(CaseLogValidator).to receive(:validate_pregnancy).and_return(true)
     end
 
     it "can route the user to a different page based on their answer on the current page" do
