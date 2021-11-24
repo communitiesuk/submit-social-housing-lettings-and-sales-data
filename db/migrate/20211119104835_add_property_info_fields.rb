e6d59173b4702dc32795d9dfe0e06428c8ba6016
magicmilo <milobascombe@gmail.com>
2021-11-24 12:10:20 +0000

    add enums and switch to ints

--- db/migrate/20211119104835_add_property_info_fields.rb
+++ db/migrate/20211119104835_add_property_info_fields.rb
@@ -1,10 +1,10 @@
 class AddPropertyInfoFields < ActiveRecord::Migration[6.1]
   def change
     change_table :case_logs, bulk: true do |t|
-      t.column :do_you_know_the_postcode, :string
-      t.column :do_you_know_the_local_authority, :string
+      t.column :do_you_know_the_postcode, :int
+      t.column :do_you_know_the_local_authority, :int
+      t.column :first_time_property_let_as_social_housing, :int
       t.column :why_dont_you_know_la, :string
-      t.column :first_time_property_let_as_social_housing, :string
       t.column :type_property_most_recently_let_as, :string
       t.column :builtype, :string
       t.rename :tenant_same_property_renewal, :renewal
