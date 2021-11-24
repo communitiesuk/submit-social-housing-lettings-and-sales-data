4eed52dc0d58f955f7e908bce4daf6e5c7655923
magicmilo <milobascombe@gmail.com>
2021-11-24 11:05:51 +0000

    put migrations into one file

--- db/migrate/20211119104835_add_property_info_fields.rb
+++ db/migrate/20211119104835_add_property_info_fields.rb
@@ -1,12 +1,13 @@
 class AddPropertyInfoFields < ActiveRecord::Migration[6.1]
   def change
     change_table :case_logs, bulk: true do |t|
-      t.column :do_you_know_the_postcode, :integer
-      t.column :do_you_know_the_local_authority, :integer
+      t.column :do_you_know_the_postcode, :string
+      t.column :do_you_know_the_local_authority, :string
       t.column :why_dont_you_know_la, :string
-      t.column :first_time_property_let_as_social_housing, :integer
-      t.column :type_property_most_recently_let_as, :integer
-      t.column :builtype, :integer
+      t.column :first_time_property_let_as_social_housing, :string
+      t.column :type_property_most_recently_let_as, :string
+      t.column :builtype, :string
+      t.rename :tenant_same_property_renewal, :renewal
     end
   end
 end
--- db/migrate/20211119142809_change_renewal.rb
+++
@@ -1,7 +0,0 @@
-class ChangeRenewal < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :case_logs, :tenant_same_property_renewal, :renewal
-  end
-end
-
-
