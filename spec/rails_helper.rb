7fd6ae200d94f59c2cf9b921fe07f128dce6f108
baarkerlounger <db@slothlife.xyz>
2021-11-25 11:21:18 +0000

    Remove the uneeded extra options

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,9 +10,7 @@ require "selenium-webdriver"
 
 Capybara.register_driver :headless do |app|
   options = Selenium::WebDriver::Firefox::Options.new
-  options.add_argument("--no-sandbox")
   options.add_argument("--headless")
-  options.add_argument("--disable-gpu")
 
   Capybara::Selenium::Driver.new(app,
     browser: :firefox,
