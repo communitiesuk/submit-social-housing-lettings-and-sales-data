568369d8e5baed1f64c734cd403e50a335648e02
baarkerlounger <db@slothlife.xyz>
2021-11-26 18:06:37 +0000

    Update spec

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe Organisation, type: :model do
   describe "#new" do
-    let!(:user) { FactoryBot.create(:user) }
-    let(:organisation) { Organisation.first }
+    let(:user) { FactoryBot.create(:user) }
+    let(:organisation) { user.organisation }
 
     it "has expected fields" do
       expect(organisation.attribute_names).to include("name", "phone", "org_type")
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,9 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "#new" do
     let(:user) { FactoryBot.create(:user) }
-    let(:organisation) { Organisation.first }
 
     it "belongs to an organisation" do
-      expect(user.organisation).to eq(organisation)
+      expect(user.organisation).to be_a(Organisation)
     end
   end
 end
