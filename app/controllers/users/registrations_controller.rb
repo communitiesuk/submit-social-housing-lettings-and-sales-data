6630284b23ac29e3c9927807087ee707c83b0c6e
baarkerlounger <db@slothlife.xyz>
2021-12-01 17:14:57 +0000

    Turbo devise strikes again

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  skip_before_action :require_no_authentication
+
+  def new
+    self.resource = resource_class.new
+    respond_with resource
+  end
+
 protected
 
   def after_update_path_for(_resource)
