5f507deab3ad56a9ee5dcda8bdea1020da25fb08
baarkerlounger <db@slothlife.xyz>
2021-12-01 09:32:22 +0000

    Move styling methods into helper

---
+++ app/helpers/user_table_helper.rb
@@ -0,0 +1,12 @@
+module UserTableHelper
+  include GovukLinkHelper
+
+  def user_cell(user)
+    [govuk_link_to(user.name, user), user.email].join("\n")
+  end
+
+  def org_cell(user)
+    role = "<span class='app-!-colour-muted'>#{user.role}</span>"
+    [user.organisation.name, role].join("\n")
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |i| "test#{i}@example.com" }
+    name { "Danny Rojas" }
     password { "pAssword1" }
     organisation
+    role { "Data Provider" }
     created_at { Time.zone.now }
     updated_at { Time.zone.now }
   end
