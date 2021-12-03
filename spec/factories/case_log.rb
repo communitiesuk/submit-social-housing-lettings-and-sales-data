ee1a9b2b55b4c5a40cb4df4066a33f9b97c2a9ff
MadeTech Dushan <dushan@madetech.com>
2021-12-03 10:24:13 +0000

    fix failing specs

--- spec/factories/case_log.rb
+++ spec/factories/case_log.rb
@@ -148,6 +148,7 @@ FactoryBot.define do
       armedforces { 1 }
       builtype { 1 }
       unitletas { 2 }
+      joint_tenancy { 1 }
     end
     created_at { Time.zone.now }
     updated_at { Time.zone.now }
