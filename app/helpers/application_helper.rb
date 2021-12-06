84d8d6ee95e98693568318145f4d5ad64713ba0f
Matthew Phelan <matthew.phelan@madetech.com>
2021-12-06 11:56:15 +0000

    Error prefix code review changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
   def browser_title(title)
-    [title, t("service_name"), "GOV.UK"].select(&:present?).join(" - ")
+    if user_log_errors || case_log_errors || resource_errors
+      ["Error", title, t("service_name"), "GOV.UK"].select(&:present?).join(" - ")
+    else
+      [title, t("service_name"), "GOV.UK"].select(&:present?).join(" - ")
+    end
+  end
+
+  def user_log_errors
+    @user.present? && @user.errors.present?
+  end
+
+  def case_log_errors
+    @case_log.present? && @case_log.errors.present?
+  end
+
+  def resource_errors
+    @resource.present? && @resource.errors.present?
   end
 end
