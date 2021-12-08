f9e85961fc6bae0e939d7ffa331f9a891334b045
baarkerlounger <5101747+baarkerlounger@users.noreply.github.com>
2021-12-08 09:59:18 +0000

    CLDC-730: Data Coordinators can edit org details (#150)
    * Data coordinators can edit some organisation details
    
    * Apply suggestions from code review
    
    PR tweaks
    
    Co-authored-by: Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
    
    * PR comments
    
    * Address line label
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
    
    Co-authored-by: Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -19,7 +19,30 @@ class OrganisationsController < ApplicationController
     render "show"
   end
 
+  def edit
+    if current_user.data_coordinator?
+      render "edit", layout: "application"
+    else
+      head :unauthorized
+    end
+  end
+
+  def update
+    if current_user.data_coordinator?
+      if @organisation.update(org_params)
+        flash[:notice] = I18n.t("organisation.updated")
+        redirect_to details_organisation_path(@organisation)
+      end
+    else
+      head :unauthorized
+    end
+  end
+
 private
+
+  def org_params
+    params.require(:organisation).permit(:name, :address_line1, :address_line2, :postcode, :phone)
+  end
 
   def authenticate_scope!
     head :not_found if current_user.organisation != @organisation
