d655b6872dbc944edf2f449f0e4b6d312ffe61f3
baarkerlounger <db@slothlife.xyz>
2021-12-22 19:13:14 +0000

    Don't progressively enhance select options multiple times

--- app/webpacker/controllers/accessible_autocomplete_controller.js
+++ app/webpacker/controllers/accessible_autocomplete_controller.js
@@ -4,9 +4,11 @@ import 'accessible-autocomplete/dist/accessible-autocomplete.min.css'
 
 export default class extends Controller {
   connect() {
-    accessibleAutocomplete.enhanceSelectElement({
-      defaultValue: '',
-      selectElement: this.element
-    })
+    if(document.querySelectorAll(".autocomplete__input").length == 0){
+      accessibleAutocomplete.enhanceSelectElement({
+        defaultValue: '',
+        selectElement: this.element
+      })
+    }
   }
 }
