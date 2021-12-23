78eb25a789660684095c3e905922b662103c53b5
baarkerlounger <db@slothlife.xyz>
2021-12-23 15:05:57 +0000

    Don't turbo cache page to make js function act each time

--- app/webpacker/controllers/accessible_autocomplete_controller.js
+++ app/webpacker/controllers/accessible_autocomplete_controller.js
@@ -3,15 +3,11 @@ import accessibleAutocomplete from "accessible-autocomplete"
 import 'accessible-autocomplete/dist/accessible-autocomplete.min.css'
 
 export default class extends Controller {
-  static values = { enhanced: Boolean, default: false }
 
   connect() {
-    if(!this.enhancedValue){
-      accessibleAutocomplete.enhanceSelectElement({
-        defaultValue: '',
-        selectElement: this.element
-      })
-      this.enhancedValue = true
-    }
+    accessibleAutocomplete.enhanceSelectElement({
+      defaultValue: '',
+      selectElement: this.element
+    })
   }
 }
