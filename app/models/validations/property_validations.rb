144c5a3810204183186179733c4f23e249f7a955
baarkerlounger <db@slothlife.xyz>
2022-01-04 14:07:06 +0000

    Fix first let validations

--- app/models/validations/property_validations.rb
+++ app/models/validations/property_validations.rb
@@ -2,6 +2,7 @@ module Validations::PropertyValidations
   # Validations methods need to be called 'validate_<page_name>' to run on model save
   # or 'validate_' to run on submit as well
   include Constants::CaseLog
+
   def validate_property_number_of_times_relet(record)
     if record.offered && !/^[1-9]$|^0[1-9]$|^1[0-9]$|^20$/.match?(record.offered.to_s)
       record.errors.add :offered, "Property number of times relet must be between 0 and 20"
@@ -14,9 +15,22 @@ module Validations::PropertyValidations
     end
   end
 
+  FIRST_LET_VACANCY_REASONS = ["First let of newbuild property",
+                               "First let of conversion/rehabilitation/acquired property",
+                               "First let of leased property"].freeze
+  def validate_rsnvac(record)
+    if !record.first_time_property_let_as_social_housing? && FIRST_LET_VACANCY_REASONS.include?(record.rsnvac)
+      record.errors.add :rsnvac, "Reason for vacancy cannot be first let if unit has been previously let as social housing"
+    end
+
+    if record.first_time_property_let_as_social_housing? && record.rsnvac.present? && !FIRST_LET_VACANCY_REASONS.include?(record.rsnvac)
+      record.errors.add :rsnvac, "Reason for vacancy must be first let if unit has been previously let as social housing"
+    end
+  end
+
   def validate_unitletas(record)
-    if record.unitletas.present? && (record.rsnvac == "First let of newbuild property" || record.rsnvac == "First let of conversion/rehabilitation/acquired property" || record.rsnvac == "First let of leased property")
-      record.errors.add :unitletas, "Can not be completed if it is the first let of the property"
+    if record.first_time_property_let_as_social_housing? && record.unitletas.present?
+      record.errors.add :unitletas, "Property cannot have a previous let type if it is being let as social housing for the first time"
     end
   end
 end
