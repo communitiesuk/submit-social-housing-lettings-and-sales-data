c06a479a38f6c5bf779635c6a8bf8fb11a0001d4
baarkerlounger <db@slothlife.xyz>
2022-01-07 15:26:29 +0000

    Hide total field by default

--- app/webpacker/controllers/numeric_question_controller.js
+++ app/webpacker/controllers/numeric_question_controller.js
@@ -1,6 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    const affectedField = this.element.dataset.target;
+    const targetQuestion = affectedField.split("case-log-")[1].split("-field")[0]
+    const div = document.getElementById(targetQuestion + "_div");
+    div.style.display = "block";
+  }
+
   calculateFields() {
     const affectedField = this.element.dataset.target;
     const fieldsToAdd = JSON.parse(this.element.dataset.calculated).map(x => `case-log-${x.replaceAll("_","-")}-field`);
