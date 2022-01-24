0881fc804f1514d93fc0029fa757cc7b20db6aac
baarkerlounger <db@slothlife.xyz>
2022-01-24 18:23:43 +0000

    Fix styling issues

---
+++ app/helpers/question_view_helper.rb
@@ -0,0 +1,25 @@
+module QuestionViewHelper
+  def caption(caption, page_header, conditional)
+    return nil unless caption && page_header.blank? && !conditional
+
+    { text: caption.html_safe, size: "l" }
+  end
+
+  def legend(question, page_header, conditional)
+    {
+      text: question.header.html_safe,
+      size: label_size(page_header, conditional),
+      tag: label_tag(page_header, conditional),
+    }
+  end
+
+private
+
+  def label_size(page_header, conditional)
+    page_header.blank? && !conditional ? "l" : "m"
+  end
+
+  def label_tag(page_header, conditional)
+    page_header.blank? && !conditional ? "h1" : "h2"
+  end
+end
