85e8493fb43093c06708da1c933b36ad2099fa0b
Kat <katrina@madetech.com>
2022-01-31 15:48:36 +0000

    fix progressive total field spec offences

--- spec/features/form/progressive_total_field_spec.rb
+++ spec/features/form/progressive_total_field_spec.rb
@@ -21,12 +21,12 @@ RSpec.describe "Accessible Automcomplete" do
 
   it "does not show when js is not enabled" do
     visit("/logs/#{case_log.id}/rent")
-    expect(page).to have_selector("#tcharge_div", visible: false)
+    expect(page).to have_selector("#tcharge_div", visible: :all)
   end
 
   it "does show when js is enabled and calculates the total", js: true do
     visit("/logs/#{case_log.id}/rent")
-    expect(page).to have_selector("#tcharge_div", visible: true)
+    expect(page).to have_selector("#tcharge_div")
     fill_in("case-log-brent-field", with: 5)
     expect(find("#case-log-tcharge-field").value).to eq("5.00")
     fill_in("case-log-pscharge-field", with: 3)
