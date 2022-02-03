b5c79d88f593acea3cfc329fd7e55ce8df8fb768
Stéphane Meny <smeny@users.noreply.github.com>
2022-02-03 15:00:11 +0000

    Adjust method calls to be more functional

--- app/services/storage_service.rb
+++ app/services/storage_service.rb
@@ -9,17 +9,13 @@ class StorageService
   end
 
   def list_files(folder)
-    file_list = []
-    @client.list_objects_v2(bucket: @configuration.bucket_name, prefix: folder).each do |response|
-      file_list += response.contents.map(&:key)
-    end
-    file_list
+    @client.list_objects_v2(bucket: @configuration.bucket_name, prefix: folder)
+           .flat_map { |response| response.contents.map(&:key) }
   end
 
   def get_file_io(file_name)
-    file_response =
-      @client.get_object(bucket: @configuration.bucket_name, key: file_name)
-    file_response.body
+    @client.get_object(bucket: @configuration.bucket_name, key: file_name)
+           .body
   end
 
   def write_file(file_name, data)
