9ceade98333518d7e43077b172d2b2e067440529
baarkerlounger <db@slothlife.xyz>
2022-02-04 10:26:47 +0000

    Less patching upstream

--- app/controllers/auth/two_factor_authentication_controller.rb
+++ app/controllers/auth/two_factor_authentication_controller.rb
@@ -2,4 +2,24 @@ class Auth::TwoFactorAuthenticationController < Devise::TwoFactorAuthenticationC
   def show_resend
     render "devise/two_factor_authentication/resend"
   end
+
+  def update
+    resource.errors.add :base, I18n.t("devise.two_factor_authentication.code_required") if resource && params_code.empty?
+    super
+  end
+
+private
+
+  def after_two_factor_fail_for(resource)
+    resource.second_factor_attempts_count += 1
+    resource.save!
+
+    if resource.max_login_attempts?
+      sign_out(resource)
+      render :max_login_attempts_reached, status: :unprocessable_entity
+    else
+      resource.errors.add :base, I18n.t("devise.two_factor_authentication.code_incorrect") if resource
+      render :show, status: :unprocessable_entity
+    end
+  end
 end
