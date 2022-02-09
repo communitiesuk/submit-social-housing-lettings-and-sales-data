c660bd145d382af22fa50aed44c76f7a147a3796
Stéphane Meny <smeny@users.noreply.github.com>
2022-02-09 17:00:20 +0000

    Adjusts sentry configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,6 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
-  # To activate performance monitoring, set one of these options.
-  # We recommend adjusting the value in production:
+  config.enabled_environments = %w[production staging]
+  config.rails.report_rescued_exceptions = true
   config.traces_sample_rate = 0.5
 end
