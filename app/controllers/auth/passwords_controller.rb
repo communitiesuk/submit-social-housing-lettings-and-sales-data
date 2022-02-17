1eea260e60fad1543cc6bc8ff315a61132639b8e
baarkerlounger <db@slothlife.xyz>
2022-02-17 15:41:41 +0000

    Update flash message for 2FA

--- app/controllers/auth/passwords_controller.rb
+++ app/controllers/auth/passwords_controller.rb
@@ -27,7 +27,31 @@ class Auth::PasswordsController < Devise::PasswordsController
     render "devise/passwords/reset_password"
   end
 
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        set_flash_message!(:notice, password_update_flash_message)
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
 protected
+
+  def password_update_flash_message
+    resource_class == AdminUser ? :updated_2FA : :updated
+  end
 
   def resource_class_name
     resource_class.name.underscore
