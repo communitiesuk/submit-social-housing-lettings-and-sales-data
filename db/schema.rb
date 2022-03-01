Dan13L3/mhclg-data-collection-beta

CLDC-959: add multiple tenant flow
CLDC-959: add ethnicity questions

CLDC-959: update nationality enum

CLDC-959: add ethnic group field

CLDC-959: updated lead tenant nationality

CLDC-959: add additional tenant questions

db/schema.rb
@@ -191,6 +191,23 @@ ActiveRecord::Schema[7.0].define(version: 202202071123100) do
     t.integer "previous_la_known"
     t.boolean "is_previous_la_inferred"
     t.integer "letting_allocation_unknown"
+    t.string "age1_known"
+    t.string "age2_known"
+    t.string "age3_known"
+    t.string "age4_known"
+    t.string "age5_known"
+    t.string "age6_known"
+    t.string "age7_known"
+    t.string "age8_known"
+    t.integer "ethnic_group"
+    t.string "ethnic_other"
+    t.string "details_known_2"
+    t.string "details_known_3"
+    t.string "details_known_4"
+    t.string "details_known_5"
+    t.string "details_known_6"
+    t.string "details_known_7"
+    t.string "details_known_8"
     t.index ["managing_organisation_id"], name: "index_case_logs_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_case_logs_on_owning_organisation_id"
   end

communitiesuk/submit-social-housing-lettings-and-sales-data

db/schema.rb
@@ -45,24 +45,32 @@ ActiveRecord::Schema[7.0].define(version: 202202071123100) do
     t.datetime "updated_at", null: false
     t.string "tenant_code"
     t.integer "age1"
+    t.string "sex1"
     t.integer "ethnic"
     t.integer "national"
     t.integer "prevten"
     t.integer "ecstat1"
     t.integer "hhmemb"
     t.integer "age2"
+    t.string "sex2"
     t.integer "ecstat2"
     t.integer "age3"
+    t.string "sex3"
     t.integer "ecstat3"
     t.integer "age4"
+    t.string "sex4"
     t.integer "ecstat4"
     t.integer "age5"
+    t.string "sex5"
     t.integer "ecstat5"
     t.integer "age6"
+    t.string "sex6"
     t.integer "ecstat6"
     t.integer "age7"
+    t.string "sex7"
     t.integer "ecstat7"
     t.integer "age8"
+    t.string "sex8"
     t.integer "ecstat8"
     t.integer "homeless"
     t.integer "underoccupation_benefitcap"
@@ -149,6 +157,7 @@ ActiveRecord::Schema[7.0].define(version: 202202071123100) do
     t.datetime "property_void_date", precision: nil
     t.bigint "owning_organisation_id"
     t.bigint "managing_organisation_id"
+    t.integer "renttype"
     t.integer "needstype"
     t.integer "lettype"
     t.integer "postcode_known"
@@ -177,16 +186,24 @@ ActiveRecord::Schema[7.0].define(version: 202202071123100) do
     t.integer "previous_postcode_known"
     t.integer "previous_la_known"
     t.boolean "is_previous_la_inferred"
+    t.integer "age1_known"
+    t.integer "age2_known"
+    t.integer "age3_known"
+    t.integer "age4_known"
+    t.integer "age5_known"
+    t.integer "age6_known"
+    t.integer "age7_known"
+    t.integer "age8_known"
+    t.integer "ethnic_group"
+    t.string "ethnic_other"
     t.integer "letting_allocation_unknown"
-    t.integer "renttype"
-    t.string "sex1"
-    t.string "sex2"
-    t.string "sex3"
-    t.string "sex4"
-    t.string "sex5"
-    t.string "sex6"
-    t.string "sex7"
-    t.string "sex8"
+    t.integer "details_known_2"
+    t.integer "details_known_3"
+    t.integer "details_known_4"
+    t.integer "details_known_5"
+    t.integer "details_known_6"
+    t.integer "details_known_7"
+    t.integer "details_known_8"
     t.integer "relat2"
     t.integer "relat3"
     t.integer "relat4"