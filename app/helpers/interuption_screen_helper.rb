2a7ea6b6727df83e643010e757ef59b9aceb65f4
Kat <katrina@madetech.com>
2022-03-08 11:05:52 +0000

    Add tests for helper

--- app/helpers/interuption_screen_helper.rb
+++ app/helpers/interuption_screen_helper.rb
@@ -1,7 +1,16 @@
 module InteruptionScreenHelper
   def display_informative_text(informative_text, case_log)
-    translation_question = informative_text["argument"].map { |x| case_log.form.get_question(x) }
-    translation = I18n.t(informative_text["translation"], informative_text["argument"][0].to_sym => translation_question[0].answer_label(case_log), informative_text["argument"][1].to_sym => translation_question[1].answer_label(case_log))
-    "<span>#{translation}</span>".html_safe
+    translation_questions = informative_text["argument"].map { |x| case_log.form.get_question(x) }
+    begin
+      case translation_questions.count
+      when 2
+        translation = I18n.t(informative_text["translation"], informative_text["argument"][0].to_sym => translation_questions[0].answer_label(case_log), informative_text["argument"][1].to_sym => translation_questions[1].answer_label(case_log))
+      when 1
+        translation = I18n.t(informative_text["translation"], informative_text["argument"][0].to_sym => translation_questions[0].answer_label(case_log))
+      end
+    rescue StandardError
+      return ""
+    end
+    translation.html_safe
   end
 end
