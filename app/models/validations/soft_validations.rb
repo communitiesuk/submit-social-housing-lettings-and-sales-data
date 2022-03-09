dafa19c10e3e92de4e8ee74167028ed9ea3bd369
MadeTech Dushan <dushan@madetech.com>
2022-03-09 16:14:40 +0000

    more changes

--- app/models/validations/soft_validations.rb
+++ app/models/validations/soft_validations.rb
@@ -13,18 +13,6 @@ module Validations::SoftValidations
     10 => OpenStruct.new(soft_min: 47, soft_max: 730, hard_min: 10, hard_max: 1300),
   }.freeze
 
-  def has_no_unresolved_soft_errors?
-    soft_errors.empty? || soft_errors_overridden?
-  end
-
-  def soft_errors
-    {}.merge(net_income_validations)
-  end
-
-  def soft_errors_overridden?
-    public_send(soft_errors.keys.first) == 1 if soft_errors.present?
-  end
-
   def net_income_in_soft_max_range?
     return unless weekly_net_income && ecstat1
 
@@ -35,25 +23,5 @@ module Validations::SoftValidations
     return unless weekly_net_income && ecstat1
 
     weekly_net_income.between?(applicable_income_range.hard_min, applicable_income_range.soft_min)
-  end
-
-private
-
-  def net_income_validations
-    net_income_errors = {}
-    if net_income_in_soft_min_range?
-      net_income_errors["net_income_value_check"] = OpenStruct.new(
-        message: I18n.t("soft_validations.net_income.in_soft_min_range.message"),
-        hint_text: I18n.t("soft_validations.net_income.hint_text", ecstat1:),
-      )
-    elsif net_income_in_soft_max_range?
-      net_income_errors["net_income_value_check"] = OpenStruct.new(
-        message: I18n.t("soft_validations.net_income.in_soft_max_range.message"),
-        hint_text: I18n.t("soft_validations.net_income.hint_text", ecstat1:),
-      )
-    else
-      update_column(:net_income_value_check, nil)
-    end
-    net_income_errors
   end
 end
