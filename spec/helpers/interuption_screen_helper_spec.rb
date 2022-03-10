190a5c64466982349d6f4ec31da904ac7c05691d
MadeTech Dushan <dushan@madetech.com>
2022-03-10 16:14:43 +0000

    content changes

--- spec/helpers/interuption_screen_helper_spec.rb
+++ spec/helpers/interuption_screen_helper_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe InteruptionScreenHelper do
           "argument" => %w[ecstat1 earnings],
         }
         expect(display_informative_text(informative_text, case_log))
-          .to eq("<p class='govuk-body'>This is based on the tenant’s work situation: <strong>Full-time – 30 hours or more</strong><br><br>The household income you have entered is <strong>£750.00 every week</strong></p>")
+          .to eq("<p class='govuk-body'>You told us the main tenant's working situation is: <strong>Full-time – 30 hours or more</strong><br><br>The household income you have entered is <strong>£750.00 every week</strong></p>")
       end
     end
 
