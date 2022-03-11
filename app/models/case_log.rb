1cb5fd43b1a79e32b2fe66ae2bb39fbfb9243423
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2022-03-11 11:06:52 +0000

    Add prevten validations (#374)
    * Add prevten validations
    
    * Fix indentation and extract a case_log method
    
    * extract refuge method

--- app/models/case_log.rb
+++ app/models/case_log.rb
@@ -219,6 +219,13 @@ class CaseLog < ApplicationRecord
     rent_type == 2 || rent_type == 4
   end
 
+  def previous_tenancy_was_foster_care?
+    prevten == 13
+  end
+
+  def previous_tenancy_was_refuge?
+    prevten == 21
+  end
 private
 
   PIO = Postcodes::IO.new
