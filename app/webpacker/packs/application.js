b5196346253db7160425cab7957ccdc2bde1f853
kiddhustle <kiddhustle@wiardweb.com>
2022-03-14 11:56:24 +0000

    CLDC-1057: add core-js polyfills

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@babel/polyfill")
+require("core-js");
 require.context('govuk-frontend/govuk/assets')
 
 import '../styles/application.scss'
