bace6dd69587719ba94363416464445f04ee1e66
baarkerlounger <5101747+baarkerlounger@users.noreply.github.com>
2022-03-15 09:10:42 +0000

    Fix conditional field clearing condition (#389)

--- app/webpacker/controllers/conditional_question_controller.js
+++ app/webpacker/controllers/conditional_question_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
       let selectedValue = this.element.value
       let conditional_for = JSON.parse(this.element.dataset.info)
       Object.entries(conditional_for).map(([targetQuestion, conditions]) => {
-          if(conditions.includes(selectedValue)) {
+          if(conditions.map(String).includes(String(selectedValue))) {
           } else {
             const textNumericInput = document.getElementById(`case-log-${targetQuestion.replaceAll("_","-")}-field`)
             if (textNumericInput == null) {
