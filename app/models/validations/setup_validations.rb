41302c54aa41b6c7725e3b73d457800ac1d473a8
Dushan Despotovic <dushan@madetech.com>
2022-03-15 16:37:07 +0000

    add wrapper method

--- app/models/validations/setup_validations.rb
+++ app/models/validations/setup_validations.rb
@@ -1,7 +1,13 @@
 module Validations::SetupValidations
   def validate_intermediate_rent_product_name(record)
-    if record.rent_type == 5 && record.intermediate_rent_product_name.blank?
+    if intermediate_product_rent_type?(record) && record.intermediate_rent_product_name.blank?
       record.errors.add :intermediate_rent_product_name, I18n.t("validations.setup.intermediate_rent_product_name.blank")
     end
+  end
+
+private
+
+  def intermediate_product_rent_type?(record)
+    record.rent_type == 5
   end
 end
