65473961526471ed234c2757880f2127c397a278
baarkerlounger <db@slothlife.xyz>
2022-03-15 09:40:57 +0000

    Switch to JSBundling with webpack

---
+++ app/frontend/active_admin.js
@@ -0,0 +1,7 @@
+// Load Active Admin's styles into Webpacker,
+// see `active_admin.scss` for customization.
+import "./styles/active_admin.scss";
+
+import "@activeadmin/activeadmin";
+
+import "chartkick/chart.js"
