6b9c8be3d6337c98d25ae055b90afc983c8d4a9f
baarkerlounger <db@slothlife.xyz>
2022-03-17 14:49:01 +0000

    Stimulus polyfills

--- app/frontend/application.js
+++ app/frontend/application.js
@@ -7,10 +7,11 @@ require.context("govuk-frontend/govuk/assets")
 // Polyfills for IE
 import "core-js/features/promise"
 import "unfetch/polyfill"
+import "@stimulus/polyfills"
 //
 
 import "./styles/application.scss"
-// import "./controllers"
+import "./controllers"
 import "@hotwired/turbo-rails"
 import { initAll } from "govuk-frontend"
 
