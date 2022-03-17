361309dc576b1ecea3fdb5ce53fe72cec895e7ee
baarkerlounger <db@slothlife.xyz>
2022-03-17 14:31:30 +0000

    Add polyfill for fetch

--- app/frontend/application.js
+++ app/frontend/application.js
@@ -3,10 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 require.context("govuk-frontend/govuk/assets")
+
+// Polyfills for IE
 import "core-js/features/promise"
+import "unfetch/polyfill"
+//
 
 import "./styles/application.scss"
-import "./controllers"
+// import "./controllers"
 import "@hotwired/turbo-rails"
 import { initAll } from "govuk-frontend"
 
