e04e3ffd89a458131eb5f262392adcae91e08a07
baarkerlounger <db@slothlife.xyz>
2022-03-18 00:20:42 +0000

    Single babel config

--- app/frontend/application.js
+++ app/frontend/application.js
@@ -2,17 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require.context("govuk-frontend/govuk/assets")
 
 // Polyfills for IE
-import "core-js/features/promise"
-import "unfetch/polyfill"
+import "regenerator-runtime/runtime"
 import "@stimulus/polyfills"
+import "custom-event-polyfill"
 //
 
+require.context("govuk-frontend/govuk/assets")
+import { initAll } from "govuk-frontend"
 import "./styles/application.scss"
 import "./controllers"
 import "@hotwired/turbo-rails"
-import { initAll } from "govuk-frontend"
 
 initAll()
