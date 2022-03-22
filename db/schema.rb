ab27692265d62e9f788c06e7f97c3bf0362eed21
kiddhustle <kiddhustle@wiardweb.com>
2022-03-22 15:42:55 +0000

    CLDC-1050: revert schema

--- db/schema.rb
+++ db/schema.rb
@@ -281,10 +281,10 @@ ActiveRecord::Schema[7.0].define(version: 202202071123100) do
     t.string "last_sign_in_ip"
     t.integer "role"
     t.string "old_user_id"
-    t.string "phone"
     t.integer "failed_attempts", default: 0
     t.string "unlock_token"
     t.datetime "locked_at", precision: nil
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
