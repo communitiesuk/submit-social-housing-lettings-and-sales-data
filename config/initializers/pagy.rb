1386a8b0c63047945f7f33c4f183d870ac0cc118
baarkerlounger <db@slothlife.xyz>
2022-04-13 14:21:04 +0100

    Double quotes

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -167,7 +167,7 @@
 
 # Overflow extra: Allow for easy handling of overflowing pages
 # See https://ddnexus.github.io/pagy/extras/overflow
-require 'pagy/extras/overflow'
+require "pagy/extras/overflow"
 # Pagy::DEFAULT[:overflow] = :empty_page    # default  (other options: :last_page and :exception)
 
 # Support extra: Extra support for features like: incremental, infinite, auto-scroll pagination
