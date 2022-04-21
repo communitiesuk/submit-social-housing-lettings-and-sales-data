bd0ce09917e77661da5669157eb0704cc258c74d
baarkerlounger <db@slothlife.xyz>
2022-04-21 13:03:51 +0100

    Remove unnecessary line break

--- app/helpers/details_table_helper.rb
+++ app/helpers/details_table_helper.rb
@@ -1,7 +1,7 @@
 module DetailsTableHelper
   def details_html(attribute)
     if attribute[:format] == :bullet
-      list = attribute[:value].map { |la| "<li>#{la}</li>" }.join("\n")
+      list = attribute[:value].map { |la| "<li>#{la}</li>" }.join
       simple_format(list, { class: "govuk-list govuk-list--bullet" }, wrapper_tag: "ul")
     else
       simple_format(attribute[:value].to_s, {}, wrapper_tag: "div")
