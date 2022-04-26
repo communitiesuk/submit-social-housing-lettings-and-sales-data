83b399ea4d64f4ec9c04d242ec3d70cded0cbfa1
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-04-26 17:17:26 +0100

    Don’t use bullet list if only one value returned

--- app/helpers/details_table_helper.rb
+++ app/helpers/details_table_helper.rb
@@ -1,10 +1,12 @@
 module DetailsTableHelper
   def details_html(attribute)
-    if attribute[:format] == :bullet
-      list = attribute[:value].map { |la| "<li>#{la}</li>" }.join
+    if attribute[:format] == :bullet && attribute[:value].length > 1
+      list = attribute[:value].map { |value| "<li>#{value}</li>" }.join
       simple_format(list, { class: "govuk-list govuk-list--bullet" }, wrapper_tag: "ul")
     else
-      simple_format(attribute[:value].to_s, {}, wrapper_tag: "div")
+      value = attribute[:value].is_a?(Array) ? attribute[:value].first : attribute[:value]
+
+      simple_format(value.to_s, { class: "govuk-body" }, wrapper_tag: "p")
     end
   end
 end
