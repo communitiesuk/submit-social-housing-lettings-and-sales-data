06bb64d20e435115757e4d3f585b0c37451840d2
Ted-U <92022120+Ted-U@users.noreply.github.com>
2022-04-28 16:52:31 +0100

    fixed total charge bug (#529)
    * fixed total charge bug, when maximum value is exceeded the total will now continue to work
    
    * Remove binding.pry left accidentally

--- app/frontend/controllers/numeric_question_controller.js
+++ app/frontend/controllers/numeric_question_controller.js
@@ -11,9 +11,17 @@ export default class extends Controller {
   calculateFields() {
     const affectedField = this.element.dataset.target;
     const fieldsToAdd = JSON.parse(this.element.dataset.calculated).map(x => `case-log-${x.replaceAll("_","-")}-field`);
-    const valuesToAdd = fieldsToAdd.map(x => document.getElementById(x).value).filter(x => x);
+    const valuesToAdd = fieldsToAdd.map(x => getFieldValue(x)).filter(x => x);
     const newValue =  valuesToAdd.map(x => parseFloat(x)).reduce((a, b) => a + b, 0).toFixed(2);
     const elementToUpdate = document.getElementById(affectedField);
     elementToUpdate.value = newValue;
   }
+  }
+  let getFieldValue = (field) => {
+    const elementFieldToAdd= document.getElementById(field)
+    if (elementFieldToAdd) {
+      return elementFieldToAdd.value
+    }
+     return document.getElementById(`${field}-error`).value
 }
+
