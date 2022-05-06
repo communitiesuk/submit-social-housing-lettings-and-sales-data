9cbbd127c0159454f0e792be45c5ecd2fbba2b11
baarkerlounger <5101747+baarkerlounger@users.noreply.github.com>
2022-05-06 11:51:37 +0100

    Allow illness type to be refused (#553)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_160536) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_092350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -221,6 +221,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_160536) do
     t.string "old_id"
     t.integer "joint"
     t.bigint "created_by_id"
+    t.integer "illness_type_0"
     t.index ["created_by_id"], name: "index_case_logs_on_created_by_id"
     t.index ["managing_organisation_id"], name: "index_case_logs_on_managing_organisation_id"
     t.index ["old_id"], name: "index_case_logs_on_old_id", unique: true
