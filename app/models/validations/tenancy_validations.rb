6e9d4a8b8745cf86a83f7ae1322c66912880457b
Dushan <47317567+dushan-madetech@users.noreply.github.com>
2022-05-06 12:29:21 +0100

    CLDC-744-joint-tenancy-validation (#549)
    * add joint tenancy validation
    
    * fix validation and spec
    
    * improvements
    
    * updates
    
    * lint fixes
    
    * fix typo
    
    * change message displayed on hhmemb page

--- app/models/validations/tenancy_validations.rb
+++ app/models/validations/tenancy_validations.rb
@@ -32,4 +32,13 @@ module Validations::TenancyValidations
   def validate_other_tenancy_type(record)
     validate_other_field(record, 3, :tenancy, :tenancyother)
   end
+
+  def validate_joint_tenancy(record)
+    return unless record.collection_start_year
+
+    if record.hhmemb == 1 && record.joint != 2 && record.collection_start_year >= 2022
+      record.errors.add :joint, I18n.t("validations.tenancy.not_joint")
+      record.errors.add :hhmemb, I18n.t("validations.tenancy.joint_more_than_one_member")
+    end
+  end
 end
