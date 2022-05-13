804a6bd00c1e7a5ed10071df12f57170ffc87086
Dushan <47317567+dushan-madetech@users.noreply.github.com>
2022-05-13 14:23:45 +0100

    remove letting_in_sheltered_accomodation field (#577)
    * remove letting_in_sheltered_accomodation field
    
    This field is now duplicated by the shelteredaccom field
    
    * lint fixes
    
    * fix all failing specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_114334) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_123456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -162,7 +162,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_114334) do
     t.integer "net_income_known"
     t.integer "nocharge"
     t.integer "is_carehome"
-    t.integer "letting_in_sheltered_accommodation"
     t.integer "household_charge"
     t.integer "referral"
     t.decimal "brent", precision: 10, scale: 2
