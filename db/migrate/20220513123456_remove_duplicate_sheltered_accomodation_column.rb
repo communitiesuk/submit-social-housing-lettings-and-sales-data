1592fbc686020a6373829ca1e04bb141914b4cce
Dushan Despotovic <dushan@madetech.com>
2022-05-13 14:01:37 +0100

    lint fixes

--- db/migrate/20220513123456_remove_duplicate_sheltered_accomodation_column.rb
+++ db/migrate/20220513123456_remove_duplicate_sheltered_accomodation_column.rb
@@ -1,5 +1,5 @@
 class RemoveDuplicateShelteredAccomodationColumn < ActiveRecord::Migration[7.0]
   def change
-    remove_column :case_logs, :letting_in_sheltered_accommodation
+    remove_column :case_logs, :letting_in_sheltered_accommodation, :integer
   end
 end
