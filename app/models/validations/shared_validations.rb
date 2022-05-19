9ee8928c73595495a74c2855fc6cc0c1c5df2605
baarkerlounger <db@slothlife.xyz>
2022-05-19 14:37:19 +0100

    Enable better other field validation labels

--- app/models/validations/shared_validations.rb
+++ app/models/validations/shared_validations.rb
@@ -1,9 +1,9 @@
 module Validations::SharedValidations
-  def validate_other_field(record, value_other = nil, main_field = nil, other_field = nil)
+  def validate_other_field(record, value_other = nil, main_field = nil, other_field = nil, main_label = nil, other_label = nil)
     return unless main_field || other_field
 
-    main_field_label = main_field.to_s.humanize(capitalize: false)
-    other_field_label = other_field.to_s.humanize(capitalize: false)
+    main_field_label = main_label || main_field.to_s.humanize(capitalize: false)
+    other_field_label = other_label || other_field.to_s.humanize(capitalize: false)
     if record[main_field] == value_other && record[other_field].blank?
       record.errors.add other_field.to_sym, I18n.t("validations.other_field_missing", main_field_label:, other_field_label:)
     end
