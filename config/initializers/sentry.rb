1fdc7dfcdc2a2d352b8bd1791f95b8362530e46c
Stéphane Meny <smeny@users.noreply.github.com>
2022-05-19 19:33:30 +0100

    Removes Sentry custom instrumentation

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.enabled_environments = %w[production staging]
-  config.traces_sample_rate = 1
+  config.traces_sample_rate = 0.2
 end
