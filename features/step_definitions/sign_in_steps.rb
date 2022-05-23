b13fbaecaf3647251ac1aa827c03e520df004aef
JG <moarpheus@gmail.com>
2022-05-23 07:35:47 +0100

    empty steps for sign in

---
+++ features/step_definitions/sign_in_steps.rb
@@ -0,0 +1,14 @@
+Given('There is a user in the database') do
+end
+
+When('I visit the sign in page') do
+end
+
+When('I fill in the sign in form') do
+end
+
+When('I click the sign in button') do
+end
+
+Then('I should see the logs page') do
+end
