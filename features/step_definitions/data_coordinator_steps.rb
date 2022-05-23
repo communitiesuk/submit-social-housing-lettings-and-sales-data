ac95453311ce36264db09ff75f027c249728d89f
JG <moarpheus@gmail.com>
2022-05-23 12:27:27 +0100

    added steps viewing your organisation

--- features/step_definitions/data_coordinator_steps.rb
+++ features/step_definitions/data_coordinator_steps.rb
@@ -16,3 +16,17 @@ end
 Then("the user navigation bar is highlighted") do
   expect(page).to have_css('[aria-current="page"]', text: "Users")
 end
+
+When('I visit the About your organisation page') do
+  click_link("About your organisation")
+end
+
+Then('I see information about your organisation') do
+  expect(page.body).to have_content @user.organisation.name
+  expect(page.body).to have_content @user.organisation.address_line1
+  expect(page.body).to have_content @user.organisation.postcode
+end
+
+Then('the about your organisation navigation bar is highlighted') do
+  expect(page).to have_css('[aria-current="page"]', text: "About your organisation")
+end
