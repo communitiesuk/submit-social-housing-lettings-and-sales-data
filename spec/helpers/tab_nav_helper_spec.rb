97fb7439f2caa01e7833067c262988d72f93c06f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-05-24 12:39:25 +0100

    Correct markup for user and organisation tables

--- spec/helpers/tab_nav_helper_spec.rb
+++ spec/helpers/tab_nav_helper_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe TabNavHelper do
 
   describe "#user_cell" do
     it "returns user link and email separated by a newline character" do
-      expected_html = "<a class=\"govuk-link govuk-!-font-weight-bold\" href=\"/users\">Danny Rojas</a>\n#{user.email}"
+      expected_html = "<a class=\"govuk-link\" href=\"/users\">Danny Rojas</a>\n<span class=\"govuk-!-font-weight-regular app-!-colour-muted\">#{user.email}</span>"
       expect(user_cell(user)).to match(expected_html)
     end
   end
 
   describe "#org_cell" do
     it "returns the users org name and role separated by a newline character" do
-      expected_html = "DLUHC\n<span class='app-!-colour-muted'>Data provider</span>"
+      expected_html = "DLUHC\n<span class=\"app-!-colour-muted\">Data provider</span>"
       expect(org_cell(user)).to match(expected_html)
     end
   end
