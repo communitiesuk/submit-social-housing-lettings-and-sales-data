7b65d73829fea89e4d8fc8d13ee91ef5a39c9696
JG <moarpheus@gmail.com>
2022-05-26 13:40:10 +0100

    removed redundant specific forms

--- app/components/log_search_component.rb
+++
@@ -1,9 +0,0 @@
-class LogSearchComponent < ViewComponent::Base
-  attr_reader :current_user, :label
-
-  def initialize(current_user:, label:)
-    @current_user = current_user
-    @label = label
-    super
-  end
-end
