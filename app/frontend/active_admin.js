fc3ada63f7b04692ab3905413b9ab767af0a76e6
baarkerlounger <5101747+baarkerlounger@users.noreply.github.com>
2022-05-26 17:02:46 +0100

    Remove active admin (#611)
    * Remove stuff
    
    * Remove ActiveAdmin from Gem file
    
    * Remove routes and table
    
    * Rubocop
    
    * Remove active admin from webpack config
    
    * Remove JQuery from webpack
    
    * Remove remaining spec references
    
    * Remove js packages
    
    * Schema

--- app/frontend/active_admin.js
+++
@@ -1,7 +0,0 @@
-// Load Active Admin's styles into Webpacker,
-// see `active_admin.scss` for customization.
-import "./styles/active_admin.scss";
-
-import "@activeadmin/activeadmin";
-
-import "chartkick/chart.js"
