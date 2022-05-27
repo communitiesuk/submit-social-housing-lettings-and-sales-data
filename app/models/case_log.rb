4bbc93201345cdd3997e9083c9e20447a963c947
JG <moarpheus@gmail.com>
2022-05-27 17:28:04 +0100

    ILIKE for search

--- app/models/case_log.rb
+++ app/models/case_log.rb
@@ -52,9 +52,9 @@ class CaseLog < ApplicationRecord
                          }
 
   scope :filter_by_id, ->(id) { where(id:) }
-  scope :filter_by_tenant_code, ->(code) { where("lower(tenant_code) = ?", code.downcase) }
-  scope :filter_by_propcode, ->(code) { where("lower(propcode) = ?", code.downcase) }
-  scope :filter_by_postcode, ->(code) { where(postcode_full: code.upcase.gsub(/\s+/, "")) }
+  scope :filter_by_tenant_code, ->(tenant_code) { where("lower(tenant_code) ILIKE ?", "%#{tenant_code.downcase}%") }
+  scope :filter_by_propcode, ->(propcode) { where("lower(propcode) ILIKE ?", "%#{propcode.downcase}%") }
+  scope :filter_by_postcode, ->(postcode_full) { where("lower(postcode_full) ILIKE ?", "%#{postcode_full.upcase.gsub(/\s+/, "")}%") }
   scope :search_by, lambda { |param|
                       filter_by_id(param)
                                 .or(filter_by_tenant_code(param))
