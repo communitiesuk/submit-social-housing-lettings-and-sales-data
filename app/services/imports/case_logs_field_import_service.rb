2a894794cf9e6973dd72a82f9f5348fac3d61bf7
baarkerlounger <db@slothlife.xyz>
2022-05-30 15:24:44 +0100

    Add task to reimport major repairs answers

--- app/services/imports/case_logs_field_import_service.rb
+++ app/services/imports/case_logs_field_import_service.rb
@@ -4,6 +4,8 @@ module Imports
       case field
       when "tenant_code"
         import_from(folder, :update_tenant_code)
+      when "major_repairs"
+        import_from(folder, :update_major_repairs)
       else
         raise "Updating #{field} is not supported by the field import service"
       end
@@ -11,9 +13,29 @@ module Imports
 
   private
 
+    def update_major_repairs(xml_doc)
+      record = log(xml_doc)
+
+      if record.present?
+        previous_status = field_value(xml_doc, "meta", "status")
+        major_repairs_date = compose_date(xml_doc, "MRCDAY", "MRCMONTH", "MRCYEAR")
+        major_repairs = if major_repairs_date.present? && previous_status.include?("submitted")
+                          1
+                        elsif previous_status.include?("submitted")
+                          0
+                        end
+        if major_repairs.present? && record.majorrepairs.blank? && record.status != "completed"
+          record.update!(mrcdate: major_repairs_date, majorrepairs: major_repairs)
+        elsif record.majorrepairs.present?
+          @logger.info("Case Log #{record.id} has a value for major repairs, skipping update")
+        end
+      else
+        @logger.warn("Could not find record matching legacy ID #{old_id}")
+      end
+    end
+
     def update_tenant_code(xml_doc)
-      old_id = field_value(xml_doc, "meta", "document-id")
-      record = CaseLog.find_by(old_id:)
+      record = log(xml_doc)
 
       if record.present?
         tenant_code = string_or_nil(xml_doc, "_2bTenCode")
@@ -24,6 +46,22 @@ module Imports
         end
       else
         @logger.warn("Could not find record matching legacy ID #{old_id}")
+      end
+    end
+
+    def log(xml_doc)
+      old_id = field_value(xml_doc, "meta", "document-id")
+      CaseLog.find_by(old_id:)
+    end
+
+    def compose_date(xml_doc, day_str, month_str, year_str)
+      day = Integer(field_value(xml_doc, "xmlns", day_str), exception: false)
+      month = Integer(field_value(xml_doc, "xmlns", month_str), exception: false)
+      year = Integer(field_value(xml_doc, "xmlns", year_str), exception: false)
+      if day.nil? || month.nil? || year.nil?
+        nil
+      else
+        Time.zone.local(year, month, day)
       end
     end
 
--- spec/services/imports/case_logs_field_import_service_spec.rb
+++ spec/services/imports/case_logs_field_import_service_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe Imports::CaseLogsFieldImportService do
   let(:real_2021_2022_form) { Form.new("config/forms/2021_2022.json", "2021_2022") }
   let(:fixture_directory) { "spec/fixtures/softwire_imports/case_logs" }
 
+  let(:case_log_id) { "0ead17cb-1668-442d-898c-0d52879ff592" }
+  let(:case_log_file) { open_file(fixture_directory, case_log_id) }
+  let(:case_log_xml) { Nokogiri::XML(case_log_file) }
+  let(:remote_folder) { "case_logs" }
+  let(:old_user_id) { "c3061a2e6ea0b702e6f6210d5c52d2a92612d2aa" }
+
   def open_file(directory, filename)
     File.open("#{directory}/#{filename}.xml")
   end
@@ -18,30 +24,24 @@ RSpec.describe Imports::CaseLogsFieldImportService do
     FactoryBot.create(:organisation, old_visible_id: "1", provider_type: "PRP")
 
     # Created by users
-    FactoryBot.create(:user, old_user_id: "c3061a2e6ea0b702e6f6210d5c52d2a92612d2aa")
+    FactoryBot.create(:user, old_user_id:)
 
     # Stub the form handler to use the real form
     allow(FormHandler.instance).to receive(:get_form).with("2021_2022").and_return(real_2021_2022_form)
 
     WebMock.stub_request(:get, /api.postcodes.io\/postcodes\/LS166FT/)
            .to_return(status: 200, body: '{"status":200,"result":{"codes":{"admin_district":"E08000035"}}}', headers: {})
+
+    # Stub the S3 file listing and download
+    allow(storage_service).to receive(:list_files)
+                                .and_return(["#{remote_folder}/#{case_log_id}.xml"])
+    allow(storage_service).to receive(:get_file_io)
+                                .with("#{remote_folder}/#{case_log_id}.xml")
+                                .and_return(case_log_file)
   end
 
-  context "when updating a specific log value" do
-    let(:case_log_id) { "0ead17cb-1668-442d-898c-0d52879ff592" }
-    let(:case_log_file) { open_file(fixture_directory, case_log_id) }
-    let(:case_log_xml) { Nokogiri::XML(case_log_file) }
-    let(:remote_folder) { "case_logs" }
+  context "when updating tenant code" do
     let(:field) { "tenant_code" }
-
-    before do
-      # Stub the S3 file listing and download
-      allow(storage_service).to receive(:list_files)
-                                  .and_return(["#{remote_folder}/#{case_log_id}.xml"])
-      allow(storage_service).to receive(:get_file_io)
-                                  .with("#{remote_folder}/#{case_log_id}.xml")
-                                  .and_return(case_log_file)
-    end
 
     context "and the case log was previously imported" do
       let(:case_log) { CaseLog.find_by(old_id: case_log_id) }
@@ -70,6 +70,52 @@ RSpec.describe Imports::CaseLogsFieldImportService do
       it "updates the case_log" do
         expect { import_service.send(:update_field, field, remote_folder) }
           .to(change { case_log.reload.tenant_code })
+      end
+    end
+  end
+
+  context "when updating major repairs" do
+    let(:field) { "major_repairs" }
+
+    context "and the case log already has a value" do
+      let(:case_log) { CaseLog.find_by(old_id: case_log_id) }
+
+      before do
+        Imports::CaseLogsImportService.new(storage_service, logger).create_logs(fixture_directory)
+        case_log_file.rewind
+        case_log.update!(majorrepairs: 0, mrcdate: Time.zone.local(2021, 10, 30, 10, 10, 10))
+      end
+
+      it "logs that major repairs already has a value and does not update major repairs" do
+        expect(logger).to receive(:info).with(/Case Log \d+ has a value for major repairs, skipping update/)
+        expect { import_service.send(:update_field, field, remote_folder) }
+          .not_to(change { case_log.reload.majorrepairs })
+      end
+
+      it "logs that major repairs already has a value and does not update the major repairs date" do
+        expect(logger).to receive(:info).with(/Case Log \d+ has a value for major repairs, skipping update/)
+        expect { import_service.send(:update_field, field, remote_folder) }
+          .not_to(change { case_log.reload.mrcdate })
+      end
+    end
+
+    context "and the case log was previously imported with empty fields" do
+      let(:case_log) { CaseLog.find_by(old_id: case_log_id) }
+
+      before do
+        Imports::CaseLogsImportService.new(storage_service, logger).create_logs(fixture_directory)
+        case_log_file.rewind
+        case_log.update!(mrcdate: nil, majorrepairs: nil)
+      end
+
+      it "updates the case_log major repairs date" do
+        expect { import_service.send(:update_field, field, remote_folder) }
+          .to(change { case_log.reload.mrcdate })
+      end
+
+      it "updates the case_log major repairs" do
+        expect { import_service.send(:update_field, field, remote_folder) }
+          .to(change { case_log.reload.majorrepairs })
       end
     end
   end
