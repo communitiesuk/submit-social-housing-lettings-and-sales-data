aa0453e7dbfde913c17c72eaffec3295f6a62fb2
JG <moarpheus@gmail.com>
2022-06-07 13:18:55 +0100

    code for title format

--- app/helpers/item_label_helper.rb
+++ app/helpers/item_label_helper.rb
@@ -2,4 +2,12 @@ module ItemLabelHelper
   def format_label(count, item)
     count > 1 ? item.pluralize : item
   end
+
+  def format_title(path, searched, page_title, current_user, item_label, count)
+    if searched.present?
+      title = "#{page_title} (#{count} #{item_label} matching ‘#{searched}’)"
+    else
+      title = page_title
+    end
+  end
 end
