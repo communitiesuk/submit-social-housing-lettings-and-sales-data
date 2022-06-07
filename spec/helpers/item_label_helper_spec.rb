d58df1771f6dc1fd8eba3d03db3efbcd7d3f6441
JG <moarpheus@gmail.com>
2022-06-07 11:05:06 +0100

    better test name

--- spec/helpers/item_label_helper_spec.rb
+++ spec/helpers/item_label_helper_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ItemLabelHelper do
   let(:item) { "organisation" }
 
-  describe "get the correct label" do
+  describe "#format_label" do
     it "returns singular when count is 1" do
       expect(format_label(1, item)).to eq("organisation")
     end
