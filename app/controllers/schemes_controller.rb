8d1351603ce6cf9a91d042a98b2b4b251797e3ff
JG <moarpheus@gmail.com>
2022-06-09 11:55:38 +0100

    code to expose title

--- app/controllers/schemes_controller.rb
+++ app/controllers/schemes_controller.rb
@@ -1,7 +1,20 @@
 class SchemesController < ApplicationController
+  include Pagy::Backend
+  include Modules::SearchFilter
+
   before_action :authenticate_user!
 
   def index
-    @all_schemes = Scheme.all
+    all_schemes = Scheme.all
+
+    @pagy, @schemes = pagy(filtered_collection(all_schemes, search_term))
+    @searched = search_term.presence
+    @total_count = all_schemes.size
+  end
+
+  private
+
+  def search_term
+    params["search"]
   end
 end
