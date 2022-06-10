2ae09df82be0b9d5d1877414d59a9c492d029676
JG <moarpheus@gmail.com>
2022-06-10 08:33:25 +0100

    testing org schemes for coordiantor user

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
       get "users", to: "organisations#users"
       get "users/invite", to: "users/account#new"
       get "logs", to: "organisations#logs"
-      get "schemes", to: "organisations#schemes"
+      get "supported-housing", to: "organisations#schemes"
     end
   end
 
