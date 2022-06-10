fe5a1240beaff91aff4c3b246285ffa6bc6cc915
JG <moarpheus@gmail.com>
2022-06-10 06:39:18 +0100

    searching by org name code

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -2,4 +2,5 @@ class Scheme < ApplicationRecord
   belongs_to :organisation
 
   scope :search_by_code, ->(code) { where("code ILIKE ?", "%#{code}%") }
+  scope :search_by_organisation, ->(name) { joins(:organisation).where("name ILIKE ?", "%#{name}%") }
 end
