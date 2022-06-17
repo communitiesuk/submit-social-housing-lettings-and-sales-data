fd4d8612d1e85af2b9a0b4c8ad1c496aafd06bc7
JG <moarpheus@gmail.com>
2022-06-17 15:08:24 +0100

    another fix

--- spec/components/search_result_caption_component_spec.rb
+++ spec/components/search_result_caption_component_spec.rb
@@ -11,14 +11,15 @@ RSpec.describe SearchResultCaptionComponent, type: :component do
 
   it "all of the nav tabs specified in the items hash are passed to it" do
     result = render_inline(described_class.new(searched:, count:, item_label:, total_count:, item:, path:))
-    expect(result.to_html).to eq("  <span class=\"govuk-!-margin-right-4\">\n      <strong>#{count}</strong> #{item_label} found matching '#{searched}' of <strong>#{total_count}</strong> total #{item}. <a class=\"govuk-link\" href=\"path\">Clear search</a>\n</span>\n")
+    expect(result.to_html).to eq("  <span class=\"govuk-!-margin-right-4\">\n      <strong>#{count}</strong> #{item_label} found matching ‘#{searched}’ of <strong>#{total_count}</strong> total #{item}. <a class=\"govuk-link\" href=\"path\">Clear search</a>\n</span>\n")
   end
 
   context "when no search results are found" do
-		let(:searched) { nil }
+    let(:searched) { nil }
     it "all of the nav tabs specified in the items hash are passed to it" do
       result = render_inline(described_class.new(searched:, count:, item_label:, total_count:, item:, path:))
-      expect(result.to_html).to eq("  <span class=\"govuk-!-margin-right-4\">\n      <strong>#{count}</strong> #{item_label} found matching '#{searched}' of <strong>#{total_count}</strong> total #{item}. <a class=\"govuk-link\" href=\"path\">Clear search</a>\n</span>\n")
+
+      expect(result.to_html).to eq("  <span class=\"govuk-!-margin-right-4\">\n      <strong>#{count}</strong> total #{item}.\n</span>\n")
     end
   end
 end
