bee82d3a33dcc9d4b51f226597cb516143b91f05
Ted <ted.booton@madetech.com>
2022-06-17 14:36:10 +0100

    push for review

--- spec/components/search_result_caption_component_spec.rb
+++ spec/components/search_result_caption_component_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe SearchResultCaptionComponent, type: :component do
   end
 
   context "when no search results are found" do
+		let(:searched) { nil }
     it "all of the nav tabs specified in the items hash are passed to it" do
       result = render_inline(described_class.new(searched:, count:, item_label:, total_count:, item:, path:))
       expect(result.to_html).to eq("  <span class=\"govuk-!-margin-right-4\">\n      <strong>#{count}</strong> #{item_label} found matching '#{searched}' of <strong>#{total_count}</strong> total #{item}. <a class=\"govuk-link\" href=\"path\">Clear search</a>\n</span>\n")
