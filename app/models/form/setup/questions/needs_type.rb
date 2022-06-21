a85a8e97c02434f1b0d0ad43f653966becfea735
baarkerlounger <db@slothlife.xyz>
2022-06-21 10:58:34 +0100

    Content tweaks

--- app/models/form/setup/questions/needs_type.rb
+++ app/models/form/setup/questions/needs_type.rb
@@ -4,7 +4,7 @@ class Form::Setup::Questions::NeedsType < ::Form::Question
     @id = "needstype"
     @check_answer_label = "Needs type"
     @header = "What is the needs type?"
-    @hint_text = ""
+    @hint_text = "General needs housing includes both self-contained and shared housing without support or specific adaptations. Supported housing can include direct access hostels, group homes, residential care and nursing homes."
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
     @derived = true
