33697d4b931dcd09bf25a626a2b23d64425c3fc3
JG <moarpheus@gmail.com>
2022-06-27 12:13:06 +0100

    checking the back functionality for returning to check answers from primary group page

--- spec/features/schemes_spec.rb
+++ spec/features/schemes_spec.rb
@@ -399,15 +399,28 @@ RSpec.describe "Schemes scheme Features" do
                       end
                     end
 
-                    it "allows changing primary-client-group question" do
-                      click_link("Change", href: "/schemes/#{scheme.id}/primary-client-group?check_answers=true")
-                      expect(page).to have_current_path("/schemes/#{scheme.id}/primary-client-group?check_answers=true")
+                    context "changing primary client group" do
+                      it "allows changing primary-client-group question" do
+                        click_link("Change", href: "/schemes/#{scheme.id}/primary-client-group?check_answers=true")
+                        expect(page).to have_current_path("/schemes/#{scheme.id}/primary-client-group?check_answers=true")
 
-                      choose "Older people with support needs"
-                      click_button "Save and continue"
+                        choose "Older people with support needs"
+                        click_button "Save and continue"
 
-                      expect(page).to have_current_path("/schemes/#{scheme.id}/check-answers")
-                      expect(page).to have_content "Older people with support needs"
+                        expect(page).to have_current_path("/schemes/#{scheme.id}/check-answers")
+                        expect(page).to have_content "Older people with support needs"
+                      end
+
+                      context "when I press the back button" do
+                        before do
+                          click_link "Back"
+                        end
+
+                        it "lets me select the support answers" do
+                          expect(page).to have_current_path("/schemes/#{scheme.id}/check-answers")
+                          expect(page).to have_content "Check your changes before updating this scheme"
+                        end
+                      end
                     end
 
                     it "allows changing secondary-client-group question" do
