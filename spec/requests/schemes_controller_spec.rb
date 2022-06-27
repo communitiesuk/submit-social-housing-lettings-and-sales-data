74373cf025eff457fa0a4f99b7c69027726522de
Kat <katrina@madetech.com>
2022-06-27 15:12:53 +0100

    Add tests for new controller method

--- spec/requests/schemes_controller_spec.rb
+++ spec/requests/schemes_controller_spec.rb
@@ -448,4 +448,56 @@ RSpec.describe SchemesController, type: :request do
       end
     end
   end
+
+  describe "#new" do
+    context "when not signed in" do
+      it "redirects to the sign in page" do
+        patch "/schemes/1"
+        expect(response).to redirect_to("/account/sign-in")
+      end
+    end
+
+    context "when signed in as a data provider" do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in user
+        get "/schemes/new"
+      end
+
+      it "returns 401 unauthorized" do
+        request
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "when signed in as a data coordinator" do
+      let(:user) { FactoryBot.create(:user, :data_coordinator) }
+
+      before do
+        sign_in user
+        get "/schemes/new"
+      end
+
+      it "returns a template for a new scheme" do
+        expect(response).to have_http_status(:ok)
+        expect(page).to have_content("Create a new supported housing scheme")
+      end
+    end
+
+    context "when signed in as a support user" do
+      let(:user) { FactoryBot.create(:user, :support) }
+
+      before do
+        allow(user).to receive(:need_two_factor_authentication?).and_return(false)
+        sign_in user
+        get "/schemes/new"
+      end
+
+      it "returns a template for a new scheme" do
+        expect(response).to have_http_status(:ok)
+        expect(page).to have_content("Create a new supported housing scheme")
+      end
+    end
+  end
 end
