4bf79587aa86a6714fb50601e1b6ea88507dd9c5
JG <moarpheus@gmail.com>
2022-06-30 16:27:59 +0100

    thanks rubocop

--- db/migrate/20220630114748_add_name_to_locations.rb
+++ db/migrate/20220630114748_add_name_to_locations.rb
@@ -1,5 +1,5 @@
 class AddNameToLocations < ActiveRecord::Migration[7.0]
-  change_table :locations, :bulk => true do |t|
+  change_table :locations, bulk: true do |t|
     t.string :name
     t.integer :total_units
   end
