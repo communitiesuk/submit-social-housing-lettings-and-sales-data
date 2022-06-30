d8773b301772d2940834cb6365a9b999d8068cb4
JG <moarpheus@gmail.com>
2022-06-30 14:52:14 +0100

    creating location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,9 +7,22 @@ class LocationsController < ApplicationController
     @location = Location.new
   end
 
+  def create
+    debugger
+    @scheme = Scheme.find(params[:id])
+    @location = Location.new(location_params)
+    @location.save
+    render "schemes/check_answers"
+  end
+
 private
 
   def authenticate_scope!
     head :unauthorized and return unless current_user.data_coordinator? || current_user.support?
+  end
+
+  def location_params
+    required_params = params.require(:location).permit(:postcode, :name, :total_units, :type_of_unit, :wheelchair_adaptation, :add_another_location).merge(scheme_id: @scheme.id)
+    required_params
   end
 end
