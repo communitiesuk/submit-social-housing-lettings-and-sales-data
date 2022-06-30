7a32099e26c522ef5588f844d6c1d0471c57e1ff
JG <moarpheus@gmail.com>
2022-06-30 11:27:43 +0100

    location controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,14 @@
+class LocationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_scope!
+
+  def new
+    @location = Location.new
+  end
+
+private
+
+  def authenticate_scope!
+    head :unauthorized and return unless current_user.data_coordinator? || current_user.support?
+  end
+end
