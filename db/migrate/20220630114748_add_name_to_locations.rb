7ea5673cae33532fe009412abbe73aa5ef9b7c2a
JG <moarpheus@gmail.com>
2022-06-30 12:53:12 +0100

    moved total units to locations

--- db/migrate/20220630114748_add_name_to_locations.rb
+++ db/migrate/20220630114748_add_name_to_locations.rb
@@ -1,5 +1,6 @@
 class AddNameToLocations < ActiveRecord::Migration[7.0]
   def change
     add_column :locations, :name, :string
+    add_column :locations, :total_units, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -248,6 +248,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_135746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "total_units"
     t.index ["scheme_id"], name: "index_locations_on_scheme_id"
   end
 
