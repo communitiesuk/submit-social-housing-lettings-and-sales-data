4f8db2eb5f79f3e504167c57329c33511955abeb
JG <moarpheus@gmail.com>
2022-06-30 15:22:12 +0100

    fixed factory

--- spec/factories/location.rb
+++ spec/factories/location.rb
@@ -3,7 +3,9 @@ FactoryBot.define do
     location_code { Faker::Name.initials(number: 10) }
     postcode { Faker::Address.postcode.delete(" ") }
     name { Faker::Address.street_name }
-    type_of_unit { Faker::Lorem.word }
+    address_line1 { Faker::Address.street_name }
+    address_line2 { Faker::Address.city }
+    type_of_unit { Faker::Number.within(range: 1..6) }
     type_of_building { Faker::Lorem.word }
     wheelchair_adaptation { 0 }
     county { Faker::Address.state }
