fd12ee72e0051b9cb984224903d1dc7e97eca5c9
JG <moarpheus@gmail.com>
2022-07-05 10:29:15 +0100

    upcasing all postcodes before creation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -51,7 +51,7 @@ private
 
   def location_params
     required_params = params.require(:location).permit(:postcode, :name, :total_units, :type_of_unit, :wheelchair_adaptation, :add_another_location).merge(scheme_id: @scheme.id)
-    required_params[:postcode] = required_params[:postcode].gsub(" ", "").encode("ASCII", "UTF-8", invalid: :replace, undef: :replace, replace: "") if required_params[:postcode]
+    required_params[:postcode] = required_params[:postcode].gsub(" ", "").upcase.encode("ASCII", "UTF-8", invalid: :replace, undef: :replace, replace: "") if required_params[:postcode]
     required_params
   end
 end
