4d15e1870619d3cf1c295c10a5c6dff614ea6d0d
JG <moarpheus@gmail.com>
2022-07-05 10:40:49 +0100

    create locations with support user

--- spec/requests/locations_controller_spec.rb
+++ spec/requests/locations_controller_spec.rb
@@ -131,7 +131,7 @@ RSpec.describe LocationsController, type: :request do
         end
       end
 
-      context "when required organisation id param is missing" do
+      context "when required postcode param is missing" do
         let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", add_another_location: "No" } } }
 
         it "displays the new page with an error message" do
@@ -191,6 +191,105 @@ RSpec.describe LocationsController, type: :request do
 
         it "creates a new location for scheme with valid params" do
           expect(Location.last.scheme.organisation_id).to eq(user.organisation_id)
+          expect(Location.last.name).to eq("Test")
+          expect(Location.last.total_units).to eq(5)
+          expect(Location.last.type_of_unit).to eq("Bungalow")
+          expect(Location.last.wheelchair_adaptation).to eq("No")
+        end
+      end
+    end
+
+    context "when signed in as a support user" do
+      let(:user) { FactoryBot.create(:user, :support) }
+      let!(:scheme) { FactoryBot.create(:scheme) }
+      let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", add_another_location: "No", postcode: "ZZ1 1ZZ" } } }
+
+      before do
+        allow(user).to receive(:need_two_factor_authentication?).and_return(false)
+        sign_in user
+        post "/schemes/#{scheme.id}/location/create", params: params
+      end
+
+      it "creates a new location for scheme with valid params and redirects to correct page" do
+        expect { post "/schemes/#{scheme.id}/location/create", params: }.to change(Location, :count).by(1)
+        follow_redirect!
+        expect(response).to have_http_status(:ok)
+        expect(page).to have_content("Check your answers before creating this scheme")
+      end
+
+      it "creates a new location for scheme with valid params" do
+        expect(Location.last.name).to eq("Test")
+        expect(Location.last.postcode).to eq("ZZ11ZZ")
+        expect(Location.last.total_units).to eq(5)
+        expect(Location.last.type_of_unit).to eq("Bungalow")
+        expect(Location.last.wheelchair_adaptation).to eq("No")
+      end
+
+      context "when postcode is submitted with lower case" do
+        let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", add_another_location: "No", postcode: "zz1 1zz" } } }
+
+        it "creates a new location for scheme with postcode " do
+          expect(Location.last.postcode).to eq("ZZ11ZZ")
+        end
+      end
+
+      context "when required postcode param is missing" do
+        let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", add_another_location: "No" } } }
+
+        it "displays the new page with an error message" do
+          post "/schemes/#{scheme.id}/location/create", params: params
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(page).to have_content(I18n.t("validations.postcode"))
+        end
+      end
+
+      context "when do you want to add another location is selected as yes" do
+        let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", add_another_location: "Yes", postcode: "ZZ1 1ZZ" } } }
+
+        it "creates a new location for scheme with valid params and redirects to correct page" do
+          expect { post "/schemes/#{scheme.id}/location/create", params: }.to change(Location, :count).by(1)
+          follow_redirect!
+          expect(response).to have_http_status(:ok)
+          expect(page).to have_content("Add a location to this scheme")
+        end
+
+        it "creates a new location for scheme with valid params" do
+          expect(Location.last.name).to eq("Test")
+          expect(Location.last.total_units).to eq(5)
+          expect(Location.last.type_of_unit).to eq("Bungalow")
+          expect(Location.last.wheelchair_adaptation).to eq("No")
+        end
+      end
+
+      context "when do you want to add another location is selected as no" do
+        let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", add_another_location: "No", postcode: "ZZ1 1ZZ" } } }
+
+        it "creates a new location for scheme with valid params and redirects to correct page" do
+          expect { post "/schemes/#{scheme.id}/location/create", params: }.to change(Location, :count).by(1)
+          follow_redirect!
+          expect(response).to have_http_status(:ok)
+          expect(page).to have_content("Check your changes before creating this scheme")
+        end
+
+        it "creates a new location for scheme with valid params" do
+          expect(Location.last.name).to eq("Test")
+          expect(Location.last.total_units).to eq(5)
+          expect(Location.last.type_of_unit).to eq("Bungalow")
+          expect(Location.last.wheelchair_adaptation).to eq("No")
+        end
+      end
+
+      context "when do you want to add another location is not selected" do
+        let(:params) { { location: { name: "Test", total_units: "5", type_of_unit: "Bungalow", wheelchair_adaptation: "No", postcode: "ZZ1 1ZZ" } } }
+
+        it "creates a new location for scheme with valid params and redirects to correct page" do
+          expect { post "/schemes/#{scheme.id}/location/create", params: }.to change(Location, :count).by(1)
+          follow_redirect!
+          expect(response).to have_http_status(:ok)
+          expect(page).to have_content("Check your changes before creating this scheme")
+        end
+
+        it "creates a new location for scheme with valid params" do
           expect(Location.last.name).to eq("Test")
           expect(Location.last.total_units).to eq(5)
           expect(Location.last.type_of_unit).to eq("Bungalow")
