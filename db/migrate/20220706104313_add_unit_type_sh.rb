aec0bf39ebef663b5f805d992d62ded702bd5b56
Dushan Despotovic <dushan@madetech.com>
2022-07-08 10:57:52 +0100

    fix schema and migration

--- db/migrate/20220706104313_add_unit_type_sh.rb
+++ db/migrate/20220706104313_add_unit_type_sh.rb
@@ -1,5 +1,7 @@
 class AddUnitTypeSh < ActiveRecord::Migration[7.0]
-  change_table :case_logs, bulk: true do |t|
-    t.integer :unittype_sh
+  def change
+    change_table :case_logs, bulk: true do |t|
+      t.integer :unittype_sh
+    end
   end
 end
