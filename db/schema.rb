8f05f2752b50d7a1ee31186b8c298f5c7e42aa3d
JG <moarpheus@gmail.com>
2022-07-11 14:54:02 +0100

    added support_services_provider

--- db/schema.rb
+++ db/schema.rb
@@ -321,6 +321,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_133937) do
     t.string "old_id"
     t.integer "old_visible_id"
     t.integer "total_units"
+    t.integer "support_services_provider"
     t.index ["managing_organisation_id"], name: "index_schemes_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_schemes_on_owning_organisation_id"
   end
