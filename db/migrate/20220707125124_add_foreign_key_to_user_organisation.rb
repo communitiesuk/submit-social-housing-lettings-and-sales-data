d077c12d5c359336b9af78ba0a43b454eb92394c
Ted <ted.booton@madetech.com>
2022-07-11 15:14:44 +0100

    rubocop

--- db/migrate/20220707125124_add_foreign_key_to_user_organisation.rb
+++ db/migrate/20220707125124_add_foreign_key_to_user_organisation.rb
@@ -1,7 +1,5 @@
 class AddForeignKeyToUserOrganisation < ActiveRecord::Migration[7.0]
- 
   def change
     add_reference :organisations, :users, foreign_key: true, on_delete: :cascade
   end
 end
-
--- db/migrate/20220707133909_add_foreign_key_to_logs_organisation.rb
+++ db/migrate/20220707133909_add_foreign_key_to_logs_organisation.rb
@@ -1,5 +1,4 @@
 class AddForeignKeyToLogsOrganisation < ActiveRecord::Migration[7.0]
-  
   def change
     add_reference :organisations, :case_logs, foreign_key: true, on_delete: :cascade
   end
