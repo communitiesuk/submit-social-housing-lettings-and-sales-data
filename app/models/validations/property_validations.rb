4c7d96099ed757c696230330174c6170cb822211
JG <moarpheus@gmail.com>
2022-07-11 07:51:56 +0100

    extracted regex into initializer

--- app/models/validations/property_validations.rb
+++ app/models/validations/property_validations.rb
@@ -1,7 +1,6 @@
 module Validations::PropertyValidations
   # Validations methods need to be called 'validate_<page_name>' to run on model save
   # or 'validate_' to run on submit as well
-  POSTCODE_REGEXP = /^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/i
 
   def validate_property_number_of_times_relet(record)
     return unless record.offered
