68767552199abd8c3eb668bead1665ec61a0a6c7
Kat <katrina@madetech.com>
2022-07-11 15:45:27 +0100

    lint

--- app/frontend/controllers/accessible_autocomplete_controller.js
+++ app/frontend/controllers/accessible_autocomplete_controller.js
@@ -1,24 +1,24 @@
-import { Controller } from "@hotwired/stimulus";
-import accessibleAutocomplete from "accessible-autocomplete";
-import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
-import { enhanceOption, suggestion, sort } from "../modules/search";
+import { Controller } from '@hotwired/stimulus'
+import accessibleAutocomplete from 'accessible-autocomplete'
+import 'accessible-autocomplete/dist/accessible-autocomplete.min.css'
+import { enhanceOption, suggestion, sort } from '../modules/search'
 
 export default class extends Controller {
-  connect() {
-    const selectEl = this.element;
-    const selectOptions = Array.from(selectEl.options);
-    const options = selectOptions.map((o) => enhanceOption(o));
+  connect () {
+    const selectEl = this.element
+    const selectOptions = Array.from(selectEl.options)
+    const options = selectOptions.map((o) => enhanceOption(o))
 
-    const matches = /^(\w+)\[(\w+)\]$/.exec(selectEl.name);
-    const rawFieldName = `${matches[1]}[${matches[2]}_raw]`;
+    const matches = /^(\w+)\[(\w+)\]$/.exec(selectEl.name)
+    const rawFieldName = `${matches[1]}[${matches[2]}_raw]`
 
     accessibleAutocomplete.enhanceSelectElement({
-      defaultValue: "",
+      defaultValue: '',
       selectElement: selectEl,
       minLength: 2,
       source: (query, populateResults) => {
         if (/\S/.test(query)) {
-          populateResults(sort(query, options));
+          populateResults(sort(query, options))
         }
       },
       autoselect: true,
@@ -28,9 +28,9 @@ export default class extends Controller {
         const selectedOption = [].filter.call(
           selectOptions,
           (option) => (option.textContent || option.innerText) === val
-        )[0];
-        if (selectedOption) selectedOption.selected = true;
-      },
-    });
+        )[0]
+        if (selectedOption) selectedOption.selected = true
+      }
+    })
   }
 }
