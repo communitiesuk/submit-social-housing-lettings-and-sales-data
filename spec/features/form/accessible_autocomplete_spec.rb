c44304093fc136a5973d78d4a92b242991a15007
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2022-07-12 09:48:26 +0100

    Add accessible autocomplete improvements (#728)
    * Add accessible autocomplete improvements
    
    * lint
    
    * Add a check for matches

--- spec/features/form/accessible_autocomplete_spec.rb
+++ spec/features/form/accessible_autocomplete_spec.rb
@@ -21,23 +21,41 @@ RSpec.describe "Accessible Automcomplete" do
     sign_in user
   end
 
-  it "allows type ahead filtering", js: true do
-    visit("/logs/#{case_log.id}/accessible-select")
-    find("#case-log-prevloc-field").click.native.send_keys("T", "h", "a", "n", :down, :enter)
-    expect(find("#case-log-prevloc-field").value).to eq("Thanet")
-  end
+  context "when using accessible autocomplete" do
+    before do
+      visit("/logs/#{case_log.id}/accessible-select")
+    end
 
-  it "maintains enhancement state across back navigation", js: true do
-    visit("/logs/#{case_log.id}/accessible-select")
-    find("#case-log-prevloc-field").click.native.send_keys("T", "h", "a", "n", :down, :enter)
-    click_button("Save and continue")
-    click_link(text: "Back")
-    expect(page).to have_selector("input", class: "autocomplete__input", count: 1)
-  end
+    it "allows type ahead filtering", js: true do
+      find("#case-log-prevloc-field").click.native.send_keys("T", "h", "a", "n", :down, :enter)
+      expect(find("#case-log-prevloc-field").value).to eq("Thanet")
+    end
 
-  it "has a disabled null option" do
-    visit("/logs/#{case_log.id}/accessible-select")
-    expect(page).to have_select("case-log-prevloc-field", disabled_options: ["Select an option"])
+    it "ignores punctuation", js: true do
+      find("#case-log-prevloc-field").click.native.send_keys("T", "h", "a", "'", "n", :down, :enter)
+      expect(find("#case-log-prevloc-field").value).to eq("Thanet")
+    end
+
+    it "ignores stop words", js: true do
+      find("#case-log-prevloc-field").click.native.send_keys("t", "h", "e", " ", "W", "e", "s", "t", "m", :down, :enter)
+      expect(find("#case-log-prevloc-field").value).to eq("Westminster")
+    end
+
+    it "does not perform an exact match", js: true do
+      find("#case-log-prevloc-field").click.native.send_keys("o", "n", "l", "y", " ", "t", "o", "w", "n", :down, :enter)
+      expect(find("#case-log-prevloc-field").value).to eq("The one and only york town")
+    end
+
+    it "maintains enhancement state across back navigation", js: true do
+      find("#case-log-prevloc-field").click.native.send_keys("T", "h", "a", "n", :down, :enter)
+      click_button("Save and continue")
+      click_link(text: "Back")
+      expect(page).to have_selector("input", class: "autocomplete__input", count: 1)
+    end
+
+    it "has a disabled null option" do
+      expect(page).to have_select("case-log-prevloc-field", disabled_options: ["Select an option"])
+    end
   end
 
   it "has the correct option selected if one has been saved" do
