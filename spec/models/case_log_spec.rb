c45be0f73207b9a37d94766d0119cd7854eaa2aa
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2022-07-18 13:50:28 +0100

    add unittype_sh column to csv export (#747)
    * add unittype_sh column to csv export
    
    * add test for csv download
    
    * rebase main
    
    * 🤦‍♀️
    
    * Remove destroy all for case log csv test

--- spec/models/case_log_spec.rb
+++ spec/models/case_log_spec.rb
@@ -2235,4 +2235,23 @@ RSpec.describe CaseLog do
       end
     end
   end
+
+  describe "csv download" do
+    let(:csv_export_file) { File.open("spec/fixtures/files/case_logs_download.csv", "r:UTF-8") }
+    let(:scheme) { FactoryBot.create(:scheme) }
+    let(:location) { FactoryBot.create(:location, scheme:, type_of_unit: 6, postcode: "SE11TE") }
+
+    before do
+      Timecop.freeze(Time.utc(2022, 6, 5))
+    end
+
+    it "generates a correct csv from a case log" do
+      case_log = FactoryBot.create(:case_log, needstype: 2, scheme:, location:)
+      expected_content = csv_export_file.read
+      expected_content.sub!(/\{id\}/, case_log["id"].to_s)
+      expected_content.sub!(/\{owning_org_id\}/, case_log["owning_organisation_id"].to_s)
+      expected_content.sub!(/\{scheme_id\}/, scheme["service_name"].to_s)
+      expect(described_class.to_csv).to eq(expected_content)
+    end
+  end
 end
