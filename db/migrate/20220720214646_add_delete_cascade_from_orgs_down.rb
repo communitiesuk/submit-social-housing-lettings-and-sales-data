b23c9a950256ee6b3594f08aad07a75feb1ea13d
Ted-U <tedbooton@gmail.com>
2022-07-21 12:22:15 +0100

    fix migration - remove duplication

--- db/migrate/20220720214646_add_delete_cascade_from_orgs_down.rb
+++ db/migrate/20220720214646_add_delete_cascade_from_orgs_down.rb
@@ -1,11 +1,9 @@
 class AddDeleteCascadeFromOrgsDown < ActiveRecord::Migration[7.0]
   def up
     add_foreign_key :schemes, :organisations, column: "owning_organisation_id", on_delete: :cascade
-    add_foreign_key :schemes, :organisations, column: "managing_organisation_id"
   end
 
   def down
-    remove_foreign_key :schemes, :organisations, column: "managing_organisation_id"
     remove_foreign_key :schemes, :organisations, column: "owning_organisation_id", on_delete: :cascade
   end
 end
