bdfa6652d30064512becc7d36ac82bd4241e69c6
Ted-U <tedbooton@gmail.com>
2022-07-22 15:51:50 +0100

    keep foreign key for managing organisations

--- db/migrate/20220720214646_add_delete_cascade_from_orgs_down.rb
+++ db/migrate/20220720214646_add_delete_cascade_from_orgs_down.rb
@@ -1,7 +1,6 @@
 class AddDeleteCascadeFromOrgsDown < ActiveRecord::Migration[7.0]
   def change
     remove_foreign_key :schemes, :organisations, column: "owning_organisation_id"
-    remove_foreign_key :schemes, :organisations, column: "managing_organisation_id"
     add_foreign_key :schemes, :organisations, column: "owning_organisation_id", on_delete: :cascade
   end
 end
