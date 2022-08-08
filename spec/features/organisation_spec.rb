c96c2acd89e19663a310d91a150f846c3cc53a4c
Ted-U <tedbooton@gmail.com>
2022-08-08 15:02:14 +0100

    added to organisation factory and fixed tests

--- spec/features/organisation_spec.rb
+++ spec/features/organisation_spec.rb
@@ -40,21 +40,25 @@ RSpec.describe "User Features" do
         expect(page).to have_current_path("/organisations/#{org_id}/details")
       end
 
-      context "when the user is a coordinator and the organisation holds housing stock" do
-        let(:organisation) { FactoryBot.create(:organisation, holds_own_stock: true) }
-
-        it "shows schemes in the navigation bar" do
-          visit("/logs")
-          expect(page).to have_link("Schemes", href: "/schemes")
-        end
-      end
-
       context "when the user is a coordinator and the organisation does not hold housing stock" do
-        let(:organisation) { FactoryBot.create(:organisation, holds_own_stock: false) }
+        before do
+          organisation.update(holds_own_stock: false)
+        end
 
         it "does not show schemes in the navigation bar" do
           visit("/logs")
           expect(page).not_to have_link("Schemes", href: "/schemes")
+        end
+      end
+
+      context "when the user is a coordinator and the organisation holds housing stock" do
+        before do
+          organisation.update(holds_own_stock: true)
+        end
+
+        it "shows schemes in the navigation bar" do
+          visit("/logs")
+          expect(page).to have_link("Schemes", href: "/schemes")
         end
       end
     end
