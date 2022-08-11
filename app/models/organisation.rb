43a894021f997ac0897537501587697ecbb806af
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-08-11 09:06:28 +0100

    Add join table for organisation self-referential parent/child relationships

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -6,6 +6,10 @@ class Organisation < ApplicationRecord
   has_many :organisation_rent_periods
   has_many :owned_schemes, class_name: "Scheme", foreign_key: "owning_organisation_id", dependent: :delete_all
   has_many :managed_schemes, class_name: "Scheme", foreign_key: "managing_organisation_id"
+  has_many :parent_organisation_relationships, foreign_key: :child_organisation_id, class_name: "OrganisationRelationship"
+  has_many :parent_organisations, through: :parent_organisation_relationships
+  has_many :child_organisation_relationships, foreign_key: :parent_organisation_id, class_name: "OrganisationRelationship"
+  has_many :child_organisations, through: :child_organisation_relationships
 
   scope :search_by_name, ->(name) { where("name ILIKE ?", "%#{name}%") }
   scope :search_by, ->(param) { search_by_name(param) }
