c994c67f93a708e0eb9dab549a982384cda2397c
Dushan Despotovic <dushan@madetech.com>
2022-08-22 15:04:55 +0100

    lint fixes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,5 +63,5 @@ Rails.application.configure do
   Faker::Config.locale = "en-GB"
 
   # see https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
-  config.active_record.yaml_column_permitted_classes = [Time]  
+  config.active_record.yaml_column_permitted_classes = [Time]
 end
