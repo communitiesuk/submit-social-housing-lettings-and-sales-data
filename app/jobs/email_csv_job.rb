7927cf99dafe24819f785ea3c89a64691ce91793
James Rose <james@jbpr.net>
2022-09-14 10:58:29 +0100

    Use service credentials to upload and presign CSV exports to S3 (#868)

--- app/jobs/email_csv_job.rb
+++ app/jobs/email_csv_job.rb
@@ -11,7 +11,7 @@ class EmailCsvJob < ApplicationJob
 
     filename = organisation.present? ? "logs-#{organisation.name}-#{Time.zone.now}.csv" : "logs-#{Time.zone.now}.csv"
 
-    storage_service = Storage::S3Service.new(Configuration::PaasConfigurationService.new, ENV["CSV_DOWNLOAD_PAAS_INSTANCE"])
+    storage_service = Storage::S3Service.new(Configuration::EnvConfigurationService.new, ENV["CSV_DOWNLOAD_PAAS_INSTANCE"])
     storage_service.write_file(filename, BYTE_ORDER_MARK + filtered_logs.to_csv(user))
 
     url = storage_service.get_presigned_url(filename, EXPIRATION_TIME)
