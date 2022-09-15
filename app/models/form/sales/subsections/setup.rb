32a99f1c5eaa149ae9c750ca95ae4b5b565d84de
baarkerlounger <5101747+baarkerlounger@users.noreply.github.com>
2022-09-15 13:55:47 +0100

    Add common organisation and created by questions to sales logs (#874)

--- app/models/form/sales/subsections/setup.rb
+++ app/models/form/sales/subsections/setup.rb
@@ -8,6 +8,8 @@ class Form::Sales::Subsections::Setup < ::Form::Subsection
 
   def pages
     @pages ||= [
+      Form::Common::Pages::Organisation.new(nil, nil, self),
+      Form::Common::Pages::CreatedBy.new(nil, nil, self),
       Form::Sales::Pages::PurchaserCode.new(nil, nil, self),
       Form::Sales::Pages::SaleDate.new(nil, nil, self),
     ]
