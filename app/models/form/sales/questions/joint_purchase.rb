d1bb8a5d6f82a196fd8a26c01bea0e8e42a3c03c
Ted-U <tedbooton@gmail.com>
2022-09-20 13:23:17 +0100

    lint

--- app/models/form/sales/questions/joint_purchase.rb
+++ app/models/form/sales/questions/joint_purchase.rb
@@ -1,18 +1,17 @@
 class Form::Sales::Questions::JointPurchase < ::Form::Question
-    def initialize(id, hsh, page)
-      super
-      @id = "type"
-      @check_answer_label = "Joint purchase"
-      @header = "Is this a joint purchase?"
-      @hint_text = ""
-      @type = "radio"
-      @answer_options = ANSWER_OPTIONS
-      @page = page
-    end
-  
-    ANSWER_OPTIONS = {
-        "1" => { "value" => "Yes" },
-        "0" => { "value" => "No" },
-      }.freeze
+  def initialize(id, hsh, page)
+    super
+    @id = "type"
+    @check_answer_label = "Joint purchase"
+    @header = "Is this a joint purchase?"
+    @hint_text = ""
+    @type = "radio"
+    @answer_options = ANSWER_OPTIONS
+    @page = page
   end
-  
+
+  ANSWER_OPTIONS = {
+    "1" => { "value" => "Yes" },
+    "0" => { "value" => "No" },
+  }.freeze
+end
