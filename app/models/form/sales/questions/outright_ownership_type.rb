a4906470ea06771323adde74b02d7cc3bb17b29b
Kat <kosiak.katrina@gmail.com>
2022-09-20 10:05:13 +0100

    Add outright sale question and page

---
+++ app/models/form/sales/questions/outright_ownership_type.rb
@@ -0,0 +1,16 @@
+class Form::Sales::Questions::OutrightOwnershipType < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "type"
+    @check_answer_label = "Type of outright sale"
+    @header = "What is the type of outright sale?"
+    @type = "radio"
+    @answer_options = ANSWER_OPTIONS
+    @page = page
+  end
+
+  ANSWER_OPTIONS = {
+    "10" => { "value" => "Outright" },
+    "12" => { "value" => "Other sale" },
+  }.freeze
+end
