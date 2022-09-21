49ae302a94120560187992291f061da14d44e691
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-09-21 17:23:10 +0100

    feat: add boyer_company page and question (migration still to be committed)

---
+++ app/models/form/sales/questions/buyer_company.rb
@@ -0,0 +1,17 @@
+class Form::Sales::Questions::BuyerCompany < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "companybuy"
+    @check_answer_label = "Company buyer"
+    @header = "Is the buyer a company?"
+    @hint_text = ""
+    @type = "radio"
+    @answer_options = ANSWER_OPTIONS
+    @page = page
+  end
+
+  ANSWER_OPTIONS = {
+    "1" => { "value" => "Yes" },
+    "2" => { "value" => "No" },
+  }.freeze
+end
