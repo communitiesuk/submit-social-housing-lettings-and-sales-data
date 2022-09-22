b367c58971077df76fe45e8feb66a2af4a36fb6a
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-09-22 09:26:23 +0100

    feat: add migration and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -235,7 +235,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_133123) do
     t.bigint "location_id"
     t.integer "major_repairs_date_value_check"
     t.integer "void_date_value_check"
-    t.integer "housingneeds_type"
     t.integer "housingneeds_other"
     t.index ["created_by_id"], name: "index_lettings_logs_on_created_by_id"
     t.index ["location_id"], name: "index_lettings_logs_on_location_id"
