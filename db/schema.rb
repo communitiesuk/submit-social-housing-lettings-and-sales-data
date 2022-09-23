61813065e628512af69e2adee7ed0307ad86e81c
Ted-U <92022120+Ted-U@users.noreply.github.com>
2022-09-23 15:07:21 +0100

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_125813) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_093628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -333,9 +332,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_125813) do
     t.string "purchid"
     t.integer "type"
     t.integer "ownershipsch"
+    t.integer "jointpur"
     t.string "othtype"
     t.integer "beds"
-    t.integer "jointpur"
     t.index ["created_by_id"], name: "index_sales_logs_on_created_by_id"
     t.index ["managing_organisation_id"], name: "index_sales_logs_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_sales_logs_on_owning_organisation_id"
