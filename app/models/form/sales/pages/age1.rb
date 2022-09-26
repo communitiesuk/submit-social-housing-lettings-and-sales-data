fb9369ce9e67314e69aafc8a3f8ffb4d870cfc37
Jack S <jacopo.scotti@softwire.com>
2022-09-26 11:18:26 +0100

    [CLDC-1484] Add Age1 page and sections

---
+++ app/models/form/sales/pages/age1.rb
@@ -0,0 +1,16 @@
+class Form::Sales::Pages::Age1 < ::Form::Page
+  def initialize(id, hsh, subsection)
+    super
+    @id = "buyer_1_age"
+    @header = ""
+    @description = ""
+    @subsection = subsection
+  end
+
+  def questions
+    @questions ||= [
+      Form::Sales::Questions::Buyer1AgeKnown.new(nil, nil, self),
+      Form::Sales::Questions::Age1.new(nil, nil, self),
+    ]
+  end
+end
---
+++ app/models/form/sales/subsections/household_characteristics.rb
@@ -0,0 +1,14 @@
+class Form::Sales::Subsections::HouseholdCharacteristics < ::Form::Subsection
+  def initialize(id, hsh, section)
+    super
+    @id = "household_characteristics"
+    @label = "Household characteristics"
+    @section = section
+  end
+
+  def pages
+    @pages ||= [
+      Form::Sales::Pages::Age1.new(nil, nil, self),
+    ]
+  end
+end
