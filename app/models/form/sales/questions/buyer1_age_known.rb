77dc2ac71413316215f044130037331a1e8424f6
Kat <kosiak.katrina@gmail.com>
2022-09-26 17:16:37 +0100

    Change quotes to apostrophes

--- app/models/form/sales/questions/buyer1_age_known.rb
+++ app/models/form/sales/questions/buyer1_age_known.rb
@@ -2,12 +2,12 @@ class Form::Sales::Questions::Buyer1AgeKnown < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "age1_known"
-    @check_answer_label = "Buyer 1's age"
-    @header = "Do you know buyer 1's age?"
+    @check_answer_label = "Buyer 1’s age"
+    @header = "Do you know buyer 1’s age?"
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
     @page = page
-    @hint_text = "Buyer 1 is the person in the household who does the most paid work. If it's a joint purchase and the buyers do the same amount of paid work, buyer 1 is whoever is the oldest."
+    @hint_text = "Buyer 1 is the person in the household who does the most paid work. If it’s a joint purchase and the buyers do the same amount of paid work, buyer 1 is whoever is the oldest."
     @conditional_for = {
       "age1" => [0],
     }
