0712c694e7d702e73266116f1c6b7bdaca8a58ae
Dushan <47317567+dushan-madetech@users.noreply.github.com>
2022-09-27 16:20:12 +0100

    changes for buyer live (#892)
    feat: add migration and updated schema
    
    fix specs
    
    fix schema
    
    fix db field name and specs

--- app/models/form/sales/subsections/setup.rb
+++ app/models/form/sales/subsections/setup.rb
@@ -16,6 +16,7 @@ class Form::Sales::Subsections::Setup < ::Form::Subsection
       Form::Sales::Pages::SharedOwnershipType.new(nil, nil, self),
       Form::Sales::Pages::DiscountedOwnershipType.new(nil, nil, self),
       Form::Sales::Pages::OutrightOwnershipType.new(nil, nil, self),
+      Form::Sales::Pages::BuyerLive.new(nil, nil, self),
       Form::Sales::Pages::JointPurchase.new(nil, nil, self),
       Form::Sales::Pages::NumberJointBuyers.new(nil, nil, self),
     ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_105552) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_133123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -330,13 +330,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_105552) do
     t.integer "type"
     t.integer "ownershipsch"
     t.string "othtype"
-    t.integer "jointpur"
     t.integer "jointmore"
+    t.integer "jointpur"
     t.integer "beds"
+    t.integer "companybuy"
     t.integer "age1"
     t.integer "age1_known"
     t.string "sex1"
     t.integer "buy1livein"
+    t.integer "buylivein"
     t.index ["created_by_id"], name: "index_sales_logs_on_created_by_id"
     t.index ["managing_organisation_id"], name: "index_sales_logs_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_sales_logs_on_owning_organisation_id"
