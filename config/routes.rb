939c848450e6a8d013b685567ca1545a331bb68e
James Rose <james@jbpr.net>
2022-09-28 14:13:36 +0100

    Add Sidekiq web to project
    This is only accessible when you are logged in as a support user. It is accessible at /sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  mount_sidekiq = -> { mount Sidekiq::Web => "/sidekiq" }
+  authenticate(:user, :support?.to_proc, &mount_sidekiq)
+
   devise_for :users, {
     path: :account,
     controllers: {
