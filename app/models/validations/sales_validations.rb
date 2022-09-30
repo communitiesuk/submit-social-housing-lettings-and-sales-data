7623ea730e5be3ab4bb4ea9d268716e4327e90ce
Ted-U <92022120+Ted-U@users.noreply.github.com>
2022-09-30 15:37:41 +0100

    lint

--- app/models/validations/sales_validations.rb
+++ app/models/validations/sales_validations.rb
@@ -6,14 +6,14 @@ module Validations::SalesValidations
     # Integer(record.offered_before_type_cast)
     if record.beds.present? && !record.beds.to_i.between?(1, 9)
       record.errors.add :beds, "Number of bedrooms must be between 1 and 9"
-    end 
+    end
   end
 
   def validate_beds_proptype(record)
-      # Integer(record.offered_before_type_cast)
+    # Integer(record.offered_before_type_cast)
     if record.beds.present? && record.beds.to_i != 1 && record.proptype == 2
-    record.errors.add :beds, "A bedsit can not have more than 1 bedroom"
-    record.errors.add :proptype, "A bedsit can not have more than 1 bedroom"
+      record.errors.add :beds, "A bedsit can not have more than 1 bedroom"
+      record.errors.add :proptype, "A bedsit can not have more than 1 bedroom"
     end
   end
 end
