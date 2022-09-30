fd67e0a5f5d4b16ec1efd893f0e1750fe6b2609b
Mo Seedat <mohammed.seedat@softwire.com>
2022-09-30 09:49:46 +0100

    Remove PubSub events/listeners

--- app/events/base.rb
+++
@@ -1,4 +0,0 @@
-module Events
-  class Base
-  end
-end
--- app/events/import.rb
+++
@@ -1,6 +0,0 @@
-
-  module Import
-    STARTED = :events_import_started
-    ITEM_PROCESSED = :events_import_item_processed
-    FINISHED = :events_import_finished
-  end
