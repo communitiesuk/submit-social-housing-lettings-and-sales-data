1609383108b458dfabec50185bf640170eb7d76e
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-10-06 10:46:59 +0100

    merge: remove changed line

--- app/models/form/sales/subsections/property_information.rb
+++ app/models/form/sales/subsections/property_information.rb
@@ -12,7 +12,6 @@ class Form::Sales::Subsections::PropertyInformation < ::Form::Subsection
       Form::Sales::Pages::PropertyNumberOfBedrooms.new(nil, nil, self),
       Form::Sales::Pages::PropertyBuildingType.new(nil, nil, self),
       Form::Sales::Pages::PropertyUnitType.new(nil, nil, self),
-      Form::Sales::Pages::BuildingType.new(nil, nil, self),
     ]
   end
 end
