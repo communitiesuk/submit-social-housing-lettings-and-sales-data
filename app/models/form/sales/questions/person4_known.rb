ade6774e630775a94d730efcfb99d0789080450c
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-10-14 10:17:29 +0100

    feat: add person 4 known question

---
+++ app/models/form/sales/questions/person4_known.rb
@@ -0,0 +1,24 @@
+class Form::Sales::Questions::Person4Known < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "details_known_4"
+    @check_answer_label = "Details known for person 4"
+    @header = "Do you know the details for person 4?"
+    @type = "radio"
+    @answer_options = ANSWER_OPTIONS
+    @page = page
+    @hint_text = ""
+    @hidden_in_check_answers = {
+      "depends_on" => [
+        {
+          "details_known_4" => 1,
+        },
+      ],
+    }
+  end
+
+  ANSWER_OPTIONS = {
+    "1" => { "value" => "Yes" },
+    "2" => { "value" => "No" },
+  }.freeze
+end
