e0a9fdac0df681f4382c33548953d772cfc14788
Sam Collard <swift@swifteagle.co.uk>
2022-11-04 04:05:02 +0000

    CLDC-1620 Extract GTM IDs to helper

---
+++ app/helpers/analytics_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module AnalyticsHelper
+  def get_gtm_container
+    # Additional environments tbc
+    "GTM-M6GS7FF"
+  end
+
+  def get_gtm_id
+    # Additional environments tbc
+    "G-1RH26G5KVP"
+  end
+end
