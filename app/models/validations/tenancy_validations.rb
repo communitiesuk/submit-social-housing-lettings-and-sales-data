3efcb20e90f65ae0b079f5b6335226826c34122e
David May-Miller <David.May-Miller@softwire.com>
2022-11-08 15:23:11 +0000

    CLDC-1698 Update tenancy length validations to have a minimum of 1 year in most cases

--- app/models/validations/tenancy_validations.rb
+++ app/models/validations/tenancy_validations.rb
@@ -5,7 +5,7 @@ module Validations::TenancyValidations
 
   def validate_fixed_term_tenancy(record)
     is_present = record.tenancylength.present?
-    is_in_range = record.tenancylength.to_i.between?(2, 99)
+    is_in_range = record.tenancylength.to_i.between?(min_tenancy_length(record), 99)
     conditions = [
       {
         condition: !(record.is_secure_tenancy? || record.is_assured_shorthold_tenancy?) && is_present,
@@ -13,11 +13,21 @@ module Validations::TenancyValidations
       },
       {
         condition: (record.is_assured_shorthold_tenancy? && !is_in_range) && is_present,
-        error: I18n.t("validations.tenancy.length.shorthold"),
+        error: I18n.t(
+          "validations.tenancy.length.shorthold",
+          min_tenancy_length: min_tenancy_length(record),
+          rent_type: rent_type_mapping(record)[record.rent_type],
+          needs_type: needs_type_mapping[record.needstype]
+        ),
       },
       {
         condition: record.is_secure_tenancy? && (!is_in_range && is_present),
-        error: I18n.t("validations.tenancy.length.secure"),
+        error: I18n.t(
+          "validations.tenancy.length.secure",
+          min_tenancy_length: min_tenancy_length(record),
+          rent_type: rent_type_mapping(record)[record.rent_type],
+          needs_type: needs_type_mapping[record.needstype]
+        ),
       },
     ]
 
@@ -40,5 +50,27 @@ module Validations::TenancyValidations
       record.errors.add :joint, I18n.t("validations.tenancy.not_joint")
       record.errors.add :hhmemb, I18n.t("validations.tenancy.joint_more_than_one_member")
     end
+  end
+
+  def min_tenancy_length(record)
+    record.needstype == 2 || record.rent_type.in?([3, 5]) ? 1 : 2
+  end
+
+  def rent_type_mapping(record)
+    {
+      1 => "Affordable Rent",
+      2 => "London Affordable Rent",
+      4 => "London Living Rent",
+      3 => "Rent to Buy",
+      0 => "Social Rent",
+      5 => record.irproduct,
+    }
+  end
+
+  def needs_type_mapping
+    {
+      1 => "General needs",
+      2 => "Supported housing",
+    }
   end
 end
