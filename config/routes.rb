ef354fada695d7f7ceaca928b07f7d5344760005
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2022-11-09 15:42:52 +0000

    Update routes (#978)
    * Update routes
    
    * Remove member from routes

--- config/routes.rb
+++ config/routes.rb
@@ -50,12 +50,9 @@ Rails.application.routes.draw do
     get "edit-name", to: "schemes#edit_name"
     get "support-services-provider", to: "schemes#support_services_provider"
 
-    member do
-      resources :locations do
-        get "edit-name", to: "locations#edit_name"
-        get "edit", to: "locations#edit"
-        get "edit-local-authority", to: "locations#edit_local_authority"
-      end
+    resources :locations do
+      get "edit-name", to: "locations#edit_name"
+      get "edit-local-authority", to: "locations#edit_local_authority"
     end
   end
 
