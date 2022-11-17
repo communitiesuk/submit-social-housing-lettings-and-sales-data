148f3126ba348ac24a81231b1ec22609b02b66fe
Kat <katrina@kosiak.co.uk>
2022-11-17 14:41:02 +0000

    refactor into a helper

---
+++ app/helpers/toggle_active_location_helper.rb
@@ -0,0 +1,17 @@
+module ToggleActiveLocationHelper
+    def toggle_location_form_path(action, scheme_id, location_id)
+        if action == "deactivate"
+            scheme_location_new_deactivation_path(scheme_id:, location_id:)
+        else
+            scheme_location_new_reactivation_path(scheme_id:, location_id:)
+        end
+    end
+
+    def date_type_question(action)
+        action == "deactivate" ? :deactivation_date_type : :reactivation_date_type
+    end
+
+    def date_question(action)
+        action == "deactivate" ? :deactivation_date : :reactivation_date
+    end
+end
