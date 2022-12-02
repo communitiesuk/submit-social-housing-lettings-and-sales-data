9772ac686c29699fbdb1b9a20c7cd718624eca20
Kat <katrina@kosiak.co.uk>
2022-12-02 08:51:28 +0000

    Add savings columns to the db

---
+++ db/migrate/20221202083418_add_savings_columns.rb
@@ -0,0 +1,8 @@
+class AddSavingsColumns < ActiveRecord::Migration[7.0]
+  def change
+    change_table :sales_logs, bulk: true do |t|
+      t.column :savingsnk, :int
+      t.column :savings, :int
+    end
+  end
+end
