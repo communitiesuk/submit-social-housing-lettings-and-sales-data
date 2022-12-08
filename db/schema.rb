e57bbe699f9e5b360b6b0c60e30dfc3ab825e6fb
Phil Lee <asmega@users.noreply.github.com>
2022-12-08 15:05:35 +0000

    store filename on bulk upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_081127) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_113235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_081127) do
     t.uuid "identifier", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "filename"
     t.index ["identifier"], name: "index_bulk_uploads_on_identifier", unique: true
     t.index ["user_id"], name: "index_bulk_uploads_on_user_id"
   end
