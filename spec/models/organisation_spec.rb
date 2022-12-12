fa296393224a4661dd26579549d3fd9dded9c55b
James Rose <james@jbpr.net>
2022-12-12 12:32:32 +0000

    Ignore unmapped rent period labels
    - It's possible that we import rent periods for an organisation that we haven't yet defined on the new service
    - In this instance an organisation as `rent_period` of 10, which correspondsto 'Weekly for 53' in the previous service
    - Until we decide how to manage that rent period we should safely generate the rent labels

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -124,11 +124,14 @@ RSpec.describe Organisation, type: :model do
       before do
         FactoryBot.create(:organisation_rent_period, organisation:, rent_period: 2)
         FactoryBot.create(:organisation_rent_period, organisation:, rent_period: 3)
+
+        # Unmapped and ignored by `rent_period_labels`
+        FactoryBot.create(:organisation_rent_period, organisation:, rent_period: 10)
         allow(RentPeriod).to receive(:rent_period_mappings).and_return(rent_period_mappings)
       end
 
       it "has rent periods associated" do
-        expect(organisation.rent_periods).to eq([2, 3])
+        expect(organisation.rent_periods).to eq([2, 3, 10])
       end
 
       it "maps the rent periods to display values" do
