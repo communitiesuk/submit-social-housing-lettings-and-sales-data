37a94e02adfaa04e8541ad6cb93682b599c7dec8
Kat <katrina@kosiak.co.uk>
2022-12-13 09:15:54 +0000

    Add mortgage and value check column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_161657) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_085819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -403,8 +403,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_161657) do
     t.integer "savings"
     t.integer "prevown"
     t.string "sex3"
-    t.integer "income1_value_check"
     t.integer "details_known_1"
+    t.integer "income1_value_check"
+    t.integer "mortgage"
+    t.integer "inc2mort"
+    t.integer "mortgage_value_check"
     t.index ["created_by_id"], name: "index_sales_logs_on_created_by_id"
     t.index ["managing_organisation_id"], name: "index_sales_logs_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_sales_logs_on_owning_organisation_id"
