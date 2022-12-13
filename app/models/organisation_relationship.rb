e49ae426fea6ac301002a32492685cc9393c78e1
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-12-13 10:51:03 +0000

    feat: add copy to en.yml

--- app/models/organisation_relationship.rb
+++ app/models/organisation_relationship.rb
@@ -1,17 +1,17 @@
 class OrganisationRelationship < ApplicationRecord
   belongs_to :child_organisation, class_name: "Organisation"
   belongs_to :parent_organisation, class_name: "Organisation"
-  validates :parent_organisation_id, presence: { message: "You must choose a housing provider" }
-  validates :child_organisation_id, presence: { message: "You must choose a managing agent" }
-  validates :parent_organisation_id, uniqueness: { scope: :child_organisation_id, message: "You have already added this housing provider" }
-  validates :child_organisation_id, uniqueness: { scope: :parent_organisation_id, message: "You have already added this managing agent" }
+  validates :parent_organisation_id, presence: { message: I18n.t("validations.organisation.housing_provider.blank") }
+  validates :child_organisation_id, presence: { message: I18n.t("validations.organisation.managing_agent.blank") }
+  validates :parent_organisation_id, uniqueness: { scope: :child_organisation_id, message: I18n.t("validations.organisation.housing_provider.already_added") }
+  validates :child_organisation_id, uniqueness: { scope: :parent_organisation_id, message: I18n.t("validations.organisation.managing_agent.already_added") }
   validate :validate_housing_provider_owns_stock, on: :housing_provider
 
 private
 
   def validate_housing_provider_owns_stock
     if parent_organisation_id.present? && !parent_organisation.holds_own_stock
-      errors.add :parent_organisation_id, I18n.t("validations.scheme.owning_organisation.does_not_own_stock")
+      errors.add :parent_organisation_id, I18n.t("validations.organisation.housing_provider.does_not_own_stock")
     end
   end
 end
