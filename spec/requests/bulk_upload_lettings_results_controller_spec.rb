9e1be87813d3d06de0837e3569a8e02049722dff
Phil Lee <asmega@users.noreply.github.com>
2022-12-14 15:14:12 +0000

    add error count to bulk upload results

---
+++ spec/requests/bulk_upload_lettings_results_controller_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe BulkUploadLettingsResultsController, type: :request do
+  let(:user) { create(:user) }
+  let(:bulk_upload) { create(:bulk_upload, user:, bulk_upload_errors:) }
+  let(:bulk_upload_errors) { create_list(:bulk_upload_error, 2) }
+
+  before do
+    sign_in user
+  end
+
+  describe "GET /lettings-logs/bulk-upload-results/:ID" do
+    it "renders correct year" do
+      get "/lettings-logs/bulk-upload-results/#{bulk_upload.id}"
+
+      expect(response).to be_successful
+      expect(response.body).to include("Bulk Upload for lettings (2022/23)")
+    end
+
+    it "renders correct number of errors" do
+      get "/lettings-logs/bulk-upload-results/#{bulk_upload.id}"
+
+      expect(response).to be_successful
+      expect(response.body).to include("We found 2 errors in your file")
+    end
+  end
+end
