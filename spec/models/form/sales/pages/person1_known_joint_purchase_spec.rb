7ec3ef4c8961f4a4e73060287c91c331424b3225
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-12-14 16:59:00 +0000

    test: update tests

---
+++ spec/models/form/sales/pages/person1_known_joint_purchase_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Pages::Person1KnownJointPurchase, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection) }
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[details_known_1])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("person_1_known_joint_purchase")
+  end
+
+  it "has the correct header" do
+    expect(page.header).to eq("")
+  end
+
+  it "has the correct header_partial" do
+    expect(page.header_partial).to eq(nil)
+  end
+
+  it "has the correct description" do
+    expect(page.description).to eq("")
+  end
+
+  it "has correct depends_on" do
+    expect(page.depends_on).to eq(
+      [
+        { "hholdcount" => 1, "jointpur" => 1 },
+        { "hholdcount" => 2, "jointpur" => 1 },
+        { "hholdcount" => 3, "jointpur" => 1 },
+        { "hholdcount" => 4, "jointpur" => 1 },
+      ],
+    )
+  end
+end
---
+++ spec/models/form/sales/pages/person4_age_joint_purchase_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Pages::Person4AgeJointPurchase, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection) }
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[age6_known age6])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("person_4_age_joint_purchase")
+  end
+
+  it "has the correct header" do
+    expect(page.header).to eq("")
+  end
+
+  it "has the correct description" do
+    expect(page.description).to eq("")
+  end
+
+  it "has correct depends_on" do
+    expect(page.depends_on).to eq(
+      [{ "details_known_4" => 1, "jointpur" => 1 }],
+    )
+  end
+end
