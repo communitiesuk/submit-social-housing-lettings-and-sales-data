07f345df71c02504f2a6ca9d58e414ae3cbb9e79
natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>
2022-12-14 09:33:39 +0000

    CLDC-1730-stock-owning-validation (#1073)
    * feat: add error message if org doesn't own stock
    
    * feat: move org relationship errors to model
    
    * feat: use correct model for form
    
    * tests: update params in tests
    
    * feat: add specific fields for parent/child errors
    
    * test: fix params in failing tests
    
    * refactor: unrelated linting
    
    * feat: simplify controller behaviour further
    
    * test: address failing tests
    
    * feat: add copy to en.yml
    
    * feat: add updated validation message and rename related housing provider -> stock owner

--- app/controllers/organisation_relationships_controller.rb
+++ app/controllers/organisation_relationships_controller.rb
@@ -5,13 +5,19 @@ class OrganisationRelationshipsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_scope!
 
+  before_action :organisations
+  before_action :target_organisation, only: %i[
+    remove_housing_provider
+    remove_managing_agent
+    delete_housing_provider
+    delete_managing_agent
+  ]
+
   def housing_providers
     housing_providers = organisation.housing_providers
     unpaginated_filtered_housing_providers = filtered_collection(housing_providers, search_term)
-    organisations = Organisation.where.not(id: @organisation.id).pluck(:id, :name)
-    respond_to :html
+
     @pagy, @housing_providers = pagy(unpaginated_filtered_housing_providers)
-    @organisations = organisations
     @searched = search_term.presence
     @total_count = housing_providers.size
   end
@@ -19,107 +25,84 @@ class OrganisationRelationshipsController < ApplicationController
   def managing_agents
     managing_agents = organisation.managing_agents
     unpaginated_filtered_managing_agents = filtered_collection(managing_agents, search_term)
-    organisations = Organisation.where.not(id: @organisation.id).pluck(:id, :name)
-    respond_to :html
+
     @pagy, @managing_agents = pagy(unpaginated_filtered_managing_agents)
-    @organisations = organisations
     @searched = search_term.presence
     @total_count = managing_agents.size
   end
 
   def add_housing_provider
-    @organisations = Organisation.where.not(id: @organisation.id).pluck(:id, :name)
-    respond_to :html
+    @organisation_relationship = organisation.parent_organisation_relationships.new
   end
 
   def add_managing_agent
-    @organisations = Organisation.where.not(id: @organisation.id).pluck(:id, :name)
-    respond_to :html
+    @organisation_relationship = organisation.child_organisation_relationships.new
   end
 
   def create_housing_provider
-    child_organisation = @organisation
-    if params[:organisation][:related_organisation_id].empty?
-      @organisation.errors.add :related_organisation_id, "You must choose a housing provider"
-      @organisations = Organisation.where.not(id: child_organisation.id).pluck(:id, :name)
-      render "organisation_relationships/add_housing_provider"
-      return
+    @organisation_relationship = organisation.parent_organisation_relationships.new(organisation_relationship_params)
+    if @organisation_relationship.save(context: :housing_provider)
+      flash[:notice] = "#{@organisation_relationship.parent_organisation.name} is now one of #{current_user.data_coordinator? ? 'your' : "this organisation's"} housing providers"
+      redirect_to housing_providers_organisation_path
     else
-      parent_organisation = related_organisation
-      if OrganisationRelationship.exists?(child_organisation:, parent_organisation:)
-        @organisation.errors.add :related_organisation_id, "You have already added this housing provider"
-        @organisations = Organisation.where.not(id: child_organisation.id).pluck(:id, :name)
-        render "organisation_relationships/add_housing_provider"
-        return
-      end
+      @organisations = Organisation.where.not(id: organisation.id).pluck(:id, :name)
+      render "organisation_relationships/add_housing_provider", status: :unprocessable_entity
     end
-    create!(child_organisation:, parent_organisation:)
-    flash[:notice] = "#{related_organisation.name} is now one of #{current_user.data_coordinator? ? 'your' : "this organisation's"} housing providers"
-    redirect_to housing_providers_organisation_path
   end
 
   def create_managing_agent
-    parent_organisation = @organisation
-    if params[:organisation][:related_organisation_id].empty?
-      @organisation.errors.add :related_organisation_id, "You must choose a managing agent"
-      @organisations = Organisation.where.not(id: parent_organisation.id).pluck(:id, :name)
-      render "organisation_relationships/add_managing_agent"
-      return
+    @organisation_relationship = organisation.child_organisation_relationships.new(organisation_relationship_params)
+    if @organisation_relationship.save
+      flash[:notice] = "#{@organisation_relationship.child_organisation.name} is now one of #{current_user.data_coordinator? ? 'your' : "this organisation's"} managing agents"
+      redirect_to managing_agents_organisation_path
     else
-      child_organisation = related_organisation
-      if OrganisationRelationship.exists?(child_organisation:, parent_organisation:)
-        @organisation.errors.add :related_organisation_id, "You have already added this managing agent"
-        @organisations = Organisation.where.not(id: parent_organisation.id).pluck(:id, :name)
-        render "organisation_relationships/add_managing_agent"
-        return
-      end
+      @organisations = Organisation.where.not(id: organisation.id).pluck(:id, :name)
+      render "organisation_relationships/add_managing_agent", status: :unprocessable_entity
     end
-    create!(child_organisation:, parent_organisation:)
-    flash[:notice] = "#{related_organisation.name} is now one of #{current_user.data_coordinator? ? 'your' : "this organisation's"} managing agents"
-    redirect_to managing_agents_organisation_path
   end
 
-  def remove_housing_provider
-    @target_organisation_id = target_organisation.id
-  end
+  def remove_housing_provider; end
 
   def delete_housing_provider
-    relationship = OrganisationRelationship.find_by!(
-      child_organisation: @organisation,
+    OrganisationRelationship.find_by!(
+      child_organisation: organisation,
       parent_organisation: target_organisation,
-    )
-    relationship.destroy!
+    ).destroy!
     flash[:notice] = "#{target_organisation.name} is no longer one of #{current_user.data_coordinator? ? 'your' : "this organisation's"} housing providers"
     redirect_to housing_providers_organisation_path
   end
 
-  def remove_managing_agent
-    @target_organisation_id = target_organisation.id
-  end
+  def remove_managing_agent; end
 
   def delete_managing_agent
-    relationship = OrganisationRelationship.find_by!(
-      parent_organisation: @organisation,
+    OrganisationRelationship.find_by!(
+      parent_organisation: organisation,
       child_organisation: target_organisation,
-    )
-    relationship.destroy!
+    ).destroy!
     flash[:notice] = "#{target_organisation.name} is no longer one of #{current_user.data_coordinator? ? 'your' : "this organisation's"} managing agents"
     redirect_to managing_agents_organisation_path
   end
 
 private
 
-  def create!(child_organisation:, parent_organisation:)
-    @resource = OrganisationRelationship.new(child_organisation:, parent_organisation:)
-    @resource.save!
-  end
-
   def organisation
-    @organisation ||= Organisation.find(params[:id])
+    @organisation ||= if current_user.support?
+                        Organisation.find(params[:id])
+                      else
+                        current_user.organisation
+                      end
   end
 
-  def related_organisation
-    @related_organisation ||= Organisation.find(params[:organisation][:related_organisation_id])
+  def organisations
+    @organisations ||= Organisation.where.not(id: organisation.id).pluck(:id, :name)
+  end
+
+  def parent_organisation
+    @parent_organisation ||= Organisation.find(params[:organisation_relationship][:parent_organisation_id])
+  end
+
+  def child_organisation
+    @child_organisation ||= Organisation.find(params[:organisation_relationship][:child_organisation_id])
   end
 
   def target_organisation
@@ -130,8 +113,12 @@ private
     params["search"]
   end
 
+  def organisation_relationship_params
+    params.require(:organisation_relationship).permit(:parent_organisation_id, :child_organisation_id)
+  end
+
   def authenticate_scope!
-    if current_user.organisation != organisation && !current_user.support?
+    if current_user.organisation != Organisation.find(params[:id]) && !current_user.support?
       render_not_found
     end
   end
--- app/models/organisation_relationship.rb
+++ app/models/organisation_relationship.rb
@@ -1,4 +1,17 @@
 class OrganisationRelationship < ApplicationRecord
   belongs_to :child_organisation, class_name: "Organisation"
   belongs_to :parent_organisation, class_name: "Organisation"
+  validates :parent_organisation_id, presence: { message: I18n.t("validations.organisation.housing_provider.blank") }
+  validates :child_organisation_id, presence: { message: I18n.t("validations.organisation.managing_agent.blank") }
+  validates :parent_organisation_id, uniqueness: { scope: :child_organisation_id, message: I18n.t("validations.organisation.housing_provider.already_added") }
+  validates :child_organisation_id, uniqueness: { scope: :parent_organisation_id, message: I18n.t("validations.organisation.managing_agent.already_added") }
+  validate :validate_housing_provider_owns_stock, on: :housing_provider
+
+private
+
+  def validate_housing_provider_owns_stock
+    if parent_organisation_id.present? && !parent_organisation.holds_own_stock
+      errors.add :parent_organisation_id, I18n.t("validations.organisation.housing_provider.does_not_own_stock")
+    end
+  end
 end
