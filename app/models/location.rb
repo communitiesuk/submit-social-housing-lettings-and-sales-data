1846e5d4762de43f9a25b2c37313dd49881a8448
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-12-15 15:12:21 +0000

    feat: remove name validation as a result of PO review

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,6 @@ class Location < ApplicationRecord
   validates :postcode, on: :postcode, presence: { message: I18n.t("validations.location.postcode_blank") }
   validate :validate_postcode, on: :postcode, if: proc { |model| model.postcode.presence }
   validates :location_admin_district, on: :location_admin_district, presence: { message: I18n.t("validations.location_admin_district") }
-  validates :name, on: :name, presence: { message: I18n.t("validations.location.name") }
   validates :units, on: :units, presence: { message: I18n.t("validations.location.units") }
   validates :type_of_unit, on: :type_of_unit, presence: { message: I18n.t("validations.location.type_of_unit") }
   validates :mobility_type, on: :mobility_type, presence: { message: I18n.t("validations.location.mobility_standards") }
