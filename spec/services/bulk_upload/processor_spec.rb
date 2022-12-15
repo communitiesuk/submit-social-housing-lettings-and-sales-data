22f51ad363738782e602ec2af2975fa736f0b6e3
Phil Lee <asmega@users.noreply.github.com>
2022-12-15 14:22:43 +0000

    fix test

--- spec/services/bulk_upload/processor_spec.rb
+++ spec/services/bulk_upload/processor_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe BulkUpload::Processor do
 
   context "when processing a bulk upload with errors" do
     describe "#call" do
-      let(:mock_downloader) { instance_double(BulkUpload::Downloader, call: nil) }
+      let(:mock_downloader) { instance_double(BulkUpload::Downloader, call: nil, path: file_fixture("2021_22_lettings_bulk_upload.csv")) }
 
       it "persist the validation errors" do
         allow(BulkUpload::Downloader).to receive(:new).with(bulk_upload:).and_return(mock_downloader)
 
-        expect { processor.call }.to change(BulkUploadError, :count).by(103)
+        expect { processor.call }.to change(BulkUploadError, :count).by(9)
       end
     end
   end
