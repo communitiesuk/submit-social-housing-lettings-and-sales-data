e4ff471005bd1e39b7dd5c541ae559b962381e84
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-12-16 11:55:39 +0000

    feat: add gender identity questions for all 4 people, combine into single page/question

---
+++ app/models/form/sales/questions/person_gender_identity.rb
@@ -0,0 +1,18 @@
+class Form::Sales::Questions::PersonGenderIdentity < ::Form::Sales::Questions::Person
+  def initialize(id, hsh, page, person_index:)
+    super
+    @check_answer_label = "Person #{person_display_number}’s gender identity"
+    @header = "Which of these best describes Person #{person_display_number}’s gender identity?"
+    @type = "radio"
+    @page = page
+    @answer_options = ANSWER_OPTIONS
+    @check_answers_card_number = person_index
+  end
+
+  ANSWER_OPTIONS = {
+    "F" => { "value" => "Female" },
+    "M" => { "value" => "Male" },
+    "X" => { "value" => "Non-binary" },
+    "R" => { "value" => "Person prefers not to say" },
+  }.freeze
+end
