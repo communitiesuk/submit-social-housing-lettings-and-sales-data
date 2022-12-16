f3095e5ce50ba49c217752f4b88689cea3dfa789
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2022-12-16 15:28:34 +0000

    test: update tests

--- spec/models/form/sales/subsections/household_characteristics_spec.rb
+++ spec/models/form/sales/subsections/household_characteristics_spec.rb
@@ -34,7 +34,6 @@ RSpec.describe Form::Sales::Subsections::HouseholdCharacteristics, type: :model 
         buyer_2_working_situation
         buyer_2_live_in_property
         number_of_others_in_property
-        person_1_known
         person_1_age
         person_1_age_joint_purchase
         person_1_gender_identity
