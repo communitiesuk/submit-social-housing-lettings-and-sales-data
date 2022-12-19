14327bd8aaffed55776215c828b4fab3901912cd
Kat <katrina@kosiak.co.uk>
2022-12-19 09:52:16 +0000

    Add deposit soft validation page to the income and benefits subsection

--- spec/models/form/sales/subsections/income_benefits_and_savings_spec.rb
+++ spec/models/form/sales/subsections/income_benefits_and_savings_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe Form::Sales::Subsections::IncomeBenefitsAndSavings, type: :model 
         buyer_2_mortgage_value_check
         savings
         savings_value_check
+        savings_deposit_value_check
         previous_ownership
       ],
     )
