58124e51930cd0d3d1c5b709b7e23bf5731a1953
Kat <katrina@kosiak.co.uk>
2022-12-20 11:03:50 +0000

    add sales information subsections

---
+++ app/models/form/sales/subsections/outright_sale.rb
@@ -0,0 +1,17 @@
+class Form::Sales::Subsections::OutrightSale < ::Form::Subsection
+  def initialize(id, hsh, section)
+    super
+    @id = "outright_sale"
+    @label = "Outright sale"
+    @section = section
+    @depends_on = [{ "ownershipsch" => 3, "setup_completed?" => true }]
+  end
+
+  def pages
+    @pages ||= []
+  end
+
+  def displayed_in_tasklist?(log)
+    log.ownershipsch == 3
+  end
+end
