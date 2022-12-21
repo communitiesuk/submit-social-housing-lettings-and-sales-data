edd85f493d2c871703d80af6ab920badfb1e5c4c
Kat <katrina@kosiak.co.uk>
2022-12-21 14:21:24 +0000

    Add exchange date page and question

---
+++ app/models/form/sales/pages/exchange_date.rb
@@ -0,0 +1,18 @@
+class Form::Sales::Pages::ExchangeDate < ::Form::Page
+  def initialize(id, hsh, subsection)
+    super
+    @id = "exchange_contracts"
+    @header = ""
+    @description = ""
+    @subsection = subsection
+    @depends_on = [{
+      "resale" => 2
+    }]
+  end
+
+  def questions
+    @questions ||= [
+      Form::Sales::Questions::ExchangeDate.new(nil, nil, self),
+    ]
+  end
+end
