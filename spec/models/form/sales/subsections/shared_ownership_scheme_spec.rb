3808c781eb536813df2727d4d9867bf6017d979d
Kat <katrina@kosiak.co.uk>
2022-12-21 09:11:14 +0000

    Add about staircasing page to the shared wnership scheme subsection

--- spec/models/form/sales/subsections/shared_ownership_scheme_spec.rb
+++ spec/models/form/sales/subsections/shared_ownership_scheme_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Form::Sales::Subsections::SharedOwnershipScheme, type: :model do
     expect(shared_ownership_scheme.pages.map(&:id)).to eq(
       %w[
         staircasing
+        about_staircasing
         previous_bedrooms
       ],
     )
