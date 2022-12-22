00219b5e08602158905e5926ba58436763d5966d
natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>
2022-12-22 15:02:22 +0000

    Cldc 1533 is resale (#1118)
    * feat: add resale question and page
    
    * tests: add new tests
    
    * test: update previous tests
    
    * refactor: linting
    
    * refactor: linting

---
+++ app/models/form/sales/pages/resale.rb
@@ -0,0 +1,23 @@
+class Form::Sales::Pages::Resale < ::Form::Page
+  def initialize(id, hsh, subsection)
+    super
+    @id = "resale"
+    @header = ""
+    @description = ""
+    @subsection = subsection
+    @depends_on = [
+      {
+        "staircase" => 2,
+      },
+      {
+        "staircase" => 3,
+      },
+    ]
+  end
+
+  def questions
+    @questions ||= [
+      Form::Sales::Questions::Resale.new(nil, nil, self),
+    ]
+  end
+end
