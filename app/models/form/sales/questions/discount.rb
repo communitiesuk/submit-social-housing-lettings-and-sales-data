d8ce0755d7c7d3cb112e3f3a89c252a5f545e775
Kat <katrina@kosiak.co.uk>
2022-12-23 08:05:59 +0000

    Add about proce questions

---
+++ app/models/form/sales/questions/discount.rb
@@ -0,0 +1,17 @@
+class Form::Sales::Questions::Discount < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "discount"
+    @check_answer_label = "Percentage discount"
+    @header = "What was the percentage discount?"
+    @type = "numeric"
+    @page = page
+    @min = 0
+    @max = 100
+    @width = 5
+    @suffix = "%"
+    @hint_text = "For Right to Buy (RTB), Preserved Right to Buy (PRTB), and Voluntary Right to Buy (VRTB)
+    If discount capped, enter capped %
+    If the property is being sold to an existing tenant under the RTB, PRTB, or VRTB schemes, enter the % discount from the full market value that is being given."
+  end
+end
