ca939f345dc6d69aa3b2f74c66e993329de391f1
Jack S <jacopo.scotti@softwire.com>
2022-12-28 11:05:16 +0000

    wip

---
+++ spec/models/form/sales/pages/buyer1_previous_tenure_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Pages::Buyer1PreviousTenure, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection) }
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[prevten])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("buyers_organisations")
+  end
+
+  it "has the correct header" do
+    expect(page.header).to eq("What was buyer 1's previous tenure?")
+  end
+end
---
+++ spec/models/form/sales/questions/buyer1_previous_tenure_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Questions::Buyer1PreviousTenure, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("prevten")
+  end
+
+  it "has the correct header" do
+    expect(question.header).to eq("What was buyer 1's previous tenure?")
+  end
+
+  it "has the correct check_answer_label" do
+    expect(question.check_answer_label).to eq("Buyer 1's previous tenure")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?).to be false
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq({
+      "1" => { "value" => "Local Authority" },
+      "2" => { "value" => "Private registered provider or housing association tenant" },
+      "3" => { "value" => "Private tenant" },
+      "4" => { "value" => "Tied home or renting with job" },
+      "5" => { "value" => "Owner occupier" },
+      "6" => { "value" => "Living with family or friends" },
+      "7" => { "value" => "Temporary accomodation" },
+      "9" => { "value" => "Other" },
+    })
+  end
+end
