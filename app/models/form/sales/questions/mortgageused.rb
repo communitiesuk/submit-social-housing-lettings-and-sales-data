ccbfbcb520033038f3736a203f8e38c55a5f2034
Kat <katrina@kosiak.co.uk>
2023-01-04 08:57:15 +0000

    Add mortgageused page and question

---
+++ app/models/form/sales/questions/mortgageused.rb
@@ -0,0 +1,17 @@
+class Form::Sales::Questions::Mortgageused < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "mortgageused"
+    @check_answer_label = "Mortgage used"
+    @header = "Was a mortgage used for the purchase of this property?"
+    @type = "radio"
+    @answer_options = ANSWER_OPTIONS
+    @page = page
+    @hint_text = ""
+  end
+
+  ANSWER_OPTIONS = {
+    "1" => { "value" => "Yes" },
+    "2" => { "value" => "No" },
+  }.freeze
+end
