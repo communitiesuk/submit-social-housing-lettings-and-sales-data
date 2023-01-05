6ed6e2392ce12e1f1476cb5023b52b3e5992bc57
Kat <katrina@kosiak.co.uk>
2023-01-05 11:18:13 +0000

    Add armed forces spouse page and question

---
+++ app/models/form/sales/questions/armed_forces_spouse.rb
@@ -0,0 +1,19 @@
+class Form::Sales::Questions::ArmedForcesSpouse < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "armedforcesspouse"
+    @check_answer_label = "Are any of the buyers a spouse or civil partner of a UK armed forces regular who died in service within the last 2 years?"
+    @header = "Are any of the buyers a spouse or civil partner of a UK armed forces regular who died in service within the last 2 years?"
+    @type = "radio"
+    @hint_text = ""
+    @answer_options = ANSWER_OPTIONS
+    @page = page
+  end
+
+  ANSWER_OPTIONS = {
+    "4" => { "value" => "Yes" },
+    "5" => { "value" => "No" },
+    "6" => { "value" => "Buyer prefers not to say" },
+    "7" => { "value" => "Don't know" },
+  }.freeze
+end
---
+++ spec/models/form/sales/questions/armed_forces_spouse_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Questions::ArmedForcesSpouse, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("armedforcesspouse")
+  end
+
+  it "has the correct header" do
+    expect(question.header).to eq("Are any of the buyers a spouse or civil partner of a UK armed forces regular who died in service within the last 2 years?")
+  end
+
+  it "has the correct check_answer_label" do
+    expect(question.check_answer_label).to eq("Are any of the buyers a spouse or civil partner of a UK armed forces regular who died in service within the last 2 years?")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?).to be false
+  end
+
+  it "has the correct hint" do
+    expect(question.hint_text).to eq("")
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq({
+      "4" => { "value" => "Yes" },
+      "5" => { "value" => "No" },
+      "6" => { "value" => "Buyer prefers not to say" },
+      "7" => { "value" => "Don't know" },
+    })
+  end
+end
