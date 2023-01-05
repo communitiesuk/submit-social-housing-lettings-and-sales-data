54e13c13c2a8fb5ee7f711b0a8cae3f8eb18d45b
Kat <katrina@kosiak.co.uk>
2023-01-05 08:44:09 +0000

    Move exchange contracts date page

--- app/models/form/sales/subsections/shared_ownership_scheme.rb
+++ app/models/form/sales/subsections/shared_ownership_scheme.rb
@@ -13,6 +13,7 @@ class Form::Sales::Subsections::SharedOwnershipScheme < ::Form::Subsection
       Form::Sales::Pages::Staircase.new(nil, nil, self),
       Form::Sales::Pages::AboutStaircase.new(nil, nil, self),
       Form::Sales::Pages::Resale.new(nil, nil, self),
+      Form::Sales::Pages::ExchangeDate.new(nil, nil, self),
       Form::Sales::Pages::LaNominations.new(nil, nil, self),
       Form::Sales::Pages::BuyerPrevious.new(nil, nil, self),
       Form::Sales::Pages::PreviousBedrooms.new(nil, nil, self),
@@ -20,7 +21,6 @@ class Form::Sales::Subsections::SharedOwnershipScheme < ::Form::Subsection
       Form::Sales::Pages::MortgageAmount.new("mortgage_amount_shared_ownership", nil, self),
       Form::Sales::Pages::AboutDeposit.new("about_deposit_shared_ownership", nil, self),
       Form::Sales::Pages::MonthlyRent.new(nil, nil, self),
-      Form::Sales::Pages::ExchangeDate.new(nil, nil, self),
     ]
   end
 
