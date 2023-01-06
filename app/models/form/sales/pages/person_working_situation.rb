3bdfd6db73174278ad7c6b9d2031f58e2178bc9f
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-01-06 09:43:41 +0000

    Refactor details known to reuse pages and questions (#1099)
    * Refactor details known to reuse pages and questions
    
    * Refactor page_depends_on
    
    * Use field_for_person
    
    * feat: tweak person_known behaviour to only show for people 2-4, and ask person_known regardless of answer to previous person_known questions. Also remove redundant personx_known files
    
    * test: update tests
    
    * feat: add person 1 known page back in
    
    Co-authored-by: natdeanlewissoftwire <nat.dean-lewis@softwire.com>

--- app/models/form/sales/pages/person_working_situation.rb
+++ app/models/form/sales/pages/person_working_situation.rb
@@ -4,14 +4,18 @@ class Form::Sales::Pages::PersonWorkingSituation < Form::Sales::Pages::Person
     @header = ""
     @description = ""
     @subsection = subsection
-    @depends_on = [
-      { details_known_question_id => 1, "jointpur" => joint_purchase? ? 1 : 2 },
-    ]
+    @depends_on = page_depends_on
   end
 
   def questions
     @questions ||= [
       Form::Sales::Questions::PersonWorkingSituation.new(field_for_person("ecstat"), nil, self, person_index: @person_index),
     ]
+  end
+
+  def page_depends_on
+    return (person_display_number..4).map { |index| { "hholdcount" => index, "jointpur" => joint_purchase? ? 1 : 2 } } if person_display_number == 1
+
+    [{ details_known_question_id => 1, "jointpur" => joint_purchase? ? 1 : 2 }]
   end
 end
