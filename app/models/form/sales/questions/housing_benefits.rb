316276fe15556167b1137a62962b995475d0208d
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-01-06 08:24:20 +0000

    Cldc 1517 housing benefits (#1096)
    * Add housing benefits column
    
    * Add housing benefits questions and pages
    
    * Add page to the section
    
    * Remove card number and fix tests
    
    * rebase tests

---
+++ app/models/form/sales/questions/housing_benefits.rb
@@ -0,0 +1,20 @@
+class Form::Sales::Questions::HousingBenefits < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "hb"
+    @check_answer_label = "Housing-related benefits buyer received before buying this property"
+    @header = "Was the buyer receiving any of these housing-related benefits immediately before buying this property?"
+    @type = "radio"
+    @page = page
+    @hint_text = ""
+    @answer_options = ANSWER_OPTIONS
+  end
+
+  ANSWER_OPTIONS = {
+    "2" => { "value" => "Housing benefit" },
+    "3" => { "value" => "Universal Credit housing element" },
+    "divider" => { "value" => true },
+    "1" => { "value" => "Neither housing benefit or Universal Credit housing element" },
+    "4" => { "value" => "Don’t know " },
+  }.freeze
+end
--- db/schema.rb
+++ db/schema.rb
@@ -457,6 +457,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_164318) do
     t.integer "prevten"
     t.integer "savings_value_check"
     t.integer "deposit_value_check"
+    t.integer "hb"
     t.index ["created_by_id"], name: "index_sales_logs_on_created_by_id"
     t.index ["managing_organisation_id"], name: "index_sales_logs_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_sales_logs_on_owning_organisation_id"
