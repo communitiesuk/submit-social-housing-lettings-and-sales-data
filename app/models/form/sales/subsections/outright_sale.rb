e591ad491475b15d68bb6e6961a6c0f4d1e94bbe
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-01-09 16:41:17 +0000

    feat: address test failure

--- app/models/form/sales/subsections/outright_sale.rb
+++ app/models/form/sales/subsections/outright_sale.rb
@@ -12,7 +12,7 @@ class Form::Sales::Subsections::OutrightSale < ::Form::Subsection
       Form::Sales::Pages::PurchasePrice.new(nil, nil, self),
       Form::Sales::Pages::Mortgageused.new("mortgage_used_outright_sale", nil, self),
       Form::Sales::Pages::MortgageAmount.new("mortgage_amount_outright_sale", nil, self),
-      Form::Sales::Pages::MortgageLength.new("mortgage_length_outright_sale_ownership", nil, self),
+      Form::Sales::Pages::MortgageLength.new("mortgage_length_outright_sale", nil, self),
       Form::Sales::Pages::AboutDepositWithoutDiscount.new("about_deposit_outright_sale", nil, self),
       Form::Sales::Pages::DepositValueCheck.new("outright_sale_deposit_value_check", nil, self),
       Form::Sales::Pages::LeaseholdCharges.new("leasehold_charges_outright_sale", nil, self),
