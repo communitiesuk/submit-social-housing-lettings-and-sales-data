8175fc53fb8e3f882d4cfa9c402b8c874ac786cc
Phil Lee <asmega@users.noreply.github.com>
2023-01-11 16:50:45 +0000

    add #household_charge to bulk upload

--- app/services/bulk_upload/lettings/row_parser.rb
+++ app/services/bulk_upload/lettings/row_parser.rb
@@ -321,6 +321,7 @@ private
 
       period: %i[field_79],
       brent: %i[field_80],
+      household_charge: %i[field_86],
       hbrentshortfall: %i[field_87],
       tshortfall: %i[field_88],
 
@@ -511,6 +512,7 @@ private
 
     attributes["period"] = field_79
     attributes["brent"] = field_80
+    attributes["household_charge"] = field_86
     attributes["hbrentshortfall"] = field_87
     attributes["tshortfall_known"] = tshortfall_known
     attributes["tshortfall"] = field_88
