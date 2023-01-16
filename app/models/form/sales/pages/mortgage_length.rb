a58fecb5578d7c9f6fb56e2cd2b74716426076ff
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-01-16 09:32:02 +0000

    feat: add depends on behaviour

--- app/models/form/sales/pages/mortgage_length.rb
+++ app/models/form/sales/pages/mortgage_length.rb
@@ -1,4 +1,11 @@
 class Form::Sales::Pages::MortgageLength < ::Form::Page
+  def initialize(id, hsh, subsection)
+    super
+    @depends_on = [{
+                     "mortgageused" => 1,
+                   }]
+  end
+
   def questions
     @questions ||= [
       Form::Sales::Questions::MortgageLength.new(nil, nil, self),
