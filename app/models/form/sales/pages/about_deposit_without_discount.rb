3d360d78a88fbd48e8c0eb84d007beefc38bc2c2
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-01-17 09:28:01 +0000

    CLDC-1543 Route to mortgage questions only if mortgage is used (#1179)
    * Route to mortagage questions only if mortgage is used
    
    * Fix tests for deposit derived var
    
    * more routing
    
    * Only display mortgage lender questions if mortgage is used

--- app/models/form/sales/pages/about_deposit_without_discount.rb
+++ app/models/form/sales/pages/about_deposit_without_discount.rb
@@ -2,7 +2,9 @@ class Form::Sales::Pages::AboutDepositWithoutDiscount < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @header = "About the deposit"
-    @depends_on = [{ "is_type_discount?" => false }]
+    @depends_on = [{ "is_type_discount?" => false, "ownershipsch" => 1 },
+                   { "ownershipsch" => 2 },
+                   { "ownershipsch" => 3, "mortgageused" => 1 }]
   end
 
   def questions
