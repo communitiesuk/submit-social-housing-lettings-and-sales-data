6235615a6da099bb44cac507aec15fe2e7486652
Jack S <jacopo.scotti@softwire.com>
2023-01-18 15:23:23 +0000

    [CLDC-863] Add hard validation for hodate

--- app/models/validations/sales/sale_information_validations.rb
+++ app/models/validations/sales/sale_information_validations.rb
@@ -6,4 +6,12 @@ module Validations::Sales::SaleInformationValidations
       record.errors.add :deposit, "Cash deposit must be £0 - £999,999"
     end
   end
+
+  def validate_pratical_completion_date_before_saledate(record)
+    return if record.saledate.blank? || record.hodate.blank?
+
+    unless record.saledate > record.hodate
+      record.errors.add :hodate, "Practical completion or handover date must be before exchange date"
+    end
+  end
 end
