2dc41a47fc71cc79e5439df1d9930ae49e41375e
Sam Seed <sam.seed@softwire.com>
2023-01-19 16:28:43 +0000

    test: fix typo propert -> property in property validations tests

--- spec/models/validations/sales/property_validations_spec.rb
+++ spec/models/validations/sales/property_validations_spec.rb
@@ -49,12 +49,12 @@ RSpec.describe Validations::Sales::PropertyValidations do
     end
   end
 
-  describe "#validate_propert_unit_type" do
+  describe "#validate_property_unit_type" do
     context "when number of bedrooms is <= 1" do
       let(:record) { FactoryBot.build(:sales_log, beds: 1, proptype: 2) }
 
       it "does not add an error if it's a bedsit" do
-        property_validator.validate_propert_unit_type(record)
+        property_validator.validate_property_unit_type(record)
 
         expect(record.errors).not_to be_present
       end
@@ -64,7 +64,7 @@ RSpec.describe Validations::Sales::PropertyValidations do
       let(:record) { FactoryBot.build(:sales_log, beds: 2, proptype: 2) }
 
       it "does add an error if it's a bedsit" do
-        property_validator.validate_propert_unit_type(record)
+        property_validator.validate_property_unit_type(record)
 
         expect(record.errors).to be_present
       end
