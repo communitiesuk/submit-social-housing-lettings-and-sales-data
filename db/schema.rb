42960263fee622f7cb1d5afe521d9900c0d122aa
Kat <katrina@kosiak.co.uk>
2023-01-23 10:14:31 +0000

    Add shared_ownership_deposit_value_check field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_151942) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_101256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -504,6 +504,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_151942) do
     t.bigint "bulk_upload_id"
     t.index ["bulk_upload_id"], name: "index_sales_logs_on_bulk_upload_id"
     t.integer "retirement_value_check"
+    t.integer "shared_ownership_deposit_value_check"
     t.index ["created_by_id"], name: "index_sales_logs_on_created_by_id"
     t.index ["managing_organisation_id"], name: "index_sales_logs_on_managing_organisation_id"
     t.index ["owning_organisation_id"], name: "index_sales_logs_on_owning_organisation_id"
