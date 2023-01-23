1bc1dbe65a87b374469784c69a356a63de6a08c0
Kat <katrina@kosiak.co.uk>
2023-01-23 16:10:33 +0000

    Add min and max for frombeds

--- app/models/form/sales/questions/previous_bedrooms.rb
+++ app/models/form/sales/questions/previous_bedrooms.rb
@@ -6,7 +6,8 @@ class Form::Sales::Questions::PreviousBedrooms < ::Form::Question
     @header = "How many bedrooms did the property have?"
     @type = "numeric"
     @width = 5
-    @min = 0
+    @min = 1
+    @max = 6
     @hint_text = "For bedsits enter 1"
   end
 end
