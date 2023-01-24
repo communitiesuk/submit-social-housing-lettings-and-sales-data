da6e7f192e79f78b57a4e5bceb89a8f053d476b0
Kat <katrina@kosiak.co.uk>
2023-01-24 12:13:19 +0000

    Add old persons shared ownership value check page and question

---
+++ app/models/form/sales/questions/old_persons_shared_ownership_value_check.rb
@@ -0,0 +1,24 @@
+class Form::Sales::Questions::OldPersonsSharedOwnershipValueCheck < ::Form::Question
+  def initialize(id, hsh, page)
+    super
+    @id = "old_persons_shared_ownership_value_check"
+    @check_answer_label = "Shared ownership confirmation"
+    @header = "Are you sure this is correct?"
+    @type = "interruption_screen"
+    @answer_options = {
+      "0" => { "value" => "Yes" },
+      "1" => { "value" => "No" },
+    }
+    @hidden_in_check_answers = {
+      "depends_on" => [
+        {
+          "old_persons_shared_ownership_value_check" => 0,
+        },
+        {
+          "old_persons_shared_ownership_value_check" => 1,
+        },
+      ],
+    }
+    @check_answers_card_number = 0
+  end
+end
