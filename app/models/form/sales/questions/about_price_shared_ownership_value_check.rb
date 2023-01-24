c845ddf14556eb065dc67a5846af341baed804e7
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-01-24 10:01:29 +0000

    feat: hide in cya

--- app/models/form/sales/questions/about_price_shared_ownership_value_check.rb
+++ app/models/form/sales/questions/about_price_shared_ownership_value_check.rb
@@ -3,22 +3,13 @@ class Form::Sales::Questions::AboutPriceSharedOwnershipValueCheck < ::Form::Ques
     super
     @id = "value_value_check"
     @check_answer_label = "Deposit confirmation"
-    @header = "Are you sure? You said purchase price was #{log.value}, which seems #{value_is_high?(log.value) ? 'high' : 'low'}"
+    @header = "Are you sure? You said purchase price was PRICE, which seems high"
     @type = "interruption_screen"
     @answer_options = {
       "0" => { "value" => "Yes" },
       "1" => { "value" => "No" },
     }
-    @hidden_in_check_answers = {
-      "depends_on" => [
-        {
-          "value_value_check" => 0,
-        },
-        {
-          "value_value_check" => 1,
-        },
-      ],
-    }
+    @hidden_in_check_answers = true
   end
 
   def value_is_high?(value)
