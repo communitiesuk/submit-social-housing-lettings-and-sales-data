ec76203d58b533f64967001ede9b9adc0fe37d83
Sam Seed <sam.seed@softwire.com>
2023-01-26 16:25:09 +0000

    feat: rename 'bulk upload failed' to 'bulk upload failed csv errors

--- app/mailers/bulk_upload_mailer.rb
+++ app/mailers/bulk_upload_mailer.rb
@@ -1,11 +1,11 @@
 class BulkUploadMailer < NotifyMailer
-  BULK_UPLOAD_FAILED_TEMPLATE_ID = "e27abcd4-5295-48c2-b127-e9ee4b781b75".freeze
+  BULK_UPLOAD_FAILED_CSV_ERRORS_TEMPLATE_ID = "e27abcd4-5295-48c2-b127-e9ee4b781b75".freeze
   BULK_UPLOAD_WITH_ERRORS_TEMPLATE_ID = "eb539005-6234-404e-812d-167728cf4274".freeze
 
-  def send_bulk_upload_failed_mail(user, bulk_upload)
+  def send_bulk_upload_failed_csv_errors_mail(user, bulk_upload)
     send_email(
       user.email,
-      BULK_UPLOAD_FAILED_TEMPLATE_ID,
+      BULK_UPLOAD_FAILED_CSV_ERRORS_TEMPLATE_ID,
       { filename: "1", upload_timestamp: "2", year_combo: "3", lettings_or_sales: "4", error_description: "5", summary_report_link: "6" },
     )
   end
