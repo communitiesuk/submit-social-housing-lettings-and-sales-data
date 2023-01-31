9518cadcb557107a3b81f69506c6a71a3f93e11e
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-01-31 12:00:47 +0000

    CLDC-1450 Update declaration question (#1098)
    * Display the correct error for privacy notice and display the partial guidance at the top by default
    
    * Always display privacy notice question
    
    * Disable the rest of the housing characteristics questions until privacynotice is answered
    
    * fix tests
    
    * remove duplicate field
    
    * Display the household questions if the buyers were not interviewed

--- app/models/form/sales/pages/number_of_others_in_property.rb
+++ app/models/form/sales/pages/number_of_others_in_property.rb
@@ -2,6 +2,14 @@ class Form::Sales::Pages::NumberOfOthersInProperty < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "number_of_others_in_property"
+    @depends_on = [
+      {
+        "privacynotice" => 1,
+      },
+      {
+        "noint" => 1,
+      },
+    ]
   end
 
   def questions
