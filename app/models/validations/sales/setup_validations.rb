caf950c17ff6bf8f145e3a8584f6674e2c11b039
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-01-31 11:55:46 +0000

    feat: check date is valid

--- app/models/validations/sales/setup_validations.rb
+++ app/models/validations/sales/setup_validations.rb
@@ -1,6 +1,6 @@
 module Validations::Sales::SetupValidations
   def validate_saledate(record)
-    return unless record.saledate
+    return unless record.saledate && date_valid?("saledate", record)
 
     unless Time.zone.local(2022, 4, 1) <= record.saledate && record.saledate < Time.zone.local(2023, 4, 1)
       record.errors.add :saledate, I18n.t("validations.setup.saledate.financial_year")
