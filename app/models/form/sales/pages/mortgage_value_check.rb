1bfb0b5d922dd0eab929857b71c9047343726e4a
Kat <katrina@kosiak.co.uk>
2023-01-31 10:46:48 +0000

    only show buyer 2 checks if it's a jjoint purchase

--- app/models/form/sales/pages/mortgage_value_check.rb
+++ app/models/form/sales/pages/mortgage_value_check.rb
@@ -1,17 +1,31 @@
 class Form::Sales::Pages::MortgageValueCheck < ::Form::Page
-  def initialize(id, hsh, subsection)
-    super
-    @depends_on = [
-      {
-        "mortgage_over_soft_max?" => true,
-      },
-    ]
+  def initialize(id, hsh, subsection, person_index = nil)
+    super(id, hsh, subsection)
+    @depends_on = depends_on
     @informative_text = {}
+    @person_index = person_index
   end
 
   def questions
     @questions ||= [
       Form::Sales::Questions::MortgageValueCheck.new(nil, nil, self),
     ]
+  end
+
+  def depends_on
+    if @person_index == 2
+      [
+        {
+          "mortgage_over_soft_max?" => true,
+          "jointpur" => 1,
+        },
+      ]
+    else
+      [
+        {
+          "mortgage_over_soft_max?" => true,
+        },
+      ]
+    end
   end
 end
