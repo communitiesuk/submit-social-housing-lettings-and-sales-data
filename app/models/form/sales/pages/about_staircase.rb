0f2ae232d391f6155f008303e2cbc35a102b1c20
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-02-10 14:05:14 +0000

    feat: conditional year behaviour and test

--- app/models/form/sales/pages/about_staircase.rb
+++ app/models/form/sales/pages/about_staircase.rb
@@ -12,7 +12,13 @@ class Form::Sales::Pages::AboutStaircase < ::Form::Page
     @questions ||= [
       Form::Sales::Questions::StaircaseBought.new(nil, nil, self),
       Form::Sales::Questions::StaircaseOwned.new(nil, nil, self),
-      Form::Sales::Questions::StaircaseSale.new(nil, nil, self),
-    ]
+      staircase_sale_question,
+    ].compact
+  end
+
+  def staircase_sale_question
+    if form.start_date.year >= 2023
+      Form::Sales::Questions::StaircaseSale.new(nil, nil, self)
+    end
   end
 end
---
+++ spec/models/form/sales/questions/staircase_sale_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Questions::StaircaseSale, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("staircasesale")
+  end
+
+  it "has the correct header" do
+    expect(question.header).to eq("Is this transaction part of a back-to-back staircasing transaction to facilitate sale of the home on the open market?")
+  end
+
+  it "has the correct check_answer_label" do
+    expect(question.check_answer_label).to eq("Part of a back-to-back staircasing transaction")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?).to be false
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq({
+                                            "1" => { "value" => "Yes" },
+                                            "2" => { "value" => "No" },
+                                            "3" => { "value" => "Don't know" },
+                                          })
+  end
+
+  it "has correct conditional for" do
+    expect(question.conditional_for).to eq(nil)
+  end
+
+  it "has the correct hint" do
+    expect(question.hint_text).to be_nil
+  end
+end
