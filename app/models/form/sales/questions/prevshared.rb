a417e7764611a7d899eca914d96d5a0fc7e56f9c
Jack S <jacopo.scotti@softwire.com>
2023-02-10 12:54:16 +0000

    Update CYA

--- app/models/form/sales/questions/prevshared.rb
+++ app/models/form/sales/questions/prevshared.rb
@@ -2,7 +2,7 @@ class Form::Sales::Questions::Prevshared < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "prevshared"
-    @check_answer_label = "Buyers previously owned a shared property"
+    @check_answer_label = "Previous property shared ownership?"
     @header = "Was the previous property a shared ownership property?"
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
