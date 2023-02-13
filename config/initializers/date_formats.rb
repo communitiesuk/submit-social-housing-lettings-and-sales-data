ce2e45eb54d9602428ea5b87f540534bad6311bd
Arthur Campbell <arfa.camble@gmail.com>
2023-02-13 11:28:02 +0000

    change hardcoded date text to today's date formatted correctly

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -2,6 +2,7 @@ Time::DATE_FORMATS[:govuk_date] = "%-d %B %Y"
 Time::DATE_FORMATS[:govuk_date_short_month] = "%-d %b %Y"
 Date::DATE_FORMATS[:govuk_date] = "%-d %B %Y"
 Date::DATE_FORMATS[:govuk_date_short_month] = "%-d %b %Y"
+Date::DATE_FORMATS[:govuk_date_number_month] = "%-d %-m %Y"
 
 Time::DATE_FORMATS[:month_and_year] = "%B %Y"
 Date::DATE_FORMATS[:month_and_year] = "%B %Y"
