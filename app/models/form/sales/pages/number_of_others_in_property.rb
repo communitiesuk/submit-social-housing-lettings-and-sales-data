5328007fd2b697c32a404549bfadcfdb4db0ef0e
Kat <katrina@kosiak.co.uk>
2023-02-15 09:39:19 +0000

    Reuse join purchase page

--- app/models/form/sales/pages/number_of_others_in_property.rb
+++ app/models/form/sales/pages/number_of_others_in_property.rb
@@ -1,22 +1,22 @@
 class Form::Sales::Pages::NumberOfOthersInProperty < ::Form::Page
-  def initialize(id, hsh, subsection)
-    super
-    @id = "number_of_others_in_property"
+  def initialize(id, hsh, subsection, joint_purchase:)
+    super(id, hsh, subsection)
     @depends_on = [
       {
         "privacynotice" => 1,
-        "jointpur" => 2,
+        "jointpur" => joint_purchase ? 1 : 2,
       },
       {
         "noint" => 1,
-        "jointpur" => 2,
+        "jointpur" => joint_purchase ? 1 : 2,
       },
     ]
+    @joint_purchase = joint_purchase
   end
 
   def questions
     @questions ||= [
-      Form::Sales::Questions::NumberOfOthersInProperty.new(nil, nil, self, joint_purchase: false),
+      Form::Sales::Questions::NumberOfOthersInProperty.new(nil, nil, self, joint_purchase: @joint_purchase),
     ]
   end
 end
--- spec/models/form/sales/pages/number_of_others_in_property_spec.rb
+++ spec/models/form/sales/pages/number_of_others_in_property_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Form::Sales::Pages::NumberOfOthersInProperty, type: :model do
-  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+  subject(:page) { described_class.new(page_id, page_definition, subsection, joint_purchase:) }
 
-  let(:page_id) { nil }
+  let(:page_id) { "number_of_others_in_property" }
   let(:page_definition) { nil }
+  let(:joint_purchase) { false }
   let(:subsection) { instance_double(Form::Subsection) }
 
   it "has correct subsection" do
@@ -38,5 +39,27 @@ RSpec.describe Form::Sales::Pages::NumberOfOthersInProperty, type: :model do
         "jointpur" => 2,
       },
     ])
+  end
+
+  context "with joint purchase" do
+    let(:page_id) { "number_of_others_in_property_joint_purchase" }
+    let(:joint_purchase) { true }
+
+    it "has the correct id" do
+      expect(page.id).to eq("number_of_others_in_property_joint_purchase")
+    end
+
+    it "has the correct depends_on" do
+      expect(page.depends_on).to eq([
+        {
+          "privacynotice" => 1,
+          "jointpur" => 1,
+        },
+        {
+          "noint" => 1,
+          "jointpur" => 1,
+        },
+      ])
+    end
   end
 end
