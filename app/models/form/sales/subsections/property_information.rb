6be860db286889e149725e43eee6db39b0c19466
natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>
2023-02-23 15:32:19 +0000

    CLDC-868 purchase price validations PO review (#1285)
    * feat: wip behaviour
    
    * feat: hide in cya
    
    * feat: show in cya
    
    * feat: purchase price range service and rake task
    
    * feat: fix hodate validation and use purchase price ranges
    
    * db:update
    
    * feat: more saledate -> exdate fixing
    
    * feat: add min and max conditional text
    
    * refactor: linting
    
    * refactor: remove duplicated behaviour
    
    * refactor: linting
    
    * feat: update tests
    
    * test: add validation tests
    
    * test: add purchase price rangeimport service tests
    
    * refactor: linting
    
    * feat: allow translation text and title text to display currency formatting even when not a log field
    
    * feat: use i18n
    
    * feat: update i18n
    
    * refactor: pratical -> practical
    
    * refactor: linting
    
    * refactor: revert unnecessary change
    
    * feat: respond to PR comments
    
    * tests: update after merge
    
    * feat: improve currency interruption screen display
    
    * feat: update docs
    
    * feat: fix typo
    
    * feat: missing end after merge
    
    * feat: respond to PR comments
    
    * feat: add soft validation to beds and la pages
    
    * db:update
    
    * feat: revert saledate/exdate work
    
    * feat: revert saledate/exdate work
    
    * feat: revert saledate/exdate work
    
    * feat: revert saledate/exdate work
    
    * feat: revert saledate/exdate work
    
    * feat: PO comment responses
    
    * tests: update
    
    * refactor: linting
    
    * refactor: simplification
    
    * refactor: railsification
    
    * test: fix
    
    * refactor: use number_to_currency units
    
    * refactor: remove duplicated copy
    
    * db:update
    
    * feat: add value check for discounted and outright sale
    
    * feat: rename files
    
    * test: update
    
    * test: update
    
    * test: update
    
    * db: update

--- app/models/form/sales/subsections/property_information.rb
+++ app/models/form/sales/subsections/property_information.rb
@@ -9,13 +9,13 @@ class Form::Sales::Subsections::PropertyInformation < ::Form::Subsection
   def pages
     @pages ||= [
       Form::Sales::Pages::PropertyNumberOfBedrooms.new(nil, nil, self),
-      Form::Sales::Pages::AboutPriceSharedOwnershipValueCheck.new("about_price_shared_ownership_bedrooms_value_check", nil, self),
+      Form::Sales::Pages::AboutPriceValueCheck.new("about_price_bedrooms_value_check", nil, self),
       Form::Sales::Pages::PropertyUnitType.new(nil, nil, self),
       Form::Sales::Pages::MonthlyChargesValueCheck.new("monthly_charges_property_type_value_check", nil, self),
       Form::Sales::Pages::PropertyBuildingType.new(nil, nil, self),
       Form::Sales::Pages::Postcode.new(nil, nil, self),
       Form::Sales::Pages::PropertyLocalAuthority.new(nil, nil, self),
-      Form::Sales::Pages::AboutPriceSharedOwnershipValueCheck.new("about_price_shared_ownership_la_value_check", nil, self),
+      Form::Sales::Pages::AboutPriceValueCheck.new("about_price_la_value_check", nil, self),
       Form::Sales::Pages::PropertyWheelchairAccessible.new(nil, nil, self),
     ]
   end
