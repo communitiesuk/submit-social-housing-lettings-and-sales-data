1b724de092841a987792c0e181037b8c743d4b15
Arthur Campbell <arfa.camble@gmail.com>
2023-02-23 15:38:31 +0000

    add new answer options to nationality, rename question, write test file

--- app/models/form/lettings/questions/nationality.rb
+++ app/models/form/lettings/questions/nationality.rb
@@ -1,9 +1,9 @@
-class Form::Lettings::Questions::National < ::Form::Question
+class Form::Lettings::Questions::Nationality < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "national"
     @check_answer_label = "Lead tenant’s nationality"
-    @header = "What is the lead tenant’s nationality?"
+    @header = "What is the nationality of the lead tenant?"
     @type = "radio"
     @check_answers_card_number = 1
     @hint_text = "The lead tenant is the person in the household who does the most paid work. If several people do the same paid work, the lead tenant is whoever is the oldest."
@@ -14,6 +14,8 @@ class Form::Lettings::Questions::National < ::Form::Question
     "18" => { "value" => "United Kingdom" },
     "17" => { "value" => "Republic of Ireland" },
     "19" => { "value" => "European Economic Area (EEA) country, excluding Ireland" },
+    "20" => { "value" => "Afghanistan" },
+    "21" => { "value" => "Ukraine" },
     "12" => { "value" => "Other" },
     "divider" => true,
     "13" => { "value" => "Tenant prefers not to say" },
