87131a63c3ee407966a66408ad81d26312e5cffd
Phil Lee <asmega@users.noreply.github.com>
2023-02-24 16:19:06 +0000

    bulk upload error summary min limit
    - the errors summary table only show errors for column where there
      at least 16 errors for that column
    - if there are fewer than 16 errors that can see detailed report to pin
      point and fix those specific issues

--- spec/components/bulk_upload_error_summary_table_component_spec.rb
+++ spec/components/bulk_upload_error_summary_table_component_spec.rb
@@ -5,10 +5,27 @@ RSpec.describe BulkUploadErrorSummaryTableComponent, type: :component do
 
   let(:bulk_upload) { create(:bulk_upload) }
 
+  before do
+    stub_const("BulkUploadErrorSummaryTableComponent::DISPLAY_THRESHOLD", 0)
+  end
+
   context "when no errors" do
     it "does not renders any rows" do
       result = render_inline(component)
       expect(result).not_to have_selector("tbody tr")
+    end
+  end
+
+  context "when below threshold" do
+    before do
+      stub_const("BulkUploadErrorSummaryTableComponent::DISPLAY_THRESHOLD", 16)
+
+      create(:bulk_upload_error, bulk_upload:, col: "A", row: 1)
+    end
+
+    it "does not render rows" do
+      result = render_inline(component)
+      expect(result).to have_selector("tbody tr", count: 0)
     end
   end
 
