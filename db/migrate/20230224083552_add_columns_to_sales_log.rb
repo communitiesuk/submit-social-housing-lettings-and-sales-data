2b965e152e74e50bc62ab428cf73a2077c5956e0
Arthur Campbell <arfa.camble@gmail.com>
2023-02-27 17:14:57 +0000

    make new pages only visible for new collection year, rename filesamend tests

--- db/migrate/20230224083552_add_columns_to_sales_log.rb
+++ db/migrate/20230224083552_add_columns_to_sales_log.rb
@@ -1,6 +1,8 @@
 class AddColumnsToSalesLog < ActiveRecord::Migration[7.0]
   def change
-    add_column :sales_logs, :buy2living, :integer
-    add_column :sales_logs, :prevtenbuy2, :integer
+    change_table :sales_logs, bulk: true do |t|
+      t.column :buy2living, :integer
+      t.column :prevtenbuy2, :integer
+    end
   end
 end
