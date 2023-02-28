b543c57cf3ae28c4cd432883a5e75759efdc8307
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-02-28 11:01:00 +0000

    refactor: lint

--- app/models/validations/setup_validations.rb
+++ app/models/validations/setup_validations.rb
@@ -52,7 +52,7 @@ private
         previous_start_year_long: previous_collection_start_date.strftime("#{previous_collection_start_date.day.ordinalize} %B %Y"),
         current_end_year_short: current_collection_end_date.strftime("%y"),
         current_end_year_long:,
-        )
+      )
     else
       I18n.t(
         "validations.setup.startdate.current_financial_year",
@@ -60,7 +60,7 @@ private
         current_end_year_short: current_collection_end_date.strftime("%y"),
         current_start_year_long: current_collection_start_date.strftime("#{current_collection_start_date.day.ordinalize} %B %Y"),
         current_end_year_long:,
-        )
+      )
     end
   end
 
