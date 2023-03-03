9d0f9a59927f513f722152ca3a09c8f090db81fe
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-03-03 14:50:49 +0000

    feat: add validation with feature flag, typo fix and update tests

--- app/models/validations/setup_validations.rb
+++ app/models/validations/setup_validations.rb
@@ -1,5 +1,14 @@
 module Validations::SetupValidations
   include Validations::SharedValidations
+  include CollectionTimeHelper
+
+  def validate_startdate_setup(record)
+    return unless record.startdate && date_valid?("startdate", record)
+
+    if !FeatureToggle.startdate_collection_window_validation_enabled? && !record.startdate.between?(active_collection_start_date, current_collection_end_date)
+      record.errors.add :startdate, validation_error_message
+    end
+  end
 
   def validate_irproduct_other(record)
     if intermediate_product_rent_type?(record) && record.irproduct_other.blank?
@@ -25,7 +34,38 @@ module Validations::SetupValidations
     end
   end
 
-private
+  private
+
+  def active_collection_start_date
+    if FormHandler.instance.lettings_in_crossover_period?
+      previous_collection_start_date
+    else
+      current_collection_start_date
+    end
+  end
+
+  def validation_error_message
+    current_end_year_long = current_collection_end_date.strftime("#{current_collection_end_date.day.ordinalize} %B %Y")
+
+    if FormHandler.instance.lettings_in_crossover_period?
+      I18n.t(
+        "validations.setup.startdate.previous_and_current_financial_year",
+        previous_start_year_short: previous_collection_start_date.strftime("%y"),
+        previous_end_year_short: previous_collection_end_date.strftime("%y"),
+        previous_start_year_long: previous_collection_start_date.strftime("#{previous_collection_start_date.day.ordinalize} %B %Y"),
+        current_end_year_short: current_collection_end_date.strftime("%y"),
+        current_end_year_long:,
+        )
+    else
+      I18n.t(
+        "validations.setup.startdate.current_financial_year",
+        current_start_year_short: current_collection_start_date.strftime("%y"),
+        current_end_year_short: current_collection_end_date.strftime("%y"),
+        current_start_year_long: current_collection_start_date.strftime("#{current_collection_start_date.day.ordinalize} %B %Y"),
+        current_end_year_long:,
+        )
+    end
+  end
 
   def intermediate_product_rent_type?(record)
     record.rent_type == 5
