5283019b649a080eca4a990ff424ad224ada46e6
Kat <katrina@kosiak.co.uk>
2023-03-08 08:44:16 +0000

    Update LAs for 23/24 sales questions

--- spec/models/form/sales/pages/property_local_authority_spec.rb
+++ spec/models/form/sales/pages/property_local_authority_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Form::Sales::Pages::PropertyLocalAuthority, type: :model do
 
   let(:page_id) { nil }
   let(:page_definition) { nil }
-  let(:subsection) { instance_double(Form::Subsection) }
+  let(:subsection) { instance_double(Form::Subsection, form: instance_double(Form, start_date:)) }
+  let(:start_date) { Time.utc(2022, 4, 1) }
 
   it "has correct subsection" do
     expect(page.subsection).to eq(subsection)
