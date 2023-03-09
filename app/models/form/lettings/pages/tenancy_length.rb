578dea7828e4e482d8ba286db5624f7329feb340
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-03-09 17:27:19 +0000

    feat: skip fixed term tenancy length question when tenancy is other

--- app/models/form/lettings/pages/tenancy_length.rb
+++ app/models/form/lettings/pages/tenancy_length.rb
@@ -2,7 +2,7 @@ class Form::Lettings::Pages::TenancyLength < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "tenancy_length"
-    @depends_on = [{ "tenancy" => 4 }, { "tenancy" => 6 }, { "tenancy" => 3 }]
+    @depends_on = [{ "tenancy" => 4 }, { "tenancy" => 6 }]
   end
 
   def questions
