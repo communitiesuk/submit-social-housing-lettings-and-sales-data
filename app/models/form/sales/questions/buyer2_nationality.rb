4281938120e1003ded85385f6a65f4f9925ae0ef
Arthur Campbell <arfa.camble@gmail.com>
2023-03-10 14:39:43 +0000

    add question number for the new question to allow CLDC-1864 to progress with fewer conflicts/complications

--- app/models/form/sales/questions/buyer2_nationality.rb
+++ app/models/form/sales/questions/buyer2_nationality.rb
@@ -7,6 +7,7 @@ class Form::Sales::Questions::Buyer2Nationality < ::Form::Question
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
     @check_answers_card_number = 2
+    @question_number = 32
     @inferred_check_answers_value = [{
       "condition" => {
         "national" => 13,
