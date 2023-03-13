a53f180524a27b1beb55873c09d3cf8d18f7a767
Kat <katrina@kosiak.co.uk>
2023-03-13 09:10:08 +0000

    Rename download path

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -5,7 +5,7 @@ class StartController < ApplicationController
     end
   end
 
-  def download_pdf
+  def download_23_24_sales_form
     send_file(
       Rails.root.join("public/files/2023_24_sales_paper_form.pdf"),
       filename: "2023-24 Sales paper form.pdf",
