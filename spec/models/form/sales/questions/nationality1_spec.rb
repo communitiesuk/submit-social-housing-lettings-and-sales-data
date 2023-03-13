b878d2c01bfddc64239728a5481b5e4e5c3fbafe
Arthur Campbell <51094020+arfacamble@users.noreply.github.com>
2023-03-13 17:37:21 +0000

    CLDC-2014 remove text field for buyer 1 nationality other (#1388)
    * remove question no longer needed and associated test file
    
    * amend page and the other question in that page that determined whether the deleted question should be shown
    update associated test files
    
    * createa a migration to remove the column for the deleted question
    
    * update test broken by changes
    
    * fix test broken by changes

--- spec/models/form/sales/questions/nationality1_spec.rb
+++ spec/models/form/sales/questions/nationality1_spec.rb
@@ -1,30 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Form::Sales::Questions::Nationality1, type: :model do
-  subject(:question) { described_class.new(question_id, question_definition, page) }
+  subject(:question) { described_class.new(nil, nil, page) }
 
-  let(:question_id) { nil }
-  let(:question_definition) { nil }
   let(:page) { instance_double(Form::Page) }
 
   it "has correct page" do
-    expect(question.page).to eq(page)
+    expect(question.page).to be page
   end
 
   it "has the correct id" do
-    expect(question.id).to eq("national")
+    expect(question.id).to eq "national"
   end
 
   it "has the correct header" do
-    expect(question.header).to eq("What is buyer 1’s nationality?")
+    expect(question.header).to eq "What is buyer 1’s nationality?"
   end
 
   it "has the correct check_answer_label" do
-    expect(question.check_answer_label).to eq("Buyer 1’s nationality")
+    expect(question.check_answer_label).to eq "Buyer 1’s nationality"
   end
 
   it "has the correct type" do
-    expect(question.type).to eq("radio")
+    expect(question.type).to eq "radio"
   end
 
   it "is not marked as derived" do
@@ -32,7 +30,7 @@ RSpec.describe Form::Sales::Questions::Nationality1, type: :model do
   end
 
   it "has the correct hint" do
-    expect(question.hint_text).to eq("Buyer 1 is the person in the household who does the most paid work. If it’s a joint purchase and the buyers do the same amount of paid work, buyer 1 is whoever is the oldest.")
+    expect(question.hint_text).to eq "Buyer 1 is the person in the household who does the most paid work. If it’s a joint purchase and the buyers do the same amount of paid work, buyer 1 is whoever is the oldest."
   end
 
   it "has the correct answer_options" do
@@ -46,23 +44,15 @@ RSpec.describe Form::Sales::Questions::Nationality1, type: :model do
   end
 
   it "has correct conditional for" do
-    expect(question.conditional_for).to eq({
-      "othernational" => [12],
-    })
+    expect(question.conditional_for).to be_nil
   end
 
   it "has correct hidden in check answers" do
-    expect(question.hidden_in_check_answers).to eq({
-      "depends_on" => [
-        {
-          "national" => 12,
-        },
-      ],
-    })
+    expect(question.hidden_in_check_answers).to be_nil
   end
 
   it "has the correct check_answers_card_number" do
-    expect(question.check_answers_card_number).to eq(1)
+    expect(question.check_answers_card_number).to be 1
   end
 
   it "has the correct inferred_check_answers_value" do
