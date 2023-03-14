c8ba475736bfc7282015be313e393f34a9be4fbd
SamSeed-Softwire <63662292+SamSeed-Softwire@users.noreply.github.com>
2023-03-14 13:57:47 +0000

    fix: get real validation messages for rent range validations (#1413)

--- app/models/form/lettings/pages/min_rent_value_check.rb
+++ app/models/form/lettings/pages/min_rent_value_check.rb
@@ -4,11 +4,11 @@ class Form::Lettings::Pages::MinRentValueCheck < ::Form::Page
     @id = "min_rent_value_check"
     @depends_on = [{ "rent_in_soft_min_range?" => true }]
     @title_text = {
-      "translation" => "soft_validations.rent.min.title_text",
+      "translation" => "soft_validations.rent.outside_range_title",
       "arguments" => [{ "key" => "brent", "label" => true, "i18n_template" => "brent" }],
     }
     @informative_text = {
-      "translation" => "soft_validations.rent.min.hint_text",
+      "translation" => "soft_validations.rent.min_hint_text",
       "arguments" => [
         {
           "key" => "soft_min_for_period",
