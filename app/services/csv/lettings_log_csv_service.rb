631976400e3cc5fec24601d6edcae2480ab4dd76
Arthur Campbell <51094020+arfacamble@users.noreply.github.com>
2023-03-14 14:55:32 +0000

    CLDC-1826 lettings log codes only download (#1268)
    * update seeds to add self in review env, change spec to reflect this, update config yml to allow csv exports in review
    
    * update interface of relevant methods
    EmailCsvJob, LettingsLog.to_csv and LettingsLogCsvService consume codes_only flag
    
    * update tests including adding a new csv file to test against
    
    * update LettingsLogCsvService to output codes only csv
    
    * correct minor error and linting
    
    * enable codes only download in UI
    - add link on lettings log index page
    - pass codes_only flag through params in relevant links and methods
    - convert flag to boolean in controller methods
    
    * ensure link displayed successfully for all renderings of logs_list and params passed through relevant methods in organisations controller
    
    * fix existing tests
    
    * correct linting thing
    
    * correct linting error
    
    * update tests for lettings log controller
    
    * correct linting errors
    
    * update organisations controller tests
    
    * make minor changes after code review
    
    * remove changes made for testing on review app
    
    * make codes only download visible to support users only
    
    * change variable names throughout after info on rauby/rails naming conventions, update tests for change in who can view codes only download link
    
    * rework csv service for readability, remove delegating methods from lettings log to keep all code to do with mapping between our domain and desired export format in one place
    
    * update test name
    
    * correct a small typo and remove a duplicated method after clever git merge conflict suggestion
    
    * point review app at staging csv bucket for csv download
    
    * change variables named codes_only_export to codes_only to avoid inconsistency
    
    * write tests to ensure that differetn user roles have the correct permissions around csv download
    
    * ensure that non support users may not download codes only exports
    
    * correct a small error in a  previous commit
    
    * correct minor linting error

--- app/services/csv/lettings_log_csv_service.rb
+++ app/services/csv/lettings_log_csv_service.rb
@@ -2,8 +2,9 @@ module Csv
   class LettingsLogCsvService
     CSV_FIELDS_TO_OMIT = %w[hhmemb net_income_value_check first_time_property_let_as_social_housing renttype needstype postcode_known is_la_inferred totchild totelder totadult net_income_known is_carehome previous_la_known is_previous_la_inferred age1_known age2_known age3_known age4_known age5_known age6_known age7_known age8_known letting_allocation_unknown details_known_2 details_known_3 details_known_4 details_known_5 details_known_6 details_known_7 details_known_8 rent_type_detail wrent wscharge wpschrge wsupchrg wtcharge wtshortfall rent_value_check old_form_id old_id retirement_value_check tshortfall_known pregnancy_value_check hhtype new_old vacdays la prevloc unresolved updated_by_id bulk_upload_id].freeze
 
-    def initialize(user)
+    def initialize(user, export_type:)
       @user = user
+      @export_type = export_type
       set_csv_attributes
     end
 
@@ -12,30 +13,126 @@ module Csv
         csv << @attributes
 
         LettingsLog.all.find_each do |record|
-          csv << @attributes.map do |att|
-            label_from_value(record, att)
-          end
+          csv << @attributes.map { |attribute| get_value(attribute, record) }
         end
       end
     end
 
   private
 
-    def label_from_value(record, att)
-      if %w[la prevloc].include? att
-        label_from_boolean_value(record.send(att))
-      elsif %w[mrcdate startdate voiddate].include? att
-        record.send(att)&.to_formatted_s(:govuk_date)
+    ATTRIBUTES_OF_RELATED_OBJECTS = {
+      location_code: {
+        labels: %i[location id],
+        codes: %i[location id],
+      },
+      location_postcode: {
+        labels: %i[location postcode],
+        codes: %i[location postcode],
+      },
+      location_name: {
+        labels: %i[location name],
+        codes: %i[location name],
+      },
+      location_units: {
+        labels: %i[location units],
+        codes: %i[location units],
+      },
+      location_type_of_unit: {
+        labels: %i[location type_of_unit],
+        codes: %i[location type_of_unit_before_type_cast],
+      },
+      location_mobility_type: {
+        labels: %i[location mobility_type],
+        codes: %i[location mobility_type_before_type_cast],
+      },
+      location_admin_district: {
+        labels: %i[location location_admin_district],
+        codes: %i[location location_admin_district],
+      },
+      location_startdate: {
+        labels: %i[location startdate],
+        codes: %i[location startdate],
+      },
+      scheme_service_name: {
+        labels: %i[scheme service_name],
+        codes: %i[scheme service_name],
+      },
+      scheme_sensitive: {
+        labels: %i[scheme sensitive],
+        codes: %i[scheme sensitive_before_type_cast],
+      },
+      scheme_type: {
+        labels: %i[scheme scheme_type],
+        codes: %i[scheme scheme_type_before_type_cast],
+      },
+      scheme_registered_under_care_act: {
+        labels: %i[scheme registered_under_care_act],
+        codes: %i[scheme registered_under_care_act_before_type_cast],
+      },
+      scheme_owning_organisation_name: {
+        labels: %i[scheme owning_organisation name],
+        codes: %i[scheme owning_organisation name],
+      },
+      scheme_primary_client_group: {
+        labels: %i[scheme primary_client_group],
+        codes: %i[scheme primary_client_group_before_type_cast],
+      },
+      scheme_has_other_client_group: {
+        labels: %i[scheme has_other_client_group],
+        codes: %i[scheme has_other_client_group_before_type_cast],
+      },
+      scheme_secondary_client_group: {
+        labels: %i[scheme secondary_client_group],
+        codes: %i[scheme secondary_client_group_before_type_cast],
+      },
+      scheme_support_type: {
+        labels: %i[scheme support_type],
+        codes: %i[scheme support_type_before_type_cast],
+      },
+      scheme_intended_stay: {
+        labels: %i[scheme intended_stay],
+        codes: %i[scheme intended_stay_before_type_cast],
+      },
+      scheme_created_at: {
+        labels: %i[scheme created_at],
+        codes: %i[scheme created_at],
+      },
+    }.freeze
+
+    def get_value(attribute, record)
+      attribute = "rent_type" if attribute == "rent_type_detail" # rent_type_detail is the requested column header for rent_type, so as not to confuse with renttype
+      if ATTRIBUTES_OF_RELATED_OBJECTS.key? attribute.to_sym
+        call_chain = ATTRIBUTES_OF_RELATED_OBJECTS[attribute.to_sym][@export_type.to_sym]
+        call_chain.reduce(record) { |object, next_call| object&.send(next_call) }
+      elsif %w[la prevloc].include? attribute # for all exports we output both the codes and labels for these location attributes
+        record.send(attribute)
+      elsif %w[la_label prevloc_label].include? attribute # as above
+        attribute = attribute.remove("_label")
+        field_value = record.send(attribute)
+        get_label(field_value, attribute, record)
+      elsif %w[mrcdate startdate voiddate].include? attribute
+        record.send(attribute)&.to_formatted_s(:govuk_date)
       else
-        record.form.get_question(att.remove("_label"), record)&.label_from_value(record.send(att.remove("_label"))) || label_from_boolean_value(record.send(att.remove("_label")))
+        field_value = record.send(attribute)
+        case @export_type
+        when "codes"
+          field_value
+        when "labels"
+          answer_label = get_label(field_value, attribute, record)
+          answer_label || label_if_boolean_value(field_value) || field_value
+        end
       end
     end
 
-    def label_from_boolean_value(value)
+    def get_label(value, attribute, record)
+      record.form
+            .get_question(attribute, record)
+            &.label_from_value(value)
+    end
+
+    def label_if_boolean_value(value)
       return "Yes" if value == true
       return "No" if value == false
-
-      value
     end
 
     def set_csv_attributes
