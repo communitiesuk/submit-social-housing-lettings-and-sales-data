48c4b458133f0f5edf2a27297339ab50d53cbd2b
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-03-14 08:07:22 +0000

    CLDC-1886 Add sales paper form download (#1408)
    * Add sales paper form download
    
    * Rename download path
    
    * Add 22/23 sales log download

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -4,4 +4,20 @@ class StartController < ApplicationController
       redirect_to(lettings_logs_path)
     end
   end
+
+  def download_23_24_sales_form
+    send_file(
+      Rails.root.join("public/files/2023_24_sales_paper_form.pdf"),
+      filename: "2023-24 Sales paper form.pdf",
+      type: "application/pdf",
+    )
+  end
+
+  def download_22_23_sales_form
+    send_file(
+      Rails.root.join("public/files/2022_23_sales_paper_form.pdf"),
+      filename: "2022-23 Sales paper form.pdf",
+      type: "application/pdf",
+    )
+  end
 end
