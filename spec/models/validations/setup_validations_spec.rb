870587656e8c9322e16976f98d1a999ae8f827b7
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-03-16 16:27:40 +0000

    refactor: replace financial year with collection yaer

--- spec/models/validations/setup_validations_spec.rb
+++ spec/models/validations/setup_validations_spec.rb
@@ -17,13 +17,13 @@ RSpec.describe Validations::SetupValidations do
         it "cannot be before the first collection window start date" do
           record.startdate = Time.zone.local(2021, 1, 1)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 21/22 or 22/23 financial years, which is between 1st April 2021 and 31st March 2023")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 21/22 or 22/23 collection years, which is between 1st April 2021 and 31st March 2023")
         end
 
         it "cannot be after the second collection window end date" do
           record.startdate = Time.zone.local(2023, 7, 1, 6)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 21/22 or 22/23 financial years, which is between 1st April 2021 and 31st March 2023")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 21/22 or 22/23 collection years, which is between 1st April 2021 and 31st March 2023")
         end
       end
 
@@ -36,13 +36,13 @@ RSpec.describe Validations::SetupValidations do
         it "cannot be before the first collection window start date" do
           record.startdate = Time.zone.local(2022, 1, 1)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 financial year, which is between 1st April 2022 and 31st March 2023")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 collection year, which is between 1st April 2022 and 31st March 2023")
         end
 
         it "cannot be after the second collection window end date" do
           record.startdate = Time.zone.local(2023, 7, 1, 6)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 financial year, which is between 1st April 2022 and 31st March 2023")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 collection year, which is between 1st April 2022 and 31st March 2023")
         end
       end
     end
@@ -57,13 +57,13 @@ RSpec.describe Validations::SetupValidations do
         it "cannot be before the first collection window start date" do
           record.startdate = Time.zone.local(2022, 1, 1)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 or 23/24 financial years, which is between 1st April 2022 and 31st March 2024")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 or 23/24 collection years, which is between 1st April 2022 and 31st March 2024")
         end
 
         it "cannot be after the second collection window end date" do
           record.startdate = Time.zone.local(2024, 7, 1, 6)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 or 23/24 financial years, which is between 1st April 2022 and 31st March 2024")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 22/23 or 23/24 collection years, which is between 1st April 2022 and 31st March 2024")
         end
       end
 
@@ -76,13 +76,13 @@ RSpec.describe Validations::SetupValidations do
         it "cannot be before the first collection window start date" do
           record.startdate = Time.zone.local(2023, 1, 1)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 23/24 financial year, which is between 1st April 2023 and 31st March 2024")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 23/24 collection year, which is between 1st April 2023 and 31st March 2024")
         end
 
         it "cannot be after the second collection window end date" do
           record.startdate = Time.zone.local(2024, 7, 1, 6)
           setup_validator.validate_startdate_setup(record)
-          expect(record.errors["startdate"]).to include(match "Enter a date within the 23/24 financial year, which is between 1st April 2023 and 31st March 2024")
+          expect(record.errors["startdate"]).to include(match "Enter a date within the 23/24 collection year, which is between 1st April 2023 and 31st March 2024")
         end
       end
     end
