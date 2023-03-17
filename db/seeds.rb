c4cb2ecd2e5146b1ad0df9fb42f8a6fa18f4854a
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-03-17 14:46:23 +0000

    Seed la data before links (#1435)
    * seed la data before links
    
    * lint
    
    * Only seed LAs once for tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -309,6 +309,12 @@ unless Rails.env.test?
     pp "Seeded 3 dummy schemes"
   end
 
+  if LocalAuthority.count.zero?
+    la_path = "config/local_authorities_data/initial_local_authorities.csv"
+    service = Imports::LocalAuthoritiesService.new(path: la_path)
+    service.call
+  end
+
   if (Rails.env.development? || Rails.env.review?) && LocalAuthorityLink.count.zero?
     links_data_path = "config/local_authorities_data/local_authority_links_2023.csv"
     service = Imports::LocalAuthorityLinksService.new(path: links_data_path)
