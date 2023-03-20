c50a5d0aa78e0e58888874d4a66f30afafcacd22
Kat <katrina@kosiak.co.uk>
2023-03-20 09:38:59 +0000

    Add soft validation pages

---
+++ app/models/form/sales/questions/student_not_child_value_check.rb
@@ -0,0 +1,24 @@
+class Form::Sales::Questions::StudentNotChildValueCheck < ::Form::Question
+  def initialize(id, hsh, page, person_index:)
+    super(id, hsh, page)
+    @id = "student_not_child_value_check"
+    @check_answer_label = "Student not child confirmation"
+    @type = "interruption_screen"
+    @answer_options = {
+      "0" => { "value" => "Yes" },
+      "1" => { "value" => "No" },
+    }
+    @hidden_in_check_answers = {
+      "depends_on" => [
+        {
+          "student_not_child_value_check" => 0,
+        },
+        {
+          "student_not_child_value_check" => 1,
+        },
+      ],
+    }
+    @check_answers_card_number = person_index
+    @header = "Are you sure this person is not child?"
+  end
+end
---
+++ spec/models/form/sales/questions/student_not_child_value_check_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Questions::StudentNotChildValueCheck, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page, person_index: 1) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("student_not_child_value_check")
+  end
+
+  it "has the correct header" do
+    expect(question.header).to eq("Are you sure this person is not child?")
+  end
+
+  it "has the correct check_answer_label" do
+    expect(question.check_answer_label).to eq("Student not child confirmation")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("interruption_screen")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?).to be false
+  end
+
+  it "has the correct hint" do
+    expect(question.hint_text).to be_nil
+  end
+
+  it "has a correct check_answers_card_number" do
+    expect(question.check_answers_card_number).to eq(1)
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq({
+      "0" => { "value" => "Yes" },
+      "1" => { "value" => "No" },
+    })
+  end
+
+  it "has the correct hidden_in_check_answers" do
+    expect(question.hidden_in_check_answers).to eq({
+      "depends_on" => [
+        {
+          "student_not_child_value_check" => 0,
+        },
+        {
+          "student_not_child_value_check" => 1,
+        },
+      ],
+    })
+  end
+end
