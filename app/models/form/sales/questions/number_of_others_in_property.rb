e1231ccb19f52b9771372d7399f53f34f6b7fd43
Arthur Campbell <arfa.camble@gmail.com>
2023-03-21 13:54:50 +0000

    correct linting error

--- app/models/form/sales/questions/number_of_others_in_property.rb
+++ app/models/form/sales/questions/number_of_others_in_property.rb
@@ -3,7 +3,7 @@ class Form::Sales::Questions::NumberOfOthersInProperty < ::Form::Question
     super(id, hsh, page)
     @id = "hholdcount"
     @check_answer_label = "Number of other people living in the property"
-    @header = "Besides the #{"buyer".pluralize(joint_purchase ? 2 : 1)}, how many other people live or will live in the property?"
+    @header = "Besides the #{'buyer'.pluralize(joint_purchase ? 2 : 1)}, how many other people live or will live in the property?"
     @type = "numeric"
     @hint_text = hint(joint_purchase)
     @width = 2
