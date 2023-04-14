cef28464d2bfe1f07d5ab7a37a3af37ae352d03b
Phil Lee <asmega@users.noreply.github.com>
2023-04-14 15:20:34 +0100

    fix ordering for bulk upload errors by cell
    - this is now delegated to the database rather than sorting in ruby

--- app/models/bulk_upload_error.rb
+++ app/models/bulk_upload_error.rb
@@ -1,3 +1,6 @@
 class BulkUploadError < ApplicationRecord
   belongs_to :bulk_upload
+
+  scope :order_by_cell, -> { order(Arel.sql("LPAD(cell, 10, '0')")) }
+  scope :order_by_col, -> { order(Arel.sql("LPAD(col, 10, '0')")) }
 end
