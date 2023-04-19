fc417aea11e1bfa708fd0ec445b21aa79ae420be
Phil Lee <asmega@users.noreply.github.com>
2023-04-19 11:15:39 +0100

    fix bulk upload summary error ordering

---
+++ spec/views/bulk_upload_lettings_results/summary.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "bulk_upload_lettings_results/summary.html.erb" do
+  let(:bulk_upload) { create(:bulk_upload, :lettings) }
+
+  before do
+    create(:bulk_upload_error, bulk_upload:, cell: "AA100", row: "100", col: "AA")
+    create(:bulk_upload_error, bulk_upload:, cell: "Z100", row: "100", col: "Z")
+  end
+
+  it "renders errors ordered by cell" do
+    assign(:bulk_upload, bulk_upload)
+
+    render
+
+    fragment = Capybara::Node::Simple.new(rendered)
+
+    expect(fragment.find_css("table tbody th").map(&:inner_text)).to eql(%w[Z100 AA100])
+  end
+end
