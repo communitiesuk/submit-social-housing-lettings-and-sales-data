55b6b8d9515765bd3f1baa4e0e83dbe16611bef9
Jack <113976590+bibblobcode@users.noreply.github.com>
2023-04-19 16:43:21 +0100

    CLDC-2229 Do not show hint text when outright sale (#1563)

--- app/models/form/sales/questions/purchase_price.rb
+++ app/models/form/sales/questions/purchase_price.rb
@@ -8,17 +8,23 @@ class Form::Sales::Questions::PurchasePrice < ::Form::Question
     @min = 0
     @width = 5
     @prefix = "£"
-    @hint_text = "For all schemes, including Right to Acquire (RTA), Right to Buy (RTB), Voluntary Right to Buy (VRTB) or Preserved Right to Buy (PRTB) sales, enter the full price of the property without any discount"
+    @hint_text = hint_text
     @ownership_sch = ownershipsch
     @question_number = question_number
   end
 
   def question_number
     case @ownership_sch
-    when 2
+    when 2 # discounted ownership scheme
       100
-    when 3
+    when 3 # outright sale
       110
     end
+  end
+
+  def hint_text
+    return if @ownership_sch == 3 # outright sale
+
+    "For all schemes, including Right to Acquire (RTA), Right to Buy (RTB), Voluntary Right to Buy (VRTB) or Preserved Right to Buy (PRTB) sales, enter the full price of the property without any discount"
   end
 end
