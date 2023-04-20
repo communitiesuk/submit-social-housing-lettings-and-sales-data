1cd8c01841cd4c84c7acd1b013e8a3f08e96bff4
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-04-20 10:06:37 +0100

    Enable collection window validation (#1567)

--- app/models/validations/setup_validations.rb
+++ app/models/validations/setup_validations.rb
@@ -3,7 +3,7 @@ module Validations::SetupValidations
   include CollectionTimeHelper
 
   def validate_startdate_setup(record)
-    return unless record.startdate && date_valid?("startdate", record) && FeatureToggle.startdate_collection_window_validation_enabled?
+    return unless record.startdate && date_valid?("startdate", record)
 
     unless record.startdate.between?(active_collection_start_date, current_collection_end_date)
       record.errors.add :startdate, startdate_validation_error_message
