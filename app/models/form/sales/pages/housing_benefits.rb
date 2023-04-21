03a545b19de139c8a438fdb40955582ecc933999
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-04-21 13:47:19 +0100

    feat: fix tests

--- app/models/form/sales/pages/housing_benefits.rb
+++ app/models/form/sales/pages/housing_benefits.rb
@@ -2,19 +2,19 @@ class Form::Sales::Pages::HousingBenefits < ::Form::Page
   def initialize(id, hsh, subsection, joint_purchase:)
     super(id, hsh, subsection)
     @joint_purchase = joint_purchase
-    @depends_on =
-      [
-        if @joint_purchase
-          { "joint_purchase?" => true }
-        else
-          { "not_joint_purchase?" => true }
-        end,
-      ]
   end
 
   def questions
     @questions ||= [
       Form::Sales::Questions::HousingBenefits.new(nil, nil, self, joint_purchase: @joint_purchase),
     ]
+  end
+
+  def depends_on
+    if @joint_purchase
+      [{ "joint_purchase?" => true }]
+    else
+      [{ "not_joint_purchase?" => true }]
+    end
   end
 end
