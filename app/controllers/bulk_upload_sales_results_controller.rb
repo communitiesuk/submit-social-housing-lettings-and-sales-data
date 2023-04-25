d1f21a5890af5a4c413bbafebac1b97a6e3b4829
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-04-25 12:01:17 +0100

    CLDC-2031 Sales bulk upload (#1574)
    * Add sales csv parser
    
    * Create log creator and update row parser for sales
    
    * Add validations
    
    * Add status_cache to sales logs
    
    * Parse completed log
    
    * Add basic validations to the row parser
    
    * Fix details known and proplen mapping
    
    * Add setup section errors
    
    * Update sales log validator
    
    * tests
    
    * Add sales resume and summary pages
    
    * send the correct emails

--- app/controllers/bulk_upload_sales_results_controller.rb
+++ app/controllers/bulk_upload_sales_results_controller.rb
@@ -6,4 +6,34 @@ class BulkUploadSalesResultsController < ApplicationController
   def show
     @bulk_upload = current_user.bulk_uploads.sales.find(params[:id])
   end
+
+  def resume
+    @bulk_upload = current_user.bulk_uploads.sales.find(params[:id])
+
+    if @bulk_upload.sales_logs.in_progress.count.positive?
+      set_bulk_upload_logs_filters
+
+      redirect_to(sales_logs_path(bulk_upload_id: [@bulk_upload.id]))
+    else
+      reset_logs_filters
+    end
+  end
+
+  def summary
+    @bulk_upload = current_user.bulk_uploads.sales.find(params[:id])
+  end
+
+  def reset_logs_filters
+    session["logs_filters"] = {}.to_json
+  end
+
+  def set_bulk_upload_logs_filters
+    hash = {
+      years: [""],
+      status: ["", "in_progress"],
+      user: "all",
+    }
+
+    session["logs_filters"] = hash.to_json
+  end
 end
