f2c3e250a36ae2bc7b56e8395efbdc0f5b43f727
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-04-26 12:29:35 +0100

    feat: delete la validator spec

--- spec/models/validations/local_authority_validations_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Validations::LocalAuthorityValidations do
-  subject(:local_auth_validator) { validator_class.new }
-
-  let(:validator_class) { Class.new { include Validations::LocalAuthorityValidations } }
-  let(:record) { FactoryBot.create(:lettings_log) }
-end
