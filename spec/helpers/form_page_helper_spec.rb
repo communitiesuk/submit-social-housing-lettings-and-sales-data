a3463c4f6a9170f59eea11264d950241e9b68829
Kat <katrina@kosiak.co.uk>
2023-04-27 12:14:24 +0100

    fix test

--- spec/helpers/form_page_helper_spec.rb
+++ spec/helpers/form_page_helper_spec.rb
@@ -2,15 +2,14 @@ require "rails_helper"
 
 RSpec.describe FormPageHelper do
   describe "#action_href" do
-    let(:lettings_log) { FactoryBot.build(:lettings_log) }
+    let(:lettings_log) { FactoryBot.create(:lettings_log) }
 
     it "has an update answer link href helper" do
-      lettings_log.id = 1
-      expect(action_href(lettings_log, "net_income")).to eq("/lettings-logs/1/net-income?referrer=check_answers")
+      expect(action_href(lettings_log, "net_income")).to eq("/lettings-logs/#{lettings_log.id}/net-income?referrer=check_answers")
     end
 
     it "returns a correct referrer in the url" do
-      expect(action_href(lettings_log, "retirement_value_check"), referrer: "interruption_screen").to eq("/lettings-logs/#{lettings_log.id}/retirement-value-check?referrer=interruption_screen")
+      expect(action_href(lettings_log, "retirement_value_check", "interruption_screen")).to eq("/lettings-logs/#{lettings_log.id}/retirement-value-check?referrer=interruption_screen")
     end
   end
 end
