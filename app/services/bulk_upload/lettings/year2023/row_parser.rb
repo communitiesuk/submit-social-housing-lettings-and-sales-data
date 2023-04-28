bbecf256814e8c50e798534f5f94312a2bc39872
Kat <katrina@kosiak.co.uk>
2023-04-28 10:57:14 +0100

    Import helper methods

--- app/services/bulk_upload/lettings/year2023/row_parser.rb
+++ app/services/bulk_upload/lettings/year2023/row_parser.rb
@@ -1,6 +1,7 @@
 class BulkUpload::Lettings::Year2023::RowParser
   include ActiveModel::Model
   include ActiveModel::Attributes
+  include InterruptionScreenHelper
 
   QUESTIONS = {
     field_1: "Which organisation owns this property?",
@@ -1406,46 +1407,6 @@ private
       1
     else
       0
-    end
-  end
-
-  def display_title_text(title_text, log)
-    return "" if title_text.nil?
-
-    translation_params = {}
-    arguments = title_text["arguments"] || {}
-    arguments.each do |argument|
-      value = get_value_from_argument(log, argument)
-      translation_params[argument["i18n_template"].to_sym] = value
-    end
-    I18n.t(title_text["translation"], **translation_params).to_s
-  end
-
-  def display_informative_text(informative_text, log)
-    return "" unless informative_text["arguments"]
-
-    translation_params = {}
-    informative_text["arguments"].each do |argument|
-      value = get_value_from_argument(log, argument)
-      translation_params[argument["i18n_template"].to_sym] = value
-    end
-
-    begin
-      translation = I18n.t(informative_text["translation"], **translation_params)
-      translation.to_s.html_safe
-    rescue I18n::MissingInterpolationArgument => e
-      Rails.logger.error(e.message)
-      ""
-    end
-  end
-
-  def get_value_from_argument(log, argument)
-    if argument["label"]
-      log.form.get_question(argument["key"], log).answer_label(log).downcase
-    elsif argument["arguments_for_key"]
-      log.public_send(argument["key"], argument["arguments_for_key"])
-    else
-      log.public_send(argument["key"])
     end
   end
 end
--- app/services/bulk_upload/sales/year2022/row_parser.rb
+++ app/services/bulk_upload/sales/year2022/row_parser.rb
@@ -1,6 +1,7 @@
 class BulkUpload::Sales::Year2022::RowParser
   include ActiveModel::Model
   include ActiveModel::Attributes
+  include InterruptionScreenHelper
 
   QUESTIONS = {
     field_1: "What is the purchaser code?",
@@ -960,46 +961,6 @@ private
           end
         end
       end
-    end
-  end
-
-  def display_title_text(title_text, log)
-    return "" if title_text.nil?
-
-    translation_params = {}
-    arguments = title_text["arguments"] || {}
-    arguments.each do |argument|
-      value = get_value_from_argument(log, argument)
-      translation_params[argument["i18n_template"].to_sym] = value
-    end
-    I18n.t(title_text["translation"], **translation_params).to_s
-  end
-
-  def display_informative_text(informative_text, log)
-    return "" unless informative_text["arguments"]
-
-    translation_params = {}
-    informative_text["arguments"].each do |argument|
-      value = get_value_from_argument(log, argument)
-      translation_params[argument["i18n_template"].to_sym] = value
-    end
-
-    begin
-      translation = I18n.t(informative_text["translation"], **translation_params)
-      translation.to_s.html_safe
-    rescue I18n::MissingInterpolationArgument => e
-      Rails.logger.error(e.message)
-      ""
-    end
-  end
-
-  def get_value_from_argument(log, argument)
-    if argument["label"]
-      log.form.get_question(argument["key"], log).answer_label(log).downcase
-    elsif argument["arguments_for_key"]
-      log.public_send(argument["key"], argument["arguments_for_key"])
-    else
-      log.public_send(argument["key"])
     end
   end
 end
