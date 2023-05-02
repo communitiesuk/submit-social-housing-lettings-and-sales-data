f5a5288dc89fd1abb643f1067e4b26d9999981bd
Kat <katrina@kosiak.co.uk>
2023-05-02 15:51:08 +0100

    Refactor validate_response

--- app/controllers/merge_requests_controller.rb
+++ app/controllers/merge_requests_controller.rb
@@ -11,7 +11,6 @@ class MergeRequestsController < ApplicationController
   ]
   before_action :authenticate_user!
   before_action :authenticate_scope!, except: [:create]
-  before_action :validate_response, only: %i[update]
 
   def absorbing_organisation; end
   def confirm_telephone_number; end
@@ -33,7 +32,9 @@ class MergeRequestsController < ApplicationController
   end
 
   def update
-    if @merge_request.update(merge_request_params)
+    validate_response
+
+    if @merge_request.errors.blank? && @merge_request.update(merge_request_params)
       redirect_to next_page_path
     else
       render previous_template, status: :unprocessable_entity
@@ -133,24 +134,20 @@ private
     when "absorbing_organisation"
       if merge_request_params[:absorbing_organisation_id].blank? && merge_request_params[:new_absorbing_organisation].blank?
         @merge_request.errors.add(:absorbing_organisation_id, I18n.t("validations.merge_request.absorbing_organisation_blank"))
-        render previous_template, status: :unprocessable_entity
       end
     when "confirm_telephone_number"
-      if merge_request_params[:telephone_number_correct].blank? && merge_request_params[:new_telephone_number].blank?
+      if merge_request_params[:telephone_number_correct].blank?
         if @merge_request.absorbing_organisation.phone.present?
           @merge_request.errors.add(:telephone_number_correct, I18n.t("validations.merge_request.telephone_number_correct_blank"))
         else
           @merge_request.errors.add(:telephone_number_correct, I18n.t("validations.merge_request.new_telephone_number_blank"))
         end
-        render previous_template, status: :unprocessable_entity
       elsif merge_request_params[:telephone_number_correct] == "0" && merge_request_params[:new_telephone_number].blank?
         @merge_request.errors.add(:new_telephone_number, I18n.t("validations.merge_request.new_telephone_number_blank"))
-        render previous_template, status: :unprocessable_entity
       end
     when "new_organisation_name"
       if merge_request_params[:new_organisation_name].blank?
         @merge_request.errors.add(:new_organisation_name, I18n.t("validations.merge_request.new_organisation_name_blank"))
-        render previous_template, status: :unprocessable_entity
       end
     end
   end
