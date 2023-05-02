7580bd556d196b922ac6e1ba1221d81f469fd7b5
Kat <katrina@kosiak.co.uk>
2023-05-02 09:43:47 +0100

    Update validation message: void date, major repairs date

--- app/models/form/lettings/pages/property_major_repairs_value_check.rb
+++ app/models/form/lettings/pages/property_major_repairs_value_check.rb
@@ -4,7 +4,10 @@ class Form::Lettings::Pages::PropertyMajorRepairsValueCheck < ::Form::Page
     @id = "property_major_repairs_value_check"
     @depends_on = [{ "major_repairs_date_in_soft_range?" => true }]
     @title_text = { "translation" => "soft_validations.major_repairs_date.title_text" }
-    @informative_text = {}
+    @informative_text = {
+      "translation" => "soft_validations.major_repairs_date.hint_text",
+      "arguments" => [],
+    }
   end
 
   def questions
