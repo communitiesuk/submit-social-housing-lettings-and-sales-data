31ea432cf6c0cd6a98337c03999f88369dd1cccf
Jack <113976590+bibblobcode@users.noreply.github.com>
2023-05-18 11:46:47 +0100

    Load FormFixture only in tests (#1643)
    It caused superclass mismatch errors. We can stop seeing this bug in local development by defining the class only in test environments

--- spec/factories/form.rb
+++ spec/factories/form.rb
@@ -1,25 +1,27 @@
-class FormFixture < Form
-  attr_accessor :sections, :subsections, :pages, :questions
-end
-
-class FormFactory
-  def initialize(year:, type:)
-    @year = year
-    @type = type
+if Rails.env.test?
+  class FormFixture < Form
+    attr_accessor :sections, :subsections, :pages, :questions
   end
 
-  def with_sections(sections)
-    @sections = sections
-    self
-  end
+  class FormFactory
+    def initialize(year:, type:)
+      @year = year
+      @type = type
+    end
 
-  def build
-    form = FormFixture.new(nil, @year, [], @type)
-    @sections.each { |section| section.form = form }
-    form.sections = @sections
-    form.subsections = form.sections.flat_map(&:subsections)
-    form.pages = form.subsections.flat_map(&:pages)
-    form.questions = form.pages.flat_map(&:questions)
-    form
+    def with_sections(sections)
+      @sections = sections
+      self
+    end
+
+    def build
+      form = FormFixture.new(nil, @year, [], @type)
+      @sections.each { |section| section.form = form }
+      form.sections = @sections
+      form.subsections = form.sections.flat_map(&:subsections)
+      form.pages = form.subsections.flat_map(&:pages)
+      form.questions = form.pages.flat_map(&:questions)
+      form
+    end
   end
 end
