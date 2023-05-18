7de82bd03865745acf0971c31cf311d7ebb9f9b9
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-05-18 16:42:36 +0100

    feat: ensure period is routed to for needstype 1 regardless of household_charge

--- app/models/form/lettings/pages/rent_period.rb
+++ app/models/form/lettings/pages/rent_period.rb
@@ -2,7 +2,7 @@ class Form::Lettings::Pages::RentPeriod < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "rent_period"
-    @depends_on = [{ "household_charge" => 0 }, { "household_charge" => nil }]
+    @depends_on = [{ "needstype" => 1 }, { "needstype" => 2, "household_charge" => 0 }, { "needstype" => 2, "household_charge" => nil }]
   end
 
   def questions
