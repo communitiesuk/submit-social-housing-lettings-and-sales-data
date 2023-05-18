426667e891731cfc72f862f03221bcc3ba887472
Kat <katrina@kosiak.co.uk>
2023-05-18 11:34:30 +0100

    Remove soft validations from sales setup and make them mandatory

--- spec/models/form/sales/subsections/setup_spec.rb
+++ spec/models/form/sales/subsections/setup_spec.rb
@@ -17,17 +17,11 @@ RSpec.describe Form::Sales::Subsections::Setup, type: :model do
         organisation
         created_by
         completion_date
-        completion_date_check
         purchaser_code
         ownership_scheme
         shared_ownership_type
         discounted_ownership_type
         outright_ownership_type
-        ownership_type_old_persons_shared_ownership_value_check
-        monthly_charges_type_value_check
-        discounted_sale_type_value_check
-        buyer_1_live_in_property_type_value_check
-        buyer_2_live_in_property_type_value_check
         buyer_company
         buyer_live
         joint_purchase
