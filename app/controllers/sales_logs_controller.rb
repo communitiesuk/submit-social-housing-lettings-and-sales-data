2df4d54eb563230859920414a749a9bcc3460bb2
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-06-05 17:17:20 +0100

    refactor: combine filter_manager into filter_service

--- app/controllers/sales_logs_controller.rb
+++ app/controllers/sales_logs_controller.rb
@@ -2,7 +2,7 @@ class SalesLogsController < LogsController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   before_action :session_filters, if: :current_user, only: %i[index email_csv download_csv]
-  before_action :set_session_filters, if: :current_user, only: %i[index email_csv download_csv]
+  before_action -> { filter_service.serialize_filters_to_session }, if: :current_user, only: %i[index email_csv download_csv]
   before_action :authenticate_scope!, only: %i[download_csv email_csv]
 
   before_action :extract_bulk_upload_from_session_filters, only: [:index]
@@ -16,7 +16,7 @@ class SalesLogsController < LogsController
     respond_to do |format|
       format.html do
         all_logs = current_user.sales_logs.visible
-        unpaginated_filtered_logs = filter_manager.filtered_logs(all_logs, search_term, session_filters)
+        unpaginated_filtered_logs = filter_service.filtered_logs(all_logs, search_term, session_filters)
 
         @search_term = search_term
         @pagy, @logs = pagy(unpaginated_filtered_logs)
@@ -58,7 +58,7 @@ class SalesLogsController < LogsController
   end
 
   def download_csv
-    unpaginated_filtered_logs = filter_manager.filtered_logs(current_user.sales_logs, search_term, session_filters)
+    unpaginated_filtered_logs = filter_service.filtered_logs(current_user.sales_logs, search_term, session_filters)
 
     render "download_csv", locals: { search_term:, count: unpaginated_filtered_logs.size, post_path: email_csv_sales_logs_path, codes_only: codes_only_export? }
   end
@@ -81,21 +81,17 @@ class SalesLogsController < LogsController
 
 private
 
-  def set_session_filters
-    filter_manager.serialize_filters_to_session
-  end
-
   def session_filters
-    filter_manager.session_filters
+    filter_service.session_filters
   end
 
-  def filter_manager
-    FilterManager.new(current_user:, session:, params:, filter_type: "sales_logs")
+  def filter_service
+    FilterService.new(current_user:, session:, params:, filter_type: "sales_logs")
   end
 
   def extract_bulk_upload_from_session_filters
-    filter_service = FilterService.new(current_user:, session:)
-    @bulk_upload = filter_service.bulk_upload("sales_logs")
+    filter_service = FilterService.new(current_user:, session:, params:, filter_type: "lettings_logs")
+    @bulk_upload = filter_service.bulk_upload
   end
 
   def redirect_if_bulk_upload_resolved
