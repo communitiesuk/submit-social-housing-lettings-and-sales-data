1829d71807a8134c29fa857619b87356234796f6
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-06-08 15:50:36 +0100

    feat: set form date to past to trigger errors to fix

--- app/models/form.rb
+++ app/models/form.rb
@@ -9,7 +9,7 @@ class Form
       @end_date = if start_year && start_year.to_i > 2022
                     Time.zone.local(start_year + 1, 6, 9)
                   else
-                    Time.zone.local(start_year + 1, 8, 7)
+                    Time.zone.local(start_year + 1, 6, 7)
                   end
       @setup_sections = type == "sales" ? [Form::Sales::Sections::Setup.new(nil, nil, self)] : [Form::Lettings::Sections::Setup.new(nil, nil, self)]
       @form_sections = sections_in_form.map { |sec| sec.new(nil, nil, self) }
