f9ed29804effd435d96233c33a4e5f34de7674d4
Arthur Campbell <51094020+arfacamble@users.noreply.github.com>
2023-06-08 09:16:13 +0100

    push back end date of forms to allow them to continue to be edited for now. Adjust the copy about the date until when logs can be edited to keep it consistent with how things have been up until now (#1688)

--- app/helpers/tasklist_helper.rb
+++ app/helpers/tasklist_helper.rb
@@ -31,9 +31,9 @@ module TasklistHelper
 
   def review_log_text(log)
     if log.collection_period_open?
-      link = log.sales? ? review_sales_log_path(id: log, sales_log: true) : review_lettings_log_path(log)
+      path = log.sales? ? review_sales_log_path(id: log, sales_log: true) : review_lettings_log_path(log)
 
-      "You can #{govuk_link_to 'review and make changes to this log', link} until #{log.form.end_date.to_formatted_s(:govuk_date)}.".html_safe
+      "You can #{govuk_link_to 'review and make changes to this log', path} until #{log.form.display_end_date.to_formatted_s(:govuk_date)}.".html_safe
     else
       start_year = log.startdate ? collection_start_year_for_date(log.startdate) : log.form.start_date.year
 
