da43274b271ba5c1ffeb2f001c013452a3444f5d
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-06-14 14:20:09 +0100

    feat: update tests

--- spec/services/bulk_upload/lettings/year2023/row_parser_spec.rb
+++ spec/services/bulk_upload/lettings/year2023/row_parser_spec.rb
@@ -1365,10 +1365,20 @@ RSpec.describe BulkUpload::Lettings::Year2023::RowParser do
     end
 
     describe "#net_income_known" do
-      let(:attributes) { { bulk_upload:, field_120: "1" } }
+      context "when 1" do
+        let(:attributes) { { bulk_upload:, field_120: "1" } }
 
-      it "sets value from correct mapping" do
-        expect(parser.log.net_income_known).to eq(0)
+        it "sets value from correct mapping" do
+          expect(parser.log.net_income_known).to eq(0)
+        end
+      end
+
+      context "when 3" do
+        let(:attributes) { { bulk_upload:, field_120: "3" } }
+
+        it "sets value from correct mapping" do
+          expect(parser.log.net_income_known).to eq(2)
+        end
       end
     end
 
