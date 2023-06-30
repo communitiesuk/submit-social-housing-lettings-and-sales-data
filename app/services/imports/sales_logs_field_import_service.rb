6bd1f983799eb9216d799dcb558565428f85d82c
Arthur Campbell <arfa.camble@gmail.com>
2023-06-30 13:38:01 +0100

    create sales log field import service and associated spec file, with methods and tests for importing the creation method of logs that have already been imported

---
+++ app/services/imports/sales_logs_field_import_service.rb
@@ -0,0 +1,37 @@
+module Imports
+  class SalesLogsFieldImportService < LogsImportService
+    def update_field(field, folder)
+      case field
+      when "creation_method"
+        import_from(folder, :update_creation_method)
+      else
+        raise "Updating #{field} is not supported by the field import service"
+      end
+    end
+
+  private
+
+    def update_creation_method(xml_doc)
+      old_id = meta_field_value(xml_doc, "document-id")
+      log = SalesLog.find_by(old_id:)
+
+      if log
+        upload_method = meta_field_value(xml_doc, "upload-method")
+
+        case upload_method
+        when "Manual Entry"
+          @logger.info "sales log with old id #{old_id} entered manually, no need for update"
+        when "Bulk Upload"
+          if log.creation_method == "bulk upload"
+            @logger.info "sales log #{log.id} creation method already set to bulk upload, no need for update"
+          else
+            log.update!(creation_method: "bulk upload")
+            @logger.info "sales log #{log.id} creation method set to bulk upload"
+          end
+        end
+      else
+        @logger.warn "sales log with old id #{old_id} not found"
+      end
+    end
+  end
+end
