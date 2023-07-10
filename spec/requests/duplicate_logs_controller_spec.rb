57553d3fde0f30bdada0339ca4ac63cba715c86e
Kat <katrina@kosiak.co.uk>
2023-07-10 15:49:11 +0100

    Update displayed questions

--- spec/requests/duplicate_logs_controller_spec.rb
+++ spec/requests/duplicate_logs_controller_spec.rb
@@ -167,7 +167,7 @@ RSpec.describe DuplicateLogsController, type: :request do
             expect(page).to have_content("Q33 - Lead tenant’s gender identity", count: 3)
             expect(page).to have_content("Q37 - Lead tenant’s working situation", count: 3)
             expect(page).to have_content("Household rent and charges", count: 3)
-            expect(page).to have_link("Change", count: 24)
+            expect(page).to have_link("Change", count: 21)
           end
 
           it "displays buttons to delete" do
@@ -190,14 +190,13 @@ RSpec.describe DuplicateLogsController, type: :request do
           end
 
           it "displays check your answers for each log with correct questions" do
-            expect(page).to have_content("Owning organisation", count: 3)
             expect(page).to have_content("Q1 - Sale completion date", count: 3)
             expect(page).to have_content("Q2 - Purchaser code", count: 3)
             expect(page).to have_content("Q20 - Lead buyer’s age", count: 3)
             expect(page).to have_content("Q21 - Buyer 1’s gender identity", count: 3)
             expect(page).to have_content("Q25 - Buyer 1's working situation", count: 3)
             expect(page).to have_content("Q15 - Postcode", count: 3)
-            expect(page).to have_link("Change", count: 21)
+            expect(page).to have_link("Change", count: 18)
           end
 
           it "displays buttons to delete" do
