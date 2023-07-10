5849cf6effe5fd64aa868e0d5585bed7fa366916
Kat <katrina@kosiak.co.uk>
2023-07-10 09:20:29 +0100

    Display all duplicate logs

--- app/controllers/duplicate_logs_controller.rb
+++ app/controllers/duplicate_logs_controller.rb
@@ -1,2 +1,7 @@
 class DuplicateLogsController < ApplicationController
+  def show
+    @log = LettingsLog.find(params[:lettings_log_id])
+    @duplicate_logs = LettingsLog.duplicate_logs_for_organisation(current_user.organisation_id, @log)
+    @all_duplicates = [@log, *@duplicate_logs]
+  end
 end
