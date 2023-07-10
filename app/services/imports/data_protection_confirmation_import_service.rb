f28da07f10deca78c227404ddeefd16cbeb05586
Jack S <jacopo.scotti@softwire.com>
2023-07-10 15:29:02 +0200

    Persist org and user data at import

--- app/services/imports/data_protection_confirmation_import_service.rb
+++ app/services/imports/data_protection_confirmation_import_service.rb
@@ -38,6 +38,12 @@ module Imports
         old_id: record_field_value(xml_document, "id"),
         old_org_id: record_field_value(xml_document, "institution"),
         created_at: record_field_value(xml_document, "change-date").to_time(:utc),
+        signed_at: record_field_value(xml_document, "change-date").to_time(:utc),
+        organisation_name: org.name,
+        organisation_address: org.address_row,
+        organisation_phone_number: org.phone,
+        data_protection_officer_email: dp_officer.email,
+        data_protection_officer_name: dp_officer.name,
       )
     end
 
