c31a5e70638a22f7c1e0caf3f29dc45b0775fd74
Arthur Campbell <arfa.camble@gmail.com>
2023-07-12 17:21:37 +0100

    check for duplicates and display notification banner on the index page where appropriate tests to cover the desired behaviour

--- app/controllers/lettings_logs_controller.rb
+++ app/controllers/lettings_logs_controller.rb
@@ -1,4 +1,6 @@
 class LettingsLogsController < LogsController
+  include DuplicateLogsHelper
+
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   before_action :find_resource, only: %i[update show]
@@ -11,20 +13,20 @@ class LettingsLogsController < LogsController
   before_action :redirect_if_bulk_upload_resolved, only: [:index]
 
   def index
-    respond_to do |format|
-      format.html do
-        all_logs = current_user.lettings_logs.visible
-        unpaginated_filtered_logs = filter_manager.filtered_logs(all_logs, search_term, session_filters)
-
-        @delete_logs_path = delete_logs_lettings_logs_path(search: search_term)
-        @pagy, @logs = pagy(unpaginated_filtered_logs)
-        @searched = search_term.presence
-        @total_count = all_logs.size
-        @unresolved_count = all_logs.unresolved.created_by(current_user).count
-        @filter_type = "lettings_logs"
-        render "logs/index"
-      end
+    if current_user.data_provider? && (@duplicates = duplicates_for_user(current_user))
+      @duplicate_sets_count = @duplicates[:lettings].count + @duplicates[:sales].count
     end
+
+    all_logs = current_user.lettings_logs.visible
+    unpaginated_filtered_logs = filter_manager.filtered_logs(all_logs, search_term, session_filters)
+
+    @delete_logs_path = delete_logs_lettings_logs_path(search: search_term)
+    @pagy, @logs = pagy(unpaginated_filtered_logs)
+    @searched = search_term.presence
+    @total_count = all_logs.size
+    @unresolved_count = all_logs.unresolved.created_by(current_user).count
+    @filter_type = "lettings_logs"
+    render "logs/index"
   end
 
   def create
