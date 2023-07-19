8274e460cab86cbe82bfe4260f7513f7d25f3ea9
Jack S <jacopo.scotti@softwire.com>
2023-07-19 14:18:45 +0100

    CLDC-2498 Prevent non-support from editing org name

--- app/helpers/organisation_helper.rb
+++ app/helpers/organisation_helper.rb
@@ -8,4 +8,20 @@ module OrganisationHelper
       current_organisation.name
     end
   end
+
+  def organisation_name_row(user:, organisation:, summary_list:)
+    summary_list.row do |row|
+      row.key { "Name" }
+      row.value { organisation.name }
+      if user.support?
+        row.action(
+          visually_hidden_text: organisation.name.humanize.downcase,
+          href: edit_organisation_path(organisation),
+          html_attributes: { "data-qa": "change-#{organisation.name.downcase}" },
+        )
+      else
+        row.action
+      end
+    end
+  end
 end
