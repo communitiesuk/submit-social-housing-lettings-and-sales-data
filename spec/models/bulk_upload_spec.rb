d51b12cfa45bd5d5a841d247a06e02cb03430e8c
Aaron Spencer <aaron.spencer@softwire.com>
2023-08-11 16:17:00 +0100

    CLDC-2505: test linting fix

--- spec/models/bulk_upload_spec.rb
+++ spec/models/bulk_upload_spec.rb
@@ -5,17 +5,15 @@ RSpec.describe BulkUpload, type: :model do
 
   describe "def bulk_upload.completed?" do
     context "when there are incomplete logs" do
-      let!(:lettings_log) { create_list(:lettings_log, 2, :in_progress, bulk_upload:) }
-
       it "returns false" do
+        create_list(:lettings_log, 2, :in_progress, bulk_upload:)
         expect(bulk_upload.completed?).to equal(false)
       end
     end
 
     context "when there are no incomplete logs" do
-      let!(:lettings_log) { create_list(:lettings_log, 2, :completed, bulk_upload:) }
-
       it "returns true" do
+        create_list(:lettings_log, 2, :completed, bulk_upload:)
         expect(bulk_upload.completed?).to equal(true)
       end
     end
