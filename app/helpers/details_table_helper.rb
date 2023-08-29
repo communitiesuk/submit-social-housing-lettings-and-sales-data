084dfb4dab553ed5e6ca0b8122701035ae60f544
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2023-08-29 16:16:14 +0200

    CLDC-2630 Location fixes (#1870)
    * Display question unanswered for empty strings
    
    * Fix back button for local authority
    
    * Update test name
    
    Co-authored-by: natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>

--- app/helpers/details_table_helper.rb
+++ app/helpers/details_table_helper.rb
@@ -4,7 +4,9 @@ module DetailsTableHelper
       list = attribute[:value].map { |value| "<li>#{value}</li>" }.join
       simple_format(list, { class: "govuk-list govuk-list--bullet" }, wrapper_tag: "ul")
     else
-      value = attribute[:value].is_a?(Array) ? attribute[:value].first : attribute[:value] || "<span class=\"app-!-colour-muted\">You didn’t answer this question</span>".html_safe
+      return simple_format(attribute[:value].first.to_s, { class: "govuk-body" }, wrapper_tag: "p") if attribute[:value].is_a?(Array)
+
+      value = attribute[:value].presence || "<span class=\"app-!-colour-muted\">You didn’t answer this question</span>".html_safe
 
       simple_format(value.to_s, { class: "govuk-body" }, wrapper_tag: "p")
     end
