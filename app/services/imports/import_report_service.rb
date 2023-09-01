01fce06e11b9de0dc95388158f320ebebc13d31f
Kat <katrina@kosiak.co.uk>
2023-09-01 12:13:52 +0200

    Add generate_unassigned_logs_report

--- app/services/imports/import_report_service.rb
+++ app/services/imports/import_report_service.rb
@@ -11,6 +11,7 @@ module Imports
     def create_reports(report_suffix)
       generate_missing_data_coordinators_report(report_suffix)
       generate_logs_report(report_suffix)
+      generate_unassigned_logs_report(report_suffix)
     end
 
     def generate_missing_data_coordinators_report(report_suffix)
@@ -52,6 +53,36 @@ module Imports
       @storage_service.write_file(report_name, BYTE_ORDER_MARK + rep)
 
       @logger.info("Logs report available in s3 import bucket at #{report_name}")
+    end
+
+    def generate_unassigned_logs_report(report_suffix)
+      Rails.logger.info("Generating unassigned logs report")
+
+      rep = CSV.generate do |report|
+        headers = ["Owning Organisation ID", "Old Owning Organisation ID", "Managing Organisation ID", "Old Managing Organisation ID", "Log ID", "Old Log ID", "Tenancy code", "Purchaser code"]
+        report << headers
+
+        @institutions_csv.each do |row|
+          name = row[0]
+          organisation = Organisation.find_by(name:)
+          next unless organisation
+
+          unassigned_user = organisation.users.find_by(name: "Unassigned")
+          next unless unassigned_user
+
+          organisation.owned_lettings_logs.where(created_by: unassigned_user).each do |lettings_log|
+            report << [organisation.id, organisation.old_org_id, lettings_log.managing_organisation.id, lettings_log.managing_organisation.old_org_id, lettings_log.id, lettings_log.old_id, lettings_log.tenancycode, nil]
+          end
+          organisation.owned_sales_logs.where(created_by: unassigned_user).each do |sales_log|
+            report << [organisation.id, organisation.old_org_id, nil, nil, sales_log.id, sales_log.old_id, nil, sales_log.purchid]
+          end
+        end
+      end
+
+      report_name = "UnassignedLogsReport_#{report_suffix}"
+      @storage_service.write_file(report_name, BYTE_ORDER_MARK + rep)
+
+      @logger.info("Unassigned logs report available in s3 import bucket at #{report_name}")
     end
   end
 end
