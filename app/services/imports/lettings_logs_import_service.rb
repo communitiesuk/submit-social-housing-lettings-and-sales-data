d27ac39a16e74f90c3169ef66ab90ad6185558e9
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-09-07 15:41:39 +0100

    feat: allow decimals but clear when not integer equivalent as per new requirements

--- app/services/imports/lettings_logs_import_service.rb
+++ app/services/imports/lettings_logs_import_service.rb
@@ -174,7 +174,7 @@ module Imports
                                      0
                                    end
 
-      attributes["offered"] = safe_string_as_integer(xml_doc, "Q20")
+      attributes["offered"] = safe_string_as_decimal(xml_doc, "Q20")
       attributes["propcode"] = string_or_nil(xml_doc, "Q21a")
       attributes["beds"] = safe_string_as_integer(xml_doc, "Q22")
       attributes["unittype_gn"] = unsafe_string_as_integer(xml_doc, "Q23")
@@ -326,6 +326,7 @@ module Imports
         %i[prevten non_temp_accommodation] => %w[prevten rsnvac],
         %i[joint not_joint_tenancy] => %w[joint],
         %i[offered outside_the_range] => %w[offered],
+        %i[offered not_integer] => %w[offered],
         %i[earnings over_hard_max] => %w[ecstat1],
         %i[tshortfall no_outstanding_charges] => %w[tshortfall hbrentshortfall],
         %i[beds outside_the_range] => %w[beds],
