5b85c68be95757f73ff028bbb3b98455735f8953
natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>
2023-09-27 14:05:40 +0100

    CLDC-2774 Update lettings materials (#1939)
    * feat: update lettings materials
    
    * feat: update tests
    
    * refactor: lint

--- spec/helpers/collection_resources_helper_spec.rb
+++ spec/helpers/collection_resources_helper_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CollectionResourcesHelper do
   describe "when displaying file metadata" do
     context "with pages" do
       it "returns correct metadata" do
-        expect(file_type_size_and_pages("2023_24_lettings_paper_form.pdf", number_of_pages: 8)).to eq("PDF, 286 KB, 8 pages")
+        expect(file_type_size_and_pages("2023_24_lettings_paper_form.pdf", number_of_pages: 8)).to eq("PDF, 285 KB, 8 pages")
       end
     end
 
