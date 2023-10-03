08282a0afb0c6db566a1a32064d17ed4aab212ec
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-10-03 15:28:31 +0100

    refactor: lint

--- app/services/csv/lettings_log_csv_service.rb
+++ app/services/csv/lettings_log_csv_service.rb
@@ -120,7 +120,7 @@ module Csv
     PERSON_DETAILS = {}.tap { |hash|
       hash["age1"] = { "refused_code" => "-9", "refused_label" => "Not known", "age_known_field" => "age1_known" }
       (2..8).each do |i|
-        hash["age#{i}"] = { "refused_code" => "-9", "refused_label" => "Not known", "details_known_field" => "details_known_#{i}", "age_known_field" => "age#{i}_known"  }
+        hash["age#{i}"] = { "refused_code" => "-9", "refused_label" => "Not known", "details_known_field" => "details_known_#{i}", "age_known_field" => "age#{i}_known" }
         hash["sex#{i}"] = { "refused_code" => "R", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
         hash["relat#{i}"] = { "refused_code" => "R", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
         hash["ecstat#{i}"] = { "refused_code" => "10", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
