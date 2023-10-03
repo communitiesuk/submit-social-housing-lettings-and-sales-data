cab5764ef406ca42dfdb778a6da1c18f2bb7b27f
Kat <katrina@kosiak.co.uk>
2023-10-03 18:28:40 +0100

    update template id for sales

--- app/mailers/csv_download_mailer.rb
+++ app/mailers/csv_download_mailer.rb
@@ -1,7 +1,7 @@
 class CsvDownloadMailer < NotifyMailer
   CSV_DOWNLOAD_TEMPLATE_ID = "7890e3b9-8c0d-4d08-bafe-427fd7cd95bf".freeze
   CSV_MISSING_LETTINGS_ADDRESSES_DOWNLOAD_TEMPLATE_ID = "7602b6c2-4f44-4da6-8a68-944e39cd8a05".freeze
-  CSV_MISSING_SALES_ADDRESSES_DOWNLOAD_TEMPLATE_ID = "7602b6c2-4f44-4da6-8a68-944e39cd8a05".freeze
+  CSV_MISSING_SALES_ADDRESSES_DOWNLOAD_TEMPLATE_ID = "1ee6da00-a65e-4a39-b5e5-1846debcb5f8".freeze
 
   def send_csv_download_mail(user, link, duration)
     send_email(
