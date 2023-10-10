ad53184678159ebb35f25995b98035b91621a88d
Kat <katrina@kosiak.co.uk>
2023-10-10 15:30:38 +0100

    Update schemes banner text

--- app/controllers/schemes_controller.rb
+++ app/controllers/schemes_controller.rb
@@ -141,7 +141,7 @@ class SchemesController < ApplicationController
         flash[:notice] = if scheme_previously_confirmed
                            "#{@scheme.service_name} has been updated."
                          else
-                           "#{@scheme.service_name} has been created."
+                           "#{@scheme.service_name} has been created. It does not require helpdesk approval."
                          end
         redirect_to scheme_path(@scheme)
       elsif check_answers
