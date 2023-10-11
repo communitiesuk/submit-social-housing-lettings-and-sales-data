13debf2e1831339cd4ec7a4443176ad8f1453ee7
Kat <katrina@kosiak.co.uk>
2023-10-11 09:29:09 +0100

    Update missing addresses threshold

--- app/jobs/email_missing_addresses_csv_job.rb
+++ app/jobs/email_missing_addresses_csv_job.rb
@@ -3,7 +3,7 @@ class EmailMissingAddressesCsvJob < ApplicationJob
 
   BYTE_ORDER_MARK = "\uFEFF".freeze # Required to ensure Excel always reads CSV as UTF-8
   EXPIRATION_TIME = 1.week.to_i
-  MISSING_ADDRESSES_THRESHOLD = 5
+  MISSING_ADDRESSES_THRESHOLD = 50
 
   def perform(user_ids, organisation, log_type, issue_types, skip_uprn_issue_organisations)
     csv_service = Csv::MissingAddressesCsvService.new(organisation, skip_uprn_issue_organisations)
