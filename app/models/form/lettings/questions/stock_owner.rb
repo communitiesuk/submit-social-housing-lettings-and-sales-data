93c2107bc79e89852e5cde58ec7c9a70560bb5cd
Kat <katrina@kosiak.co.uk>
2023-10-20 11:12:18 +0100

    Display merged orgs for owning org

--- app/models/form/lettings/questions/stock_owner.rb
+++ app/models/form/lettings/questions/stock_owner.rb
@@ -16,20 +16,38 @@ class Form::Lettings::Questions::StockOwner < ::Form::Question
     return answer_opts unless log
 
     if log.owning_organisation_id.present?
-      answer_opts = answer_opts.merge({ log.owning_organisation.id => log.owning_organisation.name })
+      answer_opts[log.owning_organisation.id] = log.owning_organisation.name
     end
 
+    recently_absorbed_organisations = user.organisation.absorbed_organisations.merged_during_open_collection_period
     if !user.support? && user.organisation.holds_own_stock?
-      answer_opts[user.organisation.id] = "#{user.organisation.name} (Your organisation)"
+      answer_opts[user.organisation.id] = if recently_absorbed_organisations.exists?
+                                            "#{user.organisation.name} (Your organisation, active as of #{user.organisation.created_at.to_fs(:govuk_date)})"
+                                          else
+                                            "#{user.organisation.name} (Your organisation)"
+                                          end
     end
 
-    user_answer_options = if user.support?
-                            Organisation.where(holds_own_stock: true)
-                          else
-                            user.organisation.stock_owners + user.organisation.absorbed_organisations.where(holds_own_stock: true)
-                          end.pluck(:id, :name).to_h
+    if user.support?
+      Organisation.where(holds_own_stock: true).find_each do |org|
+        if org.merge_date.present?
+          answer_opts[org.id] = "#{org.name} (inactive as of #{org.merge_date.to_fs(:govuk_date)})" if org.merge_date >= FormHandler.instance.start_date_of_earliest_open_for_editing_collection_period
+        elsif org.absorbed_organisations.merged_during_open_collection_period.exists?
+          answer_opts[org.id] = "#{org.name} (active as of #{org.created_at.to_fs(:govuk_date)})"
+        else
+          answer_opts[org.id] = org.name
+        end
+      end
+    else
+      user.organisation.stock_owners.each do |stock_owner|
+        answer_opts[stock_owner.id] = stock_owner.name
+      end
+      recently_absorbed_organisations.each do |absorbed_org|
+        answer_opts[absorbed_org.id] = merged_organisation_label(absorbed_org.name, absorbed_org.merge_date) if absorbed_org.holds_own_stock?
+      end
+    end
 
-    answer_opts.merge(user_answer_options)
+    answer_opts
   end
 
   def displayed_answer_options(log, user = nil)
@@ -70,5 +88,9 @@ private
 
   def selected_answer_option_is_derived?(_log)
     true
+  end
+
+  def merged_organisation_label(name, merge_date)
+    "#{name} (inactive as of #{merge_date.to_fs(:govuk_date)})"
   end
 end
