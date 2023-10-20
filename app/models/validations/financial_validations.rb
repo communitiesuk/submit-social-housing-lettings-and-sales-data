3e26d9e575f4cd6743b77700aad7e3af7ddfe964
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-10-20 16:47:06 +0100

    feat: simplify method call in validations

--- app/models/validations/financial_validations.rb
+++ app/models/validations/financial_validations.rb
@@ -182,18 +182,6 @@ private
   PROVIDER_TYPE = { 1 => :local_authority, 2 => :private_registered_provider }.freeze
   NEEDSTYPE_VALUES = { 2 => :supported_housing, 1 => :general_needs }.freeze
   CHARGE_NAMES = { scharge: "service charge", pscharge: "personal service charge", supcharg: "support charge" }.freeze
-  FREQUENCIES = {
-    2 => "every 2 weeks",
-    3 => "every 4 weeks",
-    4 => "every calendar month",
-    9 => "weekly for 46 weeks",
-    8 => "weekly for 47 weeks",
-    7 => "weekly for 48 weeks",
-    6 => "weekly for 49 weeks",
-    5 => "weekly for 50 weeks",
-    1 => "weekly for 52 weeks",
-    10 => "weekly for 53 weeks",
-  }.freeze
 
   def validate_charges(record)
     return unless record.owning_organisation
@@ -205,7 +193,7 @@ private
       next unless maximum_per_week.present? && record[:period].present? && record[charge].present? && !weekly_value_in_range(record, charge, 0.0, maximum_per_week)
 
       charge_name = CHARGE_NAMES[charge]
-      frequency = FREQUENCIES[record[:period]]
+      frequency = record.form.get_question("period", record).label_from_value(record.period).downcase
       letting_type = NEEDSTYPE_VALUES[record.needstype].to_s.humanize(capitalize: false)
       provider_type = PROVIDER_TYPE[provider_type].to_s.humanize(capitalize: false)
       record.errors.add charge, :outside_the_range, message: I18n.t("validations.financial.rent.out_of_range", charge_name:, maximum_per_week:, frequency:, letting_type:, provider_type:)
