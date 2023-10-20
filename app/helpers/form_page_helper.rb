e356365c21cdef720b9069a61ce365a0fde88a59
Kat <katrina@kosiak.co.uk>
2023-10-20 11:11:09 +0100

    Correctly route when comming from duplicate banner

--- app/helpers/form_page_helper.rb
+++ app/helpers/form_page_helper.rb
@@ -2,4 +2,12 @@ module FormPageHelper
   def action_href(log, page_id, referrer = "check_answers")
     send("#{log.model_name.param_key}_#{page_id}_path", log, referrer:)
   end
+
+  def returning_to_question_page?(page, referrer)
+    page.interruption_screen? || referrer == "check_answers"
+  end
+
+  def accessed_from_duplicate_logs?(referrer)
+    %w[duplicate_logs duplicate_logs_banner].include?(referrer)
+  end
 end
