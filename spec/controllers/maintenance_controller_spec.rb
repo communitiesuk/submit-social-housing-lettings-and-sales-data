6fd0f2ece945237e3ea1ef6f8c7d3f1821f4e717
Sam Seed <sam.seed@softwire.com>
2023-10-27 13:26:57 +0100

    CLDC-2862: write tests for MaintenanceController

---
+++ spec/controllers/maintenance_controller_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe MaintenanceController do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "GET #service_unavailable" do
+    context "when maintenance mode is enabled" do
+      it "logs the user out" do
+        allow(FeatureToggle).to receive(:maintenance_mode_enabled?).and_return(true)
+        sign_in user
+        expect(controller).to be_user_signed_in
+        get :service_unavailable
+        expect(controller).not_to be_user_signed_in
+      end
+    end
+
+    context "when maintenance mode is disabled" do
+      it "doesn't log the user out" do
+        allow(FeatureToggle).to receive(:maintenance_mode_enabled?).and_return(false)
+        sign_in user
+        expect(controller).to be_user_signed_in
+        get :service_unavailable
+        expect(controller).to be_user_signed_in
+      end
+    end
+  end
+end
