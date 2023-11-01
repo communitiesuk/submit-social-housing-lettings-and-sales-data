63a63b8fc9b7bc817064832e0ab9394ba0ef2ce1
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-11-01 10:49:06 +0000

    feat: don't validate on log save so they can be added to new orgs without dsas during a merge, and test

--- app/services/merge/merge_organisations_service.rb
+++ app/services/merge/merge_organisations_service.rb
@@ -87,17 +87,18 @@ private
         lettings_log.location = location_to_set if location_to_set.present?
       end
       lettings_log.owning_organisation = @absorbing_organisation
-      lettings_log.save!
+      lettings_log.save!(validate: false)
     end
     merging_organisation.managed_lettings_logs.after_date(@merge_date.to_time).each do |lettings_log|
       lettings_log.managing_organisation = @absorbing_organisation
-      lettings_log.save!
+      lettings_log.save!(validate: false)
     end
   end
 
   def merge_sales_logs(merging_organisation)
     merging_organisation.sales_logs.after_date(@merge_date.to_time).each do |sales_log|
-      sales_log.update(owning_organisation: @absorbing_organisation)
+      sales_log.owning_organisation = @absorbing_organisation
+      sales_log.save!(validate: false)
     end
   end
 
