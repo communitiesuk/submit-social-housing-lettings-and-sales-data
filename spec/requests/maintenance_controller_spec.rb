a123347c22a32b46d605f128f7d1a8da1ab50d3a
Sam Seed <sam.seed@softwire.com>
2023-11-02 13:24:46 +0000

    CLDC-2862: confirm cookie banner visible regardless of MM

--- spec/requests/maintenance_controller_spec.rb
+++ spec/requests/maintenance_controller_spec.rb
@@ -14,19 +14,34 @@ RSpec.describe MaintenanceController, type: :request do
     end
 
     context "when a user visits a page other than the maintenance page" do
-      it "redirects the user to the maintenance page" do
+      before do
         get "/lettings-logs"
+      end
+
+      it "redirects the user to the maintenance page" do
         expect(response).to redirect_to(service_unavailable_path)
         follow_redirect!
         expect(page).to have_content("Sorry, the service is unavailable")
       end
+
+      it "the cookie banner is visible" do
+        follow_redirect!
+        expect(page).to have_content("We’d like to use analytics cookies so we can understand how you use the service and make improvements.")
+      end
     end
 
     context "when a user visits the maintenance page" do
-      it "keeps the user on the maintenance page" do
+      before do
         get "/service-unavailable"
+      end
+
+      it "keeps the user on the maintenance page" do
         expect(response).not_to redirect_to(service_unavailable_path)
         expect(page).to have_content("Sorry, the service is unavailable")
+      end
+
+      it "the cookie banner is visible" do
+        expect(page).to have_content("We’d like to use analytics cookies so we can understand how you use the service and make improvements.")
       end
     end
   end
@@ -37,17 +52,33 @@ RSpec.describe MaintenanceController, type: :request do
     end
 
     context "when a user visits a page other than the maintenance page" do
-      it "doesn't redirect the user to the maintenance page" do
+      before do
         get "/lettings-logs"
+      end
+
+      it "doesn't redirect the user to the maintenance page" do
         expect(response).not_to redirect_to(service_unavailable_path)
         expect(page).to have_content("Create a new lettings log")
+      end
+
+      it "the cookie banner is visible" do
+        expect(page).to have_content("We’d like to use analytics cookies so we can understand how you use the service and make improvements.")
       end
     end
 
     context "when a user visits the maintenance page" do
-      it "redirects the user to the start page" do
+      before do
         get "/service-unavailable"
+      end
+
+      it "redirects the user to the start page" do
         expect(response).to redirect_to(root_path)
+      end
+
+      it "the cookie banner is visible" do
+        follow_redirect!
+        follow_redirect!
+        expect(page).to have_content("We’d like to use analytics cookies so we can understand how you use the service and make improvements.")
       end
     end
   end
