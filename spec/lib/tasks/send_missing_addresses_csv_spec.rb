bf4ac0b022e20fdb57d6d6cecbff6f4e79f19cfd
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-11-06 14:37:24 +0000

    feat: timecop around tests

--- spec/lib/tasks/send_missing_addresses_csv_spec.rb
+++ spec/lib/tasks/send_missing_addresses_csv_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe "correct_addresses" do
   describe ":send_missing_addresses_lettings_csv", type: :task do
     subject(:task) { Rake::Task["correct_addresses:send_missing_addresses_lettings_csv"] }
 
+    around do |example|
+      Timecop.freeze(Time.zone.local(2023, 10, 10)) do
+        Singleton.__init__(FormHandler)
+        example.run
+      end
+    end
+
     before do
-      Timecop.travel(Time.zone.local(2023, 10, 10))
-      Singleton.__init__(FormHandler)
       organisation.users.destroy_all
       Rake.application.rake_require("tasks/send_missing_addresses_csv")
       Rake::Task.define_task(:environment)
