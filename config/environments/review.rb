ad6b86187febdbe3d046d899cc558323e783d9ba
Rachael Booth <rachael.booth@softwire.com>
2023-11-08 12:43:35 +0000

    Add action mailer relative root config for review envs

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: ENV["APP_HOST"] }
+  config.action_mailer.default_url_options = { host: ENV["APP_HOST"], :script_name => Rails.application.config.relative_url_root }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
