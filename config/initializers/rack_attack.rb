3237286087c0a69f1e06b33a826291e723a3a67e
Rachael Booth <rachael.booth@softwire.com>
2023-11-08 16:05:28 +0000

    Rack attack path

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -17,7 +17,7 @@ else
 end
 
 Rack::Attack.throttle("password reset requests", limit: 5, period: 60.seconds) do |request|
-  if request.params["user"].present? && request.path == "/account/password" && request.post?
+  if request.params["user"].present? && request.path == user_password_path && request.post?
     request.params["user"]["email"].to_s.downcase.gsub(/\s+/, "")
   end
 end
