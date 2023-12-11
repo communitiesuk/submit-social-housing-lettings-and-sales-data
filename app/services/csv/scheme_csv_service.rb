d25fa8ded3a210d950db2acfa2a06f23af61dd36
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2023-12-11 11:16:31 +0000

    feat: show correct headers in csv

--- app/services/csv/scheme_csv_service.rb
+++ app/services/csv/scheme_csv_service.rb
@@ -19,28 +19,7 @@ module Csv
 
   private
 
-    CUSTOM_CALL_CHAINS = {
-      scheme_owning_organisation_name: %i[owning_organisation name],
-    }.freeze
-
-    SYSTEM_DATE_FIELDS = %w[
-      created_at
-    ].freeze
-
-    def value(attribute, scheme)
-      if attribute == "scheme_active_dates"
-        scheme_availability(scheme)
-      elsif CUSTOM_CALL_CHAINS.key? attribute.to_sym
-        call_chain = CUSTOM_CALL_CHAINS[attribute.to_sym]
-        call_chain.reduce(scheme) { |object, next_call| object&.public_send(next_call) }
-      elsif SYSTEM_DATE_FIELDS.include? attribute
-        scheme.public_send(attribute)&.iso8601
-      else
-        scheme.public_send(attribute)
-      end
-    end
-
-    ATTRIBUTE_MAPPINGS = {
+    FIELD_FROM_ATTRIBUTE = {
       "scheme_code" => %w[id_to_display],
       "scheme_service_name" => %w[service_name],
       "scheme_status" => %w[status],
@@ -55,11 +34,31 @@ module Csv
       "scheme_created_at" => %w[created_at],
     }.freeze
 
-    def scheme_attributes
-      attributes = %w[scheme_code scheme_service_name scheme_status scheme_sensitive scheme_type scheme_registered_under_care_act scheme_owning_organisation_name scheme_support_services_provided_by scheme_primary_client_group scheme_has_other_client_group scheme_secondary_client_group scheme_support_type scheme_intended_stay scheme_created_at scheme_active_dates]
-      attributes.flat_map do |attribute|
-        ATTRIBUTE_MAPPINGS.fetch(attribute, attribute)
+    CUSTOM_CALL_CHAINS = {
+      scheme_owning_organisation_name: %i[owning_organisation name],
+    }.freeze
+
+    SYSTEM_DATE_FIELDS = %w[
+      created_at
+    ].freeze
+
+    def value(attribute, scheme)
+      attribute = FIELD_FROM_ATTRIBUTE.fetch(attribute, attribute)
+      if attribute == "scheme_active_dates"
+        scheme_availability(scheme)
+      elsif CUSTOM_CALL_CHAINS.key? attribute.to_sym
+        call_chain = CUSTOM_CALL_CHAINS[attribute.to_sym]
+        call_chain.reduce(scheme) { |object, next_call| object&.public_send(next_call) }
+      elsif SYSTEM_DATE_FIELDS.include? attribute
+        scheme.public_send(attribute)&.iso8601
+      else
+        scheme.public_send(attribute)
       end
+    end
+
+
+    def scheme_attributes
+      %w[scheme_code scheme_service_name scheme_status scheme_sensitive scheme_type scheme_registered_under_care_act scheme_owning_organisation_name scheme_support_services_provided_by scheme_primary_client_group scheme_has_other_client_group scheme_secondary_client_group scheme_support_type scheme_intended_stay scheme_created_at scheme_active_dates]
     end
   end
 end
