19105f4d91667ff7d0f63a29b0382c8003b67d2f
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-03 14:01:34 +0000

    feat: add flex styling to match designs

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,17 @@
 module HomeHelper
-
-  def in_progress_count(user, type)
-    case type
-    when "lettings" then user.lettings_logs.in_progress.count
-    when "sales" then user.sales_logs.in_progress.count
-    when "schemes" then user.schemes.incomplete.count
+  def data_count(user, type)
+    if user.data_provider?
+      case type
+      when "lettings" then user.lettings_logs.in_progress.where(created_by: user).count
+      when "sales" then user.sales_logs.in_progress.where(created_by: user).count
+      when "misc" then user.lettings_logs.completed.where(created_by: user).count
+      end
+    else
+      case type
+      when "lettings" then user.lettings_logs.in_progress.count
+      when "sales" then user.sales_logs.in_progress.count
+      when "schemes" then user.schemes.incomplete.count
+      end
     end
   end
 
@@ -16,26 +23,47 @@ module HomeHelper
     end
   end
 
-  def in_progress_subheading(user, type)
-    if type == "schemes"
-      return"Incomplete schemes"
-    end
-    "#{user.role == "data_provider" ? :"Your " : nil}#{type} in progress".capitalize
-  end
-
-  def in_progress_path(type)
+  def data_subheading(user, type)
     case type
-    when "lettings" then lettings_logs_path(status: [:in_progress])
-    when "sales" then sales_logs_path(status: [:in_progress])
-    when "schemes" then schemes_path(status: [:incomplete])
+    when "schemes"
+      "Incomplete schemes"
+    when "misc"
+      "Your completed lettings"
+    else
+      "#{user.role == 'data_provider' ? :"Your " : nil}#{type} in progress".capitalize
     end
   end
 
-  def clear_filter_path_for_type(type)
+  def data_path(user, type)
+    if user.data_provider?
+      case type
+      when "lettings" then lettings_logs_path(status: [:in_progress], assigned_to: "you")
+      when "sales" then sales_logs_path(status: [:in_progress], assigned_to: "you")
+      when "misc" then lettings_logs_path(status: [:completed], assigned_to: "you")
+      end
+    else
+      case type
+      when "lettings" then lettings_logs_path(status: [:in_progress])
+      when "sales" then sales_logs_path(status: [:in_progress])
+      when "schemes" then schemes_path(status: [:incomplete])
+      end
+    end
+  end
+
+  def view_all_path(type)
     case type
     when "lettings" then clear_filters_path(filter_type: "lettings_logs")
     when "sales" then clear_filters_path(filter_type: "sales_logs")
     when "schemes" then clear_filters_path(filter_type: "schemes")
+    when "misc" then clear_filters_path(filter_type: "schemes")
+    end
+  end
+
+  def view_all_text(type)
+    if type == "misc"
+      "View all schemes"
+    else
+      "View all #{type}"
     end
   end
 end
