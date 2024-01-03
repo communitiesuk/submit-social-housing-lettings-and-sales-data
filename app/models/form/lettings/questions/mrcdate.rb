4db556cef689fa5663bea906552d12d1e178f462
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2024-01-03 12:51:01 +0000

    Fix lettings log accessor in date question (#2117)
    * Fix lettings log accessor in date question
    
    * Remove hardcoded date example from mrcdate question (#2118)
    
    ---------
    
    Co-authored-by: Rachael Booth <Rachael.Booth@softwire.com>

--- app/models/form/lettings/questions/mrcdate.rb
+++ app/models/form/lettings/questions/mrcdate.rb
@@ -6,7 +6,6 @@ class Form::Lettings::Questions::Mrcdate < ::Form::Question
     @header = "When were the repairs completed?"
     @type = "date"
     @check_answers_card_number = 0
-    @hint_text = "For example, 27 3 2021."
     @question_number = 24
   end
 end
