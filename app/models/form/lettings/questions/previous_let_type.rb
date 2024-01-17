77f3ae58d39e339d036e205316c8803e055646b1
Kat <katrina@kosiak.co.uk>
2024-01-17 10:05:48 +0000

    Amend previous let type answer options and hint

--- app/models/form/lettings/questions/previous_let_type.rb
+++ app/models/form/lettings/questions/previous_let_type.rb
@@ -6,8 +6,8 @@ class Form::Lettings::Questions::PreviousLetType < ::Form::Question
     @header = "What type was the property most recently let as?"
     @type = "radio"
     @check_answers_card_number = 0
-    @hint_text = ""
-    @answer_options = ANSWER_OPTIONS
+    @hint_text = form.start_year_after_2024? ? "This is the rent type of the previous tenancy in this property." : ""
+    @answer_options = form.start_year_after_2024? ? ANSWER_OPTIONS_2024 : ANSWER_OPTIONS
     @question_number = 16
   end
 
@@ -17,6 +17,17 @@ class Form::Lettings::Questions::PreviousLetType < ::Form::Question
     "5" => { "value" => "A London Affordable Rent basis" },
     "6" => { "value" => "A Rent to Buy basis" },
     "7" => { "value" => "A London Living Rent basis" },
+    "8" => { "value" => "Another Intermediate Rent basis" },
+    "divider" => { "value" => true },
+    "3" => { "value" => "Don’t know" },
+  }.freeze
+
+  ANSWER_OPTIONS_2024 = {
+    "1" => { "value" => "Social rent basis" },
+    "2" => { "value" => "Affordable rent basis" },
+    "5" => { "value" => "London Affordable Rent basis" },
+    "6" => { "value" => "Rent to Buy basis" },
+    "7" => { "value" => "London Living Rent basis" },
     "8" => { "value" => "Another Intermediate Rent basis" },
     "divider" => { "value" => true },
     "3" => { "value" => "Don’t know" },
--- spec/models/form/lettings/questions/previous_let_type_spec.rb
+++ spec/models/form/lettings/questions/previous_let_type_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe Form::Lettings::Questions::PreviousLetType, type: :model do
   subject(:question) { described_class.new(nil, nil, page) }
 
   let(:page) { instance_double(Form::Page) }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form) }
+
+  before do
+    allow(form).to receive(:start_year_after_2024?).and_return(false)
+    allow(page).to receive(:subsection).and_return(subsection)
+    allow(subsection).to receive(:form).and_return(form)
+  end
 
   it "has correct page" do
     expect(question.page).to eq page
@@ -44,5 +52,28 @@ RSpec.describe Form::Lettings::Questions::PreviousLetType, type: :model do
       "divider" => { "value" => true },
       "3" => { "value" => "Don’t know" },
     })
+  end
+
+  context "with collection year on or after 2024" do
+    before do
+      allow(form).to receive(:start_year_after_2024?).and_return(true)
+    end
+
+    it "has the correct answer options" do
+      expect(question.answer_options).to eq({
+        "1" => { "value" => "Social rent basis" },
+        "2" => { "value" => "Affordable rent basis" },
+        "5" => { "value" => "London Affordable Rent basis" },
+        "6" => { "value" => "Rent to Buy basis" },
+        "7" => { "value" => "London Living Rent basis" },
+        "8" => { "value" => "Another Intermediate Rent basis" },
+        "divider" => { "value" => true },
+        "3" => { "value" => "Don’t know" },
+      })
+    end
+
+    it "has the correct hint_text" do
+      expect(question.hint_text).to eq("This is the rent type of the previous tenancy in this property.")
+    end
   end
 end
