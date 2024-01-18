fc9be44ad8c1fc616d34d201a95151843d65ab74
Kat <katrina@kosiak.co.uk>
2024-01-18 08:34:08 +0000

    Show the absorbed sales logs

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -134,7 +134,7 @@ class OrganisationsController < ApplicationController
   end
 
   def sales_logs
-    organisation_logs = SalesLog.visible.filter_by_organisation(@organisation)
+    organisation_logs = SalesLog.visible.filter_by_organisation(@organisation.absorbed_organisations + [@organisation])
     unpaginated_filtered_logs = filter_manager.filtered_logs(organisation_logs, search_term, session_filters)
 
     respond_to do |format|
