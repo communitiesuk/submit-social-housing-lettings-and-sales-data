ae5a703ded66979346322730019355bc5956c866
Kat <katrina@kosiak.co.uk>
2024-01-19 10:24:19 +0000

    Remove archived form if the year after it is no longer editable

--- app/models/form_handler.rb
+++ app/models/form_handler.rb
@@ -43,6 +43,10 @@ class FormHandler
       "next_sales" => Form.new(nil, next_collection_start_year, SALES_SECTIONS, "sales"),
       "archived_sales" => Form.new(nil, previous_collection_start_year - 1, SALES_SECTIONS, "sales"),
     }
+    if @sales_forms.count { |_name, form| Time.zone.now.between?(form.start_date, form.edit_end_date) } == 1
+      @sales_forms.delete("archived_sales")
+    end
+    @sales_forms
   end
 
   def ordered_sales_questions_for_all_years
@@ -99,6 +103,10 @@ class FormHandler
     end
     forms["current_lettings"] = Form.new(nil, current_collection_start_year, LETTINGS_SECTIONS, "lettings") if forms["current_lettings"].blank?
     forms["next_lettings"] = Form.new(nil, next_collection_start_year, LETTINGS_SECTIONS, "lettings") if forms["next_lettings"].blank?
+
+    if forms.count { |_name, form| Time.zone.now.between?(form.start_date, form.edit_end_date) } == 1
+      forms.delete("archived_lettings")
+    end
 
     if Rails.env.test?
       forms.merge({ fake_lettings_2021: Form.new("spec/fixtures/forms/2021_2022.json"), real_lettings_2021: Form.new("config/forms/2021_2022.json") })
--- spec/services/csv/lettings_log_csv_service_spec.rb
+++ spec/services/csv/lettings_log_csv_service_spec.rb
@@ -1,44 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Csv::LettingsLogCsvService do
-  let(:form_handler_mock) { instance_double(FormHandler) }
-  let(:organisation) { create(:organisation) }
-  let(:fixed_time) { Time.zone.local(2023, 6, 26) }
-  let(:log) do
-    create(
-      :lettings_log,
-      :completed,
-      startdate: fixed_time,
-      created_at: fixed_time,
-      updated_at: fixed_time,
-      mrcdate: fixed_time - 1.day,
-      voiddate: fixed_time - 2.days,
-      propcode: "ABCDEFG",
-      tenancycode: "HIJKLMN",
-      postcode_full: "NW9 5LL",
-      ppostcode_full: "TN23 6LZ",
-      created_by: user,
-      managing_organisation: organisation,
-      hhmemb: 4,
-      details_known_3: 1,
-      details_known_4: 0,
-      sex4: "R",
-      ecstat4: 10,
-      relat4: "R",
-      age4_known: 1,
-      incref: 0,
-    )
-  end
-  let(:user) { create(:user, :support, email: "s.port@jeemayle.com") }
-  let(:service) { described_class.new(user:, export_type:) }
-  let(:export_type) { "labels" }
-  let(:csv) { CSV.parse(service.prepare_csv(LettingsLog.where(id: logs.map(&:id)))) }
-  let(:logs) { [log] }
-  let(:headers) { csv.first }
-
   before do
     Timecop.freeze(fixed_time)
     Singleton.__init__(FormHandler)
+    FormHandler.instance.use_real_forms!
     log.irproduct = 1
     log.save!(validate: false)
   end
@@ -47,180 +13,157 @@ RSpec.describe Csv::LettingsLogCsvService do
     Timecop.return
   end
 
-  it "calls the form handler to get all questions in order when initialized" do
-    allow(FormHandler).to receive(:instance).and_return(form_handler_mock)
-    allow(form_handler_mock).to receive(:ordered_lettings_questions_for_all_years).and_return([])
-    service
-    expect(form_handler_mock).to have_received(:ordered_lettings_questions_for_all_years)
-  end
-
-  it "returns a string" do
-    result = service.prepare_csv(LettingsLog.all)
-    expect(result).to be_a String
-  end
-
-  it "returns a csv with headers" do
-    expect(csv.first.first).to eq "id"
-  end
-
-  context "when stubbing :ordered_lettings_questions_for_all_years" do
-    let(:lettings_form) do
-      FormFactory.new(year: 2050, type: "lettings")
-                 .with_sections([build(:section, :with_questions, question_ids:, questions:)])
-                 .build
+  context "when downloading a csv" do
+    let(:form_handler_mock) { instance_double(FormHandler) }
+    let(:organisation) { create(:organisation) }
+    let(:fixed_time) { Time.zone.local(2023, 11, 26) }
+    let(:log) do
+      create(
+        :lettings_log,
+        :completed,
+        startdate: fixed_time,
+        created_at: fixed_time,
+        updated_at: fixed_time,
+        mrcdate: fixed_time - 1.day,
+        voiddate: fixed_time - 2.days,
+        propcode: "ABCDEFG",
+        tenancycode: "HIJKLMN",
+        postcode_full: "NW9 5LL",
+        ppostcode_full: "TN23 6LZ",
+        created_by: user,
+        managing_organisation: organisation,
+        hhmemb: 4,
+        details_known_3: 1,
+        details_known_4: 0,
+        sex4: "R",
+        ecstat4: 10,
+        relat4: "R",
+        age4_known: 1,
+        incref: 0,
+      )
     end
-    let(:question_ids) { nil }
-    let(:questions) { nil }
-
-    before do
-      allow(FormHandler).to receive(:instance).and_return(form_handler_mock)
-      allow(form_handler_mock).to receive(:form_name_from_start_year)
-      allow(form_handler_mock).to receive(:get_form).and_return(lettings_form)
-      allow(form_handler_mock).to receive(:ordered_lettings_questions_for_all_years).and_return(lettings_form.questions)
-      allow(form_handler_mock).to receive(:lettings_in_crossover_period?).and_return false
-    end
-
-    context "when it returns questions with particular ids" do
-      let(:question_ids) { %w[prevten startdate brent rent_type] }
-
-      it "includes log attributes related to questions to the headers" do
-        expect(headers).to include(*question_ids.first(3))
-      end
-
-      it "removes some log attributes related to questions from the headers and replaces them with their derived values in the correct order" do
-        expect(headers).not_to include "rent_type"
-        expect(headers).to include(*%w[wrent renttype rent_type_detail])
-      end
-    end
-
-    context "when it returns checkbox questions" do
-      let(:questions) do
-        [
-          build(:question, id: "condition_effects", type: "checkbox", answer_options: { "illness_type_1" => {}, "illness_type_2" => {}, "illness_type_3" => {} }),
-          build(:question, id: "letting_allocation", type: "checkbox", answer_options: { "cbl" => {}, "cap" => {}, "chr" => {} }),
-        ]
-      end
-
-      it "does not add the id of the checkbox question to the headers" do
-        question_ids = questions.map(&:id)
-        expect(headers).not_to include(*question_ids)
-      end
-
-      it "adds the related log attributes from the answer options to the headers" do
-        log_attributes = questions.flat_map { |q| q.answer_options.keys }
-        expect(headers).to include(*log_attributes)
-      end
-    end
-  end
-
-  it "adds log attributes not related to questions to the headers" do
-    expect(headers.first(5)).to eq %w[id status created_by is_dpo created_at]
-  end
-
-  it "adds attributes related to associated schemes and locations to the headers" do
-    expect(headers).to include(*%w[scheme_service_name scheme_sensitive SCHTYPE scheme_registered_under_care_act])
-    expect(headers.last(5)).to eq %w[location_units location_type_of_unit location_mobility_type location_local_authority location_startdate]
-  end
-
-  context "when there are many logs" do
-    let(:logs) { create_list(:lettings_log, log_count) }
-    let(:log_count) { 30 }
-
-    it "creates a CSV with the correct number of logs" do
-      expected_row_count_with_headers = log_count + 1
-      expect(csv.size).to be expected_row_count_with_headers
-    end
-  end
-
-  context "when exporting with human readable labels" do
+    let(:user) { create(:user, :support, email: "s.port@jeemayle.com") }
+    let(:service) { described_class.new(user:, export_type:) }
     let(:export_type) { "labels" }
+    let(:csv) { CSV.parse(service.prepare_csv(LettingsLog.where(id: logs.map(&:id)))) }
+    let(:logs) { [log] }
+    let(:headers) { csv.first }
 
-    it "gives answer to radio questions as labels" do
-      relat2_column_index = csv.first.index("relat2")
-      relat2_value = csv.second[relat2_column_index]
-      expect(relat2_value).to eq "Partner"
+    it "calls the form handler to get all questions in order when initialized" do
+      allow(FormHandler).to receive(:instance).and_return(form_handler_mock)
+      allow(form_handler_mock).to receive(:ordered_lettings_questions_for_all_years).and_return([])
+      service
+      expect(form_handler_mock).to have_received(:ordered_lettings_questions_for_all_years)
     end
 
-    it "gives answers to free input questions as the user input" do
-      age1_column_index = csv.first.index("age1")
-      age1_value = csv.second[age1_column_index]
-      expect(age1_value).to eq 35.to_s
+    it "returns a string" do
+      result = service.prepare_csv(LettingsLog.all)
+      expect(result).to be_a String
     end
 
-    it "exports the code for the local authority under the heading 'la'" do
-      la_column_index = csv.first.index("la")
-      la_value = csv.second[la_column_index]
-      expect(la_value).to eq "E09000003"
+    it "returns a csv with headers" do
+      expect(csv.first.first).to eq "id"
     end
 
-    it "exports the label for the local authority under the heading 'la_label'" do
-      la_label_column_index = csv.first.index("la_label")
-      la_label_value = csv.second[la_label_column_index]
-      expect(la_label_value).to eq "Barnet"
-    end
-
-    it "exports the CSV with all values correct" do
-      expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_labels.csv")
-      values_to_delete = %w[id vacdays]
-      values_to_delete.each do |attribute|
-        index = csv.first.index(attribute)
-        csv.second[index] = nil
+    context "when stubbing :ordered_lettings_questions_for_all_years" do
+      let(:lettings_form) do
+        FormFactory.new(year: 2050, type: "lettings")
+                  .with_sections([build(:section, :with_questions, question_ids:, questions:)])
+                  .build
       end
-      expect(csv).to eq expected_content
-    end
-  end
+      let(:question_ids) { nil }
+      let(:questions) { nil }
 
-  context "when exporting as codes" do
-    let(:export_type) { "codes" }
-
-    it "gives answer to radio questions as labels" do
-      relat2_column_index = csv.first.index("relat2")
-      relat2_value = csv.second[relat2_column_index]
-      expect(relat2_value).to eq "P"
-    end
-
-    it "gives answers to free input questions as the user input" do
-      age1_column_index = csv.first.index("age1")
-      age1_value = csv.second[age1_column_index]
-      expect(age1_value).to eq 35.to_s
-    end
-
-    it "exports the code for the local authority under the heading 'la'" do
-      la_column_index = csv.first.index("la")
-      la_value = csv.second[la_column_index]
-      expect(la_value).to eq "E09000003"
-    end
-
-    it "exports the label for the local authority under the heading 'la_label'" do
-      la_label_column_index = csv.first.index("la_label")
-      la_label_value = csv.second[la_label_column_index]
-      expect(la_label_value).to eq "Barnet"
-    end
-
-    it "exports the CSV with all values correct" do
-      expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_codes.csv")
-      values_to_delete = %w[id vacdays]
-      values_to_delete.each do |attribute|
-        index = csv.first.index(attribute)
-        csv.second[index] = nil
+      before do
+        allow(FormHandler).to receive(:instance).and_return(form_handler_mock)
+        allow(form_handler_mock).to receive(:form_name_from_start_year)
+        allow(form_handler_mock).to receive(:get_form).and_return(lettings_form)
+        allow(form_handler_mock).to receive(:ordered_lettings_questions_for_all_years).and_return(lettings_form.questions)
+        allow(form_handler_mock).to receive(:lettings_in_crossover_period?).and_return false
       end
-      expect(csv).to eq expected_content
+
+      context "when it returns questions with particular ids" do
+        let(:question_ids) { %w[prevten startdate brent rent_type] }
+
+        it "includes log attributes related to questions to the headers" do
+          expect(headers).to include(*question_ids.first(3))
+        end
+
+        it "removes some log attributes related to questions from the headers and replaces them with their derived values in the correct order" do
+          expect(headers).not_to include "rent_type"
+          expect(headers).to include(*%w[wrent renttype rent_type_detail])
+        end
+      end
+
+      context "when it returns checkbox questions" do
+        let(:questions) do
+          [
+            build(:question, id: "condition_effects", type: "checkbox", answer_options: { "illness_type_1" => {}, "illness_type_2" => {}, "illness_type_3" => {} }),
+            build(:question, id: "letting_allocation", type: "checkbox", answer_options: { "cbl" => {}, "cap" => {}, "chr" => {} }),
+          ]
+        end
+
+        it "does not add the id of the checkbox question to the headers" do
+          question_ids = questions.map(&:id)
+          expect(headers).not_to include(*question_ids)
+        end
+
+        it "adds the related log attributes from the answer options to the headers" do
+          log_attributes = questions.flat_map { |q| q.answer_options.keys }
+          expect(headers).to include(*log_attributes)
+        end
+      end
     end
-  end
 
-  context "when the user is not a support user" do
-    let(:user) { create(:user, email: "choreographer@owtluk.com") }
-
-    it "does not include certain attributes in the headers" do
-      expect(headers).not_to include(*%w[wrent wscharge wpschrge wsupchrg wtcharge])
+    it "adds log attributes not related to questions to the headers" do
+      expect(headers.first(5)).to eq %w[id status created_by is_dpo created_at]
     end
 
-    context "and exporting with labels" do
+    it "adds attributes related to associated schemes and locations to the headers" do
+      expect(headers).to include(*%w[scheme_service_name scheme_sensitive SCHTYPE scheme_registered_under_care_act])
+      expect(headers.last(5)).to eq %w[location_units location_type_of_unit location_mobility_type location_local_authority location_startdate]
+    end
+
+    context "when there are many logs" do
+      let(:logs) { create_list(:lettings_log, log_count) }
+      let(:log_count) { 30 }
+
+      it "creates a CSV with the correct number of logs" do
+        expected_row_count_with_headers = log_count + 1
+        expect(csv.size).to be expected_row_count_with_headers
+      end
+    end
+
+    context "when exporting with human readable labels" do
       let(:export_type) { "labels" }
 
+      it "gives answer to radio questions as labels" do
+        relat2_column_index = csv.first.index("relat2")
+        relat2_value = csv.second[relat2_column_index]
+        expect(relat2_value).to eq "Partner"
+      end
+
+      it "gives answers to free input questions as the user input" do
+        age1_column_index = csv.first.index("age1")
+        age1_value = csv.second[age1_column_index]
+        expect(age1_value).to eq 35.to_s
+      end
+
+      it "exports the code for the local authority under the heading 'la'" do
+        la_column_index = csv.first.index("la")
+        la_value = csv.second[la_column_index]
+        expect(la_value).to eq "E09000003"
+      end
+
+      it "exports the label for the local authority under the heading 'la_label'" do
+        la_label_column_index = csv.first.index("la_label")
+        la_label_value = csv.second[la_label_column_index]
+        expect(la_label_value).to eq "Barnet"
+      end
+
       it "exports the CSV with all values correct" do
-        expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_non_support_labels.csv")
-        values_to_delete = %w[id]
+        expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_labels.csv")
+        values_to_delete = %w[id vacdays]
         values_to_delete.each do |attribute|
           index = csv.first.index(attribute)
           csv.second[index] = nil
@@ -229,17 +172,77 @@ RSpec.describe Csv::LettingsLogCsvService do
       end
     end
 
-    context "and exporting values as codes" do
+    context "when exporting as codes" do
       let(:export_type) { "codes" }
 
+      it "gives answer to radio questions as labels" do
+        relat2_column_index = csv.first.index("relat2")
+        relat2_value = csv.second[relat2_column_index]
+        expect(relat2_value).to eq "P"
+      end
+
+      it "gives answers to free input questions as the user input" do
+        age1_column_index = csv.first.index("age1")
+        age1_value = csv.second[age1_column_index]
+        expect(age1_value).to eq 35.to_s
+      end
+
+      it "exports the code for the local authority under the heading 'la'" do
+        la_column_index = csv.first.index("la")
+        la_value = csv.second[la_column_index]
+        expect(la_value).to eq "E09000003"
+      end
+
+      it "exports the label for the local authority under the heading 'la_label'" do
+        la_label_column_index = csv.first.index("la_label")
+        la_label_value = csv.second[la_label_column_index]
+        expect(la_label_value).to eq "Barnet"
+      end
+
       it "exports the CSV with all values correct" do
-        expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_non_support_codes.csv")
-        values_to_delete = %w[id]
+        expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_codes.csv")
+        values_to_delete = %w[id vacdays]
         values_to_delete.each do |attribute|
           index = csv.first.index(attribute)
           csv.second[index] = nil
         end
         expect(csv).to eq expected_content
+      end
+    end
+
+    context "when the user is not a support user" do
+      let(:user) { create(:user, email: "choreographer@owtluk.com") }
+
+      it "does not include certain attributes in the headers" do
+        expect(headers).not_to include(*%w[wrent wscharge wpschrge wsupchrg wtcharge])
+      end
+
+      context "and exporting with labels" do
+        let(:export_type) { "labels" }
+
+        it "exports the CSV with all values correct" do
+          expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_non_support_labels.csv")
+          values_to_delete = %w[id]
+          values_to_delete.each do |attribute|
+            index = csv.first.index(attribute)
+            csv.second[index] = nil
+          end
+          expect(csv).to eq expected_content
+        end
+      end
+
+      context "and exporting values as codes" do
+        let(:export_type) { "codes" }
+
+        it "exports the CSV with all values correct" do
+          expected_content = CSV.read("spec/fixtures/files/lettings_log_csv_export_non_support_codes.csv")
+          values_to_delete = %w[id]
+          values_to_delete.each do |attribute|
+            index = csv.first.index(attribute)
+            csv.second[index] = nil
+          end
+          expect(csv).to eq expected_content
+        end
       end
     end
   end
