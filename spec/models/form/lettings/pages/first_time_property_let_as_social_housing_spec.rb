dc544033123680ed2a03128e50498a1f4b2346ce
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-24 14:00:12 +0000

    feat: make change for 2024/25 logs

---
+++ spec/models/form/lettings/pages/first_time_property_let_as_social_housing_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Pages::FirstTimePropertyLetAsSocialHousing, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form) }
+
+  before do
+    allow(form).to receive(:start_year_after_2024?).and_return(false)
+    allow(subsection).to receive(:form).and_return(form)
+  end
+
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[first_time_property_let_as_social_housing])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("first_time_property_let_as_social_housing")
+  end
+
+  it "has the correct header" do
+    expect(page.header).to be_nil
+  end
+
+  it "has the correct description" do
+    expect(page.description).to be_nil
+  end
+
+  it "has the correct depends_on" do
+    expect(page.depends_on).to eq([{ "is_renewal?" => false }])
+  end
+end
