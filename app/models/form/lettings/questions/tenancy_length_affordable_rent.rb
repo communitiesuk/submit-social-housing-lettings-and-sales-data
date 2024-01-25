2d042957516a01bf5004638b58d9929660c99224
Kat <katrina@kosiak.co.uk>
2024-01-25 11:38:30 +0000

    Update tenancy lenght hint text

--- app/models/form/lettings/questions/tenancy_length_affordable_rent.rb
+++ app/models/form/lettings/questions/tenancy_length_affordable_rent.rb
@@ -9,8 +9,15 @@ class Form::Lettings::Questions::TenancyLengthAffordableRent < ::Form::Question
     @check_answers_card_number = 0
     @max = 150
     @min = 0
-    @hint_text = "Do not include the starter or introductory period.</br>The minimum period is 2 years for social or affordable rent general needs logs and you do not need a log for shorter tenancies."
     @step = 1
     @question_number = 28
+  end
+
+  def hint_text
+    if form.start_year_after_2024?
+      "Do not include the starter or introductory period.</br>The minimum period is 2 years for social or affordable rent general needs logs. You do not need to submit CORE logs for these types of tenancies if they are shorter than 2 years."
+    else
+      "Do not include the starter or introductory period.</br>The minimum period is 2 years for social or affordable rent general needs logs and you do not need a log for shorter tenancies."
+    end
   end
 end
--- app/models/form/lettings/questions/tenancy_length_intermediate_rent.rb
+++ app/models/form/lettings/questions/tenancy_length_intermediate_rent.rb
@@ -9,8 +9,15 @@ class Form::Lettings::Questions::TenancyLengthIntermediateRent < ::Form::Questio
     @check_answers_card_number = 0
     @max = 150
     @min = 0
-    @hint_text = "Do not include the starter or introductory period.</br>The minimum period is 1 year for intermediate rent general needs logs and you do not need a log for shorter tenancies."
     @step = 1
     @question_number = 28
+  end
+
+  def hint_text
+    if form.start_year_after_2024?
+      "Do not include the starter or introductory period.</br>The minimum period is 1 year for intermediate rent general needs logs. You do not need to submit CORE logs for these types of tenancies if they are shorter than 1 year."
+    else
+      "Do not include the starter or introductory period.</br>The minimum period is 1 year for intermediate rent general needs logs and you do not need a log for shorter tenancies."
+    end
   end
 end
