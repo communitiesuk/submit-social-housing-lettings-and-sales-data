ea0fc713a4e0720b970a6912e0c0b5d41d5c2ff9
natdeanlewissoftwire <94526761+natdeanlewissoftwire@users.noreply.github.com>
2024-01-26 09:08:45 +0000

    CLDC-3130 Update first let question for 24/25 (#2160)
    * feat: make change for 2024/25 logs
    
    * refactor: lint
    
    * refactor: simplify

--- app/models/form/lettings/questions/first_time_property_let_as_social_housing.rb
+++ app/models/form/lettings/questions/first_time_property_let_as_social_housing.rb
@@ -7,12 +7,21 @@ class Form::Lettings::Questions::FirstTimePropertyLetAsSocialHousing < ::Form::Q
     @type = "radio"
     @check_answers_card_number = 0
     @hint_text = ""
-    @answer_options = ANSWER_OPTIONS
     @question_number = 14
   end
 
-  ANSWER_OPTIONS = {
-    "1" => { "value" => "Yes", "hint" => "This is a new let." },
-    "0" => { "value" => "No", "hint" => "This is a re-let of existing social housing." },
-  }.freeze
+  def answer_options
+    {
+      "1" => { "value" => "Yes", "hint" => yes_hint },
+      "0" => { "value" => "No", "hint" => no_hint },
+    }.freeze
+  end
+
+  def yes_hint
+    form.start_year_after_2024? ? "This property was previously used for other purposes or is a new-build." : "This is a new let."
+  end
+
+  def no_hint
+    form.start_year_after_2024? ? "This is a re-let of existing social housing stock." : "This is a re-let of existing social housing."
+  end
 end
---
+++ spec/models/form/lettings/questions/first_time_property_let_as_social_housing_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Questions::FirstTimePropertyLetAsSocialHousing, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form) }
+
+  before do
+    allow(form).to receive(:start_year_after_2024?).and_return(false)
+    allow(page).to receive(:subsection).and_return(subsection)
+    allow(subsection).to receive(:form).and_return(form)
+  end
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("first_time_property_let_as_social_housing")
+  end
+
+  context "with 2023/24 form" do
+    it "has the correct answer_options" do
+      expect(question.answer_options).to eq({
+        "1" => { "value" => "Yes", "hint" => "This is a new let." },
+        "0" => { "value" => "No", "hint" => "This is a re-let of existing social housing." },
+      })
+    end
+  end
+
+  context "with 2024/25 form" do
+    before do
+      allow(form).to receive(:start_year_after_2024?).and_return(true)
+    end
+
+    it "has the correct answer_options" do
+      expect(question.answer_options).to eq({
+        "1" => { "value" => "Yes", "hint" => "This property was previously used for other purposes or is a new-build." },
+        "0" => { "value" => "No", "hint" => "This is a re-let of existing social housing stock." },
+      })
+    end
+  end
+end
