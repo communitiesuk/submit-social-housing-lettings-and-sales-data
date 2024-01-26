7326168e24595dd21124c1afefa22a65011543d9
Rachael Booth <Rachael.Booth@softwire.com>
2024-01-26 12:34:45 +0000

    CLDC-3138: Update hint text for starter tenancy question for 2024 (#2166)
    * CLDC-3138: Update hint text for starter tenancy question for 2024
    
    * Remove blank line

--- app/models/form/lettings/questions/startertenancy.rb
+++ app/models/form/lettings/questions/startertenancy.rb
@@ -6,10 +6,18 @@ class Form::Lettings::Questions::Startertenancy < ::Form::Question
     @header = "Is this a starter tenancy?"
     @type = "radio"
     @check_answers_card_number = 0
-    @hint_text = "This is also known as an ‘introductory period’."
     @answer_options = ANSWER_OPTIONS
     @question_number = 26
   end
 
   ANSWER_OPTIONS = { "1" => { "value" => "Yes" }, "2" => { "value" => "No" } }.freeze
+
+  def hint_text
+    if form.start_year_after_2024?
+      "If the tenancy has an ‘introductory period’ answer ‘yes’.<br><br>
+       You should submit a CORE log at the beginning of the starter tenancy or introductory period, with the best information you have at the time. You do not need to submit a log when a tenant later rolls onto the main tenancy."
+    else
+      "This is also known as an ‘introductory period’."
+    end
+  end
 end
