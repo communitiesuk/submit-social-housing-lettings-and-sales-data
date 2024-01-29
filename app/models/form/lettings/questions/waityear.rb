e27857b53d64eef93d63e14ff38e5e1cd3b3c3f5
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-29 15:12:37 +0000

    feat: update header and answers, and test

--- app/models/form/lettings/questions/waityear.rb
+++ app/models/form/lettings/questions/waityear.rb
@@ -3,22 +3,38 @@ class Form::Lettings::Questions::Waityear < ::Form::Question
     super
     @id = "waityear"
     @check_answer_label = "Length of time on local authority waiting list"
-    @header = "How long has the household been on the local authority waiting list for the new letting?"
+    @header = form.start_year_after_2024? ? "How long has the household been on the local authority waiting list for the area of the new letting?" : "How long has the household been on the local authority waiting list for the new letting?"
     @type = "radio"
     @check_answers_card_number = 0
     @hint_text = ""
-    @answer_options = ANSWER_OPTIONS
     @question_number = 76
   end
 
-  ANSWER_OPTIONS = {
-    "2" => { "value" => "Less than 1 year" },
-    "7" => { "value" => "1 year but under 2 years" },
-    "8" => { "value" => "2 years but under 3 years" },
-    "9" => { "value" => "3 years but under 4 years" },
-    "10" => { "value" => "4 years but under 5 years" },
-    "5" => { "value" => "5 years or more" },
-    "divider" => { "value" => true },
-    "6" => { "value" => "Don’t know" },
-  }.freeze
+  def answer_options
+    if form.start_year_after_2024?
+      {
+        "2" => { "value" => "Less than 1 year" },
+        "7" => { "value" => "1 year but under 2 years" },
+        "8" => { "value" => "2 years but under 3 years" },
+        "9" => { "value" => "3 years but under 4 years" },
+        "10" => { "value" => "4 years but under 5 years" },
+        "11" => { "value" => "5 years but under 10 years" },
+        "12" => { "value" => "10 years or more" },
+        "divider" => { "value" => true },
+        "6" => { "value" => "Don’t know" },
+      }.freeze
+    else
+      {
+        "2" => { "value" => "Less than 1 year" },
+        "7" => { "value" => "1 year but under 2 years" },
+        "8" => { "value" => "2 years but under 3 years" },
+        "9" => { "value" => "3 years but under 4 years" },
+        "10" => { "value" => "4 years but under 5 years" },
+        "5" => { "value" => "5 years or more" },
+        "divider" => { "value" => true },
+        "6" => { "value" => "Don’t know" },
+      }.freeze
+    end
+  end
+
 end
