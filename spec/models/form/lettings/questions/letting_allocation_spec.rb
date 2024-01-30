591cbd027afb378360a04c2465d7170b8049eba5
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-30 09:29:01 +0000

    refactor: lint

--- spec/models/form/lettings/questions/letting_allocation_spec.rb
+++ spec/models/form/lettings/questions/letting_allocation_spec.rb
@@ -42,12 +42,12 @@ RSpec.describe Form::Lettings::Questions::LettingAllocation, type: :model do
   context "with 2023/24 form" do
     it "has the correct answer_options" do
       expect(question.answer_options).to eq({
-                                              "cbl" => { "value" => "Choice-based lettings (CBL)" },
-                                              "cap" => { "value" => "Common Allocation Policy (CAP)" },
-                                              "chr" => { "value" => "Common housing register (CHR)" },
-                                              "divider" => { "value" => true },
-                                              "letting_allocation_unknown" => { "value" => "None of these allocation systems" },
-                                            })
+        "cbl" => { "value" => "Choice-based lettings (CBL)" },
+        "cap" => { "value" => "Common Allocation Policy (CAP)" },
+        "chr" => { "value" => "Common housing register (CHR)" },
+        "divider" => { "value" => true },
+        "letting_allocation_unknown" => { "value" => "None of these allocation systems" },
+      })
     end
   end
 
@@ -58,13 +58,13 @@ RSpec.describe Form::Lettings::Questions::LettingAllocation, type: :model do
 
     it "has the correct answer_options" do
       expect(question.answer_options).to eq({
-                                              "cbl" => { "value" => "Choice-based lettings (CBL)", "hint" => "Where available vacant properties are advertised and applicants are able to bid for specific properties." },
-                                              "cap" => { "value" => "Common Allocation Policy (CAP)", "hint" => "Where a common system agreed between a group of housing providers is used to determine applicant’s priority for housing." },
-                                              "chr" => { "value" => "Common housing register (CHR)", "hint" => "Where a single waiting list is used by a group of housing providers to receive and process housing applications. Providers may use different approaches to determine priority." },
-                                              "accessible_register" => { "value" => "Accessible housing register", "hint" => "Where the ‘access category’ or another descriptor of whether an available vacant property meets a range of access needs is displayed to applicants during the allocations process." },
-                                              "divider" => { "value" => true },
-                                              "letting_allocation_unknown" => { "value" => "None of these allocation systems" },
-                                            })
+        "cbl" => { "value" => "Choice-based lettings (CBL)", "hint" => "Where available vacant properties are advertised and applicants are able to bid for specific properties." },
+        "cap" => { "value" => "Common Allocation Policy (CAP)", "hint" => "Where a common system agreed between a group of housing providers is used to determine applicant’s priority for housing." },
+        "chr" => { "value" => "Common housing register (CHR)", "hint" => "Where a single waiting list is used by a group of housing providers to receive and process housing applications. Providers may use different approaches to determine priority." },
+        "accessible_register" => { "value" => "Accessible housing register", "hint" => "Where the ‘access category’ or another descriptor of whether an available vacant property meets a range of access needs is displayed to applicants during the allocations process." },
+        "divider" => { "value" => true },
+        "letting_allocation_unknown" => { "value" => "None of these allocation systems" },
+      })
     end
   end
 
