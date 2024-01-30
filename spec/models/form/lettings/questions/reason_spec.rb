010396e49d105232c9a4d5baf1080c9f237fcea9
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-30 16:36:59 +0000

    feat: update ansewrs, hint text and test

--- spec/models/form/lettings/questions/reason_spec.rb
+++ spec/models/form/lettings/questions/reason_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe Form::Lettings::Questions::Reason, type: :model do
   let(:question_id) { nil }
   let(:question_definition) { nil }
   let(:page) { instance_double(Form::Page) }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form) }
+
+  before do
+    allow(form).to receive(:start_year_after_2024?).and_return(false)
+    allow(page).to receive(:subsection).and_return(subsection)
+    allow(subsection).to receive(:form).and_return(form)
+  end
 
   it "has correct page" do
     expect(question.page).to eq(page)
@@ -31,10 +39,6 @@ RSpec.describe Form::Lettings::Questions::Reason, type: :model do
     expect(question.check_answers_card_number).to eq(0)
   end
 
-  it "has the correct hint" do
-    expect(question.hint_text).to eq("The tenant’s ‘last settled home’ is their last long-standing home. For tenants who were in temporary accommodation or sleeping rough, their last settled home is where they were living previously.")
-  end
-
   it "has the correct conditional_for" do
     expect(question.conditional_for).to eq({ "reasonother" => [20] })
   end
@@ -43,116 +47,233 @@ RSpec.describe Form::Lettings::Questions::Reason, type: :model do
     expect(question).not_to be_derived
   end
 
-  it "has the correct answer_options" do
-    expect(question.answer_options).to eq({
-      "40" => {
-        "value" => "End of assured shorthold tenancy (no fault)",
-      },
-      "41" => {
-        "value" => "End of assured shorthold tenancy (eviction or tenant at fault)",
-      },
-      "42" => {
-        "value" => "End of fixed term tenancy (no fault)",
-      },
-      "43" => {
-        "value" => "End of fixed term tenancy (eviction or tenant at fault)",
-      },
-      "1" => {
-        "value" => "Permanently decanted from another property owned by this landlord",
-      },
-      "46" => {
-        "value" => "Discharged from long-stay hospital or similar institution",
-      },
-      "45" => {
-        "value" => "Discharged from prison",
-      },
-      "2" => {
-        "value" => "Left home country as a refugee",
-      },
-      "4" => {
-        "value" => "Loss of tied accommodation",
-      },
-      "9" => {
-        "value" => "Asked to leave by family or friends",
-      },
-      "44" => {
-        "value" => "Death of household member in last settled accommodation",
-      },
-      "8" => {
-        "value" => "Relationship breakdown (non-violent) with partner",
-      },
-      "16" => {
-        "value" => "To move nearer to family, friends or school",
-      },
-      "17" => {
-        "value" => "To move nearer to work",
-      },
-      "48" => {
-        "value" => "Domestic abuse - previously joint tenancy with partner",
-      },
-      "49" => {
-        "value" => "Domestic abuse - other",
-      },
-      "31" => {
-        "value" => "Hate crime",
-      },
-      "10" => {
-        "value" => "Racial harassment",
-      },
-      "11" => {
-        "value" => "Other problems with neighbours",
-      },
-      "35" => {
-        "value" => "Couldn’t afford fees attached to renewing the tenancy",
-      },
-      "36" => {
-        "value" => "Couldn’t afford increase in rent",
-      },
-      "38" => {
-        "value" => "Couldn’t afford rent or mortgage (employment)",
-      },
-      "37" => {
-        "value" => "Couldn’t afford rent or mortgage (welfare reforms)",
-      },
-      "39" => {
-        "value" => "Couldn’t afford rent or mortgage (other)",
-      },
-      "34" => {
-        "value" => "Repossession",
-      },
-      "12" => {
-        "value" => "Property unsuitable because of overcrowding",
-      },
-      "13" => {
-        "value" => "Property unsuitable because of ill health or disability",
-      },
-      "14" => {
-        "value" => "Property unsuitable because of poor condition",
-      },
-      "18" => {
-        "value" => "To move to accommodation with support",
-      },
-      "19" => {
-        "value" => "To move to independent accommodation",
-      },
-      "30" => {
-        "value" => "Under occupation (no incentive)",
-      },
-      "29" => {
-        "value" => "Under occupation (offered incentive to downsize)",
-      },
-      "20" => {
-        "value" => "Other",
-      },
-      "47" => {
-        "value" => "Tenant prefers not to say",
-      },
-      "divider" => {
-        "value" => true,
-      },
-      "28" => {
-        "value" => "Don’t know",
-      },
-    })
+  context "with 2023/24 form" do
+    it "has the correct hint" do
+      expect(question.hint_text).to eq("The tenant’s ‘last settled home’ is their last long-standing home. For tenants who were in temporary accommodation or sleeping rough, their last settled home is where they were living previously.")
+    end
+
+    it "has the correct answer_options" do
+      expect(question.answer_options).to eq({
+        "40" => {
+          "value" => "End of assured shorthold tenancy (no fault)",
+        },
+        "41" => {
+          "value" => "End of assured shorthold tenancy (eviction or tenant at fault)",
+        },
+        "42" => {
+          "value" => "End of fixed term tenancy (no fault)",
+        },
+        "43" => {
+          "value" => "End of fixed term tenancy (eviction or tenant at fault)",
+        },
+        "1" => {
+          "value" => "Permanently decanted from another property owned by this landlord",
+        },
+        "46" => {
+          "value" => "Discharged from long-stay hospital or similar institution",
+        },
+        "45" => {
+          "value" => "Discharged from prison",
+        },
+        "2" => {
+          "value" => "Left home country as a refugee",
+        },
+        "4" => {
+          "value" => "Loss of tied accommodation",
+        },
+        "9" => {
+          "value" => "Asked to leave by family or friends",
+        },
+        "44" => {
+          "value" => "Death of household member in last settled accommodation",
+        },
+        "8" => {
+          "value" => "Relationship breakdown (non-violent) with partner",
+        },
+        "16" => {
+          "value" => "To move nearer to family, friends or school",
+        },
+        "17" => {
+          "value" => "To move nearer to work",
+        },
+        "48" => {
+          "value" => "Domestic abuse - previously joint tenancy with partner",
+        },
+        "49" => {
+          "value" => "Domestic abuse - other",
+        },
+        "31" => {
+          "value" => "Hate crime",
+        },
+        "10" => {
+          "value" => "Racial harassment",
+        },
+        "11" => {
+          "value" => "Other problems with neighbours",
+        },
+        "35" => {
+          "value" => "Couldn’t afford fees attached to renewing the tenancy",
+        },
+        "36" => {
+          "value" => "Couldn’t afford increase in rent",
+        },
+        "38" => {
+          "value" => "Couldn’t afford rent or mortgage (employment)",
+        },
+        "37" => {
+          "value" => "Couldn’t afford rent or mortgage (welfare reforms)",
+        },
+        "39" => {
+          "value" => "Couldn’t afford rent or mortgage (other)",
+        },
+        "34" => {
+          "value" => "Repossession",
+        },
+        "12" => {
+          "value" => "Property unsuitable because of overcrowding",
+        },
+        "13" => {
+          "value" => "Property unsuitable because of ill health or disability",
+        },
+        "14" => {
+          "value" => "Property unsuitable because of poor condition",
+        },
+        "18" => {
+          "value" => "To move to accommodation with support",
+        },
+        "19" => {
+          "value" => "To move to independent accommodation",
+        },
+        "30" => {
+          "value" => "Under occupation (no incentive)",
+        },
+        "29" => {
+          "value" => "Under occupation (offered incentive to downsize)",
+        },
+        "20" => {
+          "value" => "Other",
+        },
+        "47" => {
+          "value" => "Tenant prefers not to say",
+        },
+        "divider" => {
+          "value" => true,
+        },
+        "28" => {
+          "value" => "Don’t know",
+        },
+      })
+    end
+  end
+
+  context "with 2024/25 form" do
+    before do
+      allow(form).to receive(:start_year_after_2024?).and_return(true)
+    end
+
+    it "has the correct hint" do
+      expect(question.hint_text).to eq("The tenant’s ‘last settled home’ is their last long-standing home. For tenants who were in temporary accommodation, sleeping rough or otherwise homeless, their last settled home is where they were living previously.")
+    end
+
+    it "has the correct answer_options" do
+      expect(question.answer_options).to eq({
+        "50" => {
+          "value" => "End of social housing tenancy - no fault",
+        },
+        "51" => {
+          "value" => "End of social housing tenancy - evicted due to anti-social behaviour (ASB)",
+        },
+        "52" => {
+          "value" => "End of social housing tenancy - evicted due to rent arrears",
+        },
+        "53" => {
+          "value" => "End of social housing tenancy - evicted for any other reason",
+        },
+        "1" => {
+          "value" => "Permanently decanted from another property owned by this landlord",
+        },
+        "2" => {
+          "value" => "Left home country as a refugee",
+        },
+        "45" => {
+          "value" => "Discharged from prison",
+        },
+        "46" => {
+          "value" => "Discharged from long-stay hospital or similar institution",
+        },
+        "4" => {
+          "value" => "Loss of tied accommodation",
+        },
+        "9" => {
+          "value" => "Asked to leave by family or friends",
+        },
+        "8" => {
+          "value" => "Relationship breakdown (non-violent) with partner",
+        },
+        "44" => {
+          "value" => "Death of household member in last settled accommodation",
+        },
+        "16" => {
+          "value" => "To move nearer to family, friends or school",
+        },
+        "17" => {
+          "value" => "To move nearer to work",
+        },
+        "48" => {
+          "value" => "Domestic abuse - previously joint tenancy with partner",
+        },
+        "49" => {
+          "value" => "Domestic abuse - other",
+        },
+        "10" => {
+          "value" => "Racial harassment",
+        },
+        "31" => {
+          "value" => "Hate crime",
+        },
+        "11" => {
+          "value" => "Other problems with neighbours",
+        },
+        "34" => {
+          "value" => "Repossession",
+        },
+        "54" => {
+          "value" => "Could no longer afford rent or mortgage",
+        },
+        "12" => {
+          "value" => "Property unsuitable because of overcrowding",
+        },
+        "13" => {
+          "value" => "Property unsuitable because of ill health or disability",
+        },
+        "14" => {
+          "value" => "Property unsuitable because of poor condition",
+        },
+        "29" => {
+          "value" => "Under occupation (offered incentive to downsize)",
+        },
+        "30" => {
+          "value" => "Under occupation (no incentive)",
+        },
+        "18" => {
+          "value" => "To move to accommodation with support",
+        },
+        "19" => {
+          "value" => "To move to independent accommodation",
+        },
+        "20" => {
+          "value" => "Other",
+        },
+        "28" => {
+          "value" => "Don’t know",
+        },
+        "divider" => {
+          "value" => true,
+        },
+        "47" => {
+          "value" => "Tenant prefers not to say",
+        },
+      })
+    end
   end
 end
