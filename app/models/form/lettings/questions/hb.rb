ede15995910992be0173056b7a4f55dd8a0c63f7
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-30 10:19:06 +0000

    feat: update hint text and test

--- app/models/form/lettings/questions/hb.rb
+++ app/models/form/lettings/questions/hb.rb
@@ -6,7 +6,7 @@ class Form::Lettings::Questions::Hb < ::Form::Question
     @header = "Is the household likely to be receiving any of these housing-related benefits?"
     @type = "radio"
     @check_answers_card_number = 0
-    @hint_text = ""
+    @hint_text = form.start_year_after_2024? ? "This is about when the tenant is in their new let. If they are unsure about the situation for their new let and their financial and working situation hasn’t changed significantly, answer based on what housing-related benefits they currently receive." : ""
     @answer_options = ANSWER_OPTIONS
     @question_number = 89
   end
