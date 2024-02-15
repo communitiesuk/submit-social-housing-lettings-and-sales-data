86614e64949937b0e86b4fdea328c13506b4906a
Rachael Booth <Rachael.Booth@softwire.com>
2024-02-15 15:09:55 +0000

    Fix: Ensure declaration doesn't use a separate check answers card in 2024 forms now it's in setup (#2244)

--- app/models/form/lettings/questions/declaration.rb
+++ app/models/form/lettings/questions/declaration.rb
@@ -5,7 +5,7 @@ class Form::Lettings::Questions::Declaration < ::Form::Question
     @check_answer_label = "Tenant has seen the privacy notice"
     @header = "Declaration"
     @type = "checkbox"
-    @check_answers_card_number = 0
+    @check_answers_card_number = 0 unless form.start_year_after_2024?
     @top_guidance_partial = form.start_year_after_2024? ? "privacy_notice_tenant_2024" : "privacy_notice_tenant"
     @question_number = 30
   end
