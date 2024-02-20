82d220b3c93ef897dfc9621bf8cf467479de6b7b
Kat <katrina@kosiak.co.uk>
2024-02-20 15:32:41 +0000

    Stub form in tests

--- spec/models/form/sales/questions/managing_organisation_spec.rb
+++ spec/models/form/sales/questions/managing_organisation_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe Form::Sales::Questions::ManagingOrganisation, type: :model do
 
   let(:question_id) { nil }
   let(:question_definition) { nil }
-  let(:page) { instance_double(Form::Page) }
-  let(:subsection) { instance_double(Form::Subsection) }
-  let(:form) { instance_double(Form) }
+  let(:page) { instance_double(Form::Page, subsection: instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2023, 4, 1)))) }
 
   it "has correct page" do
     expect(question.page).to eq(page)
