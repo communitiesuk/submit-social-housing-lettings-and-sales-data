812d05fb1c3deefeaeffacd6fe3d228b59f15e68
Kat <katrina@kosiak.co.uk>
2024-02-20 16:08:16 +0000

    Stub form start_date for pages

--- spec/models/form/sales/pages/managing_organisation_spec.rb
+++ spec/models/form/sales/pages/managing_organisation_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe Form::Sales::Pages::ManagingOrganisation, type: :model do
 
   let(:page_id) { nil }
   let(:page_definition) { nil }
-  let(:subsection) { instance_double(Form::Subsection, form:) }
-  let(:form) { instance_double(Form) }
+  let(:subsection) { instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2023, 4, 1))) }
 
   before do
     allow(form).to receive(:start_year_after_2024?).and_return(false)
