d5bb290cd09945807e27a8879ef8622c39ebb291
Robert Sullivan <robert.sullivan@softwire.com>
2024-02-23 17:25:24 +0000

    CLDC-1971: Update text for 'prefers not to say' answer to buyer 1 ethnic group question

--- spec/models/form/sales/questions/buyer1_ethnic_group_spec.rb
+++ spec/models/form/sales/questions/buyer1_ethnic_group_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Form::Sales::Questions::Buyer1EthnicGroup, type: :model do
     expect(question.answer_options).to eq({
       "0" => { "value" => "White" },
       "1" => { "value" => "Mixed or Multiple ethnic groups" },
-      "17" => { "value" => "Buyer 1 prefers not to say" },
+      "17" => { "value" => "Buyer prefers not to say" },
       "2" => { "value" => "Asian or Asian British" },
       "3" => { "value" => "Black, African, Caribbean or Black British" },
       "4" => { "value" => "Arab or other ethnic group" },
@@ -49,14 +49,5 @@ RSpec.describe Form::Sales::Questions::Buyer1EthnicGroup, type: :model do
 
   it "has the correct check_answers_card_number" do
     expect(question.check_answers_card_number).to eq(1)
-  end
-
-  it "has the correct inferred_check_answers_value" do
-    expect(question.inferred_check_answers_value).to eq([{
-      "condition" => {
-        "ethnic_group" => 17,
-      },
-      "value" => "Prefers not to say",
-    }])
   end
 end
