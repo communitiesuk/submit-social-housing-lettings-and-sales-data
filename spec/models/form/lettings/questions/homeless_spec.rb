8200e763054f36c60928090986ecd81e56c69aa2
Robert Sullivan <robert.sullivan@softwire.com>
2024-02-27 11:51:39 +0000

    CLDC-2171: Add test file for homelessness tests

---
+++ spec/models/form/lettings/questions/homeless_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Questions::Homeless, type: :model do
+  subject(:question) { described_class.new(nil, nil, page) }
+
+  let(:page) { instance_double(Form::Page) }
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("homeless")
+  end
+
+  it "has the correct header" do
+    expect(question.header).to eq("Did the household experience homelessness immediately before this letting?")
+  end
+
+  it "has the correct check_answer_label" do
+    expect(question.check_answer_label).to eq("Household homeless immediately before letting")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?).to be false
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq({
+      "11" => { "value" => "Yes - assessed by a local authority as homeless" },
+      "1" => { "value" => "No" },
+    })
+  end
+
+  it "has no hint text" do
+    expect(question.hint_text).to be_empty
+  end
+
+  it "has the correct check_answers_card_number" do
+    expect(question.check_answers_card_number).to eq(0)
+  end
+end
