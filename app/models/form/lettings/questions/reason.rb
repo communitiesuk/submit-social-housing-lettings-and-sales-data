53ab760cfceed133d7789753dbb111071be76b22
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-02-29 09:30:35 +0000

    feat: update renewal answer options accordingly

--- app/models/form/lettings/questions/reason.rb
+++ app/models/form/lettings/questions/reason.rb
@@ -21,7 +21,7 @@ class Form::Lettings::Questions::Reason < ::Form::Question
         "50" => { "value" => "End of social housing tenancy - no fault" },
         "51" => { "value" => "End of social housing tenancy - evicted due to anti-social behaviour (ASB)" },
         "52" => { "value" => "End of social housing tenancy - evicted due to rent arrears" },
-        "53" => { "value" => "End of social housing tenancy - evicted for any other reason" },
+        "53" => { "value" => "End of fixed term tenancy - evicted for any other reason" },
         "1" => { "value" => "Permanently decanted from another property owned by this landlord" },
         "2" => { "value" => "Left home country as a refugee" },
         "45" => { "value" => "Discharged from prison" },
