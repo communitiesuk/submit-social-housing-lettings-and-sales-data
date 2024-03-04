dcc4a733340e04a9e17da6702977ba545e41bebd
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-03-04 11:16:48 +0000

    feat: update data presenter and test

--- app/services/address_data_presenter.rb
+++ app/services/address_data_presenter.rb
@@ -12,15 +12,31 @@ class AddressDataPresenter
   end
 
   def address_line1
-    [data["SUB_BUILDING_NAME"], data["BUILDING_NUMBER"], data["BUILDING_NAME"], data["THOROUGHFARE_NAME"]].compact.join(", ")
+    data.values_at(
+      "PO_BOX_NUMBER",
+      "ORGANISATION_NAME",
+      "DEPARTMENT_NAME",
+      "SUB_BUILDING_NAME",
+      "BUILDING_NAME",
+      "BUILDING_NUMBER",
+      "DEPENDENT_THOROUGHFARE_NAME",
+      "THOROUGHFARE_NAME",
+    ).compact
+        .join(", ")
+        .titleize
   end
 
   def address_line2
-    data["DEPENDENT_LOCALITY"]
+    data.values_at(
+      "DOUBLE_DEPENDENT_LOCALITY", "DEPENDENT_LOCALITY"
+    ).compact
+        .join(", ")
+        .titleize
+        .presence
   end
 
   def town_or_city
-    data["POST_TOWN"]
+    data["POST_TOWN"].titleize
   end
 
   def postcode
