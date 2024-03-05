61c2c23cfcd5ced6c5648ec0668dbc6b930779c6
Rachael Booth <rachael.booth@softwire.com>
2024-03-05 12:17:29 +0000

    CLDC-2692: Add breadcrumbs to log question pages

--- app/helpers/form_page_helper.rb
+++ app/helpers/form_page_helper.rb
@@ -10,4 +10,20 @@ module FormPageHelper
   def accessed_from_duplicate_logs?(referrer)
     %w[duplicate_logs duplicate_logs_banner].include?(referrer)
   end
+
+  def form_page_breadcrumbs(current_user, log, subsection, query_parameters)
+    if accessed_from_duplicate_logs?(query_parameters["referrer"])
+      content_for :breadcrumbs, govuk_breadcrumbs(breadcrumbs: {
+        breadcrumb_logs_title(log, current_user) => breadcrumb_logs_link(log, current_user),
+        "Duplicate Logs" => duplicate_logs_path,
+        "Duplicates of Log #{log.id}" => send("#{log.class.name.underscore}_duplicate_logs_path", log, original_log_id: query_parameters["original_log_id"]),
+      })
+    else
+      content_for :breadcrumbs, govuk_breadcrumbs(breadcrumbs: {
+        breadcrumb_logs_title(log, current_user) => breadcrumb_logs_link(log, current_user),
+        "Log #{log.id}" => url_for(log),
+        subsection.label => send("#{log.class.name.underscore}_#{subsection.id}_check_answers_path", log),
+      })
+    end
+  end
 end
