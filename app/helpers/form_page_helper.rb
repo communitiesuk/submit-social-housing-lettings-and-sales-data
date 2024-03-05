ec06b7121845365202337cd39424b3ce3d75813f
Rachael Booth <rachael.booth@softwire.com>
2024-03-05 14:18:37 +0000

    CLDC-2692: Use a back link when accessing log question page from duplicate logs

--- app/helpers/form_page_helper.rb
+++ app/helpers/form_page_helper.rb
@@ -11,21 +11,11 @@ module FormPageHelper
     %w[duplicate_logs duplicate_logs_banner].include?(referrer)
   end
 
-  def form_page_breadcrumbs(current_user, log, subsection, query_parameters)
-    if accessed_from_duplicate_logs?(query_parameters["referrer"])
-      content_for :breadcrumbs, govuk_breadcrumbs(breadcrumbs: {
-        "Home" => root_path,
-        breadcrumb_logs_title(log, current_user) => breadcrumb_logs_link(log, current_user),
-        "Duplicate Logs" => duplicate_logs_path,
-        "Duplicates of Log #{log.id}" => send("#{log.class.name.underscore}_duplicate_logs_path", log, original_log_id: query_parameters["original_log_id"]),
-      })
-    else
-      content_for :breadcrumbs, govuk_breadcrumbs(breadcrumbs: {
-        "Home" => root_path,
-        breadcrumb_logs_title(log, current_user) => breadcrumb_logs_link(log, current_user),
-        "Log #{log.id}" => url_for(log),
-        subsection.label => send("#{log.class.name.underscore}_#{subsection.id}_check_answers_path", log),
-      })
-    end
+  def duplicate_log_set_path(log, original_log_id)
+    send("#{log.class.name.underscore}_duplicate_logs_path", log, original_log_id:)
+  end
+
+  def relevant_check_answers_path(log, subsection)
+    send("#{log.class.name.underscore}_#{subsection.id}_check_answers_path", log)
   end
 end
