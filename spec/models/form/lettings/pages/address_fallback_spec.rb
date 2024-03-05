5961938de1d6e7e8d0fb72efd37c0cb35db79187
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-03-05 12:28:00 +0000

    feat: add page tests

---
+++ spec/models/form/lettings/pages/address_fallback_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Pages::AddressFallback, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection) }
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[address_line1 address_line2 town_or_city county postcode_full])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("address")
+  end
+
+  it "has the correct header" do
+    expect(page.header).to eq("Q12 - What is the property's address?")
+  end
+
+  it "has the correct description" do
+    expect(page.description).to be_nil
+  end
+
+  it "has correct depends_on" do
+    expect(page.depends_on).to eq([
+      { "is_supported_housing?" => false, "uprn_known" => nil, "address_selection" => -1 },
+      { "is_supported_housing?" => false, "uprn_known" => 0, "address_selection" => -1 },
+      { "is_supported_housing?" => false, "uprn_confirmed" => 0, "address_selection" => -1 },
+      { "is_supported_housing?" => false, "uprn_known" => nil, "address_options_present?" => false },
+      { "is_supported_housing?" => false, "uprn_known" => 0, "address_options_present?" => false },
+      { "is_supported_housing?" => false, "uprn_confirmed" => 0, "address_options_present?" => false },
+    ])
+  end
+end
---
+++ spec/models/form/lettings/pages/address_matcher_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Pages::AddressMatcher, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection) }
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[address_line1_input postcode_full_input])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("address_matcher")
+  end
+
+  it "has the correct header" do
+    expect(page.header).to eq("Find an address")
+  end
+
+  it "has the correct description" do
+    expect(page.description).to be_nil
+  end
+
+  it "has correct depends_on" do
+    expect(page.depends_on).to eq([{ "is_supported_housing?" => false, "uprn_known" => 0 }, { "is_supported_housing?" => false, "uprn_confirmed" => 0 }])
+  end
+end
