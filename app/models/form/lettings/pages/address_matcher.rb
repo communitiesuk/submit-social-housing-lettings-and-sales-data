5d1d75ec61c5e6f8333885234a895eed446869ed
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-03-05 12:28:00 +0000

    feat: add page tests

--- app/models/form/lettings/pages/address_matcher.rb
+++ app/models/form/lettings/pages/address_matcher.rb
@@ -2,7 +2,7 @@ class Form::Lettings::Pages::AddressMatcher < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "address_matcher"
-    @header = "Find address"
+    @header = "Find an address"
     @depends_on = [
       { "is_supported_housing?" => false, "uprn_known" => 0 },
       { "is_supported_housing?" => false, "uprn_confirmed" => 0 },
