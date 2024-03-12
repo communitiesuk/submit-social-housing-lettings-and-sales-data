d366928cdcd991bff68c52e41a5b792ae83050d6
Kat <katrina@kosiak.co.uk>
2024-03-12 10:38:39 +0000

    Remove child option hint

--- app/models/form/lettings/questions/person_relationship.rb
+++ app/models/form/lettings/questions/person_relationship.rb
@@ -7,21 +7,33 @@ class Form::Lettings::Questions::PersonRelationship < ::Form::Question
     @type = "radio"
     @check_answers_card_number = person_index
     @hint_text = ""
-    @answer_options = ANSWER_OPTIONS
+    @answer_options = answer_options
     @person_index = person_index
     @question_number = question_number
   end
 
-  ANSWER_OPTIONS = {
-    "P" => { "value" => "Partner" },
-    "C" => {
-      "value" => "Child",
-      "hint" => "Must be eligible for child benefit: under age 16 or under 20 if still in full-time education.",
-    },
-    "X" => { "value" => "Other" },
-    "divider" => { "value" => true },
-    "R" => { "value" => "Person prefers not to say" },
-  }.freeze
+  def answer_options
+    if form.start_year_after_2024?
+      {
+        "P" => { "value" => "Partner" },
+        "C" => { "value" => "Child" },
+        "X" => { "value" => "Other" },
+        "divider" => { "value" => true },
+        "R" => { "value" => "Person prefers not to say" },
+      }
+    else
+      {
+        "P" => { "value" => "Partner" },
+        "C" => {
+          "value" => "Child",
+          "hint" => "Must be eligible for child benefit: under age 16 or under 20 if still in full-time education.",
+        },
+        "X" => { "value" => "Other" },
+        "divider" => { "value" => true },
+        "R" => { "value" => "Person prefers not to say" },
+      }
+    end
+  end
 
   def question_number
     base_question_number = case form.start_date.year
