0ba0d93bad9cc58d29e8f62985900b06aed4bcf0
Robert Sullivan <robert.sullivan@softwire.com>
2024-03-21 17:47:02 +0000

    CLDC-3355: Reorder rent types on 2024/5 forms

--- app/models/form/lettings/questions/rent_type.rb
+++ app/models/form/lettings/questions/rent_type.rb
@@ -5,8 +5,8 @@ class Form::Lettings::Questions::RentType < ::Form::Question
     @check_answer_label = "Rent type"
     @header = "What is the rent type?"
     @type = "radio"
-    @top_guidance_partial = "rent_type_definitions"
-    @answer_options = ANSWER_OPTIONS
+    @top_guidance_partial = form.start_year_after_2024? ? "rent_type_definitions_2024" : "rent_type_definitions"
+    @answer_options = form.start_year_after_2024? ? ANSWER_OPTIONS_2024 : ANSWER_OPTIONS
     @conditional_for = { "irproduct_other" => [5] }
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max] if form.start_date.present?
   end
@@ -17,6 +17,15 @@ class Form::Lettings::Questions::RentType < ::Form::Question
     "4" => { "value" => "London Living Rent" },
     "3" => { "value" => "Rent to Buy" },
     "0" => { "value" => "Social Rent" },
+    "5" => { "value" => "Other intermediate rent product" },
+  }.freeze
+
+  ANSWER_OPTIONS_2024 = {
+    "0" => { "value" => "Social Rent" },
+    "1" => { "value" => "Affordable Rent" },
+    "2" => { "value" => "London Affordable Rent" },
+    "3" => { "value" => "Rent to Buy" },
+    "4" => { "value" => "London Living Rent" },
     "5" => { "value" => "Other intermediate rent product" },
   }.freeze
 
