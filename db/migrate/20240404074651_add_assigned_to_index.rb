d4ac1adf3e49ae04a5fdfd03652cc7fdbe947559
Kat <katrina@kosiak.co.uk>
2024-04-04 13:12:53 +0100

    Update schema

--- db/migrate/20240404074651_add_assigned_to_index.rb
+++ db/migrate/20240404074651_add_assigned_to_index.rb
@@ -1,7 +1,5 @@
 class AddAssignedToIndex < ActiveRecord::Migration[7.0]
   def change
-    remove_index :sales_logs, name: "index_sales_logs_on_created_by_id", column: :created_by_id
-    remove_index :lettings_logs, name: "index_lettings_logs_on_created_by_id", column: :created_by_id
     add_index :sales_logs, :assigned_to_id
     add_index :lettings_logs, :assigned_to_id
   end
