1d326182ffe9dc3234e58474d42ba46ffe06064b
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2024-04-08 08:12:58 +0100

    CLDC-2741 Order log search results (#2299)
    * Order lettings logs search results
    
    * Order sales logs search results
    
    * lint
    
    * Update SQL casing

--- app/models/lettings_log.rb
+++ app/models/lettings_log.rb
@@ -59,11 +59,17 @@ class LettingsLog < Log
     query.all
   }
   scope :search_by, lambda { |param|
+    by_id = Arel.sql("CASE WHEN lettings_logs.id = #{param.to_i} THEN 0 ELSE 1 END")
+    by_tenant_code = Arel.sql("CASE WHEN tenancycode = '#{param}' THEN 0 WHEN tenancycode ILIKE '%#{param}%' THEN 1 ELSE 2 END")
+    by_propcode = Arel.sql("CASE WHEN propcode = '#{param}' THEN 0 WHEN propcode ILIKE '%#{param}%' THEN 1 ELSE 2 END")
+    by_postcode = Arel.sql("CASE WHEN REPLACE(postcode_full, ' ', '') = '#{param.delete(' ')}' THEN 0 when REPLACE(postcode_full, ' ', '') ILIKE '%#{param.delete(' ')}%' then 1 ELSE 2 END")
+
     filter_by_location_postcode(param)
       .or(filter_by_tenant_code(param))
       .or(filter_by_propcode(param))
       .or(filter_by_postcode(param))
       .or(filter_by_id(param.gsub(/log/i, "")))
+      .order(by_id, by_tenant_code, by_propcode, by_postcode)
   }
   scope :after_date, ->(date) { where("lettings_logs.startdate >= ?", date) }
   scope :before_date, ->(date) { where("lettings_logs.startdate < ?", date) }
