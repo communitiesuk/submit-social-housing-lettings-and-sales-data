d0f606f1d96b3dcbb00c5056300b1eca47e3e117
Kat <katrina@kosiak.co.uk>
2024-04-08 10:54:57 +0100

    Do not export created_by in non support CSV

--- app/services/csv/sales_log_csv_service.rb
+++ app/services/csv/sales_log_csv_service.rb
@@ -138,7 +138,7 @@ module Csv
       "managing_organisation_id" => %w[managing_organisation_name],
     }.freeze
 
-    SUPPORT_ONLY_ATTRIBUTES = %w[address_line1_as_entered address_line2_as_entered town_or_city_as_entered county_as_entered postcode_full_as_entered la_as_entered].freeze
+    SUPPORT_ONLY_ATTRIBUTES = %w[address_line1_as_entered address_line2_as_entered town_or_city_as_entered county_as_entered postcode_full_as_entered la_as_entered created_by].freeze
 
     def sales_log_attributes
       ordered_questions = FormHandler.instance.ordered_sales_questions_for_all_years
