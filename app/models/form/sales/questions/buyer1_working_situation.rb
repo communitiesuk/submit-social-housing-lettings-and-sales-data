c560d1726363ca9bdccbd015364d1e51a2d8ed86
Kat <katrina@kosiak.co.uk>
2024-04-10 10:56:46 +0100

    Update missed hint text

--- app/models/form/sales/questions/buyer1_working_situation.rb
+++ app/models/form/sales/questions/buyer1_working_situation.rb
@@ -6,7 +6,7 @@ class Form::Sales::Questions::Buyer1WorkingSituation < ::Form::Question
     @header = "Which of these best describes buyer 1's working situation?"
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
-    @hint_text = "Buyer 1 is the person in the household who does the most paid work. If it's a joint purchase and the buyers do the same amount of paid work, buyer 1 is whoever is the oldest."
+    @hint_text = form.start_year_after_2024? ? "" : "Buyer 1 is the person in the household who does the most paid work. If it’s a joint purchase and the buyers do the same amount of paid work, buyer 1 is whoever is the oldest."
     @check_answers_card_number = 1
     @inferred_check_answers_value = [{
       "condition" => {
