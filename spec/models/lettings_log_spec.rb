4455e6adf160b702f7df53b6fd252d2fcc808722
Rachael Booth <Rachael.Booth@softwire.com>
2024-05-16 09:53:08 +0100

    CLDC-3456: Fix bug preventing location_id from counting as a location qusetion (#2417)

--- spec/models/lettings_log_spec.rb
+++ spec/models/lettings_log_spec.rb
@@ -3549,7 +3549,7 @@ RSpec.describe LettingsLog do
     end
 
     context "when scheme and location have not been completed" do
-      let(:lettings_log) { build(:lettings_log, :setup_completed, :sh, scheme_id: nil, location: nil) }
+      let(:lettings_log) { build(:lettings_log, :setup_completed, :sh, scheme_id: nil, location_id: nil) }
 
       it "returns true" do
         expect(lettings_log).to be_non_location_setup_questions_completed
