f5461f8d790055e391d30e1bb15b2d7c521d8ef2
Kat <katrina@kosiak.co.uk>
2024-06-10 19:55:52 +0100

    Do not display deleted organisations as an option

--- app/models/form/sales/questions/owning_organisation_id.rb
+++ app/models/form/sales/questions/owning_organisation_id.rb
@@ -16,25 +16,21 @@ class Form::Sales::Questions::OwningOrganisationId < ::Form::Question
     return answer_opts unless user
     return answer_opts unless log
 
-    unless user.support?
-      if log.owning_organisation_id.present?
-        answer_opts[log.owning_organisation.id] = log.owning_organisation.name
-      end
-
-      if user.organisation.holds_own_stock?
-        answer_opts[user.organisation.id] = "#{user.organisation.name} (Your organisation)"
-      end
-
-      user.organisation.stock_owners.filter_by_active.where(holds_own_stock: true).find_each do |org|
-        answer_opts[org.id] = org.name
-      end
-    end
-
     if log.owning_organisation_id.present?
       answer_opts[log.owning_organisation.id] = log.owning_organisation.name
     end
 
-    recently_absorbed_organisations = user.organisation.absorbed_organisations.merged_during_open_collection_period
+    unless user.support?
+      if user.organisation.holds_own_stock?
+        answer_opts[user.organisation.id] = "#{user.organisation.name} (Your organisation)"
+      end
+
+      user.organisation.stock_owners.visible.filter_by_active.where(holds_own_stock: true).find_each do |org|
+        answer_opts[org.id] = org.name
+      end
+    end
+
+    recently_absorbed_organisations = user.organisation.absorbed_organisations.visible.merged_during_open_collection_period
     if !user.support? && user.organisation.holds_own_stock?
       answer_opts[user.organisation.id] = if recently_absorbed_organisations.exists? && user.organisation.available_from.present?
                                             "#{user.organisation.name} (Your organisation, active as of #{user.organisation.available_from.to_fs(:govuk_date)})"
@@ -44,7 +40,7 @@ class Form::Sales::Questions::OwningOrganisationId < ::Form::Question
     end
 
     if user.support?
-      Organisation.filter_by_active.where(holds_own_stock: true).find_each do |org|
+      Organisation.visible.filter_by_active.where(holds_own_stock: true).find_each do |org|
         if org.merge_date.present?
           answer_opts[org.id] = "#{org.name} (inactive as of #{org.merge_date.to_fs(:govuk_date)})" if org.merge_date >= FormHandler.instance.start_date_of_earliest_open_for_editing_collection_period
         elsif org.absorbed_organisations.merged_during_open_collection_period.exists? && org.available_from.present?
@@ -54,7 +50,7 @@ class Form::Sales::Questions::OwningOrganisationId < ::Form::Question
         end
       end
     else
-      recently_absorbed_organisations.each do |absorbed_org|
+      recently_absorbed_organisations.visible.each do |absorbed_org|
         answer_opts[absorbed_org.id] = merged_organisation_label(absorbed_org.name, absorbed_org.merge_date) if absorbed_org.holds_own_stock?
       end
     end
@@ -75,7 +71,7 @@ class Form::Sales::Questions::OwningOrganisationId < ::Form::Question
   def hidden_in_check_answers?(_log, user = nil)
     return false if user.support?
 
-    stock_owners = user.organisation.stock_owners + user.organisation.absorbed_organisations.where(holds_own_stock: true)
+    stock_owners = user.organisation.stock_owners.visible + user.organisation.absorbed_organisations.visible.where(holds_own_stock: true)
 
     if user.organisation.holds_own_stock?
       stock_owners.count.zero?
