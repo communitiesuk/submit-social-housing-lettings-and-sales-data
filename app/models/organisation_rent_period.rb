00c85675382c36035774731151c32efa3140e691
Arthur Campbell <51094020+arfacamble@users.noreply.github.com>
2024-06-11 15:29:05 +0100

    add paper trail to organisation rent periods in case of any further bugs (#2461)

--- app/models/organisation_rent_period.rb
+++ app/models/organisation_rent_period.rb
@@ -2,4 +2,6 @@ class OrganisationRentPeriod < ApplicationRecord
   belongs_to :organisation
 
   validates :organisation_id, uniqueness: { scope: :rent_period } # rubocop:disable Rails/UniqueValidationWithoutIndex
+
+  has_paper_trail
 end
