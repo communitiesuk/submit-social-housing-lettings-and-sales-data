a75b3cf705a38579eb6d048eb8108450c17c9ee6
Kat <katrina@kosiak.co.uk>
2024-06-14 11:55:54 +0100

    Link to check your errors page

--- app/helpers/form_page_error_helper.rb
+++ app/helpers/form_page_error_helper.rb
@@ -3,4 +3,12 @@ module FormPageErrorHelper
     other_page_error_ids = lettings_log.errors.map(&:attribute) - page.questions.map { |q| q.id.to_sym }.concat([:base])
     other_page_error_ids.each { |id| lettings_log.errors.delete(id) }
   end
+
+  def all_questions_affected_by_errors(log)
+    log.errors.map(&:attribute) - [:base]
+  end
+
+  def check_your_errors_link(log, related_question_ids, original_page_id)
+    govuk_link_to "See all related answers", lettings_log_check_your_errors_path(log, related_question_ids:, original_page_id:), method: :post
+  end
 end
