5dc17264a5da6a1002a3c102631bd37089a1f075
Arthur Campbell <arfa.camble@gmail.com>
2024-06-18 11:19:24 +0100

    mark the beds question as derived when the log is a bedsit to stop it from being cleared

--- app/models/form/lettings/questions/beds.rb
+++ app/models/form/lettings/questions/beds.rb
@@ -13,6 +13,10 @@ class Form::Lettings::Questions::Beds < ::Form::Question
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
   end
 
+  def derived?(log)
+    log.is_bedsit?
+  end
+
   def hint_text
     form.start_year_after_2024? ? "If shared accommodation, enter the number of bedrooms occupied by this household." : "If shared accommodation, enter the number of bedrooms occupied by this household. A bedsit has 1 bedroom."
   end
