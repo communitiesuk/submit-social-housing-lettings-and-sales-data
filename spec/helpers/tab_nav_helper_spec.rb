9e21f938484936338a5b1f064a8a1633ea26d897
Kat <katrina@kosiak.co.uk>
2024-06-27 14:12:45 +0100

    Fix test

--- spec/helpers/tab_nav_helper_spec.rb
+++ spec/helpers/tab_nav_helper_spec.rb
@@ -2,21 +2,21 @@ require "rails_helper"
 
 RSpec.describe TabNavHelper do
   let(:organisation) { FactoryBot.create(:organisation) }
-  let(:user) { FactoryBot.build(:user, organisation:) }
+  let(:current_user) { FactoryBot.build(:user, organisation:) }
   let(:scheme) { FactoryBot.create(:scheme, service_name: "Some name") }
   let(:location) { FactoryBot.create(:location, scheme:) }
 
   describe "#user_cell" do
     it "returns user link and email separated by a newline character" do
-      expected_html = "<a class=\"govuk-link\" href=\"/users\">#{user.name}</a>\n<span class=\"govuk-visually-hidden\">User </span><span class=\"govuk-!-font-weight-regular app-!-colour-muted\">#{user.email}</span>"
-      expect(user_cell(user)).to match(expected_html)
+      expected_html = "<a class=\"govuk-link\" href=\"/users\">#{current_user.name}</a>\n<span class=\"govuk-visually-hidden\">User </span><span class=\"govuk-!-font-weight-regular app-!-colour-muted\">#{current_user.email}</span>"
+      expect(user_cell(current_user)).to match(expected_html)
     end
   end
 
   describe "#org_cell" do
     it "returns the users org name and role separated by a newline character" do
       expected_html = "DLUHC\n<span class=\"app-!-colour-muted\">Data provider</span>"
-      expect(org_cell(user)).to match(expected_html)
+      expect(org_cell(current_user)).to match(expected_html)
     end
   end
 
