f423ec1cd597209110ca594fe178ae851de11d45
Arthur Campbell <51094020+arfacamble@users.noreply.github.com>
2024-07-12 18:33:28 +0100

    CLDC-3505 further fixes to tests for year hard end (#2493)
    * further fixes
    
    * Fix date / crossover issues
    
    * Try changing form end dates
    
    * Revert "Try changing form end dates"
    
    This reverts commit e027e29cf086e159850cd4af2a0268fc333db426.
    
    * Make use of :ignore-validation-errors trait
    
    * Try with end dates in past
    
    * Revert "Try with end dates in past"
    
    This reverts commit 6b87938d7024752f0c7debd944e1a4d19071df57.
    
    ---------
    
    Co-authored-by: Rachael Booth <rachael.booth@softwire.com>

--- spec/models/form/sales/questions/deposit_amount_spec.rb
+++ spec/models/form/sales/questions/deposit_amount_spec.rb
@@ -1,31 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Form::Sales::Questions::DepositAmount, type: :model do
-  subject(:question) { described_class.new(question_id, question_definition, page, ownershipsch: 1, optional: false) }
+  subject(:question) { described_class.new(question_id, question_definition, page, ownershipsch: 1, optional:) }
 
+  let(:optional) { false }
   let(:question_id) { nil }
   let(:question_definition) { nil }
   let(:page) { instance_double(Form::Page, subsection: instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2023, 4, 1)))) }
-
-  it "has correct page" do
-    expect(question.page).to eq(page)
-  end
-
-  it "has the correct id" do
-    expect(question.id).to eq("deposit")
-  end
-
-  it "has the correct header" do
-    expect(question.header).to eq("How much cash deposit was paid on the property?")
-  end
-
-  it "has the correct check_answer_label" do
-    expect(question.check_answer_label).to eq("Cash deposit")
-  end
-
-  it "has the correct type" do
-    expect(question.type).to eq("numeric")
-  end
 
   context "when the ownership type is shared" do
     let(:log) { build(:sales_log, :completed, ownershipsch: 1, mortgageused: 2) }
@@ -44,49 +25,48 @@ RSpec.describe Form::Sales::Questions::DepositAmount, type: :model do
   end
 
   context "when the ownership type is outright" do
-    let(:log) { build(:sales_log, :completed, ownershipsch: 3, mortgageused: 2) }
+    let(:log) { build(:sales_log, :outright_sale_setup_complete, mortgageused:) }
 
-    it "is not marked as derived when a mortgage is used" do
-      log.mortgageused = 1
-      expect(question.derived?(log)).to be false
+    context "when a mortgage is used" do
+      let(:mortgageused) { 1 }
+
+      it "is not marked as derived " do
+        expect(question.derived?(log)).to be false
+      end
     end
 
-    it "is marked as derived when a mortgage is not used" do
-      log.mortgageused = 2
-      expect(question.derived?(log)).to be true
+    context "when a mortgage is not used" do
+      let(:mortgageused) { 2 }
+
+      it "is marked as derived " do
+        expect(question.derived?(log)).to be true
+      end
     end
 
-    it "is marked as derived when the mortgage use is unknown" do
-      log.mortgageused = 3
-      expect(question.derived?(log)).to be true
+    context "when the mortgage use is unknown" do
+      let(:mortgageused) { 3 }
+
+      it "is marked as derived " do
+        expect(question.derived?(log)).to be true
+      end
     end
   end
 
-  it "has the correct hint" do
-    expect(question.hint_text).to eq("Enter the total cash sum paid by the buyer towards the property that was not funded by the mortgage. This excludes any grant or loan")
-  end
+  describe "hint text" do
+    context "when optional is false" do
+      let(:optional) { false }
 
-  it "has correct width" do
-    expect(question.width).to eq(5)
-  end
+      it "has the correct hint" do
+        expect(question.hint_text).to eq("Enter the total cash sum paid by the buyer towards the property that was not funded by the mortgage. This excludes any grant or loan")
+      end
+    end
 
-  it "has correct prefix" do
-    expect(question.prefix).to eq("£")
-  end
+    context "when optional is true" do
+      let(:optional) { true }
 
-  it "has correct min" do
-    expect(question.min).to eq(0)
-  end
-
-  it "has correct max" do
-    expect(question.max).to eq(999_999)
-  end
-
-  context "when optional iis true" do
-    subject(:question) { described_class.new(question_id, question_definition, page, ownershipsch: 1, optional: true) }
-
-    it "has a correct hint_text" do
-      expect(question.hint_text).to eq("Enter the total cash sum paid by the buyer towards the property that was not funded by the mortgage. This excludes any grant or loan. As this is a fully staircased sale this question is optional. If you do not have the information available click save and continue")
+      it "has the correct hint" do
+        expect(question.hint_text).to eq("Enter the total cash sum paid by the buyer towards the property that was not funded by the mortgage. This excludes any grant or loan. As this is a fully staircased sale this question is optional. If you do not have the information available click save and continue")
+      end
     end
   end
 end
