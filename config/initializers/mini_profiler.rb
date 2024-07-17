dccdbcc813adb8f6fd9fbd1cfc621708d09cf0d0
Kat <katrina@kosiak.co.uk>
2024-07-17 09:43:51 +0100

    Update profiler storage

---
+++ config/initializers/mini_profiler.rb
@@ -0,0 +1,12 @@
+
+require "configuration/configuration_service"
+require "configuration/env_configuration_service"
+
+# set RedisStore
+if Rails.env.staging?
+  configuration_service = Configuration::EnvConfigurationService.new
+  redis_url = configuration_service.redis_uris.to_a[0][1]
+
+  Rack::MiniProfiler.config.storage_options = { url: redis_url }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+end
