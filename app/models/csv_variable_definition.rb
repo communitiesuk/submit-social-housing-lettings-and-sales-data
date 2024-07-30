060f018026e62e093a6b7e5f3804c223342020ba
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-07-30 10:06:08 +0100

    Create CSV Variable Definitions model

---
+++ app/models/csv_variable_definition.rb
@@ -0,0 +1,8 @@
+class CsvVariableDefinition < ApplicationRecord
+  validates :variable, presence: true
+  validates :definition, presence: true
+  validates :log_type, presence: true, inclusion: { in: %w[lettings sales] }
+  validates :user_type, presence: true, inclusion: { in: %w[user support] }
+  validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 2000, less_than_or_equal_to: 2099 }
+  attribute :last_accessed, :datetime
+end
