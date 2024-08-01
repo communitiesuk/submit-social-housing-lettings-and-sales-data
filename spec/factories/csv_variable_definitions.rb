ff6050ccb064f6f59974117ca7d9b165d689899b
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-08-01 17:11:21 +0100

    Create tests, fix

--- spec/factories/csv_variable_definitions.rb
+++ spec/factories/csv_variable_definitions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :csv_variable_definition do
     variable { "variable" }
     definition { "definition" }
-    log_type { "log" }
+    log_type { "lettings" }
     user_type { "support" }
     year { 2024 }
   end
