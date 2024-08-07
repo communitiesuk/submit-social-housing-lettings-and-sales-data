2252428816d25ad582f7d72b05f5faa7ebe83c56
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2024-08-07 14:27:07 +0100

    Fix up financial equations content (#2552)
    * Tweak shared ownership validation wording
    
    * Add top_guidance_partial to missed shared ownership questions
    
    * Update discounted ownership validation message when there's no grant
    
    * Update discounted sale guidance and errors
    
    * Lint and update test
    
    * Update BU test
    
    * Update test

--- app/models/validations/sales/sale_information_validations.rb
+++ app/models/validations/sales/sale_information_validations.rb
@@ -51,13 +51,14 @@ module Validations::Sales::SaleInformationValidations
     tolerance = record.discount ? record.value * 0.05 / 100 : 1
 
     if over_tolerance?(record.mortgage_deposit_and_grant_total, record.value_with_discount, tolerance, strict: !record.discount.nil?) && record.discounted_ownership_sale?
+      deposit_and_grant_sentence = record.grant.present? ? ", cash deposit (#{record.field_formatted_as_currency('deposit')}), and grant (#{record.field_formatted_as_currency('grant')})" : " and cash deposit (#{record.field_formatted_as_currency('deposit')})"
+      discount_sentence = record.discount.present? ? " (#{record.field_formatted_as_currency('value')}) subtracted by the sum of the full purchase price (#{record.field_formatted_as_currency('value')}) multiplied by the percentage discount (#{record.discount}%)" : ""
       %i[mortgageused mortgage value deposit ownershipsch discount grant].each do |field|
         record.errors.add field, I18n.t("validations.sale_information.discounted_ownership_value",
                                         mortgage: record.mortgage&.positive? ? " (#{record.field_formatted_as_currency('mortgage')})" : "",
-                                        deposit: record.field_formatted_as_currency("deposit"),
-                                        grant: record.grant.present? ? " (#{record.field_formatted_as_currency('grant')})" : "",
+                                        deposit_and_grant_sentence:,
                                         mortgage_deposit_and_grant_total: record.field_formatted_as_currency("mortgage_deposit_and_grant_total"),
-                                        discount_sentence: record.discount.present? ? " (#{record.field_formatted_as_currency('value')}) times by the discount (#{record.discount}%)" : "",
+                                        discount_sentence:,
                                         value_with_discount: record.field_formatted_as_currency("value_with_discount")).html_safe
       end
     end
