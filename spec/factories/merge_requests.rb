743d6a6766b0fd4e635ac400ebd7c8721cc86d92
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-08-12 16:09:25 +0100

    Lowercase status

--- spec/factories/merge_requests.rb
+++ spec/factories/merge_requests.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :merge_request do
-    status { "Incomplete" }
+    status { "incomplete" }
     merge_date { nil }
     helpdesk_ticket { "MSD-99999" }
     association :requesting_organisation, factory: :organisation
