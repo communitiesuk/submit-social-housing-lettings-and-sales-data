3d7d7590299254e96470d83312c9ca194238c76b
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-08-12 13:23:05 +0100

    Refactor details view and add tests

--- spec/factories/merge_requests.rb
+++ spec/factories/merge_requests.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :merge_request do
-    status { "incomplete" }
+    status { "Incomplete" }
     merge_date { nil }
+    helpdesk_ticket { "MSD-99999" }
     association :requesting_organisation, factory: :organisation
   end
 end
