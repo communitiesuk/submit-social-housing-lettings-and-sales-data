f4223d2f97bbec83088fe5ae9378189f78428f3a
Rachael Booth <rachael.booth@softwire.com>
2024-09-02 16:18:40 +0100

    Adjust notifications helper

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -16,32 +16,28 @@ module NotificationsHelper
   end
 
   def render_for_banner(title)
-    @@banner_renderer ||= NotificationTitleRenderer.new({ invert_link_colour: true, bold_all_text: true })
-    @@banner_markdown ||= Redcarpet::Markdown.new(@@banner_renderer, no_intra_emphasis: true)
-    @@banner_markdown.render(title)
+    banner_renderer ||= NotificationTitleRenderer.new({ invert_link_colour: true, bold_all_text: true })
+    Redcarpet::Markdown.new(banner_renderer, no_intra_emphasis: true).render(title)
   end
 
   def render_for_summary(title)
-    @@plain_title_renderer ||= NotificationTitleRenderer.new({ invert_link_colour: false, bold_all_text: false })
-    @@plain_title_markdown ||= Redcarpet::Markdown.new(@@plain_title_renderer, no_intra_emphasis: true)
-    @@plain_title_markdown.render(title)
+    plain_title_renderer ||= NotificationTitleRenderer.new({ invert_link_colour: false, bold_all_text: false })
+    Redcarpet::Markdown.new(plain_title_renderer, no_intra_emphasis: true).render(title)
+  end
+end
+
+class NotificationTitleRenderer < Redcarpet::Render::HTML
+  def initialize(options = {})
+    link_class = "govuk-link"
+    link_class += " govuk-link--inverse" if options[:invert_link_colour]
+    @bold = options[:bold_all_text]
+    base_options = { escape_html: true, safe_links_only: true, link_attributes: { class: link_class } }
+    super base_options
   end
 
-private
+  def paragraph(text)
+    return %(<p class="govuk-!-font-weight-bold">#{text}</p>) if @bold
 
-  class NotificationTitleRenderer < Redcarpet::Render::HTML
-    def initialize(options = {})
-      link_class = "govuk-link"
-      link_class += " govuk-link--inverse" if options[:invert_link_colour]
-      @bold = options[:bold_all_text]
-      base_options = { escape_html: true, safe_links_only: true, link_attributes: { class: link_class } }
-      super base_options
-    end
-
-    def paragraph(text)
-      return %(<p class="govuk-!-font-weight-bold">#{text}</p>) if @bold
-
-      %(<p>#{text}</p>)
-    end
+    %(<p>#{text}</p>)
   end
 end
