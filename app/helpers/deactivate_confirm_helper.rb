f85f4ea3196e54228cc5b865c91747c425fe0120
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-09-05 11:01:28 +0100

    Fix lint offence

--- app/helpers/deactivate_confirm_helper.rb
+++ app/helpers/deactivate_confirm_helper.rb
@@ -1,8 +1,8 @@
 module DeactivateConfirmHelper
   def affected_title(affected_logs, affected_locations)
     title_parts = []
-    title_parts << pluralize(affected_logs.count, "log") if affected_logs.count > 0
-    title_parts << pluralize(affected_locations.count, "location") if affected_locations.count > 0
+    title_parts << pluralize(affected_logs.count, "log") if affected_logs.count.positive?
+    title_parts << pluralize(affected_locations.count, "location") if affected_locations.count.positive?
     "This change will affect #{title_parts.join(' and ')}."
   end
 end
