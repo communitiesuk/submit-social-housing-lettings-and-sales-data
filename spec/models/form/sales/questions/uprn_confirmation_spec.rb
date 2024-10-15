2d780a845b924844a56d573e5535dfcded4401c6
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-10-15 14:33:40 +0100

    Revert spec back to original

--- spec/models/form/sales/questions/uprn_confirmation_spec.rb
+++ spec/models/form/sales/questions/uprn_confirmation_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Form::Sales::Questions::UprnConfirmation, type: :model do
 
   let(:question_id) { nil }
   let(:question_definition) { nil }
-  let(:page) { instance_double(Form::Page, subsection: instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2023, 4, 1)))) }
+  let(:page) { instance_double(Form::Page) }
 
   it "has correct page" do
     expect(question.page).to eq(page)
