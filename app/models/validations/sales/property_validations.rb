1c40c9077ca49d636a2e304f8f36a867bcc6eac9
Kat <katrina@kosiak.co.uk>
2024-10-15 14:20:33 +0100

    Extract property information validations

--- app/models/validations/sales/property_validations.rb
+++ app/models/validations/sales/property_validations.rb
@@ -4,10 +4,10 @@ module Validations::Sales::PropertyValidations
     return unless record.ppostcode_full.present? && record.postcode_full.present?
 
     if record.discounted_ownership_sale? && record.ppostcode_full != record.postcode_full
-      record.errors.add :postcode_full, I18n.t("validations.property.postcode.must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
-      record.errors.add :ppostcode_full, I18n.t("validations.property.postcode.must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
-      record.errors.add :ownershipsch, I18n.t("validations.property.postcode.must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
-      record.errors.add :uprn, I18n.t("validations.property.postcode.must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
+      record.errors.add :postcode_full, I18n.t("validations.sales.property_information.postcode_full.postcode_must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
+      record.errors.add :ppostcode_full, I18n.t("validations.sales.property_information.ppostcode_full.postcode_must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
+      record.errors.add :ownershipsch, I18n.t("validations.sales.property_information.ownershipsch.postcode_must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
+      record.errors.add :uprn, I18n.t("validations.sales.property_information.uprn.postcode_must_match_previous", buyer_possessive: record.joint_purchase? ? "Buyers’" : "Buyer’s")
     end
   end
 
@@ -15,8 +15,8 @@ module Validations::Sales::PropertyValidations
     return unless record.proptype.present? && record.beds.present?
 
     if record.is_bedsit? && record.beds > 1
-      record.errors.add :proptype, I18n.t("validations.property.proptype.bedsits_have_max_one_bedroom")
-      record.errors.add :beds, I18n.t("validations.property.beds.bedsits_have_max_one_bedroom")
+      record.errors.add :proptype, I18n.t("validations.sales.property_information.proptype.bedsits_have_max_one_bedroom")
+      record.errors.add :beds, I18n.t("validations.sales.property_information.beds.bedsits_have_max_one_bedroom")
     end
   end
 
@@ -25,6 +25,6 @@ module Validations::Sales::PropertyValidations
 
     return if record.uprn.match?(/^[0-9]{1,12}$/)
 
-    record.errors.add :uprn, I18n.t("validations.property.uprn.invalid")
+    record.errors.add :uprn, I18n.t("validations.sales.property_information.uprn.invalid")
   end
 end
