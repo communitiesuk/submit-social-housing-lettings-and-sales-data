1803e25aecd4fe2ae652487b3eda96c3a7120019
Kat <katrina@kosiak.co.uk>
2024-10-17 12:54:30 +0100

    WIP

--- app/models/form/sales/pages/exchange_date.rb
+++ app/models/form/sales/pages/exchange_date.rb
@@ -2,6 +2,7 @@ class Form::Sales::Pages::ExchangeDate < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "exchange_contracts"
+    @copy_key = "sales.sale_information.exchange_date"
     @depends_on = [{
       "resale" => 2,
     }]
