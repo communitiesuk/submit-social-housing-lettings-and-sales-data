5759bbbafc1d2f589b8c1f5a6ce8f88e4d8438d9
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2024-10-21 09:13:04 +0100

    Add missing question to translations

--- app/models/form/sales/questions/prevshared.rb
+++ app/models/form/sales/questions/prevshared.rb
@@ -2,11 +2,8 @@ class Form::Sales::Questions::Prevshared < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "prevshared"
-    @check_answer_label = "Previous property shared ownership?"
-    @header = "Was the previous property under shared ownership?"
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
-    @hint_text = "For any buyer"
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
   end
 
