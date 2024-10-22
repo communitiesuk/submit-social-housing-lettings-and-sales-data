f0e881656b34bc9b65242c67a8751e6b74a4b388
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-10-22 10:09:17 +0100

    Lint

--- app/models/form/sales/pages/uprn.rb
+++ app/models/form/sales/pages/uprn.rb
@@ -2,7 +2,7 @@ class Form::Sales::Pages::Uprn < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "uprn"
-    @copy_key="sales.property_information.uprn"
+    @copy_key = "sales.property_information.uprn"
   end
 
   def questions
