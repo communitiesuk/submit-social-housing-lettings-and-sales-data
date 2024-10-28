05c557a0c4042cf071a417e38d92b883911e2faa
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2024-10-28 10:02:08 +0000

    Extract lettings household characteristics copy

--- app/models/form/lettings/questions/ethnic_arab.rb
+++ app/models/form/lettings/questions/ethnic_arab.rb
@@ -2,11 +2,9 @@ class Form::Lettings::Questions::EthnicArab < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "ethnic"
-    @check_answer_label = "Lead tenant’s ethnic background"
-    @header = "Which of the following best describes the lead tenant’s Arab background?"
+    @copy_key = "lettings.household_characteristics.ethnic.ethnic_background_arab"
     @type = "radio"
     @check_answers_card_number = 1
-    @hint_text = form.start_year_after_2024? ? "" : "The lead tenant is the person in the household who does the most paid work. If several people do the same paid work, the lead tenant is whoever is the oldest."
     @answer_options = ANSWER_OPTIONS
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
   end
