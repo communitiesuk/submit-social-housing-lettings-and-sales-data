1bc04b6e2cf706b8d4ce961a0ff35510ff6eae67
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-10-29 11:05:36 +0000

    Update about price value test

--- spec/models/form/sales/pages/about_price_value_check_spec.rb
+++ spec/models/form/sales/pages/about_price_value_check_spec.rb
@@ -28,6 +28,27 @@ RSpec.describe Form::Sales::Pages::AboutPriceValueCheck, type: :model do
     expect(page.interruption_screen?).to eq(true)
   end
 
+  it "has the correct title_text" do
+    expect(page.title_text).to eq({ "arguments" => [{ "i18n_template" => "value", "key" => "value", "label" => true }], "translation" => "forms.2024.sales.soft_validations.value_value_check.title_text" })
+  end
+
+  it "has the correct informative_text" do
+    expect(page.informative_text).to eq({
+      "translation" => "forms.2024.sales.soft_validations.value_value_check.informative_text",
+      "arguments" => [
+        {
+          "key" => "field_formatted_as_currency",
+          "arguments_for_key" => "purchase_price_soft_min_or_soft_max",
+          "i18n_template" => "soft_min_or_soft_max",
+        },
+        {
+          "key" => "purchase_price_higher_or_lower_text",
+          "i18n_template" => "higher_or_lower",
+        },
+      ],
+    })
+  end
+
   it "has the correct interruption_screen_question_ids" do
     expect(page.interruption_screen_question_ids).to eq(%w[value beds uprn postcode_full la])
   end
