edf86dbf1c0b052a35464cbd547c28702d6c67a1
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-10-29 10:54:49 +0000

    Update uprn confirmation test

--- spec/models/form/sales/questions/uprn_confirmation_spec.rb
+++ spec/models/form/sales/questions/uprn_confirmation_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Form::Sales::Questions::UprnConfirmation, type: :model do
 
   let(:question_id) { nil }
   let(:question_definition) { nil }
-  let(:page) { instance_double(Form::Page) }
+  let(:page) { instance_double(Form::Page, subsection: instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2023, 4, 4), type: "sales"), id: "property_information")) }
 
   it "has correct page" do
     expect(question.page).to eq(page)
