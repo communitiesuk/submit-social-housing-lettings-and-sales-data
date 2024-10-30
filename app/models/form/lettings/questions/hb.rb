b4a2261e178f30bb996fb58007c711a42e0de8ed
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-10-30 16:15:12 +0000

    Fix hb question

--- app/models/form/lettings/questions/hb.rb
+++ app/models/form/lettings/questions/hb.rb
@@ -2,11 +2,8 @@ class Form::Lettings::Questions::Hb < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "hb"
-    @check_answer_label = "Housing-related benefits received"
-    @header = "Is the household likely to be receiving any of these housing-related benefits?"
     @type = "radio"
     @check_answers_card_number = 0
-    @hint_text = form.start_year_after_2024? ? "This is about when the tenant is in their new let. If they are unsure about the situation for their new let and their financial and working situation hasn’t changed significantly, answer based on what housing-related benefits they currently receive." : ""
     @answer_options = ANSWER_OPTIONS
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
   end
