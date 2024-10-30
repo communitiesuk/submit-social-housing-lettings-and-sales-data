39aeaa2e47c9e8b03a23c31ea343d0685306c5e9
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-10-30 15:09:00 +0000

    Extract missed hint text

--- app/models/form/sales/questions/property_wheelchair_accessible.rb
+++ app/models/form/sales/questions/property_wheelchair_accessible.rb
@@ -6,7 +6,6 @@ class Form::Sales::Questions::PropertyWheelchairAccessible < ::Form::Question
     @type = "radio"
     @answer_options = ANSWER_OPTIONS
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
-    @hint_text = form.start_year_after_2024? ? "This is whether someone who uses a wheelchair is able to make full use of all of the property’s rooms and facilities, including use of both inside and outside space, and entering and exiting the property." : nil
   end
 
   ANSWER_OPTIONS = {
