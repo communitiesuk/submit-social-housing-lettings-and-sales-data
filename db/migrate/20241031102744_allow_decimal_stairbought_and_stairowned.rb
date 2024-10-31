ea9bc3a624a19526619c7f457a7cd87e6b034226
Rachael Booth <rachael.booth@softwire.com>
2024-10-31 17:46:30 +0000

    Fix lint for migration

--- db/migrate/20241031102744_allow_decimal_stairbought_and_stairowned.rb
+++ db/migrate/20241031102744_allow_decimal_stairbought_and_stairowned.rb
@@ -1,6 +1,15 @@
 class AllowDecimalStairboughtAndStairowned < ActiveRecord::Migration[7.0]
-  def change
-    change_column :sales_logs, :stairbought, :decimal
-    change_column :sales_logs, :stairowned, :decimal
+  def up
+    change_table :sales_logs, bulk: true do |t|
+      t.change :stairbought, :decimal
+      t.change :stairowned, :decimal
+    end
+  end
+
+  def down
+    change_table :sales_logs, bulk: true do |t|
+      t.change :stairbought, :integer
+      t.change :stairowned, :integer
+    end
   end
 end
