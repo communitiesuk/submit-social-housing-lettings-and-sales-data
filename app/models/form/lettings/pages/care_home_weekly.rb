fa83f3abc2c8ed49d4022d8ac565feaf2274b892
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-01 12:18:24 +0000

    CLDC-3685: Copy changes lettings income and benefits (#2732)

--- app/models/form/lettings/pages/care_home_weekly.rb
+++ app/models/form/lettings/pages/care_home_weekly.rb
@@ -2,6 +2,7 @@ class Form::Lettings::Pages::CareHomeWeekly < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "care_home_weekly"
+    @copy_key = "lettings.income_and_benefits.care_home"
     @depends_on = [
       { "rent_and_charges_paid_weekly?" => true, "is_supported_housing?" => true, "household_charge" => 0 },
       { "rent_and_charges_paid_weekly?" => true, "is_supported_housing?" => true, "household_charge" => nil },
