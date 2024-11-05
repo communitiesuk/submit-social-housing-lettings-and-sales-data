ed3dfa66d180cd36621cb4bd492d94e20b3e9172
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-05 13:49:28 +0000

    Add depend on ownershipsch == 1

--- app/models/form/sales/pages/staircase.rb
+++ app/models/form/sales/pages/staircase.rb
@@ -2,6 +2,7 @@ class Form::Sales::Pages::Staircase < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "staircasing"
+    @depends_on = [{ "ownershipsch" => 1 }]
   end
 
   def questions
