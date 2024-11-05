896663570c9245a8f23522354f37b685f255048f
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-05 16:45:54 +0000

    Use Lorem.characters instead

--- spec/factories/scheme.rb
+++ spec/factories/scheme.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :scheme do
-    service_name { Faker::Lorem.word }
+    service_name { Faker::Lorem.characters(number: 16) }
     sensitive { Faker::Number.within(range: 0..1) }
     registered_under_care_act { 1 }
     support_type { [0, 2, 3, 4, 5].sample }
