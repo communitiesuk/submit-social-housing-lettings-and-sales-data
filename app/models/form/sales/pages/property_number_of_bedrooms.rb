b685a41dc9e4d318d64916bcee0b947b95999eab
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-07 16:42:55 +0000

    Move q19, add inference and remove redundant hint text in 2025

--- app/models/form/sales/pages/property_number_of_bedrooms.rb
+++ app/models/form/sales/pages/property_number_of_bedrooms.rb
@@ -2,6 +2,7 @@ class Form::Sales::Pages::PropertyNumberOfBedrooms < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "property_number_of_bedrooms"
+    @depends_on = [{"is_beds_inferred?" => false }]
   end
 
   def questions
