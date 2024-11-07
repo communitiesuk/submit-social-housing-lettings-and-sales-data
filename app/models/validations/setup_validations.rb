53e7d33d8e4d2056b95762a05c9007bd2131c96d
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-07 10:53:28 +0000

    Lint

--- app/models/validations/setup_validations.rb
+++ app/models/validations/setup_validations.rb
@@ -77,7 +77,7 @@ module Validations::SetupValidations
       record.errors.add :scheme_id, :incomplete, message: I18n.t("validations.lettings.setup.scheme.incomplete")
     end
 
-    if record.scheme&.locations.present? && !record.scheme.locations.confirmed.any?
+    if record.scheme&.locations.present? && record.scheme.locations.confirmed.none?
       record.errors.add :scheme_id, :no_completed_locations, message: I18n.t("validations.lettings.setup.scheme.no_completed_locations")
     end
 
