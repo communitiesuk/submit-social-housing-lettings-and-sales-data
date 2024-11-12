327736d7e801fde528d976407b7517893b851a1a
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-12 11:55:55 +0000

    Update pages that depend on companybuy

--- app/models/form/sales/pages/buyer_live.rb
+++ app/models/form/sales/pages/buyer_live.rb
@@ -2,9 +2,10 @@ class Form::Sales::Pages::BuyerLive < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "buyer_live"
-    @depends_on = [{
-      "companybuy" => 2,
-    }]
+  end
+
+  def depends_on
+    [{ "companybuy" => 2 }] unless form.start_year_2025_or_later?
   end
 
   def questions
