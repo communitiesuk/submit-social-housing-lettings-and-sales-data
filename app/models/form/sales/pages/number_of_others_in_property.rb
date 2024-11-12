f8c5ec1de35d7cbe98d4f7f6431f5308bbfd9f38
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2024-11-12 10:17:01 +0000

    Route only to buyer questions for staircase transactions

--- app/models/form/sales/pages/number_of_others_in_property.rb
+++ app/models/form/sales/pages/number_of_others_in_property.rb
@@ -1,16 +1,6 @@
 class Form::Sales::Pages::NumberOfOthersInProperty < ::Form::Page
   def initialize(id, hsh, subsection, joint_purchase:)
     super(id, hsh, subsection)
-    @depends_on = [
-      {
-        "buyer_has_seen_privacy_notice?" => true,
-        "joint_purchase?" => joint_purchase,
-      },
-      {
-        "buyer_not_interviewed?" => true,
-        "joint_purchase?" => joint_purchase,
-      },
-    ]
     @joint_purchase = joint_purchase
   end
 
@@ -18,5 +8,16 @@ class Form::Sales::Pages::NumberOfOthersInProperty < ::Form::Page
     @questions ||= [
       Form::Sales::Questions::NumberOfOthersInProperty.new(nil, nil, self, joint_purchase: @joint_purchase),
     ]
+  end
+
+  def routed_to?(log, _current_user)
+    super && page_routed_to?(log)
+  end
+
+  def page_routed_to?(log)
+    return false unless log.joint_purchase? == @joint_purchase
+    return false if log.form.start_year_2025_or_later? && log.is_staircase?
+
+    log.buyer_has_seen_privacy_notice? || log.buyer_not_interviewed?
   end
 end
