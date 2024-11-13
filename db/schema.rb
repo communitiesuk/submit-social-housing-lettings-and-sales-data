8dab730c54d285a82c37e87f3bff97ce88bab7aa
Rachael Booth <Rachael.Booth@softwire.com>
2024-11-13 17:20:34 +0000

    CLDC-3704: Allow equity/stairowned/stairbought to have 1 decimal place (#2746)
    * CLDC-3666: Pull sales financial validations into translations file
    
    * CLDC-3704: Allow equity/stairowned/stairbought to have 1 decimal place
    
    * Fix lint for migration
    
    * Fix tests
    
    * Fix sale information validations tests
    
    * Fix lint
    
    * Fix form spec
    
    * Don't expect unexpected cashdis error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_11_112158) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_31_102744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -627,8 +627,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_11_112158) do
     t.integer "deposit_value_check"
     t.integer "frombeds"
     t.integer "staircase"
-    t.integer "stairbought"
-    t.integer "stairowned"
+    t.decimal "stairbought"
+    t.decimal "stairowned"
     t.decimal "mrent", precision: 10, scale: 2
     t.datetime "exdate"
     t.integer "exday"
