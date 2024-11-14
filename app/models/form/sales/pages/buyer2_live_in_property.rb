b87c107f25703e4befeaf23422f362d8c9b27ff3
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2024-11-14 08:55:41 +0000

    Route only to buyer questions for staircase transactions (#2765)

--- app/models/form/sales/pages/buyer2_live_in_property.rb
+++ app/models/form/sales/pages/buyer2_live_in_property.rb
@@ -2,33 +2,28 @@ class Form::Sales::Pages::Buyer2LiveInProperty < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "buyer_2_live_in_property"
-    @depends_on = [
-      {
-        "buyer_has_seen_privacy_notice?" => true,
-        "outright_sale?" => false,
-        "joint_purchase?" => true,
-      },
-      {
-        "buyer_not_interviewed?" => true,
-        "outright_sale?" => false,
-        "joint_purchase?" => true,
-      },
-      {
-        "buyer_has_seen_privacy_notice?" => true,
-        "joint_purchase?" => true,
-        "buyers_will_live_in?" => true,
-      },
-      {
-        "buyer_not_interviewed?" => true,
-        "joint_purchase?" => true,
-        "buyers_will_live_in?" => true,
-      },
-    ]
   end
 
   def questions
     @questions ||= [
       Form::Sales::Questions::Buyer2LiveInProperty.new(nil, nil, self),
     ]
+  end
+
+  def routed_to?(log, _current_user)
+    super && page_routed_to?(log)
+  end
+
+  def page_routed_to?(log)
+    return false if log.form.start_year_2025_or_later? && log.is_staircase?
+    return false unless log.joint_purchase?
+
+    privacy_notice_seen_or_not_interviewed = log.buyer_has_seen_privacy_notice? || log.buyer_not_interviewed?
+    not_outright_sale = !log.outright_sale?
+
+    return true if privacy_notice_seen_or_not_interviewed && not_outright_sale
+    return true if privacy_notice_seen_or_not_interviewed && log.buyers_will_live_in?
+
+    false
   end
 end
