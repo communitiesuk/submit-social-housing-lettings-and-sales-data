b6c33890c106445f2f2dae23c6a35fd1355bf4e1
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2024-11-18 16:21:25 +0000

    Lint

--- app/models/form/sales/pages/staircase_sale.rb
+++ app/models/form/sales/pages/staircase_sale.rb
@@ -11,7 +11,7 @@ class Form::Sales::Pages::StaircaseSale < ::Form::Page
 
   def questions
     @questions ||= [
-      Form::Sales::Questions::StaircaseSale.new(nil, nil, self)
+      Form::Sales::Questions::StaircaseSale.new(nil, nil, self),
     ].compact
   end
 end
