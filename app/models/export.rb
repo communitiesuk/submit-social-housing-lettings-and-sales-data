9cd6579b3c1843e407f6bb982d54a818147bd5e4
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2024-12-04 10:56:38 +0000

    Update sales export

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,5 +1,6 @@
 class Export < ApplicationRecord
   scope :lettings, -> { where(collection: "lettings") }
+  scope :sales, -> { where(collection: "sales") }
   scope :organisations, -> { where(collection: "organisations") }
   scope :users, -> { where(collection: "users") }
 end
