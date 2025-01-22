d817b17951616d04d3420d03a9d60d9766013277
Rachael Booth <rachael.booth@softwire.com>
2025-01-22 11:49:38 +0000

    CLDC-3850: Clear postcode_known when clearing postcode_full

--- app/models/log.rb
+++ app/models/log.rb
@@ -248,6 +248,7 @@ class Log < ApplicationRecord
   end
 
   def blank_compound_invalid_non_setup_fields!
+    self.postcode_known = nil if errors.attribute_names.include? :postcode_full
     self.ppcodenk = nil if errors.attribute_names.include? :ppostcode_full
     self.previous_la_known = nil if errors.attribute_names.include? :prevloc
 
@@ -258,6 +259,7 @@ class Log < ApplicationRecord
       self.address_line2 = nil
       self.town_or_city = nil
       self.postcode_full = nil
+      self.postcode_known = nil
       self.county = nil
       process_postcode_changes!
     end
