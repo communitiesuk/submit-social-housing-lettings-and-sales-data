91f95317e12b64a620cac1b35e66e2b8f7f29b42
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2025-01-30 10:40:24 +0000

    Update model tests

--- spec/models/validations/household_validations_spec.rb
+++ spec/models/validations/household_validations_spec.rb
@@ -743,8 +743,9 @@ RSpec.describe Validations::HouseholdValidations do
           record.referral = 1
           record.prevten = prevten[:code]
           household_validator.validate_previous_housing_situation(record)
+          label = record.form.start_year_2025_or_later? && prevten[:code] == 28 ? "Living with friends and family (long-term)" : prevten[:label]
           expect(record.errors["prevten"])
-            .to include(match I18n.t("validations.lettings.household.prevten.internal_transfer", prevten: prevten[:label]))
+            .to include(match I18n.t("validations.lettings.household.prevten.internal_transfer", prevten: label))
           expect(record.errors["referral"])
             .to include(match I18n.t("validations.lettings.household.referral.prevten_invalid", prevten: ""))
         end
