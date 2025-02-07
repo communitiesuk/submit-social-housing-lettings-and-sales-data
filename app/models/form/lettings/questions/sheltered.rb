901671bc2554217fc91a19ff07be8c5485d515c7
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2025-02-07 12:05:26 +0000

    Update sheltered accommodation question

--- app/models/form/lettings/questions/sheltered.rb
+++ app/models/form/lettings/questions/sheltered.rb
@@ -9,12 +9,14 @@ class Form::Lettings::Questions::Sheltered < ::Form::Question
 
   def answer_options
     if form.start_year_2025_or_later?
-      { "1" => { "value" => "Yes – sheltered housing for tenants with low support needs" },
+      {
+        "7" => { "value" => "Yes – for tenants with low support needs" },
         "2" => { "value" => "Yes – extra care housing" },
-        "7" => { "value" => "Yes - other" },
+        "8" => { "value" => "Yes – other" },
         "3" => { "value" => "No" },
         "divider" => { "value" => true },
-        "4" => { "value" => "Don’t know" } }
+        "4" => { "value" => "Don’t know" },
+      }
     else
       { "1" => { "value" => "Yes – specialist retirement housing" },
         "2" => { "value" => "Yes – extra care housing" },
