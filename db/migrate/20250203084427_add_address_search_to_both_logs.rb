f9c9bdd3c031cdceaedb9e3f0251df7e59231ed6
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-02-11 13:40:50 +0000

    Lint

--- db/migrate/20250203084427_add_address_search_to_both_logs.rb
+++ db/migrate/20250203084427_add_address_search_to_both_logs.rb
@@ -1,6 +1,6 @@
 class AddAddressSearchToBothLogs < ActiveRecord::Migration[7.2]
   def change
-      add_column :sales_logs, :address_search, :string
-      add_column :lettings_logs, :address_search, :string
+    add_column :sales_logs, :address_search, :string
+    add_column :lettings_logs, :address_search, :string
   end
 end
