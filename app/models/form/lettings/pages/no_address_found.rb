8e7c32a38dd33731a4cadec33596eb23ec1c66f9
Kat <54268893+kosiakkatrina@users.noreply.github.com>
2025-02-21 14:45:52 +0000

    Use depends_on in no addresses found

--- app/models/form/lettings/pages/no_address_found.rb
+++ app/models/form/lettings/pages/no_address_found.rb
@@ -12,6 +12,11 @@ class Form::Lettings::Pages::NoAddressFound < ::Form::Page
       "translation" => "forms.#{form.start_date.year}.#{@copy_key}.informative_text",
       "arguments" => [],
     }
+    @depends_on = [
+      { "is_supported_housing?" => false, "uprn_known" => nil, "address_options_present?" => false, "is_new_build?" => false },
+      { "is_supported_housing?" => false, "uprn_known" => 0, "address_options_present?" => false, "is_new_build?" => false },
+      { "is_supported_housing?" => false, "uprn_confirmed" => 0, "address_options_present?" => false, "is_new_build?" => false },
+    ]
   end
 
   def questions
@@ -22,14 +27,5 @@ class Form::Lettings::Pages::NoAddressFound < ::Form::Page
 
   def interruption_screen_question_ids
     %w[address_line1_input]
-  end
-
-  def routed_to?(log, _)
-    return false unless super
-    return false if log.is_supported_housing? || log.address_options_present?
-    return false if !log.uprn_known.nil? && log.uprn_known != 0 && log.uprn_confirmed != 0
-    return false if log.is_new_build? && log.form.start_year_2025_or_later?
-
-    true
   end
 end
