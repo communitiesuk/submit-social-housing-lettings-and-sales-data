61efd4de0ff056b1bc980df381ae41ac8c7298f4
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-02-25 16:52:52 +0000

    Rename old renewal prp page and question

--- app/models/form/lettings/pages/referral_general_needs.rb
+++ app/models/form/lettings/pages/referral_general_needs.rb
@@ -1,4 +1,4 @@
-class Form::Lettings::Pages::ReferralPrp < ::Form::Page
+class Form::Lettings::Pages::ReferralGeneralNeeds < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "referral_prp"
@@ -7,6 +7,6 @@ class Form::Lettings::Pages::ReferralPrp < ::Form::Page
   end
 
   def questions
-    @questions ||= [Form::Lettings::Questions::ReferralPrp.new(nil, nil, self)]
+    @questions ||= [Form::Lettings::Questions::ReferralGeneralNeeds.new(nil, nil, self)]
   end
 end
