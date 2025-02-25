97d5ed782b0af045477a27d033b350ad8cb0cf6c
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-02-25 09:41:09 +0000

    Test updated at, excluding pre 2024

--- spec/lib/tasks/recalculate_invalid_reasonpref_dontknow_spec.rb
+++ spec/lib/tasks/recalculate_invalid_reasonpref_dontknow_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe "recalculate_invalid_reasonpref_dontknow" do
     task.reenable
   end
 
-  let(:invalid_logs) { create_list(:lettings_log, 5, :completed, reasonpref: 1, rp_dontknow: 1, rp_homeless: 1, rp_insan_unsat: rand(2), rp_medwel: rand(2), rp_hardship: rand(2)) }
+  let(:invalid_logs) { create_list(:lettings_log, 5, :completed, reasonpref: 1, rp_dontknow: 1, rp_homeless: 1, rp_insan_unsat: rand(2), rp_medwel: rand(2), rp_hardship: rand(2), updated_at: Time.zone.local(2024, 4, 2, 12, 0, 0)) }
   let(:pre_2024_invalid_logs) do
     create_list(:lettings_log, 5, :completed, reasonpref: 1, rp_dontknow: 1, rp_homeless: 1, rp_insan_unsat: rand(2), rp_medwel: rand(2), rp_hardship: rand(2)).each do |log|
       log.startdate = Time.zone.local(rand(2021..2023), 4, 1)
       log.save!(validate: false)
     end
   end
-  let(:valid_logs) { create_list(:lettings_log, 3, :completed, reasonpref: 1, rp_dontknow: 0, rp_homeless: 1, rp_insan_unsat: 1, rp_medwel: rand(2), rp_hardship: rand(2)) }
+  let(:valid_logs) { create_list(:lettings_log, 3, :completed, reasonpref: 1, rp_dontknow: 0, rp_homeless: 1, rp_insan_unsat: 1, rp_medwel: rand(2), rp_hardship: rand(2), updated_at: Time.zone.local(2024, 4, 2, 12, 0, 0)) }
 
   it "updates the logs from 2024/25 with invalid rp_dontknow values" do
     invalid_logs.each do |log|
@@ -31,6 +31,7 @@ RSpec.describe "recalculate_invalid_reasonpref_dontknow" do
       expect(log.reasonpref).to eq(1)
       expect(log.rp_dontknow).to eq(0)
       expect(log.rp_homeless).to eq(1)
+      expect(log.updated_at).not_to eq(Time.zone.local(2024, 4, 2, 12, 0, 0))
     end
   end
 
@@ -63,6 +64,7 @@ RSpec.describe "recalculate_invalid_reasonpref_dontknow" do
       expect(log.rp_dontknow).to eq(0)
       expect(log.rp_homeless).to eq(1)
       expect(log.rp_insan_unsat).to eq(1)
+      expect(log.updated_at).to eq(Time.zone.local(2024, 4, 2, 12, 0, 0))
     end
   end
 end
