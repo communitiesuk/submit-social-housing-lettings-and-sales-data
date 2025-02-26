9ea12911c4a5dd18b72111eb4650f2e5d5fc30c1
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-02-26 11:05:50 +0000

    Update referral type

--- app/models/form/lettings/questions/referral_type.rb
+++ app/models/form/lettings/questions/referral_type.rb
@@ -2,7 +2,7 @@ class Form::Lettings::Questions::ReferralType < ::Form::Question
   def initialize(id, hsh, page)
     super
     @id = "referral_type"
-    @copy_key = "lettings.household_situation.referral"
+    @copy_key = "lettings.household_situation.referral.type"
     @type = "radio"
     @check_answers_card_number = 0
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
