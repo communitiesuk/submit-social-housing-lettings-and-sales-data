9f9d7f7c6d12ceb2ec121353702ab4bd414eb00b
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-02-27 15:52:13 +0000

    Undo a change

--- app/models/validations/sales/property_validations.rb
+++ app/models/validations/sales/property_validations.rb
@@ -40,8 +40,7 @@ module Validations::Sales::PropertyValidations
   end
 
   def validate_la_in_england(record)
-    return unless record.form.start_year_2025_or_later?
-    return unless record.la
+    return unless record.form.start_year_2025_or_later? && record.la.present?
     return if record.la.in?(LocalAuthority.england.pluck(:code))
 
     record.errors.add :la, I18n.t("validations.sales.property_information.la.not_in_england")
