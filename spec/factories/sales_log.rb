6ad3ca3750c3270e539dbeb8e9fd93a333d373e8
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-02-27 12:48:24 +0000

    Remove unused variables from factory

--- spec/factories/sales_log.rb
+++ spec/factories/sales_log.rb
@@ -170,8 +170,6 @@ FactoryBot.define do
       proplen_asked { 1 }
       after(:build) do |log, evaluator|
         if log.saledate >= Time.zone.local(2024, 4, 1)
-          log.address_line1_input = log.address_line1
-          log.postcode_full_input = log.postcode_full
           log.nationality_all_group = 826
           log.nationality_all_buyer2_group = 826
           log.uprn = evaluator.uprn || "10033558653"
