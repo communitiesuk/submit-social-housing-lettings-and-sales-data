79238d919af7fbf9b021adcb7f9f20a8dbb26d0a
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-03-13 09:45:47 +0000

    Add new rent period to num weeks constant

--- app/models/lettings_log.rb
+++ app/models/lettings_log.rb
@@ -148,7 +148,7 @@ class LettingsLog < Log
   OPTIONAL_FIELDS = %w[tenancycode propcode chcharge].freeze
   RENT_TYPE_MAPPING_LABELS = { 1 => "Social Rent", 2 => "Affordable Rent", 3 => "Intermediate Rent", 4 => "Specified accommodation" }.freeze
   HAS_BENEFITS_OPTIONS = [1, 6, 8, 7].freeze
-  NUM_OF_WEEKS_FROM_PERIOD = { 2 => 26, 3 => 13, 4 => 12, 5 => 50, 6 => 49, 7 => 48, 8 => 47, 9 => 46, 1 => 52, 10 => 53 }.freeze
+  NUM_OF_WEEKS_FROM_PERIOD = { 2 => 26, 3 => 13, 4 => 12, 5 => 50, 6 => 49, 7 => 48, 8 => 47, 9 => 46, 11 => 51,  1 => 52, 10 => 53 }.freeze
   SUFFIX_FROM_PERIOD = { 2 => "every 2 weeks", 3 => "every 4 weeks", 4 => "every month" }.freeze
   DUPLICATE_LOG_ATTRIBUTES = %w[owning_organisation_id tenancycode startdate age1_known age1 sex1 ecstat1 tcharge household_charge chcharge].freeze
   RENT_TYPE = {
