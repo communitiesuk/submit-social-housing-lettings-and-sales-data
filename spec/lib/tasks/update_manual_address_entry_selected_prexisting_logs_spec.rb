e94491a3530a71ebe9c434a2ba135fe3b3b2b933
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-03-20 11:14:47 +0000

    Add bu created log test

--- spec/lib/tasks/update_manual_address_entry_selected_prexisting_logs_spec.rb
+++ spec/lib/tasks/update_manual_address_entry_selected_prexisting_logs_spec.rb
@@ -139,6 +139,10 @@ RSpec.describe "update_manual_address_entry_selected_preexisting_logs_spec", typ
       build(:lettings_log, :inprogress_without_address_fields, startdate: Time.zone.local(2024, 6, 1), updated_at: Time.zone.parse("2025-03-19 16:30:00"))
     end
 
+    let(:bu_lettings_log_to_fix) do
+      build(:lettings_log, :inprogress_without_address_fields, startdate: Time.zone.local(2024, 6, 1), creation_method: "bulk upload", updated_at: Time.zone.parse("2025-03-19 16:30:00"))
+    end
+
     let(:lettings_log_not_to_fix) do
       build(:lettings_log, :inprogress_without_address_fields, startdate: Time.zone.local(2024, 6, 1), updated_at: Time.zone.parse("2025-03-19 15:30:00"))
     end
@@ -152,13 +156,22 @@ RSpec.describe "update_manual_address_entry_selected_preexisting_logs_spec", typ
       lettings_log_to_fix.address_line1_input = "1 Test Street"
       lettings_log_to_fix.postcode_full_input = "SW1 2BB"
       lettings_log_to_fix.save!(validate: false)
+
+      bu_lettings_log_to_fix.manual_address_entry_selected = true
+      bu_lettings_log_to_fix.address_line1 = "1 Test Street"
+      bu_lettings_log_to_fix.address_line2 = "Testville"
+      bu_lettings_log_to_fix.town_or_city = "Testford"
+      bu_lettings_log_to_fix.postcode_full = nil
+      bu_lettings_log_to_fix.address_line1_as_entered = "1 Test Street"
+      bu_lettings_log_to_fix.postcode_full_as_entered = "SW1 2BB"
+      bu_lettings_log_to_fix.save!(validate: false)
+
       lettings_log_not_to_fix.postcode_full = nil
       lettings_log_not_to_fix.save!(validate: false)
     end
 
     context "when running the task" do
       it "updates logs that meet the criteria" do
-
         expect(lettings_log_to_fix.postcode_full).to be_nil
         expect(lettings_log_to_fix.address_line1).to eq("1 Test Street")
         expect(lettings_log_to_fix.address_line2).to eq("Testville")
@@ -166,11 +179,23 @@ RSpec.describe "update_manual_address_entry_selected_preexisting_logs_spec", typ
         expect(lettings_log_to_fix.address_line1_input).to eq("1 Test Street")
         expect(lettings_log_to_fix.postcode_full_input).to eq("SW1 2BB")
 
+        expect(bu_lettings_log_to_fix.postcode_full).to be_nil
+        expect(bu_lettings_log_to_fix.address_line1_input).to be_nil
+        expect(bu_lettings_log_to_fix.address_line1).to eq("1 Test Street")
+        expect(bu_lettings_log_to_fix.address_line2).to eq("Testville")
+        expect(bu_lettings_log_to_fix.town_or_city).to eq("Testford")
+        expect(bu_lettings_log_to_fix.address_line1_as_entered).to eq("1 Test Street")
+        expect(bu_lettings_log_to_fix.postcode_full_as_entered).to eq("SW1 2BB")
+
         task.invoke
 
         lettings_log_to_fix.reload
+        bu_lettings_log_to_fix.reload
 
         expect(lettings_log_to_fix.postcode_full).to eq(lettings_log_to_fix.postcode_full_input)
+        expect(lettings_log_to_fix.postcode_full).to eq("SW1 2BB")
+        expect(bu_lettings_log_to_fix.postcode_full).to eq(bu_lettings_log_to_fix.postcode_full_as_entered)
+        expect(bu_lettings_log_to_fix.postcode_full).to eq("SW1 2BB")
       end
 
       it "does not update logs that do not meet the criteria" do
