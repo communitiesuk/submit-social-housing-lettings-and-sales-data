566f7b5910036b2ce888740bab5c41e7e9dff3ce
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-03-25 12:39:57 +0000

    Reorder org js controller

--- app/frontend/controllers/organisations_controller.js
+++ app/frontend/controllers/organisations_controller.js
@@ -1,27 +1,27 @@
 import { Controller } from '@hotwired/stimulus'
 
-const profitStatusSelect = document.getElementById('organisation-profit-status-field')
-const localAuthorityOption = profitStatusSelect.querySelector('option[value="local_authority"]')
-const nonProfitOption = profitStatusSelect.querySelector('option[value="non_profit"]')
-const profitOption = profitStatusSelect.querySelector('option[value="profit"]')
 export default class extends Controller {
-  updateProfitStatusOptions (event) {
-    const providerType = event.target.value
+    updateProfitStatusOptions (event) {
+        const profitStatusSelect = document.getElementById('organisation-profit-status-field')
+        if (!profitStatusSelect) return
 
-    if (profitStatusSelect) {
-      profitStatusSelect.disabled = false
-      localAuthorityOption.hidden = false
-      nonProfitOption.hidden = false
-      profitOption.hidden = false
+        const localAuthorityOption = profitStatusSelect.querySelector('option[value="local_authority"]')
+        const nonProfitOption = profitStatusSelect.querySelector('option[value="non_profit"]')
+        const profitOption = profitStatusSelect.querySelector('option[value="profit"]')
+        const providerType = event.target.value
 
-      if (providerType === 'LA') {
-        profitStatusSelect.value = 'local_authority'
-        nonProfitOption.hidden = true
-        profitOption.hidden = true
-      } else if (providerType === 'PRP') {
-        profitStatusSelect.value = ''
-        localAuthorityOption.hidden = true
-      }
+        profitStatusSelect.disabled = false
+        localAuthorityOption.hidden = false
+        nonProfitOption.hidden = false
+        profitOption.hidden = false
+
+        if (providerType === 'LA') {
+            profitStatusSelect.value = 'local_authority'
+            nonProfitOption.hidden = true
+            profitOption.hidden = true
+        } else if (providerType === 'PRP') {
+            profitStatusSelect.value = ''
+            localAuthorityOption.hidden = true
+        }
     }
-  }
 }
