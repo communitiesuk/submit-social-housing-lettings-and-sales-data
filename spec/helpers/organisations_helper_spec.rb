34a9bea4d8250ca1467cc1808b903fd3da788818
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-03-25 10:25:32 +0000

    Lint

--- spec/helpers/organisations_helper_spec.rb
+++ spec/helpers/organisations_helper_spec.rb
@@ -28,21 +28,21 @@ RSpec.describe OrganisationsHelper do
 
     it "has the correct values and editable status for non-support users" do
       expect(display_organisation_attributes(coordinator_user, organisation)).to eq(
-         [{ editable: false, name: "Organisation ID", value: "ORG#{organisation.id}" },
-          { editable: true,
-            name: "Address",
-            value: "2 Marsham Street\nLondon\nSW1P 4DF" },
-          { editable: true, name: "Telephone number", value: nil },
-          { editable: false, name: "Registration number", value: "1234" },
-          { editable: false, name: "Type of provider", value: "Local authority" },
-          { editable: false, name: "Owns housing stock", value: "Yes" },
-          { editable: false, name: "Part of group", value: "Yes" },
-          { editable: false, name: "Group number", value: "GROUP1" },
-          { editable: false, name: "For profit", value: "" },
-          { editable: true, format: :bullet, name: "Rent periods", value: [] },
-          { name: "Data Sharing Agreement" },
-          { editable: false, name: "Status", value: status_tag(organisation.status) }],
-         )
+        [{ editable: false, name: "Organisation ID", value: "ORG#{organisation.id}" },
+         { editable: true,
+           name: "Address",
+           value: "2 Marsham Street\nLondon\nSW1P 4DF" },
+         { editable: true, name: "Telephone number", value: nil },
+         { editable: false, name: "Registration number", value: "1234" },
+         { editable: false, name: "Type of provider", value: "Local authority" },
+         { editable: false, name: "Owns housing stock", value: "Yes" },
+         { editable: false, name: "Part of group", value: "Yes" },
+         { editable: false, name: "Group number", value: "GROUP1" },
+         { editable: false, name: "For profit", value: "" },
+         { editable: true, format: :bullet, name: "Rent periods", value: [] },
+         { name: "Data Sharing Agreement" },
+         { editable: false, name: "Status", value: status_tag(organisation.status) }],
+      )
     end
   end
 
