2a9e42dba322af0dfab45a0a69a128ee6f1c077a
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-03-31 16:52:18 +0100

    Refactor generate_different_date_within_collection_year

--- app/helpers/collection_time_helper.rb
+++ app/helpers/collection_time_helper.rb
@@ -55,8 +55,8 @@ module CollectionTimeHelper
   end
 
   def generate_different_date_within_collection_year(date, start_date_override: nil, end_date_override: nil)
-    start_date = [start_date_override || collection_start_date(date).to_date, collection_start_date(date).to_date].max.to_date
-    end_date = [end_date_override || collection_end_date(date).to_date, collection_end_date(date).to_date].min.to_date
+    start_date = [start_date_override, collection_start_date(date).to_date].compact.max.to_date
+    end_date = [end_date_override, collection_end_date(date).to_date].compact.min.to_date
     ((start_date..end_date).to_a - [date.to_date]).sample
   end
 end
