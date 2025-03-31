3c272782f9ee6075d588fa72a85010a4c547ce92
carolynbarker <8038496+carolynbarker@users.noreply.github.com>
2025-03-31 18:33:34 +0100

    fix tests for new FY (#3030)
    Co-authored-by: Carolyn <carolyn.barker@softwire.com>
    Co-authored-by: Manny Dinssa <44172848+Dinssa@users.noreply.github.com>

--- spec/models/sales_log_spec.rb
+++ spec/models/sales_log_spec.rb
@@ -104,6 +104,7 @@ RSpec.describe SalesLog, type: :model do
 
     after do
       Timecop.return
+      Singleton.__init__(FormHandler)
     end
 
     it "has returns the correct form based on the start date" do
