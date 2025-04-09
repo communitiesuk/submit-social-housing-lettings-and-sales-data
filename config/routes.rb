349146aa44fd88eb911cb0088ad6f2616f047b19
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-04-09 13:28:06 +0100

    Separate change organisations name from organisation edit view

--- config/routes.rb
+++ config/routes.rb
@@ -156,6 +156,7 @@ Rails.application.routes.draw do
       get "details", to: "organisations#details"
       get "data-sharing-agreement", to: "organisations#data_sharing_agreement"
       post "data-sharing-agreement", to: "organisations#confirm_data_sharing_agreement"
+      get "change-name", to: "organisations#change_name", as: "change_name"
 
       get "users", to: "organisations#users"
       get "lettings-logs", to: "organisations#lettings_logs"
