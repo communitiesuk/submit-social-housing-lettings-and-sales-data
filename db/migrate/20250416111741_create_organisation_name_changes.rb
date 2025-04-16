f782340259e2ead6f2cae0aa9e5561d956e50ae1
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2025-04-16 12:21:03 +0100

    Add organisation_name_changes table

---
+++ db/migrate/20250416111741_create_organisation_name_changes.rb
@@ -0,0 +1,15 @@
+class CreateOrganisationNameChanges < ActiveRecord::Migration[7.0]
+  def change
+    create_table :organisation_name_changes do |t|
+      t.references :organisation, null: false, foreign_key: true
+      t.string :name, null: false
+      t.date :startdate, null: false
+      t.date :discarded_at
+      t.integer :change_type
+
+      t.timestamps
+    end
+
+    add_index :organisation_name_changes, %i[organisation_id startdate], unique: true, name: "index_org_name_changes_on_org_id_and_startdate"
+  end
+end
