ca72558c7c11c16e8331fc04a27e4b506cb68be9
Samuel Young <samuel.young@softwire.com>
2025-10-07 09:37:55 +0100

    CLDC-4046: Remove forcing on Q84 based on Q76 answer (#3094)
    * CLDC-4046: Remove forcing on Q84 based on Q76 answer
    
    * CLDC-4046: Removing forcing on latter half of Q84 as well
    
    * CLDC-4046: Remove orphaned translations
    
    * CLDC-4086: Fix tests

--- app/models/validations/household_validations.rb
+++ app/models/validations/household_validations.rb
@@ -30,17 +30,6 @@ module Validations::HouseholdValidations
     end
     validate_other_field(record, 20, :reason, :reasonother)
 
-    if record.is_reason_permanently_decanted?
-      if record.referral_type.present? && !record.is_from_prp_only_housing_register_or_waiting_list?
-        record.errors.add :referral_type, I18n.t("validations.lettings.household.referral_type.leaving_last_settled_home.reason_permanently_decanted")
-      end
-
-      if record.referral.present? && !record.is_internal_transfer?
-        record.errors.add :referral, I18n.t("validations.lettings.household.referral.leaving_last_settled_home.reason_permanently_decanted")
-        record.errors.add :reason, I18n.t("validations.lettings.household.reason.leaving_last_settled_home.not_internal_transfer")
-      end
-    end
-
     return unless record.form.start_year_2024_or_later?
 
     if record.reason == 20 && PHRASES_INDICATING_HOMELESSNESS_REGEX.match?(record.reasonother)
