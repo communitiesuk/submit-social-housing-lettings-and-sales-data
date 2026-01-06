ce82276a5db01d19ef32ea93b7c7a1b79228425d
Samuel Young <samuel.young@softwire.com>
2026-01-06 10:54:36 +0000

    fixup! CLDC-4136: Hide question from CDS export

--- spec/services/csv/lettings_log_csv_service_spec.rb
+++ spec/services/csv/lettings_log_csv_service_spec.rb
@@ -318,7 +318,7 @@ RSpec.describe Csv::LettingsLogCsvService do
             hbrentshortfall: 1,
             tshortfall_known: 1,
             tshortfall: 12,
-            )
+          )
         end
 
         context "when exporting with human readable labels" do
