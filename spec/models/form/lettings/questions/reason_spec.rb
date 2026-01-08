3a1f5f3e274bb9278f3e64c38c4af7522995d4b3
Katherine Langford <katherine.langford@softwire.com>
2026-01-08 18:23:50 +0000

    CLDC-4148: Code refactor and removes 2023 question data.

--- spec/models/form/lettings/questions/reason_spec.rb
+++ spec/models/form/lettings/questions/reason_spec.rb
@@ -41,49 +41,6 @@ RSpec.describe Form::Lettings::Questions::Reason, type: :model do
     expect(question).not_to be_derived(nil)
   end
 
-  context "with 2023/24 form" do
-    it "has the correct answer_options" do
-      expect(question.answer_options).to eq({
-        "40" => { "value" => "End of assured shorthold tenancy (no fault)" },
-        "41" => { "value" => "End of assured shorthold tenancy (eviction or tenant at fault)" },
-        "42" => { "value" => "End of fixed term tenancy (no fault)" },
-        "43" => { "value" => "End of fixed term tenancy (eviction or tenant at fault)" },
-        "1" => { "value" => "Permanently decanted from another property owned by this landlord" },
-        "46" => { "value" => "Discharged from long-stay hospital or similar institution" },
-        "45" => { "value" => "Discharged from prison" },
-        "2" => { "value" => "Left home country as a refugee" },
-        "4" => { "value" => "Loss of tied accommodation" },
-        "9" => { "value" => "Asked to leave by family or friends" },
-        "44" => { "value" => "Death of household member in last settled accommodation" },
-        "8" => { "value" => "Relationship breakdown (non-violent) with partner" },
-        "16" => { "value" => "To move nearer to family, friends or school" },
-        "17" => { "value" => "To move nearer to work" },
-        "48" => { "value" => "Domestic abuse - previously joint tenancy with partner" },
-        "49" => { "value" => "Domestic abuse - other" },
-        "31" => { "value" => "Hate crime" },
-        "10" => { "value" => "Racial harassment" },
-        "11" => { "value" => "Other problems with neighbours" },
-        "35" => { "value" => "Couldn’t afford fees attached to renewing the tenancy" },
-        "36" => { "value" => "Couldn’t afford increase in rent" },
-        "38" => { "value" => "Couldn’t afford rent or mortgage (employment)" },
-        "37" => { "value" => "Couldn’t afford rent or mortgage (welfare reforms)" },
-        "39" => { "value" => "Couldn’t afford rent or mortgage (other)" },
-        "34" => { "value" => "Repossession" },
-        "12" => { "value" => "Property unsuitable because of overcrowding" },
-        "13" => { "value" => "Property unsuitable because of ill health or disability" },
-        "14" => { "value" => "Property unsuitable because of poor condition" },
-        "18" => { "value" => "To move to accommodation with support" },
-        "19" => { "value" => "To move to independent accommodation" },
-        "30" => { "value" => "Under occupation (no incentive)" },
-        "29" => { "value" => "Under occupation (offered incentive to downsize)" },
-        "20" => { "value" => "Other" },
-        "47" => { "value" => "Tenant prefers not to say" },
-        "divider" => { "value" => true },
-        "28" => { "value" => "Don’t know" },
-      })
-    end
-  end
-
   context "with 2024/25 form" do
     before do
       allow(form).to receive(:start_year_2024_or_later?).and_return(true)
