0fdcd85d31bc240a36eb1bca1d78a35942a50890
Samuel Young <samuel.young@softwire.com>
2026-01-13 09:54:25 +0000

    fixup! CLDC-4136: Hide question on UI flow
    update tests to account

--- spec/models/form/lettings/subsections/property_information_spec.rb
+++ spec/models/form/lettings/subsections/property_information_spec.rb
@@ -15,50 +15,17 @@ RSpec.describe Form::Lettings::Subsections::PropertyInformation, type: :model do
 
     before do
       allow(form).to receive(:start_year_2024_or_later?).and_return(true)
-      allow(form).to receive(:start_year_2025_or_later?).and_return(false)
-    end
-
-    context "when 2023" do
-      let(:start_date) { Time.utc(2023, 2, 8) }
-
-      before do
-        allow(form).to receive(:start_year_2024_or_later?).and_return(false)
-        allow(form).to receive(:start_year_2025_or_later?).and_return(false)
-      end
-
-      it "has correct pages" do
-        expect(property_information.pages.map(&:id)).to eq(
-          %w[
-            uprn
-            uprn_confirmation
-            address
-            property_local_authority
-            local_authority_rent_value_check
-            first_time_property_let_as_social_housing
-            property_let_type
-            property_vacancy_reason_not_first_let
-            property_vacancy_reason_first_let
-            property_number_of_times_relet
-            property_unit_type
-            property_building_type
-            property_wheelchair_accessible
-            property_number_of_bedrooms
-            beds_rent_value_check
-            void_date
-            void_date_value_check
-            property_major_repairs
-            property_major_repairs_value_check
-          ],
-        )
-      end
+      allow(form).to receive(:start_year_2025_or_later?).and_return(true)
+      allow(form).to receive(:start_year_2026_or_later?).and_return(true)
     end
 
     context "when 2024" do
-      let(:start_date) { Time.utc(2024, 2, 8) }
+      let(:start_date) { Time.utc(2024, 4, 8) }
 
       before do
         allow(form).to receive(:start_year_2024_or_later?).and_return(true)
         allow(form).to receive(:start_year_2025_or_later?).and_return(false)
+        allow(form).to receive(:start_year_2026_or_later?).and_return(false)
       end
 
       it "has correct pages" do
@@ -95,11 +62,12 @@ RSpec.describe Form::Lettings::Subsections::PropertyInformation, type: :model do
     end
 
     context "when 2025" do
-      let(:start_date) { Time.utc(2025, 2, 8) }
+      let(:start_date) { Time.utc(2025, 4, 8) }
 
       before do
         allow(form).to receive(:start_year_2024_or_later?).and_return(true)
         allow(form).to receive(:start_year_2025_or_later?).and_return(true)
+        allow(form).to receive(:start_year_2026_or_later?).and_return(false)
       end
 
       it "has correct pages" do
@@ -133,6 +101,40 @@ RSpec.describe Form::Lettings::Subsections::PropertyInformation, type: :model do
         it "is displayed in tasklist" do
           expect(property_information.displayed_in_tasklist?(log)).to eq(true)
         end
+      end
+    end
+
+    context "when 2026" do
+      let(:start_date) { Time.utc(2026, 4, 8) }
+
+      before do
+        allow(form).to receive(:start_year_2024_or_later?).and_return(true)
+        allow(form).to receive(:start_year_2025_or_later?).and_return(true)
+        allow(form).to receive(:start_year_2026_or_later?).and_return(true)
+      end
+
+      it "has correct pages" do
+        expect(property_information.pages.map(&:id)).to eq(
+                                                          %w[
+            first_time_property_let_as_social_housing
+            property_let_type
+            property_vacancy_reason_not_first_let
+            property_vacancy_reason_first_let
+            address_search
+            address
+            property_local_authority
+            local_authority_rent_value_check
+            property_unit_type
+            property_wheelchair_accessible
+            property_number_of_bedrooms
+            beds_rent_value_check
+            void_date
+            void_date_value_check
+            property_major_repairs
+            property_major_repairs_value_check
+            sheltered_accommodation
+          ],
+                                                          )
       end
     end
   end
