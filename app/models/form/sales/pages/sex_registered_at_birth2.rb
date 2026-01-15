bbbf5095e34a7c24d848352454a8d2bba14aa5fc
Katherine Langford <katherine.langford@softwire.com>
2026-01-15 11:48:36 +0000

    CLDC-4177: Adds sex-at-birth question for other household members

---
+++ app/models/form/sales/pages/sex_registered_at_birth2.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Form::Sales::Pages::SexRegisteredAtBirth2 < ::Form::Page
+  def initialize(id, hsh, subsection)
+    super
+    @id = "buyer_2_sex_registered_at_birth"
+    @copy_key = "sales.household_characteristics.sex2.buyer"
+    @depends_on = [
+      {
+        "joint_purchase?" => true,
+        "buyer_has_seen_privacy_notice?" => true,
+      },
+      {
+        "joint_purchase?" => true,
+        "buyer_not_interviewed?" => true,
+      },
+    ]
+  end
+
+  def questions
+    @questions ||= [
+      Form::Sales::Questions::SexRegisteredAtBirth2.new(nil, nil, self),
+    ]
+  end
+end
--- app/models/form/sales/questions/sex_registered_at_birth2.rb
+++ app/models/form/sales/questions/sex_registered_at_birth2.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-class Form::Sales::Questions::SexRegisteredAtBirth1 < ::Form::Question
+class Form::Sales::Questions::SexRegisteredAtBirth2 < ::Form::Question
   def initialize(id, hsh, page)
     super
-    @id = "sexRAB1"
+    @id = "sexRAB2"
     @type = "radio"
-    @check_answers_card_number = 1
+    @copy_key = "sales.household_characteristics.sexRAB2.buyer"
+    @check_answers_card_number = 2
+    @inferred_check_answers_value = [{
+      "condition" => {
+        "sex2" => "R",
+      },
+      "value" => "Prefers not to say",
+    }]
     @answer_options = ANSWER_OPTIONS
     @question_number = QUESTION_NUMBER_FROM_YEAR[form.start_date.year] || QUESTION_NUMBER_FROM_YEAR[QUESTION_NUMBER_FROM_YEAR.keys.max]
   end
