04d0a54353393bfbf0a6089dd8fa73d3e3a0ed0b
Katherine Langford <katherine.langford@softwire.com>
2026-01-15 16:58:40 +0000

    CLCD-4177: Unit tests

---
+++ spec/models/form/sales/questions/sex_registered_at_birth1_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe Form::Sales::Questions::SexRegisteredAtBirth1, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form, start_date: Time.zone.local(2026, 4, 1)) }
+
+  before do
+    allow(page).to receive(:subsection).and_return(subsection)
+    allow(subsection).to receive(:form).and_return(form)
+  end
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("sexRAB1")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?(nil)).to be false
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq({
+      "F" => { "value" => "Female" },
+      "M" => { "value" => "Male" },
+      "divider" => { "value" => true },
+      "R" => { "value" => "Buyer prefers not to say" },
+    })
+  end
+
+  it "has the correct check_answers_card_number" do
+    expect(question.check_answers_card_number).to eq(1)
+  end
+end
