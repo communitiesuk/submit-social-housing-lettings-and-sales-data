0d42e009bc650d26cdf1abecc5be01d8a6138b46
Katherine Langford <katherine.langford@softwire.com>
2026-01-15 11:48:36 +0000

    CLDC-4177: Adds sex-at-birth question for other household members

---
+++ app/models/form/sales/pages/person_sex_registered_at_birth.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Form::Sales::Pages::PersonSexRegisteredAtBirth < ::Form::Page
+  def initialize(id, hsh, subsection, person_index:)
+    super(id, hsh, subsection)
+    @copy_key = "sales.household_characteristics.sex2.person" if person_index == 2
+    @person_index = person_index
+    @depends_on = [
+      { "details_known_#{person_index}" => 1 },
+    ]
+  end
+
+  def questions
+    @questions ||= [
+      Form::Sales::Questions::PersonSexRegisteredAtBirth.new("sexRAB#{@person_index}", nil, self, person_index:@person_index),
+    ]
+  end
+end
