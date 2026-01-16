24e147ab9a0ea6dbc7d0d3a0bd21b9eece3d670b
Katherine Langford <katherine.langford@softwire.com>
2026-01-16 14:49:04 +0000

    CLDC-4177: Misc inclusions where sex is also used

--- app/services/csv/sales_log_csv_service.rb
+++ app/services/csv/sales_log_csv_service.rb
@@ -114,6 +114,7 @@ module Csv
       hash["age1"] = { "refused_code" => "-9", "refused_label" => "Not known", "age_known_field" => "age1_known" }
       (2..6).each do |i|
         hash["age#{i}"] = { "refused_code" => "-9", "refused_label" => "Not known", "details_known_field" => "details_known_#{i}", "age_known_field" => "age#{i}_known" }
+        hash["sexRAB#{i}"] = { "refused_code" => "R", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
         hash["sex#{i}"] = { "refused_code" => "R", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
         hash["relat#{i}"] = { "refused_code" => "R", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
         hash["ecstat#{i}"] = { "refused_code" => "10", "refused_label" => "Prefers not to say", "details_known_field" => "details_known_#{i}" }
