9bff01bab2ce31b5c085de28cd26247e015d7e68
Samuel Young <samuel.young@softwire.com>
2026-01-19 12:25:07 +0000

    Revert "CLDC-4156: Migrate existing scheme options"
    This reverts commit b81ac1c7e467cef551d103b11e05d4276a75c77f.

--- db/migrate/20260114160036_update_scheme_care_standards_values.rb
+++
@@ -1,5 +0,0 @@
-class UpdateSchemeCareStandardsValues < ActiveRecord::Migration[7.2]
-  def change
-    Scheme.where(registered_under_care_act: [3, 4]).update!(registered_under_care_act: 5)
-  end
-end
