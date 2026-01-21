7abbebb47b838ea75e28c33d80e7ff1d171a6f33
Oscar Richardson <116292912+oscar-richardson-softwire@users.noreply.github.com>
2026-01-21 13:54:13 +0000

    CLDC-4144: Reorder questions (#3140)
    * CLDC-4136: Hide question on UI flow
    
    * CLDC-4136: Hide question from log download csv
    
    * CLDC-4136: Remove question from BU parser
    
    relabel all following fields
    
    * Revert "fixup! CLDC-4136: Remove question from BU parser"
    
    This reverts commit cda3ec109943d23120c6e55a05ae02026283d147.
    
    * Revert "CLDC-4136: Remove question from BU parser"
    
    This reverts commit e4715ee976c6a4ade30a61b11f51af8bce211324.
    
    * fixup! CLDC-4136: Hide question on UI flow
    
    remove from question list instead
    
    * fixup! CLDC-4136: Hide question from CDS export
    
    use correct year in dates
    
    * fixup! fixup! CLDC-4136: Hide question from CDS export
    
    use correct year in all dates
    
    * Reorder questions
    
    * Revert "Reorder questions"
    
    This reverts commit ffbd4d94dc88369556700db4909ee256eb4e9b03.
    
    * Reorder questions for 2026 onwards only
    
    * Fix tests after rebase
    
    # Conflicts:
    #	spec/fixtures/files/lettings_log_csv_export_codes_26.csv
    #	spec/fixtures/files/lettings_log_csv_export_labels_26.csv
    #	spec/fixtures/files/lettings_log_csv_export_non_support_codes_26.csv
    #	spec/fixtures/files/lettings_log_csv_export_non_support_labels_26.csv
    
    * Fix model tests
    
    * Lint
    
    * Correct test descriptions
    
    * Fix after rebase
    
    ---------
    
    Co-authored-by: Samuel Young <samuel.young@softwire.com>

--- spec/models/form/lettings/subsections/household_characteristics_spec.rb
+++ spec/models/form/lettings/subsections/household_characteristics_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe Form::Lettings::Subsections::HouseholdCharacteristics, type: :mod
   context "with start year before 2024" do
     before do
       allow(form).to receive(:start_year_2024_or_later?).and_return(false)
+      allow(form).to receive(:start_year_2026_or_later?).and_return(false)
     end
 
     it "has correct pages" do
@@ -151,9 +152,10 @@ RSpec.describe Form::Lettings::Subsections::HouseholdCharacteristics, type: :mod
     end
   end
 
-  context "with start year >= 2024" do
+  context "with start year 2024" do
     before do
       allow(form).to receive(:start_year_2024_or_later?).and_return(true)
+      allow(form).to receive(:start_year_2026_or_later?).and_return(false)
     end
 
     it "has correct pages" do
@@ -305,10 +307,11 @@ RSpec.describe Form::Lettings::Subsections::HouseholdCharacteristics, type: :mod
     end
   end
 
-  context "with start year >= 2025" do
+  context "with start year 2025" do
     before do
       allow(form).to receive(:start_year_2024_or_later?).and_return(true)
       allow(form).to receive(:start_year_2025_or_later?).and_return(true)
+      allow(form).to receive(:start_year_2026_or_later?).and_return(false)
     end
 
     it "has correct pages" do
@@ -443,6 +446,162 @@ RSpec.describe Form::Lettings::Subsections::HouseholdCharacteristics, type: :mod
           relationship_8_partner_under_16_value_check
           relationship_8_multiple_partners_value_check
           person_8_age
+          no_females_pregnant_household_person_8_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_8_age_value_check
+          age_8_under_retirement_value_check
+          age_8_over_retirement_value_check
+          age_8_partner_under_16_value_check
+          person_8_gender_identity
+          no_females_pregnant_household_person_8_value_check
+          females_in_soft_age_range_in_pregnant_household_person_8_value_check
+          gender_8_over_retirement_value_check
+          person_8_working_situation
+          working_situation_8_under_retirement_value_check
+          working_situation_8_over_retirement_value_check
+        ],
+      )
+    end
+  end
+
+  context "with start year >= 2026" do
+    before do
+      allow(form).to receive(:start_year_2024_or_later?).and_return(true)
+      allow(form).to receive(:start_year_2025_or_later?).and_return(true)
+      allow(form).to receive(:start_year_2026_or_later?).and_return(true)
+    end
+
+    it "has correct pages" do
+      expect(household_characteristics.pages.map(&:id)).to eq(
+        %w[
+          household_members
+          no_females_pregnant_household_lead_hhmemb_value_check
+          females_in_soft_age_range_in_pregnant_household_lead_hhmemb_value_check
+          lead_tenant_age
+          no_females_pregnant_household_lead_age_value_check
+          females_in_soft_age_range_in_pregnant_household_lead_age_value_check
+          age_lead_tenant_under_retirement_value_check
+          age_lead_tenant_over_retirement_value_check
+          lead_tenant_gender_identity
+          no_females_pregnant_household_lead_value_check
+          females_in_soft_age_range_in_pregnant_household_lead_value_check
+          gender_lead_tenant_over_retirement_value_check
+          lead_tenant_ethnic_group
+          lead_tenant_ethnic_background_arab
+          lead_tenant_ethnic_background_asian
+          lead_tenant_ethnic_background_black
+          lead_tenant_ethnic_background_mixed
+          lead_tenant_ethnic_background_white
+          lead_tenant_nationality
+          lead_tenant_working_situation
+          working_situation_lead_tenant_under_retirement_value_check
+          working_situation_lead_tenant_over_retirement_value_check
+          person_2_known
+          person_2_age
+          person_2_lead_partner
+          relationship_2_partner_under_16_value_check
+          relationship_2_multiple_partners_value_check
+          no_females_pregnant_household_person_2_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_2_age_value_check
+          age_2_under_retirement_value_check
+          age_2_over_retirement_value_check
+          age_2_partner_under_16_value_check
+          person_2_gender_identity
+          no_females_pregnant_household_person_2_value_check
+          females_in_soft_age_range_in_pregnant_household_person_2_value_check
+          gender_2_over_retirement_value_check
+          person_2_working_situation
+          working_situation_2_under_retirement_value_check
+          working_situation_2_over_retirement_value_check
+          person_3_known
+          person_3_age
+          person_3_lead_partner
+          relationship_3_partner_under_16_value_check
+          relationship_3_multiple_partners_value_check
+          no_females_pregnant_household_person_3_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_3_age_value_check
+          age_3_under_retirement_value_check
+          age_3_over_retirement_value_check
+          age_3_partner_under_16_value_check
+          person_3_gender_identity
+          no_females_pregnant_household_person_3_value_check
+          females_in_soft_age_range_in_pregnant_household_person_3_value_check
+          gender_3_over_retirement_value_check
+          person_3_working_situation
+          working_situation_3_under_retirement_value_check
+          working_situation_3_over_retirement_value_check
+          person_4_known
+          person_4_age
+          person_4_lead_partner
+          relationship_4_partner_under_16_value_check
+          relationship_4_multiple_partners_value_check
+          no_females_pregnant_household_person_4_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_4_age_value_check
+          age_4_under_retirement_value_check
+          age_4_over_retirement_value_check
+          age_4_partner_under_16_value_check
+          person_4_gender_identity
+          no_females_pregnant_household_person_4_value_check
+          females_in_soft_age_range_in_pregnant_household_person_4_value_check
+          gender_4_over_retirement_value_check
+          person_4_working_situation
+          working_situation_4_under_retirement_value_check
+          working_situation_4_over_retirement_value_check
+          person_5_known
+          person_5_age
+          person_5_lead_partner
+          relationship_5_partner_under_16_value_check
+          relationship_5_multiple_partners_value_check
+          no_females_pregnant_household_person_5_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_5_age_value_check
+          age_5_under_retirement_value_check
+          age_5_over_retirement_value_check
+          age_5_partner_under_16_value_check
+          person_5_gender_identity
+          no_females_pregnant_household_person_5_value_check
+          females_in_soft_age_range_in_pregnant_household_person_5_value_check
+          gender_5_over_retirement_value_check
+          person_5_working_situation
+          working_situation_5_under_retirement_value_check
+          working_situation_5_over_retirement_value_check
+          person_6_known
+          person_6_age
+          person_6_lead_partner
+          relationship_6_partner_under_16_value_check
+          relationship_6_multiple_partners_value_check
+          no_females_pregnant_household_person_6_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_6_age_value_check
+          age_6_under_retirement_value_check
+          age_6_over_retirement_value_check
+          age_6_partner_under_16_value_check
+          person_6_gender_identity
+          no_females_pregnant_household_person_6_value_check
+          females_in_soft_age_range_in_pregnant_household_person_6_value_check
+          gender_6_over_retirement_value_check
+          person_6_working_situation
+          working_situation_6_under_retirement_value_check
+          working_situation_6_over_retirement_value_check
+          person_7_known
+          person_7_age
+          person_7_lead_partner
+          relationship_7_partner_under_16_value_check
+          relationship_7_multiple_partners_value_check
+          no_females_pregnant_household_person_7_age_value_check
+          females_in_soft_age_range_in_pregnant_household_person_7_age_value_check
+          age_7_under_retirement_value_check
+          age_7_over_retirement_value_check
+          age_7_partner_under_16_value_check
+          person_7_gender_identity
+          no_females_pregnant_household_person_7_value_check
+          females_in_soft_age_range_in_pregnant_household_person_7_value_check
+          gender_7_over_retirement_value_check
+          person_7_working_situation
+          working_situation_7_under_retirement_value_check
+          working_situation_7_over_retirement_value_check
+          person_8_known
+          person_8_age
+          person_8_lead_partner
+          relationship_8_partner_under_16_value_check
+          relationship_8_multiple_partners_value_check
           no_females_pregnant_household_person_8_age_value_check
           females_in_soft_age_range_in_pregnant_household_person_8_age_value_check
           age_8_under_retirement_value_check
