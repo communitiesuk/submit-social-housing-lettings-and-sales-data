aa1ddb7c10e9a7b58468b4212126583f6afd9aac
Samuel Young <samuel.young@softwire.com>
2026-01-29 17:19:44 +0000

    CLDC-4151: Add model tests for new questions

---
+++ spec/models/form/lettings/pages/referral_register_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Pages::ReferralRegister, type: :model do
+  subject(:page) { described_class.new(page_id, page_definition, subsection) }
+
+  let(:page_id) { nil }
+  let(:page_definition) { nil }
+  let(:subsection) { instance_double(Form::Subsection, form:) }
+  let(:form) { instance_double(Form, start_date: Time.zone.today) }
+
+  before do
+    allow(subsection).to receive(:form).and_return(form)
+  end
+
+  it "has correct subsection" do
+    expect(page.subsection).to eq(subsection)
+  end
+
+  it "has correct questions" do
+    expect(page.questions.map(&:id)).to eq(%w[referral_register])
+  end
+
+  it "has the correct id" do
+    expect(page.id).to eq("referral_register")
+  end
+
+  it "has the correct description" do
+    expect(page.description).to be_nil
+  end
+
+  it "has the correct depends_on" do
+    expect(page.depends_on).to be nil
+  end
+end
---
+++ spec/models/form/lettings/questions/referral_register_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe Form::Lettings::Questions::ReferralRegister, type: :model do
+  subject(:question) { described_class.new(question_id, question_definition, page) }
+
+  let(:question_id) { nil }
+  let(:question_definition) { nil }
+  let(:page) { instance_double(Form::Page) }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form, start_date: Time.zone.today) }
+
+  before do
+    allow(page).to receive(:subsection).and_return(subsection)
+    allow(subsection).to receive(:form).and_return(form)
+  end
+
+  it "has correct page" do
+    expect(question.page).to eq(page)
+  end
+
+  it "has the correct id" do
+    expect(question.id).to eq("referral_register")
+  end
+
+  it "has the correct type" do
+    expect(question.type).to eq("radio")
+  end
+
+  it "is not marked as derived" do
+    expect(question.derived?(nil)).to be false
+  end
+
+  it "has the correct answer_options" do
+    expect(question.answer_options).to eq(
+      {
+        "1" => {
+          "value" => "Answer A",
+        },
+        "2" => {
+          "value" => "Answer B",
+        },
+      }.freeze,
+    )
+  end
+
+  it "has the correct question_number" do
+    expect(question.question_number).to eq(84)
+  end
+end
