e409dd79f9c20a5e8b18a061c25b987ccf8d78c2
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-02-04 09:01:21 +0000

    CLDC-4141: update row parser test

--- spec/services/bulk_upload/lettings/year2026/row_parser_spec.rb
+++ spec/services/bulk_upload/lettings/year2026/row_parser_spec.rb
@@ -245,6 +245,9 @@ RSpec.describe BulkUpload::Lettings::Year2026::RowParser do
             field_4: "1",
 
             field_18: "12",
+
+            field_130: "F",
+            field_131: "M",
           }
         end
 
@@ -1839,7 +1842,7 @@ RSpec.describe BulkUpload::Lettings::Year2026::RowParser do
       let(:attributes) { { bulk_upload: } }
 
       before do
-        build(:lettings_log, owning_organisation: nil, startdate: nil, tenancycode: nil, location: nil, age1: nil, sex1: nil, ecstat1: nil, brent: nil, scharge: nil, pscharge: nil, supcharg: nil).save(validate: false)
+        build(:lettings_log, owning_organisation: nil, startdate: nil, tenancycode: nil, location: nil, age1: nil, sexrab1: nil, sex1: nil, ecstat1: nil, brent: nil, scharge: nil, pscharge: nil, supcharg: nil).save(validate: false)
       end
 
       it "does not add duplicate logs validation to the blank row" do
