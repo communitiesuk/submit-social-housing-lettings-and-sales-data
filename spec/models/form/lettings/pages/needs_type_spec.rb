8213e7a58f68db97835dcc066a1a17231f641fb1
Samuel Young <samuel.young@softwire.com>
2026-02-09 12:27:01 +0000

    CLDC-4154: Update tests

--- spec/models/form/lettings/pages/needs_type_spec.rb
+++ spec/models/form/lettings/pages/needs_type_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Form::Lettings::Pages::NeedsType, type: :model do
 
   let(:page_id) { nil }
   let(:page_definition) { nil }
-  let(:subsection) { instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2024, 4, 1))) }
+  let(:subsection) { instance_double(Form::Subsection, form: instance_double(Form, start_date: Time.zone.local(2024, 4, 1), start_year_2026_or_later?: false)) }
 
   it "has correct subsection" do
     expect(page.subsection).to eq(subsection)
