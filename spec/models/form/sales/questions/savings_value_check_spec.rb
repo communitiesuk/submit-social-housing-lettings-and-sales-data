71138476d4226d4456d1358f47cdab224568b65e
Nat Dean-Lewis <94526761+natdeanlewissoftwire@users.noreply.github.com>
2026-02-16 14:28:13 +0000

    CLDC-1980: Add income and benefits CYA label (#3172)
    * CLDC-1980: copy setup from CLDC-4200
    
    * CLDC-1980: add All buyers labels
    
    * CLDC-1980: add All buyers labels to soft validations for 2026 onwards
    
    * CLDC-1980: update tests
    
    * CLDC-1980: update base q tests
    
    * CLDC-1980: reduce number of questions changed to first per section
    
    * CLDC-1980: undo redundant test changes
    
    * CLDC-1980: add new required args to page specs
    
    * CLDC-1980: add new required args to page specs

--- spec/models/form/sales/questions/savings_value_check_spec.rb
+++ spec/models/form/sales/questions/savings_value_check_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe Form::Sales::Questions::SavingsValueCheck, type: :model do
   let(:question_id) { nil }
   let(:question_definition) { nil }
   let(:page) { instance_double(Form::Page) }
+  let(:subsection) { instance_double(Form::Subsection) }
+  let(:form) { instance_double(Form) }
+
+  before do
+    allow(form).to receive(:start_year_2026_or_later?).and_return(true)
+    allow(page).to receive(:subsection).and_return(subsection)
+    allow(subsection).to receive(:form).and_return(form)
+  end
 
   it "has correct page" do
     expect(question.page).to eq(page)
@@ -23,8 +31,24 @@ RSpec.describe Form::Sales::Questions::SavingsValueCheck, type: :model do
     expect(question.derived?(nil)).to be false
   end
 
-  it "has a correct check_answers_card_number" do
-    expect(question.check_answers_card_number).to eq(0)
+  context "when in 2025", { year: 25 } do
+    before do
+      allow(form).to receive(:start_year_2026_or_later?).and_return(false)
+    end
+
+    it "has check_answers_card_number set to 0" do
+      expect(question.check_answers_card_number).to eq(0)
+    end
+  end
+
+  context "when in 2026", { year: 26 } do
+    before do
+      allow(form).to receive(:start_year_2026_or_later?).and_return(true)
+    end
+
+    it "does not have a check_answers_card_number" do
+      expect(question.check_answers_card_number).to be_nil
+    end
   end
 
   it "has the correct answer_options" do
