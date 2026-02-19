1d385f0fc1a65513cc8205d3b5e32971b9e99400
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-02-19 09:39:16 +0000

    CLDC-4173: add new buildheightclass db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_04_100051) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_19_093257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -818,6 +818,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_04_100051) do
     t.string "sexrab4"
     t.string "sexrab5"
     t.string "sexrab6"
+    t.integer "buildheightclass"
     t.index ["assigned_to_id"], name: "index_sales_logs_on_assigned_to_id"
     t.index ["bulk_upload_id"], name: "index_sales_logs_on_bulk_upload_id"
     t.index ["created_by_id"], name: "index_sales_logs_on_created_by_id"
