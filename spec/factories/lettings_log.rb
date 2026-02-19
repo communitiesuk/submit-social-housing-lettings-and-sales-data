d4d1c00c0589bd39c11c62c7143d398b5ebba4f5
samyou-softwire <samuel.young@softwire.com>
2026-02-19 17:23:55 +0000

    CLDC-4119: Add UPRN to duplicate log trait

--- spec/factories/lettings_log.rb
+++ spec/factories/lettings_log.rb
@@ -37,7 +37,8 @@ FactoryBot.define do
       tenancycode { "same tenancy code" }
       address_line1 { "same address line 1" }
       postcode_full { "A1 1AA" }
-      uprn_known { 0 }
+      uprn_known { 1 }
+      uprn { "1234567890" }
       declaration { 1 }
       age1_known { 0 }
       age1 { 18 }
