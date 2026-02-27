c0230571a07f6eba3fd104711db7e54934a8d862
samyou-softwire <samuel.young@softwire.com>
2026-02-27 14:16:22 +0000

    fixup! CLDC-4168: Rename mortgage_length_known to mortlen_known

--- db/migrate/20260224141705_add_mortlen_known_to_sales_logs.rb
+++ db/migrate/20260224141705_add_mortlen_known_to_sales_logs.rb
@@ -1,4 +1,4 @@
-class AddMortgageLengthKnownToSalesLogs < ActiveRecord::Migration[7.2]
+class AddMortlenKnownToSalesLogs < ActiveRecord::Migration[7.2]
   def change
     add_column :sales_logs, :mortlen_known, :integer
   end
