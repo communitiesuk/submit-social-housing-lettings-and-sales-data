06db6228f76fc5ba6089c8275ffc5772a967dea2
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-03-05 16:35:12 +0000

    CLDC-4175: update row parser spec and examples

--- spec/services/bulk_upload/sales/year2026/row_parser_spec.rb
+++ spec/services/bulk_upload/sales/year2026/row_parser_spec.rb
@@ -113,6 +113,8 @@ RSpec.describe BulkUpload::Sales::Year2026::RowParser do
       field_106: "2023",
       field_110: "900",
       field_122: "1",
+      field_123: "1",
+      field_124: "150",
     }
   end
 
