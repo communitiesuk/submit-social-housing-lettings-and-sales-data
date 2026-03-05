889a232f87192686bd0a3f736dacd0ebd8ef1398
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-03-05 10:06:05 +0000

    CLDC-4175: lint

--- db/migrate/20260305095832_add_service_charge_changed_to_sales_logs.rb
+++ db/migrate/20260305095832_add_service_charge_changed_to_sales_logs.rb
@@ -1,6 +1,8 @@
 class AddServiceChargeChangedToSalesLogs < ActiveRecord::Migration[7.2]
   def change
-    add_column :sales_logs, :hasservicechargeschanged, :integer
-    add_column :sales_logs, :newservicecharges, :decimal, precision: 10, scale: 2
+    change_table :sales_logs, bulk: true do |t|
+      t.column :hasservicechargeschanged, :integer
+      t.column :newservicecharges, :decimal, precision: 10, scale: 2
+    end
   end
 end
