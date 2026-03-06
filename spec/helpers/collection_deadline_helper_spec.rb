4a4187863039df8549639a389c46c67b8f93920f
samyou-softwire <samuel.young@softwire.com>
2026-03-06 11:25:43 +0000

    CLDC-4221: Add 2026 collection deadlines

--- spec/helpers/collection_deadline_helper_spec.rb
+++ spec/helpers/collection_deadline_helper_spec.rb
@@ -5,25 +5,25 @@ RSpec.describe CollectionDeadlineHelper do
   let(:user) { create(:user, :data_coordinator) }
 
   describe "#quarter_for_date" do
-    it "returns correct cutoff date for the first quarter of 2024/25" do
-      quarter = quarter_for_date(date: Time.zone.local(2024, 4, 1))
-      expect(quarter.cutoff_date).to eq(Time.zone.local(2024, 7, 12))
-      expect(quarter.quarter_start_date).to eq(Time.zone.local(2024, 4, 1))
-      expect(quarter.quarter_end_date).to eq(Time.zone.local(2024, 6, 30))
+    it "returns correct cutoff date for the first quarter of 2025/26" do
+      quarter = quarter_for_date(date: Time.zone.local(2025, 4, 1))
+      expect(quarter.cutoff_date).to eq(Time.zone.local(2025, 7, 11))
+      expect(quarter.quarter_start_date).to eq(Time.zone.local(2025, 4, 1))
+      expect(quarter.quarter_end_date).to eq(Time.zone.local(2025, 6, 30))
     end
 
-    it "returns correct cutoff date for the second quarter of 2024/25" do
-      quarter = quarter_for_date(date: Time.zone.local(2024, 9, 30))
-      expect(quarter.cutoff_date).to eq(Time.zone.local(2024, 10, 11))
-      expect(quarter.quarter_start_date).to eq(Time.zone.local(2024, 7, 1))
-      expect(quarter.quarter_end_date).to eq(Time.zone.local(2024, 9, 30))
+    it "returns correct cutoff date for the second quarter of 2025/26" do
+      quarter = quarter_for_date(date: Time.zone.local(2025, 9, 30))
+      expect(quarter.cutoff_date).to eq(Time.zone.local(2025, 10, 10))
+      expect(quarter.quarter_start_date).to eq(Time.zone.local(2025, 7, 1))
+      expect(quarter.quarter_end_date).to eq(Time.zone.local(2025, 9, 30))
     end
 
-    it "returns correct cutoff date for the third quarter of 2024/25" do
-      quarter = quarter_for_date(date: Time.zone.local(2024, 10, 25))
-      expect(quarter.cutoff_date).to eq(Time.zone.local(2025, 1, 10))
-      expect(quarter.quarter_start_date).to eq(Time.zone.local(2024, 10, 1))
-      expect(quarter.quarter_end_date).to eq(Time.zone.local(2024, 12, 31))
+    it "returns correct cutoff date for the third quarter of 2025/26" do
+      quarter = quarter_for_date(date: Time.zone.local(2025, 10, 25))
+      expect(quarter.cutoff_date).to eq(Time.zone.local(2026, 1, 16))
+      expect(quarter.quarter_start_date).to eq(Time.zone.local(2025, 10, 1))
+      expect(quarter.quarter_end_date).to eq(Time.zone.local(2025, 12, 31))
     end
   end
 end
