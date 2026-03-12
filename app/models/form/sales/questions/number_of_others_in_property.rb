cd51f7c2c452066ff5a7fc99bc2f75b57b0d9362
Nat Dean-Lewis <94526761+natdeanlewissoftwire@users.noreply.github.com>
2026-03-12 12:53:47 +0000

    CLDC-4166: How many live in property updates (#3194)
    * CLDC-4166: question updaets
    
    * CLDC-4166: question updates
    
    * CLDC-4166: test updates
    
    * CLDC-4166: test updates
    
    * CLDC-4166: test updates
    
    * CLDC-4226: Index logs by org and ID (#3199)
    
    * CLDC-4226: index logs by org and id
    
    * CLDC-4226: include teamwide non-functional schema change
    
    * CLDC-4226: schema update
    
    * CLDC-4176: fix failing tests
    
    * CLDC-4176: renumber case insensitive fields test
    
    * CLDC-4166: update csv export spec
    
    * CLDC-4166: linting
    
    * CLDC-4166: copy updates in fixtures
    
    * CLDC-4166: copy updates in fixtures
    
    * CLDC-4166: copy updates in fixtures
    
    * CLDC-4166: var defs count update
    
    * CLDC-4166: fixtures tweak for hhtype
    
    * CLDC-4166: fixtures tweak for hhtype
    
    * CLDC-4166: fixtures tweak for hhtype
    
    * CLDC-4166: export update
    
    * CLDC-4166: whitespace linting
    
    * CLDC-4166: update number_of_others_in_property_spec.rb
    
    * CLDC-4166: update log_variable_definitions_spec.rb
    
    * CLDC-4166: update sales_log_csv_service_spec.rb
    
    * CLDC-4166: update tests
    
    * CLDC-4166: update tests

--- app/models/form/sales/questions/number_of_others_in_property.rb
+++ app/models/form/sales/questions/number_of_others_in_property.rb
@@ -5,7 +5,7 @@ class Form::Sales::Questions::NumberOfOthersInProperty < ::Form::Question
     @copy_key = joint_purchase ? "sales.household_characteristics.hholdcount.joint_purchase" : "sales.household_characteristics.hholdcount.not_joint_purchase"
     @type = "numeric"
     @width = 2
-    @min = 0
+    @min = form.start_year_2026_or_later? ? 1 : 0
     @max = 15
     @step = 1
     @question_number = get_question_number_from_hash(QUESTION_NUMBER_FROM_YEAR)
