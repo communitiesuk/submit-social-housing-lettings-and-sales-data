c085cbb57270eb6be12ce88834e53e5e94cd8bc9
Samuel Young <samuel.young@softwire.com>
2026-03-12 09:46:57 +0000

    CLDC-4179: Amend sales duplicate log check (#3207)
    * CLDC-4179: Add sexrab & postcode to frontend duplicate log checks
    
    * CLDC-4179: Add sexrab & postcode to BU duplicate log checks
    
    * CLDC-4179: Update tests

--- spec/services/bulk_upload/sales/year2026/row_parser_spec.rb
+++ spec/services/bulk_upload/sales/year2026/row_parser_spec.rb
@@ -790,6 +790,8 @@ RSpec.describe BulkUpload::Sales::Year2026::RowParser do
           :field_1, # Sale completion date
           :field_2, # Sale completion date
           :field_3, # Sale completion date
+          :field_16, # UPRN
+          :field_17, # Address line 1
           :field_21, # Postcode
           :field_22, # Postcode
           :field_28, # Buyer 1 age
@@ -820,6 +822,8 @@ RSpec.describe BulkUpload::Sales::Year2026::RowParser do
           :field_1, # Sale completion date
           :field_2, # Sale completion date
           :field_3, # Sale completion date
+          :field_16, # UPRN
+          :field_17, # Address line 1
           :field_21, # Postcode
           :field_22, # Postcode
           :field_28, # Buyer 1 age
