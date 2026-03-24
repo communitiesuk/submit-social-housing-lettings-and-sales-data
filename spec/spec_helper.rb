0a0335ea0cfb5659102546dbf9cd938898119948
samyou-softwire <samuel.young@softwire.com>
2026-03-24 17:33:15 +0000

    CLDC-4325: Run tests on 31st march

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -123,6 +123,14 @@ RSpec.configure do |config|
     RequestHelper.real_http_requests
     Singleton.__init__(FormHandler)
   end
+
+  config.around do |example|
+    Timecop.travel(Time.zone.local(2026, 3, 31))
+    Singleton.__init__(FormHandler)
+    example.run
+    Timecop.travel(Time.zone.local(2026, 3, 31))
+    Singleton.__init__(FormHandler)
+  end
 end
 
 RSpec::Matchers.define_negated_matcher :not_change, :change
