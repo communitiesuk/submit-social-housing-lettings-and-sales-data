a4235b0cb0ec660aac55e802510de8d935d619aa
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-04-23 16:41:36 +0100

    CLDC-4269: reformat

--- app/models/form/lettings/pages/person_known.rb
+++ app/models/form/lettings/pages/person_known.rb
@@ -2,14 +2,23 @@ class Form::Lettings::Pages::PersonKnown < ::Form::Page
   def initialize(id, hsh, subsection, person_index:)
     super(id, hsh, subsection)
     @id = "person_#{person_index}_known"
-    @depends_on = [{ "hhmemb" => {
-      "operator" => ">=",
-      "operand" => @person_index,
-    } }]
+    @depends_on = [{
+      "hhmemb" => {
+        "operator" => ">=",
+        "operand" => @person_index,
+      },
+    }]
     @person_index = person_index
   end
 
   def questions
     @questions ||= [Form::Lettings::Questions::DetailsKnown.new(nil, nil, self, person_index: @person_index)]
+  end
+
+  def depends_on
+    [{ "hhmemb" => {
+      "operator" => ">=",
+      "operand" => @person_index,
+    } }]
   end
 end
