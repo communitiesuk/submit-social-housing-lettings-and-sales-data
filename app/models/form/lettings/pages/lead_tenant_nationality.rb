2d8b1d3e25a3986d880e6e6e295212d57a4c6db1
samyou-softwire <samuel.young@softwire.com>
2026-04-24 10:05:04 +0100

    CLDC-4402: lettings pages

--- app/models/form/lettings/pages/lead_tenant_nationality.rb
+++ app/models/form/lettings/pages/lead_tenant_nationality.rb
@@ -2,18 +2,14 @@ class Form::Lettings::Pages::LeadTenantNationality < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "lead_tenant_nationality"
-    @copy_key = "lettings.household_characteristics.#{form.start_year_2024_or_later? ? 'nationality_all' : 'national'}"
+    @copy_key = "lettings.household_characteristics.nationality_all"
     @depends_on = [{ "declaration" => 1 }]
   end
 
   def questions
-    @questions ||= if form.start_year_2024_or_later?
-                     [
-                       Form::Lettings::Questions::NationalityAllGroup.new(nil, nil, self),
-                       Form::Lettings::Questions::NationalityAll.new(nil, nil, self),
-                     ]
-                   else
-                     [Form::Lettings::Questions::Nationality.new(nil, nil, self)]
-                   end
+    @questions ||= [
+      Form::Lettings::Questions::NationalityAllGroup.new(nil, nil, self),
+      Form::Lettings::Questions::NationalityAll.new(nil, nil, self),
+    ]
   end
 end
