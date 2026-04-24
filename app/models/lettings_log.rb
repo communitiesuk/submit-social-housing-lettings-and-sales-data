c035372eec33fcfad149bdee582d4e2a15401d9c
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-04-24 17:04:48 +0100

    CLDC-4269: comment for not fully implemented const

--- app/models/lettings_log.rb
+++ app/models/lettings_log.rb
@@ -191,7 +191,7 @@ class LettingsLog < Log
   NUM_OF_WEEKS_FROM_PERIOD = { 2 => 26, 3 => 13, 4 => 12, 5 => 50, 6 => 49, 7 => 48, 8 => 47, 9 => 46, 11 => 51, 1 => 52, 10 => 53 }.freeze
   SUFFIX_FROM_PERIOD = { 2 => "every 2 weeks", 3 => "every 4 weeks", 4 => "every month" }.freeze
   DUPLICATE_LOG_ATTRIBUTES = %w[owning_organisation_id tenancycode startdate age1_known age1 sex1 sexrab1 ecstat1 tcharge household_charge chcharge].freeze
-  MAX_PEOPLE_WITH_DETAILS = 8
+  MAX_PEOPLE_WITH_DETAILS = 8 # This is not yet used in all lettings validations etc. so check for other occurrences of this concept if updating this
   RENT_TYPE = {
     social_rent: 0,
     affordable_rent: 1,
