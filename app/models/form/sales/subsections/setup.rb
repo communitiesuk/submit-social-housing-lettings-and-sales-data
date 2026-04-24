bb8f0b8321553212197795369883d74ce667f5c9
samyou-softwire <samuel.young@softwire.com>
2026-04-24 10:26:25 +0100

    CLDC-4402: sales subsections

--- app/models/form/sales/subsections/setup.rb
+++ app/models/form/sales/subsections/setup.rb
@@ -21,10 +21,10 @@ class Form::Sales::Subsections::Setup < ::Form::Subsection
       (Form::Sales::Pages::BuyerLive.new(nil, nil, self) unless form.start_year_2025_or_later?),
       Form::Sales::Pages::JointPurchase.new(nil, nil, self),
       Form::Sales::Pages::NumberJointBuyers.new(nil, nil, self),
-      (Form::Sales::Pages::BuyerInterview.new("buyer_interview_joint_purchase", nil, self, joint_purchase: true) if form.start_year_2024_or_later?),
-      (Form::Sales::Pages::BuyerInterview.new("buyer_interview", nil, self, joint_purchase: false) if form.start_year_2024_or_later?),
-      (Form::Sales::Pages::PrivacyNotice.new("privacy_notice_joint_purchase", nil, self, joint_purchase: true) if form.start_year_2024_or_later?),
-      (Form::Sales::Pages::PrivacyNotice.new("privacy_notice", nil, self, joint_purchase: false) if form.start_year_2024_or_later?),
+      Form::Sales::Pages::BuyerInterview.new("buyer_interview_joint_purchase", nil, self, joint_purchase: true),
+      Form::Sales::Pages::BuyerInterview.new("buyer_interview", nil, self, joint_purchase: false),
+      Form::Sales::Pages::PrivacyNotice.new("privacy_notice_joint_purchase", nil, self, joint_purchase: true),
+      Form::Sales::Pages::PrivacyNotice.new("privacy_notice", nil, self, joint_purchase: false),
     ].flatten.compact
   end
 end
