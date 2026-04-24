53ccfd9c048ab527ffe2439196dcf087277f5443
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-04-24 16:39:00 +0100

    CLDC-4269: move depends_on after person_index for safety

--- app/models/form/lettings/pages/person_known.rb
+++ app/models/form/lettings/pages/person_known.rb
@@ -2,8 +2,8 @@ class Form::Lettings::Pages::PersonKnown < ::Form::Page
   def initialize(id, hsh, subsection, person_index:)
     super(id, hsh, subsection)
     @id = "person_#{person_index}_known"
-    @depends_on = depends_on
     @person_index = person_index
+    @depends_on = depends_on
   end
 
   def questions
