cb4e16a5a224fae082417c9e7be993d50b5203ff
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-04-24 11:07:16 +0100

    CLDC-4352: update hint text as dropdown

--- app/models/form/sales/pages/building_height_class.rb
+++ app/models/form/sales/pages/building_height_class.rb
@@ -2,6 +2,7 @@ class Form::Sales::Pages::BuildingHeightClass < ::Form::Page
   def initialize(id, hsh, subsection)
     super
     @id = "building_height_class"
+    @copy_key = "sales.property_information.buildheightclass"
     @depends_on = [
       { "proptype" => 1 },
       { "proptype" => 2 },
