35ab71b01a75b2b8024d7498591f03e643f89125
Nat Dean-Lewis <94526761+natdeanlewissoftwire@users.noreply.github.com>
2026-07-21 11:07:21 +0100

    CLDC-4497: Allow users with no role to download data (#3360)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_18_101455) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_13_105230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,7 +111,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_18_101455) do
     t.string "download_filters", null: false
     t.bigint "user_id", null: false
     t.bigint "user_organisation_id", null: false
-    t.integer "user_role", null: false
+    t.integer "user_role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_download_records_on_user_id"
