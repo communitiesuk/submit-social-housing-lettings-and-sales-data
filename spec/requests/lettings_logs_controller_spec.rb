6853da4ea420a4994323d9e090fd623b1acf48f3
Nat Dean-Lewis <nat.dean-lewis@softwire.com>
2026-09-15 11:45:21 +0100

    CLDC-4515: update lettings log controller and lettings cya tests

--- spec/requests/lettings_logs_controller_spec.rb
+++ spec/requests/lettings_logs_controller_spec.rb
@@ -1252,7 +1252,7 @@ RSpec.describe LettingsLogsController, type: :request do
 
             before do
               create_list(:lettings_log, further_unresolved_logs_count, unresolved: true, assigned_to: user)
-              lettings_log.update!(needstype: 2, scheme:, location:)
+              lettings_log.update!(needstype: 2, scheme:, location:, postcode_full: nil)
               get lettings_log_path(lettings_log)
             end
 
